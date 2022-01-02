0287b9823a1a500051d98d7c9727a0e41807c67d
Frozennide <sebastien@sdeu.fr>
2022-01-02 02:37:45 +0100

    feat(personal): create menu with HUD management
    Former-commit-id: e517116fa029233d508b81c60fceaf52ccdb3ac1

---
+++ resources/[soz]/soz-personal-menu/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+description 'SOZ personal menu'
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'client/components/*.lua',
+    'client/main.lua',
+}
+
+dependencies {
+    'qb-core',
+    'soz-hud',
+    'menuv',
+}
