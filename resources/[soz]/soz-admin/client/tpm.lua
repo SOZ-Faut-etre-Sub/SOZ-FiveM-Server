295c4057a4c13981cf2ac10305f7089bb9451dca
leaudevian <mastersogood@gmail.com>
2022-01-03 00:39:34 +0100

    remove start chat and lint lua tmp
    Former-commit-id: 4334720e76ca5b97559d71603b2e5c7884e1dfc5

--- resources/[soz]/soz-admin/client/tpm.lua
+++ resources/[soz]/soz-admin/client/tpm.lua
@@ -3,23 +3,17 @@ RegisterCommand("tpm", function(source)
 end)
 
 TeleportToWaypoint = function()
-            local WaypointHandle = GetFirstBlipInfoId(8)
-
-            if DoesBlipExist(WaypointHandle) then
-                local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
-
-                for height = 1, 1000 do
+local WaypointHandle = GetFirstBlipInfoId(8)
+    if DoesBlipExist(WaypointHandle) then
+        local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
+        for height = 1, 1000 do
+            SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+            local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
+                if foundGround then
                     SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                    local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                    if foundGround then
-                        SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                        break
-                    end
-
-                    Citizen.Wait(5)
+                    break
                 end
+                Citizen.Wait(5)
             end
+        end
 end
