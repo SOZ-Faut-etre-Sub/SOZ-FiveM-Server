0dc6e57c0a92f701896de64c6c22bb52ec1fb416
Frozennide <sebastien@sdeu.fr>
2022-01-06 18:53:48 +0100

    feat(inventory): add notification on error
    Former-commit-id: d1bb179eae58f890c63fca4a936720b0707e5cc7

--- resources/[soz]/soz-inventory/config.lua
+++ resources/[soz]/soz-inventory/config.lua
@@ -2,4 +2,11 @@ Config = {}
 Config.MaxWeight = 80000 -- Max weight a player can carry (currently 120kg, written in grams)
 Config.MaxInvSlots = 30 -- Max inventory slots for a player
 
+Config.ErrorMessage = {
+    ['invalid_item'] = "~r~L'objet a transférer est invalide !",
+    ['invalid_inventory'] = "~r~L'inventaire n'est pas disponible !",
+    ['nonexistent_item'] = "~r~L'objet a transférer est invalide !",
+    ['inventory_full'] = "~r~L'inventaire n'a plus de place !",
+}
+
 Config.Storages = {}
