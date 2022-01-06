83f924bda4b811adf2058e8cb38271185663f131
Frozennide <sebastien@sdeu.fr>
2022-01-06 18:53:04 +0100

    fix(hud): force close input to force render
    Usefully if player was kick after AFK
    
    
    Former-commit-id: b615e422cfa979f978b9e69dba4a6335ed1a1b97

--- resources/[soz]/soz-hud/client/input.lua
+++ resources/[soz]/soz-hud/client/input.lua
@@ -1,5 +1,8 @@
 
 local function Input(title, maxChar, content)
+    ForceCloseTextInputBox()
+    CancelOnscreenKeyboard()
+    -- Ensure last input is closed
     AddTextEntry("FMMC_MPM_NA", title or "")
     DisplayOnscreenKeyboard(1, "FMMC_MPM_NA", "", content or "", "", "", "", maxChar or 32)
     while (UpdateOnscreenKeyboard() == 0) do
