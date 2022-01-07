e309fe4401242744117250f2007fd8d78c299160
Frozennide <sebastien@sdeu.fr>
2022-01-07 12:06:02 +0100

    feat(qb-core): split shared configuration
    Former-commit-id: 0e88a31dd77844784a9c18db698af2cbfaf367fc

--- resources/[qb]/qb-core/fxmanifest.lua
+++ resources/[qb]/qb-core/fxmanifest.lua
@@ -5,37 +5,42 @@ description 'QB-Core'
 version '1.0.0'
 
 shared_scripts {
-	'config.lua',
-	'shared.lua'
+    'config.lua',
+    'shared/main.lua',
+    'shared/items.lua',
+    'shared/jobs.lua',
+    'shared/vehicles.lua',
+    'shared/gangs.lua',
+    'shared/weapons.lua',
 }
 
 client_scripts {
-	'client/main.lua',
-	'client/functions.lua',
-	'client/loops.lua',
-	'client/events.lua'
+    'client/main.lua',
+    'client/functions.lua',
+    'client/loops.lua',
+    'client/events.lua'
 }
 
 server_scripts {
-	'server/main.lua',
-	'server/functions.lua',
-	'server/player.lua',
-	'server/events.lua',
-	'server/commands.lua',
-	'server/debug.lua'
+    'server/main.lua',
+    'server/functions.lua',
+    'server/player.lua',
+    'server/events.lua',
+    'server/commands.lua',
+    'server/debug.lua'
 }
 
 ui_page 'html/index.html'
 
 files {
-	'html/index.html',
-	'html/style.css',
-	'html/*.js'
+    'html/index.html',
+    'html/style.css',
+    'html/*.js'
 }
 
 dependencies {
-	'oxmysql',
-	'progressbar'
+    'oxmysql',
+    'progressbar'
 }
 
 lua54 'yes'
