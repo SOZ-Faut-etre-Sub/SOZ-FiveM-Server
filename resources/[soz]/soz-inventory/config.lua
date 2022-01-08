79c882e373ac7a72ae307781a496bc710f11b07d
Frozennide <sebastien@sdeu.fr>
2022-01-08 14:12:06 +0100

    fix(storage): reject transfert if amount is 0
    Former-commit-id: e309dca94ea1dea10fb78b31398a01ebf01d4d79

--- resources/[soz]/soz-inventory/config.lua
+++ resources/[soz]/soz-inventory/config.lua
@@ -5,6 +5,7 @@ Config.MaxInvSlots = 30 -- Max inventory slots for a player
 Config.ErrorMessage = {
     ['invalid_item'] = "~r~L'objet a transférer est invalide !",
     ['invalid_inventory'] = "~r~L'inventaire n'est pas disponible !",
+    ['invalid_quantity'] = "~r~La quantité indiqué n'est pas valide",
     ['nonexistent_item'] = "~r~L'objet a transférer est invalide !",
     ['inventory_full'] = "~r~L'inventaire n'a plus de place !",
     ['not_allowed_item'] = "~r~L'objet ne peut aller dans ce stockage !",
