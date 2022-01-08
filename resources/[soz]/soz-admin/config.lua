114123380adee7396c3ec8748bac4e3bb864b805
Frozennide <sebastien@sdeu.fr>
2022-01-08 13:57:30 +0100

    feat(admin): bind admin menu to F7 and check if player is allowed
    Former-commit-id: 7e743239547c48dda9c3705ccd6543bc564b295d

---
+++ resources/[soz]/soz-admin/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.AllowedRole = {
+    'admin',
+}
--- resources/[soz]/soz-admin/server/main.lua
+++ resources/[soz]/soz-admin/server/main.lua
@@ -1,5 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+QBCore.Functions.CreateCallback('admin:server:isAllowed', function(source, cb)
+    local isAllowed = false
+
+    for _,role in ipairs(Config.AllowedRole) do
+        if QBCore.Functions.HasPermission(source, role) then
+            isAllowed = true
+        end
+    end
+
+    cb(isAllowed)
+end)
+
 QBCore.Functions.CreateCallback('admin:server:getplayers', function(source, cb)
     local players = {}
     for k, v in pairs(QBCore.Functions.GetPlayers()) do
