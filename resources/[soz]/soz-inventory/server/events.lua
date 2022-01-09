028efce6644b64dbb5ce039fa28cb937cc630244
Frozennide <sebastien@sdeu.fr>
2022-01-09 15:49:05 +0100

    feat(inventory): create vehicle trunk storage
    Former-commit-id: 0e449d8a6eb8cc02567e5531e7f87bc7f289ecb1

--- resources/[soz]/soz-inventory/server/events.lua
+++ resources/[soz]/soz-inventory/server/events.lua
@@ -18,6 +18,30 @@ RegisterServerEvent('inventory:server:openInventory', function(invID)
     end
 end)
 
+RegisterServerEvent('inventory:server:openTrunkInventory', function(plate)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    local sourceInv = Inventory(Player.PlayerData.source)
+    local targetInv = Inventory('plate_'..plate)
+
+    if targetInv == nil then
+        targetInv = Inventory.Create('plate_'..plate, plate, 'trunk', Config.StorageMaxInvSlots, Config.StorageMaxWeight, plate)
+    end
+
+    if targetInv.open == false and Inventory.AccessGranted(targetInv, Player.PlayerData.source) then
+        TriggerClientEvent('inventory:client:updateStorageState', -1, targetInv.id, Player.Functions.GetName())
+        targetInv.open = true
+        targetInv.user = Player.PlayerData.source
+
+        TriggerClientEvent('inventory:client:openInventory', Player.PlayerData.source,
+                Inventory.FilterItems(sourceInv, targetInv.type),
+                Inventory.FilterItems(targetInv, sourceInv.type)
+        )
+    else
+        TriggerClientEvent('hud:client:DrawNotification', Player.PlayerData.source, "~r~Vous n'avez pas accès à ce coffre")
+    end
+end)
+
 QBCore.Functions.CreateCallback("inventory:server:TransfertItem", function(source, cb, inventorySource, inventoryTarget, item, amount, slot)
     Inventory.TransfertItem(inventorySource, inventoryTarget, item, amount, false, slot, function(success, reason)
         local sourceInv = Inventory(inventorySource)
