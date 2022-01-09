7fbcf29a649de0b9ff6480dd64ec704b640529a5
Frozennide <sebastien@sdeu.fr>
2022-01-09 13:38:08 +0100

    feat(inventory): append missing storages type
    Former-commit-id: 11f77a238ff5f32e18f8f282ab446420930da11d

---
+++ resources/[soz]/soz-inventory/server/containers/seizure.lua
@@ -0,0 +1,72 @@
+--- @class SeizureInventory
+SeizureInventory = {}
+
+function SeizureInventory.new()
+    return setmetatable({}, {
+        __index = SeizureInventory,
+        __tostring = function() return 'SeizureInventory' end
+    })
+end
+
+--- load
+--- @param id any
+--- @param citizenid any
+--- @return table
+function SeizureInventory:load(id, owner)
+    local result = exports.oxmysql:scalar_async('SELECT inventory FROM storages WHERE name = ?', { id })
+    if result == nil then
+        exports.oxmysql:execute('INSERT INTO storages(name,type,owner) VALUES (?,?,?) ON DUPLICATE KEY UPDATE name=name', { id, 'seizure', owner })
+    end
+    return result and json.decode(result) or {}
+end
+
+--- save
+--- @param id any
+--- @param owner any
+--- @param inventory table
+--- @return boolean
+function SeizureInventory:save(id, owner, inventory)
+    inventory = json.encode(self:CompactInventory(inventory))
+    exports.oxmysql:update_async('UPDATE storages SET inventory = ? WHERE name = ?', { inventory, id })
+    return true
+end
+
+--- AllowedItems
+--- @param item table
+--- @return boolean
+function SeizureInventory:AllowedItems(item)
+    local typeAllowed = {
+        ['weapon'] = true,
+        ['weapon_attachment'] = true,
+        ['weapon_ammo'] = true,
+        ['drug'] = true,
+        ['item'] = true,
+    }
+    return typeAllowed[item.type or ''] or false
+end
+
+--- AccessAllowed
+--- @param owner string
+--- @param player Player
+--- @return boolean
+function SeizureInventory:AccessAllowed(owner, playerId)
+    local Player = QBCore.Functions.GetPlayer(tonumber(playerId))
+
+    if Player then
+        return Player.PlayerData.job.name == owner
+    else
+        return false
+    end
+end
+
+--- sync
+--- @param id any
+--- @param items table
+--- @return boolean
+function SeizureInventory:sync(id, items)
+    -- Do nothing
+end
+
+--- Exports functions
+setmetatable(SeizureInventory, { __index = InventoryShell })
+_G.Container['seizure'] = SeizureInventory.new()
---
+++ resources/[soz]/soz-inventory/server/containers/storage.lua
@@ -0,0 +1,65 @@
+--- @class StorageInventory
+StorageInventory = {}
+
+function StorageInventory.new()
+    return setmetatable({}, {
+        __index = StorageInventory,
+        __tostring = function() return 'StorageInventory' end
+    })
+end
+
+--- load
+--- @param id any
+--- @param citizenid any
+--- @return table
+function StorageInventory:load(id, owner)
+    local result = exports.oxmysql:scalar_async('SELECT inventory FROM storages WHERE name = ?', { id })
+    if result == nil then
+        exports.oxmysql:execute('INSERT INTO storages(name,type,owner) VALUES (?,?,?) ON DUPLICATE KEY UPDATE name=name', { id, 'storage', owner })
+    end
+    return result and json.decode(result) or {}
+end
+
+--- save
+--- @param id any
+--- @param owner any
+--- @param inventory table
+--- @return boolean
+function StorageInventory:save(id, owner, inventory)
+    inventory = json.encode(self:CompactInventory(inventory))
+    exports.oxmysql:update_async('UPDATE storages SET inventory = ? WHERE name = ?', { inventory, id })
+    return true
+end
+
+--- AllowedItems
+--- @param item table
+--- @return boolean
+function StorageInventory:AllowedItems(item)
+    return true
+end
+
+--- AccessAllowed
+--- @param owner string
+--- @param player Player
+--- @return boolean
+function StorageInventory:AccessAllowed(owner, playerId)
+    local Player = QBCore.Functions.GetPlayer(tonumber(playerId))
+
+    if Player then
+        return Player.PlayerData.job.name == owner
+    else
+        return false
+    end
+end
+
+--- sync
+--- @param id any
+--- @param items table
+--- @return boolean
+function StorageInventory:sync(id, items)
+    -- Do nothing
+end
+
+--- Exports functions
+setmetatable(StorageInventory, { __index = InventoryShell })
+_G.Container['storage'] = StorageInventory.new()
