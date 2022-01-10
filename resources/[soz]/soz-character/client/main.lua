920dee99e4dafcb6efcb7b9fc262d0f94ed4312d
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-01-10 20:59:44 +0100

    Spawnv2
    Former-commit-id: 100e44816065688b021fc36e8734b9d21774d9be

--- resources/[soz]/soz-character/client/main.lua
+++ resources/[soz]/soz-character/client/main.lua
@@ -96,24 +96,6 @@ RegisterNetEvent('soz-character:client:NcDataPed', function(source)
                         Wait(500)
                         DoScreenFadeIn(250)
                     end)
-                else
-                    CreateThread(function()
-                        local randommodels = {
-                            "mp_m_freemode_01",
-                            "mp_f_freemode_01",
-                        }
-                        local model = GetHashKey(randommodels[math.random(1, #randommodels)])
-                        RequestModel(model)
-                        while not HasModelLoaded(model) do
-                            Wait(0)
-                        end
-                        charPed = CreatePed(2, model, Config.PedCoords.x, Config.PedCoords.y, Config.PedCoords.z - 0.98, Config.PedCoords.w, false, true)
-                        SetPedComponentVariation(charPed, 0, 0, 0, 2)
-                        FreezeEntityPosition(charPed, false)
-                        SetEntityInvincible(charPed, true)
-                        PlaceObjectOnGroundProperly(charPed)
-                        SetBlockingOfNonTemporaryEvents(charPed, true)
-                    end)
                 end
             end)
         else
