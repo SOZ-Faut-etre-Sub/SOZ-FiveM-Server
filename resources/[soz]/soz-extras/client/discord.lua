386b4690cb5571a6f8cc16a02221c9a5a76ba6af
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-01-11 16:30:37 +0100

    Discord
    Former-commit-id: 815c7f37cd957df456fd4faf084c0563df84c6ff

--- resources/[soz]/soz-extras/client/discord.lua
+++ resources/[soz]/soz-extras/client/discord.lua
@@ -1,8 +1,8 @@
 -- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
 CreateThread(function()
-    while false do
+    while true do
         -- This is the Application ID (Replace this with you own)
-        SetDiscordAppId()
+        SetDiscordAppId(926572229557383208)
 
         -- Here you will have to put the image name for the "large" icon.
         SetDiscordRichPresenceAsset("soz")
@@ -19,7 +19,7 @@ CreateThread(function()
         SetDiscordRichPresenceAssetSmallText("discord.gg/xxx")
 
         QBCore.Functions.TriggerCallback("smallresources:server:GetCurrentPlayers", function(result)
-            SetRichPresence("Players: " .. result .. "/200")
+            SetRichPresence("Joueurs : " .. result .. "/200")
         end)
 
         -- (26-02-2021) New Native:
@@ -29,8 +29,6 @@ CreateThread(function()
             First paramater is the button index (0 or 1), second is the title and 
             last is the url (this has to start with "fivem://connect/" or "https://") 
         ]] --
-        SetDiscordRichPresenceAction(0, "SoZething is coming", "https://liquipedia.net/commons/images/6/63/WIP-logo.png")
-        -- SetDiscordRichPresenceAction(1, "Second Button!", "fivem://connect/localhost:30120")
 
         -- It updates every minute just in case.
         Wait(60000)
