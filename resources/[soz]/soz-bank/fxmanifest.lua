41786b6e2f30c146bff272fb24127621c4b2f70f
Frozennide <sebastien@sdeu.fr>
2022-01-19 12:06:11 +0100

    feat(bank): add ped to open bank menu
    Former-commit-id: e4790611732e7f7506a14a28ffe52baf1354142d

--- resources/[soz]/soz-bank/fxmanifest.lua
+++ resources/[soz]/soz-bank/fxmanifest.lua
@@ -6,6 +6,6 @@ shared_scripts {"config.lua"}
 
 client_scripts {"@menuv/menuv.lua", "client/main.lua", "client/moneycase.lua", "client/police.lua"}
 
-server_scripts {"@oxmysql/lib/MySQL.lua", "server/main.lua", "server/police.lua"}
+server_scripts {"@oxmysql/lib/MySQL.lua", "server/main.lua", "server/bank_ped.lua", "server/police.lua"}
 
 dependencies {"oxmysql", "qb-core", "soz-hud", "menuv"}
