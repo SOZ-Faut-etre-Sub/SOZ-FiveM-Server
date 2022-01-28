54bb1b0d98722609767fb833f7161c63b4a01fc7
Leaudevian <53706800+madstersogoo@users.noreply.github.com>
2022-01-28 10:06:00 +0100

    livraison & adsl
    Former-commit-id: fc2b5a78a0730c26f66f211d40aa4008ed897494

---
+++ resources/[soz]/soz-jobs/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+games {"gta5"}
+lua54 "yes"
+
+description "job entreprise et pole emploie"
+
+shared_script "config.lua"
+
+client_script {"client/adsl.lua" , "client/pedlist.lua" , "client/spawnped.lua", "@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "client/livraison.lua"}
+
+server_script "server/server.lua"
+
+
