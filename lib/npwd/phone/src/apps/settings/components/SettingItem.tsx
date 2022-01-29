dbcffc94a0159813be3c4235de7ae3bc243bdc1b
Frozennide <sebastien@sdeu.fr>
2022-01-29 14:16:37 +0100

    feat(phone): update phone screen background
    Former-commit-id: 5a410d1fec35125078c1e1c367fe29eed1fb5640

--- lib/npwd/phone/src/apps/settings/components/SettingItem.tsx
+++ lib/npwd/phone/src/apps/settings/components/SettingItem.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  ListItemSecondaryAction,
-  Box,
-  Slider,
-  IconButton,
-  Typography,
-  Switch,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+    ListItemSecondaryAction,
+    Box,
+    Slider,
+    IconButton,
+    Typography,
+    Switch, styled, SwitchProps,
 } from '@mui/material';
 import { Tooltip } from '@ui/components/Tooltip';
 
@@ -75,10 +75,61 @@ export const SettingSwitch = ({ label, value, onClick, icon, secondary }: ISetti
     <ListItemIcon>{icon}</ListItemIcon>
     <ListItemText primary={label} secondary={secondary} />
     <ListItemSecondaryAction>
-      <Switch color="primary" checked={value} onChange={() => onClick(value)} />
+      <IOSSwitch color="primary" checked={value} onChange={() => onClick(value)} />
     </ListItemSecondaryAction>
   </ListItem>
 );
+
+const IOSSwitch = styled((props: SwitchProps) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
 
 export const SettingItemIconAction = ({
   icon,
