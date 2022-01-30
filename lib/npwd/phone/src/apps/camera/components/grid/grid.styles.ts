c602b195627cce18814cad5998453f033e9cb524
Frozennide <sebastien@sdeu.fr>
2022-01-30 11:06:41 +0100

    feat(phone): rework camera app
    Former-commit-id: ffec1dd7e2d722bebe9dc7be5f49fb9273f30be3

--- lib/npwd/phone/src/apps/camera/components/grid/grid.styles.ts
+++ lib/npwd/phone/src/apps/camera/components/grid/grid.styles.ts
@@ -1,21 +1,27 @@
-import { Theme } from '@mui/material/styles';
+import {Theme} from '@mui/material/styles';
 
 import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  photo: {
-    border: '1px inset #fff',
-    height: 129,
-    width: 129,
-    maxWidth: 150,
-    maxHeight: 150,
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-  },
+    root: {
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gridGap: '1px'
+    },
+    photo: {
+        height: 129,
+        width: 129,
+        maxWidth: 150,
+        maxHeight: 150,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        cursor: 'pointer',
+    },
+    absolute: {
+        position: 'absolute',
+        right: theme.spacing(3),
+        top: theme.spacing(5),
+    },
 }));
 
 export default useStyles;
