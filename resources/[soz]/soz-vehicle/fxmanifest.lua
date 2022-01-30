40b240f8343fb963ea0dcd5f036ccec979063e38
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-01-30 17:21:00 +0100

    forme
    Former-commit-id: 4bd3be19a1f3340149cff29feba5c5e936396ef3

--- resources/[soz]/soz-vehicle/fxmanifest.lua
+++ resources/[soz]/soz-vehicle/fxmanifest.lua
@@ -1,27 +1,11 @@
 fx_version "cerulean"
-games {
-    "gta5",
-}
+games {"gta5"}
 lua54 "yes"
 
 shared_script "config.lua"
 
-client_scripts {
-    "@PolyZone/client.lua",
-    "@PolyZone/BoxZone.lua",
-    "@menuv/menuv.lua",
-    "client/*.lua",
-}
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@menuv/menuv.lua", "client/*.lua"}
 
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "server/*.lua",
-}
+server_scripts {"@oxmysql/lib/MySQL.lua", "server/*.lua"}
 
-dependencies {
-    "qb-core",
-    "soz-hud",
-    "menuv",
-    "qb-target",
-    "PolyZone",
-}
+dependencies {"qb-core", "soz-hud", "menuv", "qb-target", "PolyZone"}
