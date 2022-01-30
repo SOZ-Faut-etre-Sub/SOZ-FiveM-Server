009bde0a9c5f757f8cbe893a0d0a2d147cf9261b
Frozennide <sebastien@sdeu.fr>
2022-01-30 22:35:50 +0100

    chore: format code
    Former-commit-id: e9aa2888eb7f90393a434adfe6d5c85d99454822

--- resources/[soz]/soz-voip/fxmanifest.lua
+++ resources/[soz]/soz-voip/fxmanifest.lua
@@ -1,69 +1,54 @@
-game 'common'
+game "common"
 
-fx_version 'cerulean'
-author 'AvarianKnight'
-description 'VOIP built using FiveM\'s built in mumble.'
+fx_version "cerulean"
+author "AvarianKnight"
+description "VOIP built using FiveM's built in mumble."
 
-dependencies {
-   '/onesync',
-}
+dependencies {"/onesync"}
 
-lua54 'yes'
+lua54 "yes"
 
-shared_script 'shared.lua'
+shared_script "shared.lua"
 
 client_scripts {
-	'client/utils/*',
-	'client/init/proximity.lua',
-	'client/init/init.lua',
-	'client/init/main.lua',
-	'client/module/*.lua',
-    'client/*.lua',
+    "client/utils/*",
+    "client/init/proximity.lua",
+    "client/init/init.lua",
+    "client/init/main.lua",
+    "client/module/*.lua",
+    "client/*.lua",
 }
 
-server_scripts {
-    'server/**/*.lua',
-	'server/**/*.js'
-}
+server_scripts {"server/**/*.lua", "server/**/*.js"}
 
-files {
-    'ui/*.ogg',
-    'ui/css/*.css',
-    'ui/js/*.js',
-    'ui/index.html',
-}
+files {"ui/*.ogg", "ui/css/*.css", "ui/js/*.js", "ui/index.html"}
 
-ui_page 'ui/index.html'
+ui_page "ui/index.html"
 
-provides {
-	'mumble-voip',
-    'tokovoip',
-    'toko-voip',
-    'tokovoip_script'
-}
+provides {"mumble-voip", "tokovoip", "toko-voip", "tokovoip_script"}
 
-convar_category 'PMA-Voice' {
+convar_category "PMA-Voice" {
     "PMA-Voice Configuration Options",
     {
-        { "Use native audio", "$voice_useNativeAudio", "CV_BOOL", "false" },
-	{ "Use 2D audio", "$voice_use2dAudio", "CV_BOOL", "false" },
-	{ "Use sending range only", "$voice_useSendingRangeOnly", "CV_BOOL", "false" },
-	{ "Enable UI", "$voice_enableUi", "CV_INT", "1" },
-	{ "Enable F11 proximity key", "$voice_enableProximityCycle", "CV_INT", "1" },
-	{ "Proximity cycle key", "$voice_defaultCycle", "CV_STRING", "F11" },
-	{ "Voice radio volume", "$voice_defaultRadioVolume", "CV_STRING", "0.3" },
-	{ "Voice phone volume", "$voice_defaultPhoneVolume", "CV_STRING", "0.6" },
-	{ "Enable radios", "$voice_enableRadios", "CV_INT", "1" },
-	{ "Enable phones", "$voice_enablePhones", "CV_INT", "1" },
-	{ "Enable sublix", "$voice_enableSubmix", "CV_INT", "0" },
-        { "Enable radio animation", "$voice_enableRadioAnim", "CV_INT", "0" },
-	{ "Radio key", "$voice_defaultRadio", "CV_STRING", "LALT" },
-	{ "UI refresh rate", "$voice_uiRefreshRate", "CV_INT", "200" },
-	{ "Allow players to set audio intent", "$voice_allowSetIntent", "CV_INT", "1" },
-	{ "External mumble server address", "$voice_externalAddress", "CV_STRING", "" },
-	{ "External mumble server port", "$voice_externalPort", "CV_INT", "0" },
-	{ "Voice debug mode", "$voice_debugMode", "CV_INT", "0" },
-	{ "Disable players being allowed to join", "$voice_externalDisallowJoin", "CV_INT", "0" },
-	{ "Hide server endpoints in logs", "$voice_hideEndpoints", "CV_INT", "1" },
-    }
+        {"Use native audio", "$voice_useNativeAudio", "CV_BOOL", "false"},
+        {"Use 2D audio", "$voice_use2dAudio", "CV_BOOL", "false"},
+        {"Use sending range only", "$voice_useSendingRangeOnly", "CV_BOOL", "false"},
+        {"Enable UI", "$voice_enableUi", "CV_INT", "1"},
+        {"Enable F11 proximity key", "$voice_enableProximityCycle", "CV_INT", "1"},
+        {"Proximity cycle key", "$voice_defaultCycle", "CV_STRING", "F11"},
+        {"Voice radio volume", "$voice_defaultRadioVolume", "CV_STRING", "0.3"},
+        {"Voice phone volume", "$voice_defaultPhoneVolume", "CV_STRING", "0.6"},
+        {"Enable radios", "$voice_enableRadios", "CV_INT", "1"},
+        {"Enable phones", "$voice_enablePhones", "CV_INT", "1"},
+        {"Enable sublix", "$voice_enableSubmix", "CV_INT", "0"},
+        {"Enable radio animation", "$voice_enableRadioAnim", "CV_INT", "0"},
+        {"Radio key", "$voice_defaultRadio", "CV_STRING", "LALT"},
+        {"UI refresh rate", "$voice_uiRefreshRate", "CV_INT", "200"},
+        {"Allow players to set audio intent", "$voice_allowSetIntent", "CV_INT", "1"},
+        {"External mumble server address", "$voice_externalAddress", "CV_STRING", ""},
+        {"External mumble server port", "$voice_externalPort", "CV_INT", "0"},
+        {"Voice debug mode", "$voice_debugMode", "CV_INT", "0"},
+        {"Disable players being allowed to join", "$voice_externalDisallowJoin", "CV_INT", "0"},
+        {"Hide server endpoints in logs", "$voice_hideEndpoints", "CV_INT", "1"},
+    },
 }
--- resources/[soz]/soz-voip/shared.lua
+++ resources/[soz]/soz-voip/shared.lua
@@ -6,67 +6,66 @@ gameVersion = GetGameName()
 
 -- these are just here to satisfy linting
 if not IsDuplicityVersion() then
-	LocalPlayer = LocalPlayer
-	playerServerId = GetPlayerServerId(PlayerId())
+    LocalPlayer = LocalPlayer
+    playerServerId = GetPlayerServerId(PlayerId())
 end
 Player = Player
 Entity = Entity
 
-if GetConvar('voice_useNativeAudio', 'false') == 'true' then
-	-- native audio distance seems to be larger then regular gta units
-	Cfg.voiceModes = {
-		{1.5, "Whisper"}, -- Whisper speech distance in gta distance units
-		{3.0, "Normal"}, -- Normal speech distance in gta distance units
-		{6.0, "Shouting"} -- Shout speech distance in gta distance units
-	}
+if GetConvar("voice_useNativeAudio", "false") == "true" then
+    -- native audio distance seems to be larger then regular gta units
+    Cfg.voiceModes = {
+        {1.5, "Whisper"}, -- Whisper speech distance in gta distance units
+        {3.0, "Normal"}, -- Normal speech distance in gta distance units
+        {6.0, "Shouting"}, -- Shout speech distance in gta distance units
+    }
 else
-	Cfg.voiceModes = {
-		{3.0, "Whisper"}, -- Whisper speech distance in gta distance units
-		{7.0, "Normal"}, -- Normal speech distance in gta distance units
-		{15.0, "Shouting"} -- Shout speech distance in gta distance units
-	}
+    Cfg.voiceModes = {
+        {3.0, "Whisper"}, -- Whisper speech distance in gta distance units
+        {7.0, "Normal"}, -- Normal speech distance in gta distance units
+        {15.0, "Shouting"}, -- Shout speech distance in gta distance units
+    }
 end
 
 logger = {
-	['log'] = function(message, ...)
-		print((message):format(...))
-	end,
-	['info'] = function(message, ...)
-		if GetConvarInt('voice_debugMode', 0) >= 1 then
-			print(('[info] ' .. message):format(...))
-		end
-	end,
-	['warn'] = function(message, ...)
-		print(('[^1WARNING^7] ' .. message):format(...))
-	end,
-	['error'] = function(message, ...)
-		error((message):format(...))
-	end,
-	['verbose'] = function(message, ...)
-		if GetConvarInt('voice_debugMode', 0) >= 4 then
-			print(('[verbose] ' .. message):format(...))
-		end
-	end,
+    ["log"] = function(message, ...)
+        print((message):format(...))
+    end,
+    ["info"] = function(message, ...)
+        if GetConvarInt("voice_debugMode", 0) >= 1 then
+            print(("[info] " .. message):format(...))
+        end
+    end,
+    ["warn"] = function(message, ...)
+        print(("[^1WARNING^7] " .. message):format(...))
+    end,
+    ["error"] = function(message, ...)
+        error((message):format(...))
+    end,
+    ["verbose"] = function(message, ...)
+        if GetConvarInt("voice_debugMode", 0) >= 4 then
+            print(("[verbose] " .. message):format(...))
+        end
+    end,
 }
 
-
 function tPrint(tbl, indent)
-	indent = indent or 0
-	for k, v in pairs(tbl) do
-		local tblType = type(v)
-		local formatting = string.rep("  ", indent) .. k .. ": "
+    indent = indent or 0
+    for k, v in pairs(tbl) do
+        local tblType = type(v)
+        local formatting = string.rep("  ", indent) .. k .. ": "
 
-		if tblType == "table" then
-			print(formatting)
-			tPrint(v, indent + 1)
-		elseif tblType == 'boolean' then
-			print(formatting .. tostring(v))
-		elseif tblType == "function" then
-			print(formatting .. tostring(v))
-		else
-			print(formatting .. v)
-		end
-	end
+        if tblType == "table" then
+            print(formatting)
+            tPrint(v, indent + 1)
+        elseif tblType == "boolean" then
+            print(formatting .. tostring(v))
+        elseif tblType == "function" then
+            print(formatting .. tostring(v))
+        else
+            print(formatting .. v)
+        end
+    end
 end
 
 local function types(args)
