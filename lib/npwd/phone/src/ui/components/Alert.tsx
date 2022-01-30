5f33b65a2b6044dd22b16ab55438cc00c1ef9682
Frozennide <sebastien@sdeu.fr>
2022-01-30 22:16:02 +0100

    feat(phone): global redesign for snackbar
    Former-commit-id: 1ab079cb4e59bbedf38a05b0e9a8a9c0ce6a7421

--- lib/npwd/phone/src/ui/components/Alert.tsx
+++ lib/npwd/phone/src/ui/components/Alert.tsx
@@ -1,27 +1,29 @@
-import React, { forwardRef } from 'react';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import { Typography } from '@mui/material';
+import React, {forwardRef} from 'react';
+import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import {Typography} from '@mui/material';
 
 import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = makeStyles({
-  root: {
-    zIndex: 10000,
-  },
-  msg: {
-    maxWidth: '300px',
-    wordWrap: 'break-word',
-    fontSize: '1.1em',
-  },
+    root: {
+        zIndex: 10000,
+        width: '90%',
+        background: 'rgba(255,255,255,.35)',
+        backdropFilter: 'blur(10px)'
+    },
+    msg: {
+        wordWrap: 'break-word',
+        fontSize: '1.1em',
+    },
 });
 
 export const Alert: React.FC<AlertProps> = forwardRef((props, ref) => {
-  const classes = useStyles();
-  return (
-    <MuiAlert className={classes.root} elevation={4} variant="filled" {...props} ref={ref}>
-      <Typography className={classes.msg}>{props.children}</Typography>
-    </MuiAlert>
-  );
+    const classes = useStyles();
+    return (
+        <MuiAlert className={classes.root} elevation={4} variant="filled" {...props} ref={ref}>
+            <Typography className={classes.msg}>{props.children}</Typography>
+        </MuiAlert>
+    );
 });
 
 export default Alert;
