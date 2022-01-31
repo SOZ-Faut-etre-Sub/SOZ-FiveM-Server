8bc72478ff8ec11886d9e3344664271d87280f81
leaudevian <mastersogood@gmail.com>
2022-01-31 20:07:36 +0100

    ajout ped et zone pole emploi
    Former-commit-id: 3d74a92ace96b645c64f91c3aea06273aa3fa074

--- resources/[soz]/soz-jobs/client/pole.lua
+++ resources/[soz]/soz-jobs/client/pole.lua
@@ -1,6 +1,28 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 
+local PoleMenu = MenuV:CreateMenu(nil, "Le menu de métal", "menu_pole", "soz", "job-panel")
+
+exports["qb-target"]:AddBoxZone("pole emploi", vector3(236.46, -409.33, 47.92), 1, 1, {
+    name = "pole emploi",
+    heading = 0,
+    debugPoly = false,
+}, {
+    options = {
+        {
+            type = "client",
+            event = "jobs:pole:menu",
+            icon = "fas fa-sign-in-alt",
+            label = "information pôle emploi",
+            job = "unemployed",
+        },
+    },
+    distance = 2.5,
+})
+
+-- function general au job pole emploi
+
 function DeleteVehicule(vehicule)
     SetEntityAsMissionEntity(vehicule, true, true)
     DeleteVehicle(vehicule)
 end
+
