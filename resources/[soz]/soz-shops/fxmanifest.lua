f621d5959446441648dccb1e6343491c35e7ac35
Frozennide <sebastien@sdeu.fr>
2022-01-31 17:45:15 +0100

    feat(shop): create supermarket and weapon shop
    Former-commit-id: 3091d16c940f8d5fa7ae11e5f572a926be74734b

---
+++ resources/[soz]/soz-shops/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "cerulean"
+games {"gta5"}
+lua54 "yes"
+
+shared_scripts {"config.lua"}
+
+client_scripts {
+    "@menuv/menuv.lua",
+    "@PolyZone/client.lua",
+    "@PolyZone/BoxZone.lua",
+
+    "client/location/main.lua",
+    "client/location/weedshop.lua",
+    "client/location/ammunation.lua",
+    "client/location/robsliquor.lua",
+    "client/location/ltdgasoline.lua",
+    "client/location/247supermarket.lua",
+
+    "client/main.lua",
+}
+server_scripts {"server/main.lua"}
+
+dependencies {"qb-core", "PolyZone", "qb-target", "soz-hud", "menuv"}
