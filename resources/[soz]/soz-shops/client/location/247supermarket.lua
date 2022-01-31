ebc02cf9192bc277508b1cda7898ae24fe1aa67b
Frozennide <sebastien@sdeu.fr>
2022-01-31 17:45:15 +0100

    feat(shop): create supermarket and weapon shop
    Former-commit-id: 3091d16c940f8d5fa7ae11e5f572a926be74734b

---
+++ resources/[soz]/soz-shops/client/location/247supermarket.lua
@@ -0,0 +1,48 @@
+-- 24/7 PolyZone Locations
+ShopLocation["247supermarket"] = {
+    [1] = BoxZone:Create(vector3(29.16, -1345.45, 29.5), 7.2, 11.0, {
+        name = "247supermarket",
+        heading = 0,
+        debugPoly = LocationDebugPoly,
+    }),
+    [2] = BoxZone:Create(vector3(-3042.15, 588.45, 7.91), 7.2, 11.0, {
+        name = "247supermarket2",
+        heading = 288,
+        debugPoly = LocationDebugPoly,
+    }),
+    [3] = BoxZone:Create(vector3(-3243.69, 1004.8, 12.83), 7.2, 11.0, {
+        name = "247supermarket3",
+        heading = 265,
+        debugPoly = LocationDebugPoly,
+    }),
+    [4] = BoxZone:Create(vector3(1732.86, 6414.78, 35.04), 7.2, 11.0, {
+        name = "247supermarket4",
+        heading = 334,
+        debugPoly = LocationDebugPoly,
+    }),
+    [5] = BoxZone:Create(vector3(1963.23, 3743.93, 32.34), 7.2, 11.0, {
+        name = "247supermarket5",
+        heading = 30,
+        debugPoly = LocationDebugPoly,
+    }),
+    [6] = BoxZone:Create(vector3(544.69, 2668.9, 42.16), 7.2, 11.0, {
+        name = "247supermarket6",
+        heading = 8,
+        debugPoly = LocationDebugPoly,
+    }),
+    [7] = BoxZone:Create(vector3(2678.7, 3284.4, 55.24), 7.2, 11.0, {
+        name = "247supermarket7",
+        heading = 241,
+        debugPoly = LocationDebugPoly,
+    }),
+    [8] = BoxZone:Create(vector3(2555.6, 385.53, 108.62), 7.2, 11.0, {
+        name = "247supermarket8",
+        heading = 88,
+        debugPoly = LocationDebugPoly,
+    }),
+    [9] = BoxZone:Create(vector3(377.55, 327.08, 103.57), 7.2, 11.0, {
+        name = "247supermarket9",
+        heading = 166,
+        debugPoly = LocationDebugPoly,
+    }),
+}
---
+++ resources/[soz]/soz-shops/client/location/weedshop.lua
@@ -0,0 +1,8 @@
+-- WeedShop PolyZone Locations
+ShopLocation["weedshop"] = {
+    [1] = BoxZone:Create(vector3(-1170.34, -1572.04, 4.66), 4.8, 5.4, {
+        name = "weedshop",
+        heading = 305,
+        debugPoly = LocationDebugPoly,
+    }),
+}
