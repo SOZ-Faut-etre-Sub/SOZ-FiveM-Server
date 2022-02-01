66b6dd00655b58243edbddf0e45d06f31fc2d180
blaqqendwhyt <57453491+blaqqendwhyt@users.noreply.github.com>
2022-02-01 13:45:27 +0100

    paystationsparking
    Former-commit-id: 432e37892fcff750d2a6c6210c20a9c8654e8189

---
+++ resources/[mapping]/[mapping-test]/soz-paystations/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+data_file('DLC_ITYP_REQUEST')('stream/prop_z_park.ytyp')
+
+files {
+
+"interiorproxies.meta"
+}
+
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
