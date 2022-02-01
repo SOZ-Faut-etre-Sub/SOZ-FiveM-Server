686664d01d5edbc21f6689182388b78c2dc2a35e
Frozennide <sebastien@sdeu.fr>
2022-02-01 10:10:06 +0100

    feat(voip): update hud for voip
    Former-commit-id: ee8e04579f1381e5d0d4adda444554694fa80abd

--- resources/[soz]/soz-voip/voice-ui/src/App.vue
+++ resources/[soz]/soz-voip/voice-ui/src/App.vue
@@ -1,112 +1,117 @@
 <template>
-	<body>
-		<audio id="audio_on" src="mic_click_on.ogg"></audio>
-		<audio id="audio_off" src="mic_click_off.ogg"></audio>
-		<div v-if="voice.uiEnabled" class="voiceInfo">
-			<p v-if="voice.callInfo !== 0" :class="{ talking: voice.talking }">
-				[Call]
-			</p>
-			<p v-if="voice.radioEnabled && voice.radioChannel !== 0" :class="{ talking: voice.usingRadio }">
-				{{ voice.radioChannel }} Mhz [Radio]
-			</p>
-			<p v-if="voice.voiceModes.length" :class="{ talking: voice.talking }">
-				{{ voice.voiceModes[voice.voiceMode][1] }} [Range]
-			</p>
-		</div>
-	</body>
+    <body>
+    <audio id="audio_on" src="mic_click_on.ogg"></audio>
+    <audio id="audio_off" src="mic_click_off.ogg"></audio>
+    <div v-if="voice.uiEnabled && voice.voiceModes.length" class="voiceInfo">
+        <transition name="display">
+            <svg v-if="voice.voiceMode === 0"><use xlink:href="sprite.svg#whisper"/></svg>
+        </transition>
+        <transition name="display">
+            <svg v-if="voice.voiceMode === 1"><use xlink:href="sprite.svg#normal"/></svg>
+        </transition>
+        <transition name="display">
+            <svg v-if="voice.voiceMode === 2"><use xlink:href="sprite.svg#shouting"/></svg>
+        </transition>
+    </div>
+    </body>
 </template>
 
 <script>
-import { reactive } from "vue";
+import {reactive} from "vue";
+
 export default {
-	name: "App",
-	setup() {
-		const voice = reactive({
-			uiEnabled: true,
-			voiceModes: [],
-			voiceMode: 0,
-			radioChannel: 0,
-			radioEnabled: true,
-			usingRadio: false,
-			callInfo: 0,
-			talking: false,
-		});
+    name: "App",
+    setup() {
+        const voice = reactive({
+            uiEnabled: true,
+            voiceModes: [],
+            voiceMode: 0,
+            radioChannel: 0,
+            radioEnabled: true,
+            usingRadio: false,
+            talking: false,
+        });
 
-		// stops from toggling voice at the end of talking
-		window.addEventListener("message", function(event) {
-			const data = event.data;
+        // stops from toggling voice at the end of talking
+        window.addEventListener("message", function (event) {
+            const data = event.data;
 
-			if (data.uiEnabled !== undefined) {
-				voice.uiEnabled = data.uiEnabled
-			}
+            if (data.uiEnabled !== undefined) {
+                voice.uiEnabled = data.uiEnabled
+            }
 
-			if (data.voiceModes !== undefined) {
-				voice.voiceModes = JSON.parse(data.voiceModes);
-				// Push our own custom type for modes that have their range changed
-				let voiceModes = [...voice.voiceModes]
-				voiceModes.push([0.0, "Custom"])
-				voice.voiceModes = voiceModes
-			}
+            if (data.voiceModes !== undefined) {
+                voice.voiceModes = JSON.parse(data.voiceModes);
+                // Push our own custom type for modes that have their range changed
+                let voiceModes = [...voice.voiceModes]
+                voiceModes.push([0.0, "Custom"])
+                voice.voiceModes = voiceModes
+            }
 
-			if (data.voiceMode !== undefined) {
-				voice.voiceMode = data.voiceMode;
-			}
+            if (data.voiceMode !== undefined) {
+                voice.voiceMode = data.voiceMode;
+            }
 
-			if (data.radioChannel !== undefined) {
-				voice.radioChannel = data.radioChannel;
-			}
+            if (data.radioChannel !== undefined) {
+                voice.radioChannel = data.radioChannel;
+            }
 
-			if (data.radioEnabled !== undefined) {
-				voice.radioEnabled = data.radioEnabled;
-			}
+            if (data.radioEnabled !== undefined) {
+                voice.radioEnabled = data.radioEnabled;
+            }
 
-			if (data.callInfo !== undefined) {
-				voice.callInfo = data.callInfo;
-			}
+            if (data.usingRadio !== undefined && data.usingRadio !== voice.usingRadio) {
+                voice.usingRadio = data.usingRadio;
+            }
 
-			if (data.usingRadio !== undefined && data.usingRadio !== voice.usingRadio) {
-				voice.usingRadio = data.usingRadio;
-			}
-			
-			if ((data.talking !== undefined) && !voice.usingRadio) {
-				voice.talking = data.talking;
-			}
+            if ((data.talking !== undefined) && !voice.usingRadio) {
+                voice.talking = data.talking;
+            }
 
-			if (data.sound && voice.radioEnabled && voice.radioChannel !== 0) {
-				let click = document.getElementById(data.sound);
-				// discard these errors as its usually just a 'uncaught promise' from two clicks happening too fast.
-				click.load();
-				click.volume = data.volume;
-				click.play().catch((e) => {});
-			}
-		});
+            if (data.sound && voice.radioEnabled && voice.radioChannel !== 0) {
+                let click = document.getElementById(data.sound);
+                // discard these errors as its usually just a 'uncaught promise' from two clicks happening too fast.
+                click.load();
+                click.volume = data.volume;
+                click.play().catch((e) => {
+                });
+            }
+        });
 
-		fetch(`https://${GetParentResourceName()}/uiReady`, { method: 'POST' });
+        fetch(`https://${GetParentResourceName()}/uiReady`, {method: 'POST'});
 
-		return { voice };
-	}
+        return {voice};
+    }
 };
 </script>
 
 <style>
 .voiceInfo {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	position: fixed;
-	text-align: right;
-	bottom: 5px;
-	padding: 0;
-	right: 5px;
-	font-size: 12px;
-	font-weight: bold;
-	color: rgb(148, 150, 151);
-	/* https://stackoverflow.com/questions/4772906/css-is-it-possible-to-add-a-black-outline-around-each-character-in-text */
-	text-shadow: 1.25px 0 0 #000, 0 -1.25px 0 #000, 0 1.25px 0 #000,
-		-1.25px 0 0 #000;
+    position: fixed;
+    right: 13vw;
+    bottom: 1.5rem;
+    color: rgba(255, 255, 255, .75);
 }
-.talking {
-	color: rgba(255, 255, 255, 0.822);
+
+.voiceInfo svg {
+    opacity: 0;
+    width: 3rem;
+    height: 3rem;
 }
-p {
-	margin: 0;
+
+.display-enter-active {
+    animation: display-in 3s;
+}
+
+@keyframes display-in {
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
 }
 </style>
