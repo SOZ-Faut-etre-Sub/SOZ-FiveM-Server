0b44e64a51911460dca2f8e132cb1d12e48eddd3
Frozennide <sebastien@sdeu.fr>
2022-02-04 16:48:44 +0100

    feat(phone): update bank apps
    Former-commit-id: b3fb2c9cbdc5cec4d798b99c3ac0778f9370d14a

--- lib/npwd/phone/src/apps/bank/components/BankApp.tsx
+++ lib/npwd/phone/src/apps/bank/components/BankApp.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { BankTitle } from './BankTitle';
-import { AppWrapper } from '@ui/components';
-import { AppContent } from '@ui/components/AppContent';
+import {AppWrapper} from '@ui/components';
+import {AppContent} from '@ui/components/AppContent';
 import './BankApp.css';
 
-import { Switch, Route } from 'react-router-dom';
-import { BankHome } from './home/BankHome';
+import {Switch, Route} from 'react-router-dom';
+import {BankHome} from './home/BankHome';
+import {Transition} from '@headlessui/react';
+import {AppTitle} from "@ui/components/AppTitle";
+import {useTranslation} from "react-i18next";
 
 export const BankApp = () => {
-  return (
-    <AppWrapper id="bank-app">
-      <BankTitle />
-      <AppContent>
-        <Switch>
-          <Route path="/bank" exact component={BankHome} />
-          {/*<Route path="/bank/account" exact component={BankAccount} />*/}
-        </Switch>
-      </AppContent>
-      {/*<NavigationBar />*/}
-    </AppWrapper>
-  );
+    const [t] = useTranslation();
+
+    return (
+        <Transition
+            appear={true}
+            show={true}
+            className="mt-4 h-full flex flex-col"
+            enter="transition-all origin-[80%_10%] duration-500"
+            enterFrom="scale-[0.0] opacity-0"
+            enterTo="scale-100 opacity-100"
+            leave="transition-all origin-[80%_10%] duration-500"
+            leaveFrom="scale-100 opacity-100"
+            leaveTo="scale-[0.0] opacity-0"
+        >
+            <AppWrapper>
+                <AppTitle title={t('APPS_BANK')} isBigHeader={true}/>
+                <AppContent>
+                    <Switch>
+                        <Route path="/bank" exact component={BankHome}/>
+                        {/*<Route path="/bank/account" exact component={BankAccount} />*/}
+                    </Switch>
+                </AppContent>
+                {/*<NavigationBar />*/}
+            </AppWrapper>
+        </Transition>
+    );
 };
--- lib/npwd/phone/src/apps/bank/components/home/BankHome.tsx
+++ lib/npwd/phone/src/apps/bank/components/home/BankHome.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { useCredentials } from '../../hooks/useCredentials';
+import {useCredentials} from '../../hooks/useCredentials';
 import {BankCard} from "../account/BankCard";
+import {AppContent} from "@ui/components/AppContent";
 
 export const BankHome = () => {
-  const credentials = useCredentials();
+    const credentials = useCredentials();
 
-  if (!credentials) return <div>Could not load credentials</div>;
-
-  return (
-    <div >
-      <div >
-        <BankCard name={credentials.name} account="Checking" balance={credentials.balance} />
-      </div>
-    </div>
-  );
+    return (
+        <AppContent className="mt-14 mb-4">
+            {credentials ? (
+                <div>
+                    <BankCard name={credentials.name} account="Checking" balance={credentials.balance}/>
+                </div>
+            ) : (
+                <div className="h-[600px] w-full flex justify-center items-center text-white">
+                    Could not load credentials
+                </div>
+            )}
+        </AppContent>
+    );
 };
