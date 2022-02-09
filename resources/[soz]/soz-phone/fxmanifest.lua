1300eeeb524abf91ad99ee488dd073647de61427
Frozennide <sebastien@sdeu.fr>
2022-02-09 10:31:10 +0100

    feat(phone): lint lua
    Former-commit-id: 0eb4a09dd50c67a1a7e5e70e0240a40422cbba2b

--- resources/[soz]/soz-phone/fxmanifest.lua
+++ resources/[soz]/soz-phone/fxmanifest.lua
@@ -1,25 +1,12 @@
 fx_version "cerulean"
 game "gta5"
-description 'js runtime monkaW'
-authors { "itschip", "erik-sn", "TasoOneAsia", "kidz", "RockySouthpaw" }
 
-client_scripts {
-    'dist/client/client.js',
-    'client/*.lua'
-}
+client_scripts {"dist/client/client.js", "client/*.lua"}
 
-server_script {
-    'dist/server/server.js',
-}
+server_script {"dist/server/server.js"}
 
-ui_page 'html/index.html'
+ui_page "html/index.html"
 
-files {
-    'config.json',
-    'html/index.html',
-    'html/**/*',
-}
+files {"config.json", "html/index.html", "html/**/*"}
 
-dependency {
-    'screenshot-basic'
-}
+dependency {"screenshot-basic"}
