c3fd07794473092db431a48311ff7341c4176ef1
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-02-12 21:45:22 +0100

    lockpick
    Former-commit-id: c834fb911461677e975eb3cffe5e8e2f7d8c4dcc

--- resources/[soz]/soz-extras/server/consumables.lua
+++ resources/[soz]/soz-extras/server/consumables.lua
@@ -102,3 +102,15 @@ RegisterNetEvent("qb-smallpenis:server:AddParachute", function()
     local Player = QBCore.Functions.GetPlayer(src)
     exports["soz-inventory"]:AddItem(Player.PlayerData.source, "parachute", 1)
 end)
+
+----------- / Lockpicking
+
+QBCore.Functions.CreateUseableItem("lockpick", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("lockpicks:UseLockpick", source, false)
+end)
+
+QBCore.Functions.CreateUseableItem("advancedlockpick", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("lockpicks:UseLockpick", source, true)
+end)
