c443f30b6cfa1ca96553590d4071309d109a5106
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-02-12 22:28:59 +0100

    format
    Former-commit-id: f64893aeb8f622f373cf97e338df990bd70800fb

--- resources/[soz]/soz-vehicle/server/concess.lua
+++ resources/[soz]/soz-vehicle/server/concess.lua
@@ -2,9 +2,7 @@ local QBCore = exports["qb-core"]:GetCoreObject()
 
 local function GeneratePlate()
     local plate = QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(2)
-    local result = MySQL.Sync.fetchScalar("SELECT plate FROM player_vehicles WHERE plate = ?", {
-        plate,
-    })
+    local result = MySQL.Sync.fetchScalar("SELECT plate FROM player_vehicles WHERE plate = ?", {plate})
     if result then
         return GeneratePlate()
     else
