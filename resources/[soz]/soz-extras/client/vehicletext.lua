7d5bbf16dd860e1517865dcb7e207fedb45ba04c
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-02-16 00:27:45 +0100

    smallajouts
    Former-commit-id: 1b5f211ae3da04004a85b413b635115018acf99e

--- resources/[soz]/soz-extras/client/vehicletext.lua
+++ resources/[soz]/soz-extras/client/vehicletext.lua
@@ -8,6 +8,8 @@ CreateThread(function()
         else
             text = v["name"]
         end
-        AddTextEntryByHash(v["hash"], text)
+        if v['hash'] ~= 0 and v['hash'] ~= nil then
+			AddTextEntryByHash(v["hash"],text)
+		end
     end
 end)
--- resources/[soz]/soz-extras/config.lua
+++ resources/[soz]/soz-extras/config.lua
@@ -30,8 +30,20 @@ ConsumeablesAlcohol = {
 }
 
 Config.BlacklistedScenarios = {
-    ["TYPES"] = {"WORLD_VEHICLE_MILITARY_PLANES_SMALL", "WORLD_VEHICLE_MILITARY_PLANES_BIG"},
-    ["GROUPS"] = {2017590552, 2141866469, 1409640232, GetHashKey("ng_planes")},
+    ["TYPES"] = {
+        "WORLD_VEHICLE_MILITARY_PLANES_SMALL",
+        "WORLD_VEHICLE_MILITARY_PLANES_BIG",
+        "WORLD_VEHICLE_AMBULANCE",
+        "WORLD_VEHICLE_POLICE_NEXT_TO_CAR",
+        "WORLD_VEHICLE_POLICE_CAR",
+        "WORLD_VEHICLE_POLICE_BIKE",
+    },
+    ["GROUPS"] = {
+        2017590552,
+        2141866469,
+        1409640232,
+        GetHashKey("ng_planes"),
+    },
 }
 
 Config.BlacklistedVehs = {
