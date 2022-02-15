e05dae115cb2d6eac2245eb2dfae2b6acd4ca8c0
Valentin <valentin@waldeck.dev>
2022-02-15 16:19:48 +0100

    Create soz-driving-license resource
    Former-commit-id: 293e1404f1cd53d40943a5ed1f7cfe071a2b7448

---
+++ resources/[soz]/soz-driving-license/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+games {"gta5"}
+lua54 "yes"
+
+description "Driving licenses - car, truck, motorcycle"
+
+shared_script "config.lua"
+
+client_script "client.lua"
+
+server_script "server.lua"
