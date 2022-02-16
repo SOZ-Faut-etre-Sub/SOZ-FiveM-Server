aea999eb0af0380bb25aa34ab8a026d4732c3ba1
Valentin <valentin@waldeck.dev>
2022-02-16 17:25:50 +0100

    Add payment logic server-side
    Former-commit-id: 3e0407ed05ede61ac8ffe4e18b9df37efec22911

---
+++ resources/[soz]/soz-driving-license/server.lua
@@ -0,0 +1,19 @@
+local QBCore = exports["qb-core"]:GetCoreObject()
+
+RegisterNetEvent("soz-driving-license:server:pay", function(licenseType)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then return end
+
+    local lData = Config.Licenses[licenseType]
+    if not lData or type(lData.price) ~= "number" then return end
+
+    if Player.Functions.RemoveMoney("money", lData.price) then
+        TriggerClientEvent("soz-driving-license:client:spawn_car", Player.PlayerData.source)
+    else
+        TriggerClientEvent(
+            "hud:client:DrawNotification",
+            Player.PlayerData.source,
+            "~r~Vous n'avez pas assez d'argent"
+        )
+    end
+end)
