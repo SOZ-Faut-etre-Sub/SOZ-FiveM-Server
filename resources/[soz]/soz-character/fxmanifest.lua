0bc7afb7be3501347d6ec6c96e49bd1d78e9c1ee
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-02-17 20:24:58 +0100

    SpawnV2
    Former-commit-id: 254fbd6ae6bb8a85b47b1ca2540c04b1bcc6249b

--- resources/[soz]/soz-character/fxmanifest.lua
+++ resources/[soz]/soz-character/fxmanifest.lua
@@ -4,9 +4,13 @@ game "gta5"
 description "Soz-character"
 version "1.0.0"
 
+ui_page "html/index.html"
+
 shared_script "config.lua"
 client_script "client/main.lua"
 server_script "server/main.lua"
+
+files {"html/index.html", "html/css/style.css", "html/js/script.js", "html/img/*"}
 
 dependencies {"qb-core"}
 
