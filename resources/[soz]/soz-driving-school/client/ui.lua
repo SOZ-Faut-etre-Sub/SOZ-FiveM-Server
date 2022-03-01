7bf86b62043c89be2d582218bbdaa576374349e3
Valentin <valentin@waldeck.dev>
2022-03-01 20:47:03 +0100

    Updated soz-hud notifications
    Former-commit-id: ec1c1472545fbddf3a528f27c33acf6d93c9a0a5

--- resources/[soz]/soz-driving-school/client/ui.lua
+++ resources/[soz]/soz-driving-school/client/ui.lua
@@ -3,19 +3,16 @@ function DiplayInstructorNotification(type_, penalty)
     if type_ == "INFO" then
         subtitle = "Information"
         message = penalty
-        color = 170
 
     elseif type_ == "WARNING" then
         penalty.warning = true
         subtitle = "Avertissement"
         message = penalty.warningMsg
-        color = 90
 
     elseif type_ == "FAIL" then
         subtitle = "Fin de l'examen"
         message = penalty.failMsg
-        color = 130
     end
 
-    exports["soz-hud"]:DrawAdvancedNotification("Instructeur auto-école", subtitle, message, "CHAR_BLANK_ENTRY", 1, false, false, color)
+    exports["soz-hud"]:DrawAdvancedNotification("Instructeur auto-école", subtitle, message, "CHAR_BLANK_ENTRY", false, Config.NotificationDelay)
 end
