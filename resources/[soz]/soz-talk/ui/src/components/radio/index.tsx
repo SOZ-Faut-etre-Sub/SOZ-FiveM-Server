35caca051c98533e03f528fdd1e5a0ae0f467f0c
Frozennide <sebastien@sdeu.fr>
2022-03-05 17:23:24 +0100

    feat(talk): add cibi to vehicle menu
    Former-commit-id: eee06d68fba37ef2d4008374d39870abc9c63003

--- resources/[soz]/soz-talk/ui/src/components/radio/index.tsx
+++ resources/[soz]/soz-talk/ui/src/components/radio/index.tsx
@@ -1,21 +1,27 @@
-import {FunctionalComponent} from 'preact';
-import radio from '../../assets/img/radio-sr.png';
-import style from './style.module.css';
+import {ComponentProps, FunctionalComponent} from 'preact';
+import radio_sr from '../../assets/img/radio-sr.png';
+import radio_lr from '../../assets/img/radio-lr.png';
+import radioStyle from './radio.module.css';
+import cibiStyle from './cibi.module.css';
 import Screen from "@components/screen";
 import {useCallback, useEffect, useState} from "preact/hooks";
 import {Ear, Frequency, FrequencyType} from "../../types/RadioScreen";
 import fetchAPI from "../../hooks/fetchAPI";
 import {TalkMessageData} from "../../types/TalkMessageEvent";
 
-const Radio: FunctionalComponent = () => {
+const Radio: FunctionalComponent<ComponentProps<any>> = (props) => {
     const [display, setDisplay] = useState<boolean>(false)
     const [enabled, setEnabled] = useState<boolean>(false);
     const [currentFrequency, setCurrentFrequency] = useState<FrequencyType>('primary');
     const [primaryFrequency, setPrimaryFrequency] = useState<Frequency>({frequency: 0.0, volume: 100, ear: Ear.Both});
     const [secondaryFrequency, setSecondaryFrequency] = useState<Frequency>({frequency: 0.0, volume: 100, ear: Ear.Both});
 
+    /* Design */
+    const radio = props.type === 'radio' ? radio_sr : radio_lr
+    const style = props.type === 'radio' ? radioStyle : cibiStyle
+
     const toggleRadio = useCallback(() => {
-        fetchAPI('/radio/enable', {state: !enabled}, () => {
+        fetchAPI(`/${props.type}/enable`, {state: !enabled}, () => {
             setEnabled(s => !s)
         })
     }, [enabled, setEnabled])
@@ -26,7 +32,7 @@ const Radio: FunctionalComponent = () => {
         const ear = (currentFrequency === 'primary' ? primaryFrequency.ear : secondaryFrequency.ear)
         let targetEar = Ear[ear+1] !== undefined ? ear+1 : 0
 
-        fetchAPI('/radio/change_ear', {[currentFrequency]: targetEar}, () => {
+        fetchAPI(`/${props.type}/change_ear`, {[currentFrequency]: targetEar}, () => {
             if (currentFrequency === 'primary') {
                 setPrimaryFrequency(s => ({...s, ...{ear: targetEar}}))
             } else {
@@ -36,7 +42,7 @@ const Radio: FunctionalComponent = () => {
     }, [currentFrequency, primaryFrequency, secondaryFrequency, setPrimaryFrequency, setSecondaryFrequency])
     const handleVolumeChange = useCallback((volume: number) => {
         if (volume >= 0 && volume <= 100) {
-            fetchAPI('/radio/change_volume', {[currentFrequency]: volume}, () => {
+            fetchAPI(`/${props.type}/change_volume`, {[currentFrequency]: volume}, () => {
                 if (currentFrequency === 'primary') {
                     setPrimaryFrequency(s => ({...s, ...{volume: volume}}))
                 } else {
@@ -48,7 +54,7 @@ const Radio: FunctionalComponent = () => {
     const handleFrequencyChange = useCallback(() => {
         const frequency = (currentFrequency === 'primary' ? primaryFrequency.frequency : secondaryFrequency.frequency) * 10
         if (frequency >= 1000 && frequency <= 9999) {
-            fetchAPI('/radio/change_frequency', {
+            fetchAPI(`/${props.type}/change_frequency`, {
                 [currentFrequency]: frequency
             }, () => {})
         }
@@ -58,20 +64,40 @@ const Radio: FunctionalComponent = () => {
     * Events handlers
     */
     const onMessageReceived = useCallback((event: MessageEvent) => {
-        const {type, action} = event.data as TalkMessageData;
+        const {type, action, frequency, volume, isPrimary, isEnabled} = event.data as TalkMessageData;
 
-        if (type === 'radio') {
+        if (type === props.type) {
             if (action === 'open') {
                 setDisplay(true)
             } else if (action === 'close') {
                 setDisplay(false)
+            } else if (action === 'enabled') {
+                if (isEnabled !== undefined) {
+                    setEnabled(isEnabled)
+                }
+            } else if (action === 'frequency_change') {
+                if (frequency) {
+                    if (isPrimary) {
+                        setPrimaryFrequency(s => ({...s, ...{frequency: frequency/10}}))
+                    } else {
+                        setSecondaryFrequency(s => ({...s, ...{frequency: frequency/10}}))
+                    }
+                }
+            } else if (action === 'volume_change') {
+                if (volume) {
+                    if (isPrimary) {
+                        setPrimaryFrequency(s => ({...s, ...{volume}}))
+                    } else {
+                        setSecondaryFrequency(s => ({...s, ...{volume}}))
+                    }
+                }
             }
         }
     }, [])
 
     const onEscapeFunction = useCallback((event: KeyboardEvent) => {
         if (event.key === 'Escape') {
-            fetchAPI('/radio/toggle', {state: false}, () => {
+            fetchAPI(`/${props.type}/toggle`, {state: false}, () => {
                 setDisplay(false)
             })
         }
