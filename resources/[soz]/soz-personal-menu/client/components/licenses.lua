2f19c64cb163c378be8bdaefc5ff6ed31e74de6f
Valentin <valentin@waldeck.dev>
2022-03-08 16:20:44 +0100

    Add identity entry to personal menu
    Former-commit-id: 29754dd9fab4208e0f7cbf4f39bca3d25654777e

--- resources/[soz]/soz-personal-menu/client/components/licenses.lua
+++ resources/[soz]/soz-personal-menu/client/components/licenses.lua
@@ -1,35 +1,48 @@
 function LicensesEntry(menu)
-    local licensesSlider = menu:AddSlider({
-        label = "Mes permis",
-        description = "Voir/Montrer vos permis",
-        values = {
-            {label = "Voir", value = "see"},
-            {label = "Montrer", value = "show"},
-            {label = "Masquer", value = "hide"},
-        },
-    })
+    local identitySubmenu = MenuV:InheritMenu(menu, {subtitle="Gestion de l'identité"})
 
-    licensesSlider:On("select", function(_, value)
-        if value == "hide" then
-            TriggerEvent("soz-identity:client:hide-license")
-            return
-        end
+    menu:AddButton({label = "Mon identité", value = identitySubmenu, description = "Voir/Montrer vos papiers d'identité"})
 
-        local target
-        if value == "see" then
-            target = QBCore.Functions.GetPlayerData().source
+    local sliders = {
+        {label = "Ma carte d'identité", description = "Voir/Montrer vos papiers d'identité", event = "soz-identity:client:request-identity-data"},
+        {label = "Mes permis", description = "Voir/Montrer vos permis", event = "soz-identity:client:request-licenses-data"},
+    }
 
-        elseif value == "show" then
-            local closestPlayer, distance = QBCore.Functions.GetClosestPlayer()
-            if closestPlayer ~= -1 and distance < 2.0 then
-                target = GetPlayerServerId(closestPlayer)
-            else
-                exports["soz-hud"]:DrawNotification("~r~Il n'y a personne à proximité", false, 3000)
-                return
+    -- SLIDERS (identity + licenses)
+    for _, data in ipairs(sliders) do
+        local slider = identitySubmenu:AddSlider({
+            label = data.label,
+            discription = data.description,
+            values = {
+                {label = "Voir", value = "see"},
+                {label = "Montrer", value = "show"},
+            },
+        })
+
+        slider:On("select", function (_, value)
+            local target
+            if value == "see" then
+                target = QBCore.Functions.GetPlayerData().source
+
+            elseif value == "show" then
+                local closestPlayer, distance = QBCore.Functions.GetClosestPlayer()
+                if closestPlayer ~= -1 and distance < 2.0 then
+                    target = GetPlayerServerId(closestPlayer)
+                else
+                    exports["soz-hud"]:DrawNotification("~r~Il n'y a personne à proximité", false, 3000)
+                    return
+                end
             end
-        end
 
-        -- Request data to server and display NUI
-        TriggerEvent("soz-identity:client:request-licenses-data", target)
+            -- Request data to server and display NUI
+            TriggerEvent(data.event, target)
+        end)
+    end
+
+    --- HIDE BUTTON
+    local hideButton = identitySubmenu:AddButton({label = "Masquer les papiers", value = "hide", description = "Masquer carte d'identité ou permis"})
+
+    hideButton:On("select", function ()
+        TriggerEvent("soz-identity:client:hide")
     end)
 end
