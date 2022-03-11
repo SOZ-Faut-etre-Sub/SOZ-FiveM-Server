9d9adf803e07c853f07268539e9de4a159b21a95
Frozennide <sebastien@sdeu.fr>
2022-03-11 17:57:20 +0100

    Revert "feat(phone): use JS animate() with AnimationFrame on Home app"
    This reverts commit 10344dcbfb88258c6fe4d81f1866a8e166e7ef4f [formerly 2e3c90eed3c664bef97249e5b3f9db7063698df0].
    
    
    Former-commit-id: e0cb29c825b467e1aa6ce8e66424f96e5006c989

--- lib/npwd/phone/src/apps/home/components/Home.tsx
+++ lib/npwd/phone/src/apps/home/components/Home.tsx
@@ -1,12 +1,12 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext} from 'react';
 import {AppWrapper} from '@ui/components';
 import {useApps} from '@os/apps/hooks/useApps';
 import {useMySocietyPhoneNumber} from "@os/simcard/hooks/useMyPhoneNumber";
+import {Transition} from '@headlessui/react';
 import {AppsGrid} from "./AppsGrid";
 import {ThemeContext} from "../../../styles/themeProvider";
 
 export const HomeApp: React.FC = () => {
-    const appRef = useRef(null);
     const {apps} = useApps();
     const {theme} = useContext(ThemeContext);
     const societyNumber = useMySocietyPhoneNumber();
@@ -14,22 +14,27 @@ export const HomeApp: React.FC = () => {
     const filteredApps = (societyNumber === null) ? apps.filter((app) => app.home !== true && app.id !== 'SOCIETY_MESSAGES') : apps.filter((app) => app.home !== true)
     const homeApps = apps.filter((app) => app.home === true)
 
-    useEffect(() => {
-        appRef.current.animate([
-            { transform: 'scale(3.0)' }, { transform: 'scale(1.0)' }
-        ], {duration: 500});
-    });
-
     return (
         <AppWrapper>
-            <div ref={appRef} className="mt-4 h-full flex flex-col justify-between">
+            <Transition
+                appear={true}
+                show={true}
+                unmount={false}
+                className="mt-4 h-full flex flex-col justify-between"
+                enter="transition-transform duration-700"
+                enterFrom="scale-[3.0]"
+                enterTo="scale-100"
+                leave="transition-transform duration-700"
+                leaveFrom="scale-100"
+                leaveTo="scale-[3.0]"
+            >
                 <div className="mt-4 px-1">
                     <AppsGrid items={filteredApps}/>
                 </div>
                 <div className={`${theme === 'dark' ? 'bg-black' : 'bg-[#F2F2F6]'} bg-opacity-25 backdrop-blur rounded-[20px] mb-10 py-2 mx-2`}>
                     <AppsGrid items={homeApps}/>
                 </div>
-            </div>
+            </Transition>
         </AppWrapper>
     );
 };
