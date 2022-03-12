cfad10f78d21b38ab7be5c569bc9b0975fb8acdd
Valentin <valentin@waldeck.dev>
2022-03-12 16:41:32 +0100

    lua-tool
    Former-commit-id: 6ab49ddd1975c18551d98d04389ec92c4c1c02c1

--- resources/[soz]/soz-driving-school/config.lua
+++ resources/[soz]/soz-driving-school/config.lua
@@ -71,23 +71,68 @@ Config.InstructorStartSpeech = {
 
 Config.CheckpointCount = 6 -- Number of checkpoints during exam (including final checkpoint)
 Config.Checkpoints = {
-    {x = -606.1, y = -957.79, z = 20.39, message = "Ici, ce sont les bureaux de ~p~Twitch News~s~. Ils ne racontent que des salades…"},
+    {
+        x = -606.1,
+        y = -957.79,
+        z = 20.39,
+        message = "Ici, ce sont les bureaux de ~p~Twitch News~s~. Ils ne racontent que des salades…",
+    },
     {x = 31.24, y = -767.01, z = 42.67, message = "Connaissez-vous Stonks Depository ?"},
-    {x = 248.3, y = -369.19, z = 42.89, message = "T'auras besoin de ton permis si tu veux un job du Pole emploi, alors concentre-toi !"},
+    {
+        x = 248.3,
+        y = -369.19,
+        z = 42.89,
+        message = "T'auras besoin de ton permis si tu veux un job du Pole emploi, alors concentre-toi !",
+    },
     {x = 667.22, y = -27.01, z = 80.96, message = "LSPD Vinewood. Au premier excès de vitesse, tu finis ici !"},
     {x = 204.08, y = 195.72, z = 104.01, message = "La Pacific Bank. Je crois que c'est ici qu'ils rendent l'argent."},
-    {x = 807.06, y = -1290.38, z = 24.72, message = "LSPD, La Mesa. C'est cette patrouille autoroutière qui te coinceras si tu fais n'importe quoi…"},
+    {
+        x = 807.06,
+        y = -1290.38,
+        z = 24.72,
+        message = "LSPD, La Mesa. C'est cette patrouille autoroutière qui te coinceras si tu fais n'importe quoi…",
+    },
     {x = 305.58, y = -1367.15, z = 30.44, message = "Les mauvais conducteurs finissent souvent ici, à l'Hôpital !"},
-    {x = 506.42, y = -1306.97, z = 27.76, message = "Ta maman ne sera pas fière si tu dois venir chercher ta voiture à cette fourrière…"},
+    {
+        x = 506.42,
+        y = -1306.97,
+        z = 27.76,
+        message = "Ta maman ne sera pas fière si tu dois venir chercher ta voiture à cette fourrière…",
+    },
     {x = -222.02, y = -2053.33, z = 26.06, message = "L'affiche dit : \"Maze Bank Arena, le 4 juin 2022\""},
     {x = -1028.3, y = -871.1, z = 5.83, message = "Los Santos Police Department ! De chouettes types !"},
     {x = -696.01, y = 40.51, z = 41.56, message = "Kifflom !"},
-    {x = -1379.76, y = 55.13, z = 52.04, message = "Tu essayes de m'acheter avec une partie de golf ?! Dommage je n'ai pas mes clubs…"},
+    {
+        x = -1379.76,
+        y = 55.13,
+        z = 52.04,
+        message = "Tu essayes de m'acheter avec une partie de golf ?! Dommage je n'ai pas mes clubs…",
+    },
     {x = 399.83, y = -981.8, z = 27.77, message = "Le LSPD surveille tous les conducteurs. Regarde la route !"},
-    {x = -262.13, y = -1310.05, z = 29.65, message = "Benny's ! J'ai le sentiment que tu vas passer beaucoup de temps ici !"},
-    {x = -1601.2, y = 156.95, z = 58.05, message = "Pfff! Regarde-moi tous ces étudiants qui passent leur vie sur les jeux-vidéos…"},
-    {x = -86.89, y = -1101.69, z = 24.46, message = "Termine cet examen avant de lorgner sur le concessionnaire. Chaque chose en son temps."},
-    {x = -552.76, y = -151.6, z = 36.62, message = "Il paraît que c'est dans ce commisariat qu'il y a les meilleurs donuts."},
+    {
+        x = -262.13,
+        y = -1310.05,
+        z = 29.65,
+        message = "Benny's ! J'ai le sentiment que tu vas passer beaucoup de temps ici !",
+    },
+    {
+        x = -1601.2,
+        y = 156.95,
+        z = 58.05,
+        message = "Pfff! Regarde-moi tous ces étudiants qui passent leur vie sur les jeux-vidéos…",
+    },
+    {
+        x = -86.89,
+        y = -1101.69,
+        z = 24.46,
+        message = "Termine cet examen avant de lorgner sur le concessionnaire. Chaque chose en son temps.",
+    },
+    {
+        x = -552.76,
+        y = -151.6,
+        z = 36.62,
+        message = "Il paraît que c'est dans ce commisariat qu'il y a les meilleurs donuts.",
+    },
 }
 
 Config.FinalCheckpointLandVehicle = {
