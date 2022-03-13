f1fc65c406aeba2a9ff1e36f3c56a2e420d90f9b
Joel Wurtz <joel.wurtz@gmail.com>
2022-03-13 22:04:31 +0100

    Temporary set music volume to 0
    Former-commit-id: 4eb3c7855fcbc201cacb917478a3c3e25aa29770

--- resources/[soz]/soz-loadscreen/assets/app.js
+++ resources/[soz]/soz-loadscreen/assets/app.js
@@ -1,27 +1,28 @@
-const app = document.getElementById('app')
-const musique = app.querySelector('audio')
+const app = document.getElementById('app');
+const musique = app.querySelector('audio');
 
-musique.volume = 0.2
+// @TODO [Start] Set to 0.2 when launching server
+musique.volume = 0;
 
 document.querySelector('.info').addEventListener('click', function () {
     if (musique.paused) {
-        musique.play()
-        app.querySelector('.play').style.display = 'block'
-        app.querySelector('.pause').style.display = 'none'
+        musique.play();
+        app.querySelector('.play').style.display = 'block';
+        app.querySelector('.pause').style.display = 'none';
     } else {
-        musique.pause()
-        app.querySelector('.play').style.display = 'none'
-        app.querySelector('.pause').style.display = 'block'
+        musique.pause();
+        app.querySelector('.play').style.display = 'none';
+        app.querySelector('.pause').style.display = 'block';
     }
 })
 
 window.addEventListener('message', function(e) {
     if (e.data.eventName === 'initFunctionInvoking' && e.data.name === 'FinalizeLoad') {
-        document.getElementById('video').style.opacity = '0'
+        document.getElementById('video').style.opacity = '0';
 
         setInterval(function () {
             if (musique.volume > 0.01) {
-                musique.volume -= 0.01
+                musique.volume -= 0.01;
             }
         }, 50);
     }
