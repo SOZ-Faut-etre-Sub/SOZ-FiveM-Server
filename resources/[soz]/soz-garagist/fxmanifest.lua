e3eeeaa8a51e81e70700dd0d239d7b3ab1a53852
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-03-14 01:55:03 +0100

    Format
    Former-commit-id: 528b6776b80f71434cb9f01eb43dd006f570fac7

--- resources/[soz]/soz-garagist/fxmanifest.lua
+++ resources/[soz]/soz-garagist/fxmanifest.lua
@@ -6,22 +6,11 @@ version "1.0.0"
 
 shared_script "config.lua"
 
-client_scripts {
-    "@PolyZone/client.lua",
-    "@PolyZone/BoxZone.lua",
-    "@menuv/menuv.lua",
-    "client/*.lua",
-}
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@menuv/menuv.lua", "client/*.lua"}
 
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "server/*.lua",
-}
+server_scripts {"@oxmysql/lib/MySQL.lua", "server/*.lua"}
 
-dependencies {
-    "menuv",
-    "PolyZone",
-}
+dependencies {"menuv", "PolyZone"}
 
 lua54 "yes"
 
