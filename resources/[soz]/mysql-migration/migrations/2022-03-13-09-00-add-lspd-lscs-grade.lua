10d0d728391a78815182bf8db5c13c0519e0ffbc
Frozennide <sebastien@sdeu.fr>
2022-03-15 14:10:37 +0100

    fix: delete useless permissions in migration
    Former-commit-id: a58fea46fefebebe7475175bdf91949c40cb5f69

--- resources/[soz]/mysql-migration/migrations/2022-03-13-09-00-add-lspd-lscs-grade.lua
+++ resources/[soz]/mysql-migration/migrations/2022-03-13-09-00-add-lspd-lscs-grade.lua
@@ -6,7 +6,7 @@ table.insert(migrations, {
         ]],
         [[
             INSERT INTO job_grades (jobId, name, salary, owner, is_default, permissions) VALUES
-                ('lspd', 'Commissaire', 0, 1, 0, '["society-private-storage"]'),
+                ('lspd', 'Commissaire', 0, 1, 0, '[]'),
                 ('lspd', 'Capitaine', 0, 0, 0, '["society-private-storage"]'),
                 ('lspd', 'Lieutenant', 0, 0, 0, '[]'),
                 ('lspd', 'Sergent-Chef', 0, 0, 0, '[]'),
@@ -17,7 +17,7 @@ table.insert(migrations, {
         ]],
         [[
             INSERT INTO job_grades (jobId, name, salary, owner, is_default, permissions) VALUES
-                ('lscs', 'Sheriff', 0, 1, 0, '["society-private-storage"]'),
+                ('lscs', 'Sheriff', 0, 1, 0, '[]'),
                 ('lscs', 'Adjoint', 0, 0, 0, '["society-private-storage"]'),
                 ('lscs', 'Chef de Division', 0, 0, 0, '[]'),
                 ('lscs', 'Deputy-Chief', 0, 0, 0, '[]'),
