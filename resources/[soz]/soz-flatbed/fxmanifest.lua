b40593829c9b538f3965d74340729539b45b250c
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-03-17 21:46:29 +0100

    Flatbed
    Former-commit-id: 218d891f0b1ccc6240767820a43d46b687e16ff6

---
+++ resources/[soz]/soz-flatbed/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "gta5"
+
+shared_scripts {"config.lua"}
+
+client_scripts {"client/main.lua"}
+
+server_scripts {"server/main.lua"}
+
+files {"stream/def_flatbed3_props.ytyp","meta/*.meta"}
+
+data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
+data_file "DLC_ITYP_REQUEST" "stream/def_flatbed3_props.ytyp"
