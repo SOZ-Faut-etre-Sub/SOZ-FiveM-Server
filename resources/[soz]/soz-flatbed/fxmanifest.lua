ac8ddbc8cf9b1006b6ffacdca92813a8762d6296
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-03-17 21:48:42 +0100

    Format
    Former-commit-id: 5a540585d8cc71536e68f731f814bc2eb7d11a73

--- resources/[soz]/soz-flatbed/fxmanifest.lua
+++ resources/[soz]/soz-flatbed/fxmanifest.lua
@@ -7,7 +7,7 @@ client_scripts {"client/main.lua"}
 
 server_scripts {"server/main.lua"}
 
-files {"stream/def_flatbed3_props.ytyp","meta/*.meta"}
+files {"stream/def_flatbed3_props.ytyp", "meta/*.meta"}
 
 data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
 data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
