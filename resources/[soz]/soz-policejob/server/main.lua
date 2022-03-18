400c3297bc0e52a3fe07be99e7833573dbc6f93f
Frozennide <sebastien@sdeu.fr>
2022-03-18 17:06:53 +0100

    feat(police): implement drag system
    Former-commit-id: afc2cf7ecc05e8835b1fe1a982e54ea2ba8b6e52

--- resources/[soz]/soz-policejob/server/main.lua
+++ resources/[soz]/soz-policejob/server/main.lua
@@ -37,6 +37,52 @@ RegisterNetEvent("police:server:UnCuffPlayer", function(targetId)
     end
 end)
 
+RegisterNetEvent("police:server:EscortPlayer", function(playerId)
+    local player = QBCore.Functions.GetPlayer(source)
+    local target = QBCore.Functions.GetPlayer(playerId)
+
+    if player and target and player ~= target then
+        for _, allowedJob in ipairs(Config.AllowedJobInteraction) do
+            if player.PlayerData.job.id == allowedJob then
+                if target.PlayerData.metadata["ishandcuffed"] or target.PlayerData.metadata["isdead"] or target.PlayerData.metadata["inlaststand"] then
+                    Player(player.PlayerData.source).state:set("isEscorting", true, true)
+                    Player(player.PlayerData.source).state:set("escorting", target.PlayerData.source, true)
+                    Player(target.PlayerData.source).state:set("isEscorted", true, true)
+
+                    TriggerClientEvent("police:client:GetEscorted", target.PlayerData.source, player.PlayerData.source)
+
+                    return
+                end
+            end
+        end
+    end
+end)
+
+RegisterNetEvent("police:server:DeEscortPlayer", function(playerId)
+    local player = QBCore.Functions.GetPlayer(source)
+    local target = QBCore.Functions.GetPlayer(playerId)
+
+    local playerState = Player(player.PlayerData.source).state
+    local targetState = Player(target.PlayerData.source).state
+
+    if player and target and player ~= target then
+        for _, allowedJob in ipairs(Config.AllowedJobInteraction) do
+            if player.PlayerData.job.id == allowedJob then
+                print(playerState.isEscorting, playerState.escorting == target.PlayerData.source, targetState.isEscorted)
+                if playerState.isEscorting and playerState.escorting == target.PlayerData.source and targetState.isEscorted then
+                    Player(player.PlayerData.source).state:set("isEscorting", false, true)
+                    Player(player.PlayerData.source).state:set("escorting", nil, true)
+                    Player(target.PlayerData.source).state:set("isEscorted", false, true)
+
+                    TriggerClientEvent("police:client:DeEscort", target.PlayerData.source)
+
+                    return
+                end
+            end
+        end
+    end
+end)
+
 AddEventHandler("entityCreating", function(handle)
     local entityModel = GetEntityModel(handle)
 
