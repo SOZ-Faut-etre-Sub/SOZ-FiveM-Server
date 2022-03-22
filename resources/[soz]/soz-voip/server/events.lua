693579a2504f5d1198c1b26a30389f2d9c9cd344
Frozennide <sebastien@sdeu.fr>
2022-03-22 18:48:49 +0100

    feat(voip): allow player to self mute
    Former-commit-id: 1c873bbb1f05f63cfd26e55a7440435fb15ad1a8

--- resources/[soz]/soz-voip/server/events.lua
+++ resources/[soz]/soz-voip/server/events.lua
@@ -5,3 +5,7 @@ end)
 RegisterNetEvent("voip:server:setPlayerTalking", function(module, talking, extra)
     voiceModule[module]:setTalking(source, talking, extra)
 end)
+
+RegisterNetEvent("voip:server:muteMe", function(mute)
+    MumbleSetPlayerMuted(source, mute)
+end)
