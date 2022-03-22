00f20cf1c8def79951c24f6c1a75cc642bd6e6ad
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-03-22 18:03:35 +0100

    Servercfg
    Former-commit-id: 9e19b64f0de5dd5356f15f83a4e5bf4d1b0060aa

---
+++ resources/[mapping]/[mapping-prod]/soz-propsexterieurs/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+files {"interiorproxies.meta"}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_z_park.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/soz_prop_radar.ytyp'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
