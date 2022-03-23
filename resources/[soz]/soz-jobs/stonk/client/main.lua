d33c0b88f968775eb80442be2aa28f8d1cf3dab7
Valentin <valentin@waldeck.dev>
2022-03-23 17:48:11 +0100

    Add blip
    Former-commit-id: 052c7e311f0425520df3bf8498d02603db9d49cb

--- resources/[soz]/soz-jobs/stonk/client/main.lua
+++ resources/[soz]/soz-jobs/stonk/client/main.lua
@@ -3,3 +3,13 @@ PlayerData = QBCore.Functions.GetPlayerData()
 
 -- In-memory, player-based save
 CollectedShops = {}
+
+Citizen.CreateThread(function()
+    QBCore.Functions.CreateBlip("stonk-dep", {
+        name = StonkConfig.Blip.Name,
+        coords = StonkConfig.Blip.Coords,
+        sprite = StonkConfig.Blip.Icon,
+        color = StonkConfig.Blip.Color,
+        scale = StonkConfig.Blip.Scale,
+    })
+end)
