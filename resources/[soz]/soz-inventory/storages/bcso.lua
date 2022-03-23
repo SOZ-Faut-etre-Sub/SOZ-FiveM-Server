e8effe3142c9c17d6eaadd7638ac34dc46b7dee7
Frozennide <sebastien@sdeu.fr>
2022-03-23 17:21:25 +0100

    feat(bcso): rename lscs to bcso
    Former-commit-id: e72a799e2bbc816c04303e133f25cfc09025d837

---
+++ resources/[soz]/soz-inventory/storages/bcso.lua
@@ -0,0 +1,27 @@
+Config.Storages["bcso_fridge"] = {
+    label = "Frigo BCSO",
+    type = "fridge",
+    owner = "bcso",
+    position = vector3(-443.49, 6009.12, 30.75),
+}
+
+Config.Storages["bcso_armory"] = {
+    label = "Armurerie BCSO",
+    type = "armory",
+    owner = "bcso",
+    position = vector3(-437.64, 5988.32, 30.73),
+}
+
+Config.Storages["bcso_seizure"] = {
+    label = "Saisie BCSO",
+    type = "seizure",
+    owner = "bcso",
+    position = vector3(-442.3, 5987.31, 30.73),
+}
+
+Config.Storages["bcso_boss_storage"] = {
+    label = "Coffre Capitaine BCSO",
+    type = "boss_storage",
+    owner = "bcso",
+    position = vector3(-434.93, 6003.19, 30.75),
+}
