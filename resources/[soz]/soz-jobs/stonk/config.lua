998a76a36bfc7d94da807f4b877d9c8e4a9ad5a5
Valentin <valentin@waldeck.dev>
2022-03-23 17:48:11 +0100

    Add blip
    Former-commit-id: 052c7e311f0425520df3bf8498d02603db9d49cb

--- resources/[soz]/soz-jobs/stonk/config.lua
+++ resources/[soz]/soz-jobs/stonk/config.lua
@@ -4,3 +4,11 @@ StonkConfig = {
     CollectionRange = {min = 5, max = 8},
     BagItem = "goldbar"
 }
+
+StonkConfig.Blip = {
+    Name = "Stonk Depository",
+    Icon = 605,
+    Color = 47,
+    Coords = vector2(6.25, -709.11),
+    Scale = 0.8,
+}
