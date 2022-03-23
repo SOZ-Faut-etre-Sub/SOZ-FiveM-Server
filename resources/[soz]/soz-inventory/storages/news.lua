1f6d4b8797790230156a60fbc209418561786089
Frozennide <sebastien@sdeu.fr>
2022-03-23 16:18:51 +0100

    feat(news): add storage
    Former-commit-id: 96d746a0f2e16fe8375ef3b1713109656f5c3f6a

---
+++ resources/[soz]/soz-inventory/storages/news.lua
@@ -0,0 +1,20 @@
+Config.Storages["news_fridge"] = {
+    label = "Frigo du Twitch News",
+    type = "fridge",
+    owner = "news",
+    position = vector3(-562.73, -937.69, 32.34),
+}
+
+Config.Storages["news_storage"] = {
+    label = "Stockage du Twitch News",
+    type = "storage",
+    owner = "news",
+    position = vector3(-556.89, -915.25, 32.34),
+}
+
+Config.Storages["news_boss_storage"] = {
+    label = "Coffre patron",
+    type = "boss_storage",
+    owner = "news",
+    position = vector3(-572.84, -939.31, 27.82),
+}
