44820e6bf1e9895271c55f8eb4a6e0789b2d8c6f
Valentin <valentin@waldeck.dev>
2022-03-25 17:56:58 +0100

    (WIP) Continue stonk job
    Former-commit-id: aa7ca5b7d66e2e882eaa65b4dedcb0ee1b61ea58

--- resources/[soz]/soz-jobs/stonk/client/permissions.lua
+++ resources/[soz]/soz-jobs/stonk/client/permissions.lua
@@ -1,8 +1,8 @@
-function CanBagsBeCollected(shopId)
-    if not PlayerData.job.onduty then
-        return false
-    end
+local function isOnDuty()
+    return PlayerData.job.onduty
+end
 
+function CanBagsBeCollected(shopId)
     local hasJobPermission = SozJobCore.Functions.HasPermission(PlayerData.job.id, PlayerData.job.grade, SozJobCore.JobPermission.CashTransfer.CollectBags)
 
     if hasJobPermission then
@@ -15,6 +15,14 @@ function CanBagsBeCollected(shopId)
         end
     end
 
-    return hasJobPermission
+    return isOnDuty() and hasJobPermission
 end
 exports("CanBagsBeCollected", CanBagsBeCollected)
+
+function CanBagsBeResold()
+    local hasJobPermission = SozJobCore.Functions.HasPermission(PlayerData.job.id, PlayerData.job.grade, SozJobCore.JobPermission.CashTransfer.ResaleBags)
+    print("JOB", PlayerData.job.id, PlayerData.job.grade, hasJobPermission)
+
+    return isOnDuty() and hasJobPermission
+end
+exports("CanBagsBeResold", CanBagsBeResold)
--- resources/[soz]/soz-jobs/stonk/config.lua
+++ resources/[soz]/soz-jobs/stonk/config.lua
@@ -1,18 +1,16 @@
-StonkConfig = {
+StonkConfig = {}
+
+StonkConfig.Collection = {
     Cooldown = 60000, -- in ms
-    CollectionDuration = 2000, -- in ms
-    CollectionRange = {min = 5, max = 8},
-    BagItem = "goldbar"
+    Duration = 2000, -- in ms
+    Range = {min = 5, max = 8},
+    BagItem = "goldbar",
 }
 
 StonkConfig.Resale = {
     TargetEntities = {-2018598162, -735318549},
+    Duration = 2000, -- in ms
+    Price = 250,
 }
 
-StonkConfig.Blip = {
-    Name = "Stonk Depository",
-    Icon = 605,
-    Color = 47,
-    Coords = vector2(6.25, -709.11),
-    Scale = 0.8,
-}
+StonkConfig.Blip = {Name = "Stonk Depository", Icon = 605, Color = 47, Coords = vector2(6.25, -709.11), Scale = 0.8}
