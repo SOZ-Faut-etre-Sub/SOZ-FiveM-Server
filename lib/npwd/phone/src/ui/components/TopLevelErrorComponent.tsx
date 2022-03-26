76fbde9c8b7ec2a23d4ad44d34e90bbef2bdf3c6
Frozennide <sebastien@sdeu.fr>
2022-03-26 22:20:41 +0100

    feat(phone): remove sentry
    Former-commit-id: 33255fa5eb370b055bc6edea41ff679597637be7

--- lib/npwd/phone/src/ui/components/TopLevelErrorComponent.tsx
+++ lib/npwd/phone/src/ui/components/TopLevelErrorComponent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { captureException } from '@sentry/react';
 
 interface TopLevelErrorCompProps {
   hasError: boolean;
