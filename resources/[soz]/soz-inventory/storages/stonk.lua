9b9898c8a2d5f666ba6c0eb17519fba314fdb2ae
Valentin <valentin@waldeck.dev>
2022-03-26 17:56:44 +0100

    Add inventory and bank fake pos
    Former-commit-id: a7fc5043eb4342a132b70bb3dd93caa31339beb6

--- resources/[soz]/soz-inventory/storages/stonk.lua
+++ resources/[soz]/soz-inventory/storages/stonk.lua
@@ -2,19 +2,19 @@ Config.Storages["stonk_fridge"] = {
     label = "Frigo Stonk Depository",
     type = "fridge",
     owner = "cash-transfer",
-    position = vector3(0.0, 0.0, 0.0),
+    position = vector3(-22.51, -698.64, 45.01),
 }
 
 Config.Storages["stonk_storage"] = {
     label = "Stockage Stonk Depository",
     type = "storage",
     owner = "cash-transfer",
-    position = vector3(0.0, 0.0, 0.0),
+    position = vector3(-21.99, -701.70, 44.99),
 }
 
 Config.Storages["stonk_boss_storage"] = {
     label = "Coffre patron",
     type = "boss_storage",
     owner = "cash-transfer",
-    position = vector3(0.0, 0.0, 0.0),
+    position = vector3(-19.14, -701.48, 45.00),
 }
