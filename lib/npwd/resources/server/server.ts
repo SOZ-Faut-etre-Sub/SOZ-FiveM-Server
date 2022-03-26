50ea3fb91ae7ddf1c2a88c66f11fd4f3f873dc59
Frozennide <sebastien@sdeu.fr>
2022-03-26 22:20:41 +0100

    feat(phone): remove sentry
    Former-commit-id: 33255fa5eb370b055bc6edea41ff679597637be7

--- lib/npwd/resources/server/server.ts
+++ lib/npwd/resources/server/server.ts
@@ -1,5 +1,4 @@
 import { ResourceConfig } from '../../typings/config';
-import { RewriteFrames } from '@sentry/integrations';
 // Setup and export config loaded at runtime
 export const config: ResourceConfig = JSON.parse(
   LoadResourceFile(GetCurrentResourceName(), 'config.json'),
@@ -18,20 +17,6 @@ import './societies/societies.controller';
 import './settings/settings.controller';
 
 import { mainLogger } from './sv_logger';
-import * as Sentry from '@sentry/node';
-
-// Setup sentry tracing
-if (config.debug.sentryEnabled && process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://1bb64a7c88794547834d7ed7643e9575@o1133416.ingest.sentry.io/6185350',
-    integrations: [new RewriteFrames()],
-    release: process.env.SENTRY_RELEASE || '0.0.0',
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-}
 
 on('onServerResourceStart', (resource: string) => {
   if (resource === GetCurrentResourceName()) {
