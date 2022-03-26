fbd9d42fff7a9eaab43af9b873d0653db8421b86
Valentin <valentin@waldeck.dev>
2022-03-26 17:36:14 +0100

    Setup Stonk Dep storages
    Former-commit-id: 65edb4a9e4321723277be624fdbbfed9e7683b36

---
+++ resources/[soz]/soz-inventory/storages/stonk.lua
@@ -0,0 +1,20 @@
+Config.Storages["stonk_fridge"] = {
+    label = "Frigo Stonk Depository",
+    type = "fridge",
+    owner = "cash-transfer",
+    position = vector3(0.0, 0.0, 0.0),
+}
+
+Config.Storages["stonk_storage"] = {
+    label = "Stockage Stonk Depository",
+    type = "storage",
+    owner = "cash-transfer",
+    position = vector3(0.0, 0.0, 0.0),
+}
+
+Config.Storages["stonk_boss_storage"] = {
+    label = "Coffre patron",
+    type = "boss_storage",
+    owner = "cash-transfer",
+    position = vector3(0.0, 0.0, 0.0),
+}
