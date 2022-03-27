90ea263bcc741fa38f4f3512ac2d5d2876ff9bea
Valentin <valentin@waldeck.dev>
2022-03-27 12:14:33 +0200

    Complete money bags resale
    Former-commit-id: 8652594e075371dea3b9bd47243befa2c88c1dfb

--- resources/[soz]/soz-jobs/stonk/config.lua
+++ resources/[soz]/soz-jobs/stonk/config.lua
@@ -4,15 +4,17 @@ StonkConfig.NotifDelay = 2000 -- in ms
 
 StonkConfig.Collection = {
     Cooldown = 60000, -- in ms
-    Duration = 2000, -- in ms
+    Duration = 3000, -- in ms
     Range = {min = 5, max = 8},
     BagItem = "moneybag",
 }
 
 StonkConfig.Resale = {
     TargetEntities = {-2018598162, -735318549},
-    Duration = 2000, -- in ms
-    Price = 250,
+    Duration = 3000, -- in ms
+    Price = 100,
 }
 
 StonkConfig.Blip = {Name = "Stonk Depository", Icon = 605, Color = 47, Coords = vector2(6.25, -709.11), Scale = 0.8}
+
+StonkConfig.SafeStorageName = "safe_stonk"
