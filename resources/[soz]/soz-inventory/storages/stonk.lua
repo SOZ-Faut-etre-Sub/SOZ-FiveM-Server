6e7967ce05d36ee4dbdc9ca1ad4ababe12d34f8c
Valentin <valentin@waldeck.dev>
2022-03-27 11:01:28 +0200

    Update fridge / armory position based on updated MLO
    Former-commit-id: 5bd0c6992a71d6bdcfe18995238769d5eac41837

--- resources/[soz]/soz-inventory/storages/stonk.lua
+++ resources/[soz]/soz-inventory/storages/stonk.lua
@@ -2,7 +2,9 @@ Config.Storages["stonk_fridge"] = {
     label = "Frigo Stonk Depository",
     type = "fridge",
     owner = "cash-transfer",
-    position = vector3(-22.51, -698.64, 45.01),
+    position = vector3(-10.0, -700.62, 46.25),
+    size = vector2(1.5, 1.5),
+    heading = 204.6,
 }
 
 Config.Storages["stonk_storage"] = {
@@ -17,4 +19,13 @@ Config.Storages["stonk_boss_storage"] = {
     type = "boss_storage",
     owner = "cash-transfer",
     position = vector3(-19.14, -701.48, 45.00),
+}
+
+Config.Storages["stonk_armory"] = {
+    label = "Armurerie Stonk Depository",
+    type = "armory",
+    owner = "cash-transfer",
+    position = vector3(-17.72, -715.79, 46.02),
+    size = vector2(2.75, 3.75),
+    heading = 204.63,
 }
