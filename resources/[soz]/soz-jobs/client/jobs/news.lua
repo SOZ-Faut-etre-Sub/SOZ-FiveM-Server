35c98701ef831ea556a4c763291789378b01bec8
Frozennide <sebastien@sdeu.fr>
2022-03-27 22:55:16 +0200

    feat(news): create menu entry for sending news
    Former-commit-id: b6eaba3f87671dbf0137f7759123bb22590a32a6

--- resources/[soz]/soz-jobs/client/jobs/news.lua
+++ resources/[soz]/soz-jobs/client/jobs/news.lua
@@ -1,6 +1,66 @@
----
+local societyMenu = MenuV:CreateMenu(nil, "", "menu_job_news", "soz", "news:menu")
 
+--- Targets
+CreateThread(function()
+    exports["qb-target"]:AddGlobalPlayer({
+        options = {
+            {
+                label = "Facture",
+                icon = "fas fa-file-invoice-dollar",
+                event = "jobs:client:news:InvoicePlayer",
+                job = "news",
+            },
+        },
+        distance = 1.5,
+    })
+end)
 
+--- Events
+RegisterNetEvent("jobs:client:news:InvoicePlayer", function(data)
+    local player = NetworkGetPlayerIndexFromPed(data.entity)
+
+    local title = exports["soz-hud"]:Input("Titre", 200)
+    if title == nil or title == "" then
+        exports["soz-hud"]:DrawNotification("~r~Vous devez spécifier un title")
+        return
+    end
+
+    local amount = exports["soz-hud"]:Input("Montant", 10)
+    if amount == nil or tonumber(amount) == nil or tonumber(amount) <= 0 then
+        exports["soz-hud"]:DrawNotification("~r~Vous devez spécifier un montant")
+        return
+    end
+
+    TriggerServerEvent("banking:server:sendInvoice", GetPlayerServerId(player), title, amount)
+end)
+
+RegisterNetEvent("jobs:client:news:OpenSocietyMenu", function()
+    societyMenu:ClearItems()
+
+    societyMenu:AddSlider({
+        label = "Faire une communication",
+        value = "nil",
+        values = {
+            {label = "Annonce", value = "annonce"},
+            {label = "Breaking News", value = "breaking-news"},
+            {label = "Publicité", value = "publicité"},
+            {label = "Fait Divers", value = "fait-divers"},
+            {label = "Info Traffic", value = "info-traffic"},
+        },
+        select = function(_, value)
+            local message = exports["soz-hud"]:Input("Message de la communication", 512)
+            if message == nil or message == "" then
+                exports["soz-hud"]:DrawNotification("~r~Vous devez spécifier un message")
+                return
+            end
+
+            TriggerServerEvent("phone:app:news:createNewsBroadcast", "phone:app:news:createNewsBroadcast:" .. QBCore.Shared.UuidV4(),
+                               {type = value, message = message})
+        end,
+    })
+
+    societyMenu:Open()
+end)
 
 --- Threads
 CreateThread(function()
