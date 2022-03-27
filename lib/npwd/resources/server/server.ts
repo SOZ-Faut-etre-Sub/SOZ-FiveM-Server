6b68cbc28d9e6728ed24641ab06c93aa56649bfd
Frozennide <sebastien@sdeu.fr>
2022-03-27 22:11:00 +0200

    feat(phone): add twitch-news app
    Former-commit-id: 7e052b914a624deea4c4e4734217a4552cd28ad5

--- lib/npwd/resources/server/server.ts
+++ lib/npwd/resources/server/server.ts
@@ -14,6 +14,7 @@ import './photo/photo.controller';
 import './messages/messages.controller';
 import './marketplace/marketplace.controller';
 import './societies/societies.controller';
+import './twitch-news/twitch-news.controller';
 import './settings/settings.controller';
 
 import { mainLogger } from './sv_logger';
