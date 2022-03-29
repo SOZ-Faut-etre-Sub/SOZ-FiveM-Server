543c35636ae4d398a69494ef0b8a884c15016eb7
leaudevian <leaudevian@dt.com>
2022-03-29 21:45:27 +0200

    menu and cloakroom
    Former-commit-id: 50b26337ce58d330a9a3d8018262999b704be8b9

---
+++ resources/[soz]/soz-ems/client/cloakroom.lua
@@ -0,0 +1,21 @@
+RegisterNetEvent("lsmc:client:OpenCloakroomMenu", function()
+    EmsJob.Functions.Menu.GenerateMenu(PlayerData.job.id, function(menu)
+        menu:AddButton({
+            label = "Tenue civile",
+            value = nil,
+            select = function()
+                TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+            end,
+        })
+
+        for name, skin in pairs(Config.Cloakroom[PlayerData.job.id][PlayerData.skin.Model.Hash]) do
+            menu:AddButton({
+                label = name,
+                value = nil,
+                select = function()
+                    TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                end,
+            })
+        end
+    end)
+end)
