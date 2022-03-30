5f5ce2ecac933ce74fedf3d9205cb3f35b739236
Frozennide <sebastien@sdeu.fr>
2022-03-31 00:29:38 +0200

    feat(garbage): add farm logic
    Former-commit-id: 41d35b218ddc2980655f87777eaefd834f81b49c

--- resources/[soz]/soz-jobs/client/jobs/garbage.lua
+++ resources/[soz]/soz-jobs/client/jobs/garbage.lua
@@ -15,6 +15,20 @@ CreateThread(function()
         },
         distance = 2.5,
     })
+
+    exports["qb-target"]:AddBoxZone("garbage:process", vector3(-583.05, -1610.89, 27.01), 5.8, 26.6,
+                                    {name = "garbage:process", heading = 355, minZ = 26.01, maxZ = 30.01}, {
+        options = {
+            {
+                label = "Recycler le contenu du sac",
+                event = "jobs:client:garbage:processBags",
+                targeticon = "fas fa-recycle",
+                icon = "fas fa-recycle",
+                job = "garbage",
+            },
+        },
+        distance = 2.5,
+    })
 end)
 
 --- Events
@@ -24,7 +38,8 @@ RegisterNetEvent("QBCore:Player:SetPlayerData", function(playerData)
             local player = PlayerPedId()
             if garbageBagProp == nil then
                 garbageBagProp = CreateObject(GetHashKey("prop_cs_rub_binbag_01"), GetEntityCoords(player), true)
-                AttachEntityToEntity(garbageBagProp, player, GetPedBoneIndex(player, 57005), 0.12, 0.0, -0.05, 220.0, 120.0, 0.0, true, true, false, true, 1, true)
+                AttachEntityToEntity(garbageBagProp, player, GetPedBoneIndex(player, 57005), 0.12, 0.0, -0.05, 220.0, 120.0, 0.0, true, true, false, true, 1,
+                                     true)
             end
 
             CreateThread(function()
@@ -32,10 +47,10 @@ RegisterNetEvent("QBCore:Player:SetPlayerData", function(playerData)
                 haveGarbageBag = true
 
                 while haveGarbageBag do
-                    if not IsEntityPlayingAnim(ped, 'missfbi4prepp1', '_bag_walk_garbage_man', 3) then
+                    if not IsEntityPlayingAnim(ped, "missfbi4prepp1", "_bag_walk_garbage_man", 3) then
                         ClearPedTasksImmediately(ped)
-                        QBCore.Functions.RequestAnimDict('missfbi4prepp1')
-                        TaskPlayAnim(ped, 'missfbi4prepp1', '_bag_walk_garbage_man', 6.0, -6.0, -1, 49, 0, 0, 0, 0)
+                        QBCore.Functions.RequestAnimDict("missfbi4prepp1")
+                        TaskPlayAnim(ped, "missfbi4prepp1", "_bag_walk_garbage_man", 6.0, -6.0, -1, 49, 0, 0, 0, 0)
                     end
                     Wait(200)
                 end
@@ -56,6 +71,18 @@ RegisterNetEvent("QBCore:Player:SetPlayerData", function(playerData)
     haveGarbageBag = false
 end)
 
+RegisterNetEvent("jobs:client:garbage:processBags", function()
+    QBCore.Functions.Progressbar("Recyclage du sac", "Recyclage du sac en cours...", math.random(4000, 8000), false, true,
+                                 {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function() -- Done
+        TriggerServerEvent("jobs:server:garbage:processBags")
+    end)
+end)
+
 RegisterNetEvent("jobs:client:garbage:OpenSocietyMenu", function()
     societyMenu:ClearItems()
 
@@ -74,7 +101,7 @@ RegisterNetEvent("jobs:client:garbage:OpenSocietyMenu", function()
 
                 QBCore.Functions.HideBlip("garbage_bin_" .. binId, not checked)
             end
-        end
+        end,
     })
 
     societyMenu:Open()
