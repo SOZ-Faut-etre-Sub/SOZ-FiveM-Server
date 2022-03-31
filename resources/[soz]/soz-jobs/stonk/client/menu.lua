6eae5be093496b7fbdfd25257a741afe58d1d636
Valentin <valentin@waldeck.dev>
2022-03-31 20:49:59 +0200

    Added job related menu
    Former-commit-id: 0251ace5f8c64edc50215c0f9904a8dcb297e16b

---
+++ resources/[soz]/soz-jobs/stonk/client/menu.lua
@@ -0,0 +1,69 @@
+StonkJob = {}
+StonkJob.Functions = {}
+StonkJob.Functions.Menu = {}
+StonkJob.Menus = {}
+
+CreateThread(function()
+    StonkJob.Menus["cash-transfer"] = {
+        menu = MenuV:CreateMenu(nil, "Stonk Depository", "menu_job_lspd", "soz", "stonk:menu"),
+    }
+end)
+
+local function PropsEntity(menu)
+    menu:AddSlider({
+        icon = "🚧",
+        label = "Poser un objet",
+        value = nil,
+        values = {
+            {label = "Cone de circulation", value = {item = "cone", props = "prop_roadcone02a"}},
+            {label = "Barrière", value = {item = "police_barrier", props = "prop_barrier_work05"}},
+        },
+        select = function(_, value)
+            TriggerServerEvent("job:server:placeProps", value.item, value.props)
+        end,
+    })
+end
+
+StonkJob.Functions.Menu.GenerateMenu = function(job, cb)
+    if not StonkJob.Functions.Menu.MenuAccessIsValid(job) then
+        return
+    end
+
+    --- @type Menu
+    local menu = StonkJob.Menus[job].menu
+    menu:ClearItems()
+
+    cb(menu)
+
+    if menu.IsOpen then
+        MenuV:CloseAll(function()
+            menu:Close()
+        end)
+    else
+        MenuV:CloseAll(function()
+            menu:Open()
+        end)
+    end
+end
+
+StonkJob.Functions.Menu.GenerateJobMenu = function(job)
+    StonkJob.Functions.Menu.GenerateMenu(job, function(menu)
+        if PlayerData.job.onduty then
+            PropsEntity(menu)
+        else
+            menu:AddButton({label = "Tu n'es pas en service !", disabled = true})
+        end
+    end)
+end
+
+--- Events
+RegisterNetEvent("stonk:client:OpenSocietyMenu", function()
+    StonkJob.Functions.Menu.GenerateJobMenu(PlayerData.job.id)
+end)
+
+StonkJob.Functions.Menu.MenuAccessIsValid = function(job)
+    if PlayerData.job.id == "cash-transfer" then
+        return true
+    end
+    return false
+end
