d3ebd252b062d5d75b9f25875e598c2024a5268d
Frozennide <sebastien@sdeu.fr>
2022-04-02 14:44:20 +0200

    feat(personal): add walking style
    Former-commit-id: a9f16cd682590dcc2a0f8f56495593c659e48b91

--- resources/[soz]/soz-personal-menu/client/components/animations.lua
+++ resources/[soz]/soz-personal-menu/client/components/animations.lua
@@ -1,8 +1,9 @@
 local animationMenu = MenuV:InheritMenu(personalMenu, {subtitle = "Animations"})
 local allAnimationMenu = MenuV:InheritMenu(personalMenu, {subtitle = "Animations"})
+local allWalksMenu = MenuV:InheritMenu(personalMenu, {subtitle = "Démarches"})
 local personalAnimationMenu = MenuV:InheritMenu(personalMenu, {subtitle = "Animations"})
 
-local animationCatalogMenu = {}
+local animationCatalogMenu, walkCatalogMenu = {}, {}
 local favoriteAnimationRegister, favoriteAnimationKey = false, 1
 
 local PlayEmote = function(animation)
@@ -18,10 +19,19 @@ local PlayEmote = function(animation)
     end
 end
 
+local PlayWalking = function(animation)
+    RequestAnimSet(animation)
+    while not HasAnimSetLoaded(animation) do
+        Wait(1)
+    end
+
+    SetPedMovementClipset(PlayerPedId(), animation, 0.2)
+    RemoveAnimSet(animation)
+end
+
 GenerateAnimationList = function(menu, category, content)
     if type(category) ~= "number" then
         local categoryID = menu.UUID .. category
-
         if animationCatalogMenu[categoryID] == nil then
             animationCatalogMenu[categoryID] = MenuV:InheritMenu(menu, {subtitle = category})
             menu:AddButton({icon = "📋", label = category, value = animationCatalogMenu[categoryID]})
@@ -30,15 +40,12 @@ GenerateAnimationList = function(menu, category, content)
         for cat, cont in pairs(content) do
             GenerateAnimationList(animationCatalogMenu[categoryID], cat, cont)
         end
-
         return
     end
 
     menu:AddButton({
         label = content[1],
-        -- description = content[2],
         rightLabel = content[3] or nil,
-        -- content[4],
         select = function()
             if favoriteAnimationRegister then
                 SetResourceKvp("soz/animation/" .. favoriteAnimationKey, json.encode(content[5]))
@@ -55,13 +62,39 @@ GenerateAnimationList = function(menu, category, content)
     Wait(5)
 end
 
+GenerateWalksList = function(menu, category, content)
+    if type(category) ~= "number" then
+        local categoryID = menu.UUID .. category
+        if walkCatalogMenu[categoryID] == nil then
+            walkCatalogMenu[categoryID] = MenuV:InheritMenu(menu, {subtitle = category})
+            menu:AddButton({icon = "📋", label = category, value = walkCatalogMenu[categoryID]})
+        end
+
+        for cat, cont in pairs(content) do
+            GenerateWalksList(walkCatalogMenu[categoryID], cat, cont)
+        end
+        return
+    end
+
+    menu:AddButton({
+        label = content.name,
+        select = function()
+            PlayWalking(content.walk)
+            TriggerServerEvent("QBCore:Server:SetMetaData", "walk", content.walk)
+        end,
+    })
+
+    Wait(5)
+end
+
 function AnimationsEntry()
     personalMenu:AddButton({label = "Animations", value = animationMenu})
 end
 
 CreateThread(function()
-    animationMenu:AddButton({label = "Toutes les animations", value = allAnimationMenu})
-    animationMenu:AddButton({label = "Mes animations personnelles", value = personalAnimationMenu})
+    animationMenu:AddButton({label = "Animations", value = allAnimationMenu})
+    animationMenu:AddButton({label = "Démarches", value = allWalksMenu})
+    animationMenu:AddButton({label = "Mes Animations", value = personalAnimationMenu})
 
     for shortcut = 1, 10 do
         personalAnimationMenu:AddButton({
@@ -84,7 +117,28 @@ CreateThread(function()
         end, true)
     end
 
+    --- Walk style menu
+    allWalksMenu:AddButton({
+        label = "Démarche par défaut",
+        select = function()
+            ResetPedMovementClipset(PlayerPedId())
+            TriggerServerEvent("QBCore:Server:SetMetaData", "walk", nil)
+        end,
+    })
+    for category, content in pairs(Config.WalkStyle) do
+        GenerateWalksList(allWalksMenu, category, content)
+    end
+
+    --- Animation menu
     for category, content in pairs(Config.AnimationsList) do
         GenerateAnimationList(allAnimationMenu, category, content)
+    end
+end)
+
+RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
+    local walk = QBCore.Functions.GetPlayerData().metadata.walk
+
+    if walk then
+        PlayWalking(walk)
     end
 end)
