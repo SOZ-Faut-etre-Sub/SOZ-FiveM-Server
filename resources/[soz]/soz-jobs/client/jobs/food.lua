bd0af9897504cf6bad4be40a244b02aabd9e3137
Valentin <valentin@waldeck.dev>
2022-04-03 18:36:17 +0200

    Add zone for duty and cloakroom
    Former-commit-id: b6efd36024a307cf08c4ddb17ad974f230cf6da2

--- resources/[soz]/soz-jobs/client/jobs/food.lua
+++ resources/[soz]/soz-jobs/client/jobs/food.lua
@@ -7,4 +7,51 @@ Citizen.CreateThread(function()
         color = FoodConfig.Blip.Color,
         scale = FoodConfig.Blip.Scale,
     })
+
+    -- DUTY
+    exports["qb-target"]:AddBoxZone("food:duty", vector2(-1898.66, 2075.36), 0.5, 0.75, {
+        heading = 70.25,
+        minZ = 140.0,
+        maxZ = 142.5,
+        debugPoly = true,
+    }, {
+        options = {
+            {
+                icon = "fas fa-sign-in-alt",
+                label = "Prise de service",
+                type = "server",
+                event = "QBCore:ToggleDuty",
+                canInteract = function()
+                    return not PlayerData.job.onduty
+                end,
+            },
+            {
+                icon = "fas fa-sign-out-alt",
+                label = "Fin de service",
+                type = "server",
+                event = "QBCore:ToggleDuty",
+                canInteract = function()
+                    return PlayerData.job.onduty
+                end,
+            },
+        },
+    })
+
+    -- CLOAKROOM
+    exports["qb-target"]:AddBoxZone("food:cloakroom", vector2(-1866.8, 2059.98), 0.5, 1.5, {
+        heading = 340.76,
+        minZ = 140.0,
+        maxZ = 142.5,
+        debugPoly = true,
+    }, {
+        options = {
+            {
+                targeticon = "fas fa-box",
+                icon = "fas fa-tshirt",
+                event = "jobs:client:food:OpenCloakroomMenu",
+                label = "Se changer",
+                job = "cash-transfer",
+            },
+        },
+    })
 end)
