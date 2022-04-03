0fc51b09a8262ba63326a804c82b020f2943706a
leaudevian <leaudevian@dt.com>
2022-04-04 00:25:58 +0200

    update item
    Former-commit-id: 0804714f7d909e0306e841158dde024ec6767f2e

--- resources/[soz]/soz-ems/server/item.lua
+++ resources/[soz]/soz-ems/server/item.lua
@@ -2,7 +2,7 @@ QBCore = exports["qb-core"]:GetCoreObject()
 
 --- item
 
-QBCore.Functions.CreateUseableItem("mouchoir", function(source)
+QBCore.Functions.CreateUseableItem("tissue", function(source)
     TriggerClientEvent("lsmc:client:mouchoir", source)
 end)
 
@@ -10,7 +10,7 @@ QBCore.Functions.CreateUseableItem("pommade", function(source)
     TriggerClientEvent("lsmc:client:pommade", source)
 end)
 
-QBCore.Functions.CreateUseableItem("antibiotique", function(source)
+QBCore.Functions.CreateUseableItem("antibiotic", function(source)
     TriggerClientEvent("lsmc:client:antibiotique", source)
 end)
 
@@ -18,6 +18,6 @@ QBCore.Functions.CreateUseableItem("ifaks", function(source)
     TriggerClientEvent("lsmc:client:ifaks", source)
 end)
 
-QBCore.Functions.CreateUseableItem("défibrillateur", function(source)
+QBCore.Functions.CreateUseableItem("defibrillator", function(source)
     TriggerClientEvent("lsmc:client:défibrillateur", source)
 end)
