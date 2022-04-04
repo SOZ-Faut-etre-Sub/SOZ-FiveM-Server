d6cdaf1b39a44e74a51ffa96ef7c68ad9fd3e059
Frozennide <sebastien@sdeu.fr>
2022-04-04 18:19:05 +0200

    feat(shops): use new shop configuration model for ammunation
    Former-commit-id: 313903f9f867b46ca817117b682abbd76ee81ea1

--- resources/[soz]/soz-shops/server/main.lua
+++ resources/[soz]/soz-shops/server/main.lua
@@ -1,47 +1,39 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 
-RegisterNetEvent("shops:server:pay", function(shop, product, amount)
+--- Items functions
+local function getItemPrice(product, productID, Player)
+    if product == "tattoo" then
+        for _, tattoo in pairs(Config.Products[product]) do
+            local overlayField = Player.PlayerData.charinfo.gender == 0 and "HashNameMale" or "HashNameFemale"
+
+            if tattoo["Collection"] == productID.collection and tattoo[overlayField] == productID.overlay then
+                return tattoo["Price"]
+            end
+        end
+    else
+        return Config.Products[product][productID].price
+    end
+end
+
+RegisterNetEvent("shops:server:pay", function(product, productID, amount)
     local Player = QBCore.Functions.GetPlayer(source)
     amount = tonumber(amount) or 1
 
-    if Config.Locations[shop] == nil or (Config.Locations[shop].type ~= "tattoo" and Config.Locations[shop].products[product] == nil) then
+    if Config.Products[product] == nil then
         return
     end
 
     if Player then
-        local item = Config.Locations[shop].products[product]
-        if Config.Locations[shop].type == "tattoo" then
-            for _, tattoo in pairs(Config.Locations[shop].products) do
-                local overlayField = Player.PlayerData.charinfo.gender == 0 and "HashNameMale" or "HashNameFemale"
+        local item = Config.Products[product][productID]
+        local price = getItemPrice(product, productID, Player) * amount
 
-                if tattoo["Collection"] == product.collection and tattoo[overlayField] == product.overlay then
-                    item = {price = tattoo["Price"]}
-                    break
-                end
-            end
-        end
-        local price = item.price * amount
-
-        if Config.Locations[shop].type ~= "tattoo" and item.amount < amount then
+        if product ~= "tattoo" and item.amount < amount then
             TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Ce magasin n'a pas assez de stock", "error")
             return
         end
 
         if Player.Functions.RemoveMoney("money", price) then
-            if Config.Locations[shop].type ~= "tattoo" then
-                exports["soz-inventory"]:AddItem(Player.PlayerData.source, item.name, amount, nil, nil, function(success, reason)
-                    if success then
-                        Config.Locations[shop].products[product].amount = Config.Locations[shop].products[product].amount - amount
-                        if Config.Locations[shop].products[product].amount <= 0 then
-                            Config.Locations[shop].products[product].amount = 0
-                        end
-                        TriggerClientEvent("shops:client:SetShopItems", -1, shop, Config.Locations[shop].products)
-
-                        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source,
-                                           ("Vous venez d'acheter ~b~%s %s~s~ pour ~g~$%s"):format(amount, QBCore.Shared.Items[item.name].label, price))
-                    end
-                end)
-            else
+            if product == "tattoo" then
                 local skin = Player.PlayerData.skin
                 skin.Tattoos = skin.Tattoos or {}
 
@@ -51,8 +43,20 @@ RegisterNetEvent("shops:server:pay", function(shop, product, amount)
                 })
 
                 Player.Functions.SetSkin(skin)
-
                 TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, ("Vous venez de vous faire tatouer pour ~g~$%s"):format(price))
+            else
+                exports["soz-inventory"]:AddItem(Player.PlayerData.source, item.name, amount, nil, nil, function(success, reason)
+                    if success then
+                        Config.Products[product][productID].amount = Config.Products[product][productID].amount - amount
+                        if Config.Products[product][productID].amount <= 0 then
+                            Config.Products[product][productID].amount = 0
+                        end
+                        TriggerClientEvent("shops:client:SetShopItems", -1, product, Config.Products[product])
+
+                        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source,
+                            ("Vous venez d'acheter ~b~%s %s~s~ pour ~g~$%s"):format(amount, QBCore.Shared.Items[item.name].label, price))
+                    end
+                end)
             end
         else
             TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Vous n'avez pas assez d'argent", "error")
