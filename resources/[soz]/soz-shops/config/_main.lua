8dc1058680860b3948b93e1cf00e2ac8f14795d1
Frozennide <sebastien@sdeu.fr>
2022-04-05 11:53:19 +0200

    feat(shops): use new shop configuration model for barber
    Former-commit-id: 88592c6d0bf610e0e54c24198113f8802c915d51

--- resources/[soz]/soz-shops/config/_main.lua
+++ resources/[soz]/soz-shops/config/_main.lua
@@ -1,10 +1,41 @@
 Config = {}
 
-if IsDuplicityVersion() then
-    Config.ComponentList = {}
-else
-    Config.ComponentList = exports["soz-character"]:GetLabels()
-end
-
 Config.Products = {}
 Config.Locations = {}
+
+if IsDuplicityVersion() then
+    Config.CharacterComponentList = {}
+    Config.CharacterComponentColors = {}
+else
+    Config.CharacterComponentList = exports["soz-character"]:GetLabels()
+    Config.CharacterComponentColors = exports["soz-character"]:GetColors()
+
+    CreateThread(function()
+        local numHairColors = GetNumHairColors();
+        local numMakeupColors = GetNumMakeupColors();
+
+        for i = 0, numHairColors - 1 do
+            local r, g, b = GetHairRgbColor(i);
+
+            table.insert(Config.CharacterComponentColors.Hair, {
+                value = i,
+                label = "HAIR_COLOR_" .. i,
+                r = r,
+                g = g,
+                b = b,
+            });
+        end
+
+        for i = 0, numMakeupColors - 1 do
+            local r, g, b = GetMakeupRgbColor(i);
+
+            table.insert(Config.CharacterComponentColors.Makeup, {
+                value = i,
+                label = "MAKEUP_COLOR_" .. i,
+                r = r,
+                g = g,
+                b = b,
+            });
+        end
+    end)
+end
