a04711cd57c57d1d022c32eafe9cfd010930544a
Frozennide <sebastien@sdeu.fr>
2022-04-07 00:32:05 +0200

    feat(news): player can collect news items
    Former-commit-id: d96fdb619a1801a9a7d4127d13f88483a9c80d50

--- resources/[soz]/soz-jobs/server/objects.lua
+++ resources/[soz]/soz-jobs/server/objects.lua
@@ -1,3 +1,9 @@
+local CollectObjects = {
+    [GetHashKey("prop_ld_greenscreen_01")] = "n_fix_greenscreen",
+    [GetHashKey("prop_tv_cam_02")] = "n_fix_camera",
+    [GetHashKey("prop_kino_light_01")] = "n_fix_light",
+    [GetHashKey("v_ilev_fos_mic")] = "n_fix_mic",
+}
 local Objects = {}
 
 RegisterNetEvent("job:server:placeProps", function(item, props, rotation)
@@ -22,5 +28,20 @@ RegisterNetEvent("job:server:RemoveObject", function(objNet)
     if Objects[objNet] then
         DeleteEntity(NetworkGetEntityFromNetworkId(objNet))
         Objects[objNet] = nil
+    end
+end)
+
+RegisterNetEvent("job:server:CollectObject", function(objNet)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local object = NetworkGetEntityFromNetworkId(objNet)
+    local item = CollectObjects[GetEntityModel(object)]
+
+    if item then
+        if exports["soz-inventory"]:CanCarryItem(Player.PlayerData.source, item, 1) then
+            exports["soz-inventory"]:AddItem(Player.PlayerData.source, item, 1)
+            TriggerEvent("job:server:RemoveObject", objNet)
+        else
+            TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Vous ne pouvez pas récupérer cet objet", "error")
+        end
     end
 end)
