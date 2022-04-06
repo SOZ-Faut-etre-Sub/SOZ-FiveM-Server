761cecee67d438208634a6864fc0ececd41253d7
leaudevian <leaudevian@dt.com>
2022-04-06 22:05:38 +0200

    les fix du canard
    Former-commit-id: 90f57f6f50c16f878e078ec172ceb88008fa0572

--- resources/[qb]/qb-core/server/events.lua
+++ resources/[qb]/qb-core/server/events.lua
@@ -136,15 +136,13 @@ RegisterNetEvent('QBCore:UpdatePlayer', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player then
+        local newHunger = Player.PlayerData.metadata['hunger'] - QBCore.Config.Player.HungerRate
         if Player.PlayerData.metadata['foie'] then
-            local newHunger = Player.PlayerData.metadata['hunger'] - 8.4
-        else
-            local newHunger = Player.PlayerData.metadata['hunger'] - QBCore.Config.Player.HungerRate
+            newHunger = Player.PlayerData.metadata['hunger'] - 8.4
         end
+        local newThirst = Player.PlayerData.metadata['thirst'] - QBCore.Config.Player.ThirstRate
         if Player.PlayerData.metadata['rein'] then
-            local newThirst = Player.PlayerData.metadata['thirst'] - 7.6
-        else
-            local newThirst = Player.PlayerData.metadata['thirst'] - QBCore.Config.Player.ThirstRate
+            newThirst = Player.PlayerData.metadata['thirst'] - 7.6
         end
         if newHunger <= 0 then
             newHunger = 0
