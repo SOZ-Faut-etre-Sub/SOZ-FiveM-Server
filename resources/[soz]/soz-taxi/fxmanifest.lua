5c1055c9937cef26223987445fd5069647f92668
leaudevian <leaudevian@dt.com>
2022-04-06 10:28:24 +0200

    soz-taxi base
    Former-commit-id: 37a69f4b47e5da85ad07ae027d58c60b04309228

---
+++ resources/[soz]/soz-taxi/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+games {"gta5"}
+
+shared_scripts {"config.lua"}
+
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@menuv/menuv.lua", "client/*.lua"}
+
+server_scripts {"server/*.lua"}
+
+dependencies {"qb-core", "menuv", "qb-target", "PolyZone"}
