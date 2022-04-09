d54cc7d501447a9e19c3179409d3f6d0a5d12307
Valentin <valentin@waldeck.dev>
2022-04-09 13:31:03 +0200

    Add ingredient collection logic
    Former-commit-id: d3156f223226da6c896d0ca4a5923d4ca3dcc60d

--- resources/[soz]/soz-jobs/server/jobs/food.lua
+++ resources/[soz]/soz-jobs/server/jobs/food.lua
@@ -1,1 +1,45 @@
+local function AddItem(source, item)
+    local receivedItem = false
+    exports["soz-inventory"]:AddItem(source, item, 1, nil, nil, function(success, reason)
+        if success then
+            receivedItem = true
+        else
+            if reason == "invalid_weight" then
+                TriggerClientEvent("hud:client:DrawNotification", source, "Vos poches sont pleines...", "error")
+            else
+                TriggerClientEvent("hud:client:DrawNotification", source,
+                                   string.format("Vous n'avez pas collecté d'ingrédients'. Il y a eu une erreur : `%s`", reason), "error")
+            end
+        end
+    end)
+    return receivedItem
+end
 
+QBCore.Functions.CreateCallback("soz-jobs:server:food-collect-ingredients", function(source, cb, count)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then
+        return
+    end
+
+    local items = {}
+    for i = 1, count, 1 do
+        local itemIdx = math.random(#FoodConfig.Collect.Items)
+        table.insert(items, FoodConfig.Collect.Items[itemIdx])
+    end
+
+    local collectedItems = {}
+    for _, item in ipairs(items) do
+        if AddItem(Player.PlayerData.source, item) then
+            if collectedItems[item] == nil then
+                collectedItems[item] = 1
+            else
+                collectedItems[item] = collectedItems[item] + 1
+            end
+        else
+            cb(collectedItems)
+            return
+        end
+    end
+
+    cb(collectedItems)
+end)
