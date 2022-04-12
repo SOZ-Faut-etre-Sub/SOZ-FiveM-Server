98bbffef3a90a0afed515a2c635b051f98352c9d
Joel Wurtz <joel.wurtz@gmail.com>
2022-04-13 01:37:37 +0200

    Add player, money and inventory metrics for prometheus
    Former-commit-id: 339d8e25ce40933f7152ef6b3c506dea2d9975b7

---
+++ resources/[soz]/soz-monitor/server/prometheus/inventory.lua
@@ -0,0 +1,152 @@
+-- Get all metrics about inventory
+local playerInventoryList = {}
+
+local function BuildListFromDatabase()
+    local players = MySQL.query.await("SELECT * FROM player WHERE is_default = 1", {})
+
+    for _, player in ipairs(players) do
+        local charinfo = json.decode(player.charinfo)
+
+        playerInventoryList[player.citizenid] = {
+            name = charinfo.firstname .. " " .. charinfo.lastname,
+            items = json.decode(player.inventory),
+            type = "player",
+            slots = 30,
+            maxWeight = 80000,
+        }
+    end
+end
+
+local function OverrideConnected()
+    local players = QBCore.Functions.GetQBPlayers()
+
+    for _, player in ipairs(players) do
+        local playerData = player.PlayerData
+        playerInventoryList[playerData.citizenid] = {
+            name = playerData.charinfo.firstname .. " " .. playerData.charinfo.lastname,
+            items = playerData.inventory,
+            type = "player",
+            slots = 30,
+            maxWeight = 80000,
+        }
+    end
+end
+
+Citizen.CreateThread(function()
+    BuildListFromDatabase()
+
+    while true do
+        OverrideConnected()
+
+        Citizen.Wait(3000)
+    end
+end)
+
+local function BuildInventoryMetrics(inventoryMetrics, name, description, type, getValue)
+    local metricsString = ([[
+
+# HELP %s %s
+# TYPE %s %s
+]]):format(name, description, name, type)
+
+    for _, inventoryMetric in pairs(inventoryMetrics) do
+        if inventoryMetric.type ~= "player" then
+            metricsString = metricsString .. string.format([[
+%s{id="%s",type="%s",label="%s",owner="%s"} %d
+]], name, inventoryMetric.id, inventoryMetric.type, inventoryMetric.label, inventoryMetric.owner, getValue(inventoryMetric))
+        end
+    end
+
+    for id, inventoryPlayer in pairs(playerInventoryList) do
+        metricsString = metricsString .. string.format([[
+%s{id="%s",type="player",label="%s",owner="%s"} %d
+]], name, id, inventoryPlayer.name, id, getValue(inventoryPlayer))
+    end
+
+    return metricsString
+end
+
+function GetInventoryMetrics()
+    local inventoryMetrics = exports['soz-inventory']:GetMetrics()
+    local metricsString = BuildInventoryMetrics(
+        inventoryMetrics,
+        "soz_inventory_slots",
+        "Inventory slots",
+        "gauge",
+        function(inventoryMetric)
+            return inventoryMetric.slots
+        end
+    )
+
+    metricsString = metricsString .. BuildInventoryMetrics(
+        inventoryMetrics,
+        "soz_inventory_total_item_count",
+        "Total number of items in inventory",
+        "gauge",
+        function(inventoryMetric)
+            return #inventoryMetric.items
+        end
+    )
+
+    metricsString = metricsString .. BuildInventoryMetrics(
+        inventoryMetrics,
+        "soz_inventory_weight",
+        "Inventory weight",
+        "gauge",
+        function(inventoryMetric)
+            if inventoryMetric.type ~= "player" then
+                return inventoryMetric.weight
+            end
+
+            local weight = 0
+
+            for _, item in ipairs(inventoryMetric.items) do
+                local itemDef = QBCore.Shared.Items[item.name]
+
+                if itemDef then
+                    weight = weight + (itemDef.weight * item.amount)
+                end
+            end
+
+            return weight
+        end
+    )
+
+    metricsString = metricsString .. BuildInventoryMetrics(
+        inventoryMetrics,
+        "soz_inventory_max_weight",
+        "Max inventory weight",
+        "gauge",
+        function(inventoryMetric)
+            return inventoryMetric.maxWeight
+        end
+    )
+
+    metricsString = metricsString .. [[
+
+# HELP soz_inventory_item_count Count item by inventory
+# TYPE soz_inventory_item_count gauge
+]]
+
+    for _, inventoryMetric in pairs(inventoryMetrics) do
+        if inventoryMetric.type ~= "player" then
+            for _, item in pairs(inventoryMetric.items) do
+                metricsString = metricsString .. string.format([[
+soz_inventory_item_count{id="%s",type="%s",label="%s",owner="%s",item_type="%s",item_name="%s",item_label="%s"} %d
+]], inventoryMetric.id, inventoryMetric.type, inventoryMetric.label, inventoryMetric.owner, item.type, item.name, item.label, item.amount)
+            end
+        end
+    end
+
+    for id, inventoryPlayer in pairs(playerInventoryList) do
+        for _, item in pairs(inventoryPlayer.items) do
+            local itemDef = QBCore.Shared.Items[item.name]
+
+            metricsString = metricsString .. string.format([[
+soz_inventory_item_count{id="%s",type="player",label="%s",owner="%s",item_type="%s",item_name="%s",item_label="%s"} %d
+]], id, inventoryPlayer.name, id, item.type, item.name, itemDef.label, item.amount)
+        end
+    end
+
+    return metricsString
+end
---
+++ resources/[soz]/soz-monitor/server/prometheus/money.lua
@@ -0,0 +1,28 @@
+-- Get all metrics about money
+function GetBankMetrics()
+    local bankMetrics = exports['soz-bank']:GetMetrics()
+    local metricsString = [[
+# HELP soz_bank_account_money Amount of money in a bank account
+# TYPE soz_bank_account_money gauge
+]]
+
+    for _, banKMetric in pairs(bankMetrics) do
+        metricsString = metricsString .. string.format([[
+soz_bank_account_money{type="%s",label="%s",owner="%s",id="%s"} %d
+]], banKMetric.type, banKMetric.label, banKMetric.owner, banKMetric.id, banKMetric.money)
+    end
+
+    metricsString = metricsString .. [[
+
+# HELP soz_bank_account_marked_money Amount of marked money in a bank account
+# TYPE soz_bank_account_marked_money gauge
+]]
+
+    for _, banKMetric in pairs(bankMetrics) do
+        metricsString = metricsString .. string.format([[
+soz_bank_account_marked_money{type="%s",label="%s",owner="%s",id="%s"} %d
+]], banKMetric.type, banKMetric.label, banKMetric.owner, banKMetric.id, banKMetric.marked_money)
+    end
+
+    return metricsString
+end
