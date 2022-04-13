157303bcc109917c15f99e690b5c91258e3b5fdf
Frozennide <sebastien@sdeu.fr>
2022-04-13 17:11:58 +0200

    feat(police): add animations on vehicle interaction
    Former-commit-id: 5ee024f25266467aef4942bc7071d028396f1333

--- resources/[soz]/soz-policejob/client/interactions/vehicle.lua
+++ resources/[soz]/soz-policejob/client/interactions/vehicle.lua
@@ -29,7 +29,23 @@ end)
 
 --- Events
 RegisterNetEvent("police:client:getVehicleOwner", function(data)
-    QBCore.Functions.Progressbar("police:vehicle:check", "Vérification de la plaque en cours...", 2500, false, true, {}, {}, {}, {}, function()
+    QBCore.Functions.Progressbar("police:vehicle:check", "Vérification de la plaque en cours...", 5000, false, true,
+                                 {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {animDict = "missheistdockssetup1clipboard@base", anim = "base", flags = 16}, {
+        model = "prop_notepad_01",
+        bone = 18905,
+        coords = {x = 0.1, y = 0.02, z = 0.05},
+        rotation = {x = 10.0, y = 0.0, z = 0.0},
+    }, {
+        model = "prop_pencil_01",
+        bone = 58866,
+        coords = {x = 0.11, y = -0.02, z = 0.001},
+        rotation = {x = -120.0, y = 0.0, z = 0.0},
+    }, function()
         local plate = QBCore.Functions.GetPlate(data.entity)
         local playerName = QBCore.Functions.TriggerRpc("police:server:getVehicleOwner", plate)
 
@@ -38,7 +54,13 @@ RegisterNetEvent("police:client:getVehicleOwner", function(data)
 end)
 
 RegisterNetEvent("police:client:SearchVehicle", function(data)
-    QBCore.Functions.Progressbar("police:vehicle:check", "Vérification du coffre en cours...", 2500, false, true, {}, {}, {}, {}, function()
+    QBCore.Functions.Progressbar("police:vehicle:check", "Vérification du coffre en cours...", 5000, false, true,
+                                 {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {animDict = "mp_arresting", anim = "a_uncuff", flags = 16}, {}, {}, function()
         local plate = QBCore.Functions.GetPlate(data.entity)
 
         TriggerServerEvent("inventory:server:openInventory", "trunk", plate)
