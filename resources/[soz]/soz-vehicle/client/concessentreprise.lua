8b004a1b034b1b43cd2fba74aeae38a169f149a7
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-04-15 01:57:15 +0200

    Icones_Blip
    Former-commit-id: 606fef265e2e084c60afb67b097366ab113c6f27

--- resources/[soz]/soz-vehicle/client/concessentreprise.lua
+++ resources/[soz]/soz-vehicle/client/concessentreprise.lua
@@ -111,7 +111,7 @@ exports["qb-target"]:SpawnPed({
             {
                 type = "client",
                 event = "soz-concessentreprise:client:Menu",
-                icon = "fas fa-car",
+                icon = "c:concess/lister.png",
                 label = "Demander la liste des véhicules",
                 targeticon = "fas fa-clipboard-list",
                 action = function(entity)
