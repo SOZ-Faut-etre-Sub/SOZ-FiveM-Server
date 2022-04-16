8bda2a81c4d944e90644bc2451243d51973fd4a7
leaudevian <mastersogood@gmail.com>
2022-04-16 18:59:53 +0200

    clean
    Former-commit-id: 2ee48b5fb9e21fd518fb39f8307364a8a2eb97dc

--- resources/[soz]/soz-core/client/sitchair.lua
+++ resources/[soz]/soz-core/client/sitchair.lua
@@ -1,5 +1,16 @@
 local sit = false
 local lastCoord = nil
+local sitchair = {
+    1580642483,
+    -1278649385,
+    -109356459,
+    -1633198649,
+    -377849416,
+    1037469683,
+    603897027,
+    49088219,
+    1339364336,
+}
 
 RegisterCommand("unsit", function()
     local player = GetPlayerPed(-1)
@@ -11,27 +22,26 @@ end, false)
 
 RegisterKeyMapping("unsit", "", "keyboard", "x")
 
-local sitchair = {1580642483, -1278649385, -109356459, -1633198649, -377849416, 1037469683, 603897027, 49088219}
-
 exports["qb-target"]:AddTargetModel(sitchair, {
-    options = {{
-        icon = "c:global/sit.png", 
-        label = "S'asseoir", 
-        action = function(entity)
-            local player = GetPlayerPed(-1)
-            local coords = GetEntityCoords(entity)
-            local heading = GetEntityHeading(entity)
-            sit = true
-            lastCoord = GetEntityCoords(player)
-            if heading >= 180 then
-                heading = heading - 179
-            else
-                heading = heading + 179
-            end
-            TaskStartScenarioAtPosition(player, "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER", coords.x, coords.y, coords.z, heading, 0, true, true)
-
-        end,
-    }},
+    options = {
+        {
+            icon = "c:global/sit.png",
+            label = "S'asseoir",
+            action = function(entity)
+                local player = GetPlayerPed(-1)
+                local coords = GetEntityCoords(entity)
+                local heading = GetEntityHeading(entity)
+                sit = true
+                lastCoord = GetEntityCoords(player)
+                if heading >= 180 then
+                    heading = heading - 179
+                else
+                    heading = heading + 179
+                end
+                TaskStartScenarioAtPosition(player, "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER", coords.x, coords.y, coords.z - 0.05, heading, 0, true, true)
+            end,
+        },
+    },
     distance = 1,
 })
 
