bde2234bc1d94335103e844fd4a27ad90fa743d6
Frozennide <sebastien@sdeu.fr>
2022-04-16 13:56:15 +0200

    feat(consumable): add drug and alcool persistence
    Former-commit-id: 88e2805372af1d4a21fac33a5ad1bfd4152210b7

--- resources/[soz]/soz-hud/ui/src/context/player.tsx
+++ resources/[soz]/soz-hud/ui/src/context/player.tsx
@@ -6,11 +6,15 @@ export const PlayerContext = createContext({
     armor: 0,
     hunger: 100,
     thirst: 100,
+    alcohol: 0,
+    drug: 0,
     updateInVehicle: (i: boolean) => {},
     updateHealth: (v: number) => {},
     updateArmor: (v: number) => {},
     updateHunger: (v: number) => {},
     updateThirst: (v: number) => {},
+    updateAlcohol: (v: number) => {},
+    updateDrug: (v: number) => {},
 })
 
 const PlayerProvider: FunctionComponent<PropsWithChildren<{}>> = ({children}) => {
@@ -19,19 +23,23 @@ const PlayerProvider: FunctionComponent<PropsWithChildren<{}>> = ({children}) =>
     const [armor, setArmor] = useState<number>(0)
     const [hunger, setHunger] = useState<number>(100)
     const [thirst, setThirst] = useState<number>(100)
+    const [alcohol, setAlcohol] = useState<number>(0)
+    const [drug, setDrug] = useState<number>(0)
 
     const updateInVehicle = useCallback((i: boolean) => setInVehicle(i), [setInVehicle])
     const updateHealth = useCallback((v: number) => setHealth(v), [setHealth])
     const updateArmor = useCallback((v: number) => setArmor(v), [setArmor])
     const updateHunger = useCallback((v: number) => setHunger(v), [setHunger])
     const updateThirst = useCallback((v: number) => setThirst(v), [setThirst])
+    const updateAlcohol = useCallback((v: number) => setAlcohol(v), [setAlcohol])
+    const updateDrug = useCallback((v: number) => setDrug(v), [setDrug])
 
     const value = useMemo(function () {
         return {
-            inVehicle, health, armor, hunger, thirst,
-            updateInVehicle, updateHealth, updateArmor, updateHunger, updateThirst
+            inVehicle, health, armor, hunger, thirst, alcohol, drug,
+            updateInVehicle, updateHealth, updateArmor, updateHunger, updateThirst, updateAlcohol, updateDrug
         }
-    }, [inVehicle, health, armor, hunger, thirst, updateInVehicle, updateHealth, updateArmor, updateHunger, updateThirst])
+    }, [inVehicle, health, armor, hunger, thirst, alcohol, drug, updateInVehicle, updateHealth, updateArmor, updateHunger, updateThirst, updateAlcohol, updateDrug])
 
     return (
         <PlayerContext.Provider value={value} >
