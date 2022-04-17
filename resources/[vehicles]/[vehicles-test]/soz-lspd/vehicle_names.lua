2884431c155f662095438abf551fb02e56c2c3e5
rigonkmalk <rigonkmalk@gmail.com>
2022-04-17 22:38:28 +0200

    feat(WIP): custom car for smur and sheriff and other
    Former-commit-id: e7f70d3068c97ce673087b5b74dac4b6727f003d

---
+++ resources/[vehicles]/[vehicles-test]/soz-lspd/vehicle_names.lua
@@ -0,0 +1,47 @@
+Citizen.CreateThread(function()
+    AddTextEntry('0x3AB04817','CallSign - 0')   -- NKS_A0
+    AddTextEntry('0x896BE589','CallSign - 1')   -- NKS_A1
+    AddTextEntry('0x5765017C','CallSign - 2')   -- NKS_A2
+    AddTextEntry('0xA977259F','CallSign - 3')   -- NKS_A3
+    AddTextEntry('0x772C410A','CallSign - 4')   -- NKS_A4
+    AddTextEntry('0xD5EAFE86','CallSign - 5')  -- NKS_A5
+    AddTextEntry('0xA3A91A03','CallSign - 6')  -- NKS_A6
+    AddTextEntry('0xF26AB789','CallSign - 7')  -- NKS_A7
+    AddTextEntry('0xC045D33C','CallSign - 8')  -- NKS_A8
+    AddTextEntry('0xF6F0C095','CallSign - 9')  -- NKS_A9
+
+    AddTextEntry('0x5F8914A0','CallSign - 0')   -- NKS_B0
+    AddTextEntry('0x31A4B8CC','CallSign - 1')   -- NKS_B1
+    AddTextEntry('0x7F1453B6','CallSign - 2')   -- NKS_B2
+    AddTextEntry('0xC70E63A9','CallSign - 3')   -- NKS_B3
+    AddTextEntry('0x98E8875E','CallSign - 4')   -- NKS_B4
+    AddTextEntry('0x6A9A2AC2','CallSign - 5')  -- NKS_B5
+    AddTextEntry('0xBC5A4E41','CallSign - 6')   -- NKS_B6
+    AddTextEntry('0xE1231832','CallSign - 7')   -- NKS_B7
+    AddTextEntry('0x14CCFF85','CallSign - 8')   -- NKS_B8
+    AddTextEntry('0x3E975319','CallSign - 9')   -- NKS_B9
+
+    AddTextEntry('0x6DA23036','CallSign - 0')   -- NKS_C0
+    AddTextEntry('0xAB50AB92','CallSign - 1')   -- NKS_C1
+    AddTextEntry('0x9D170F1F','CallSign - 2')   -- NKS_C2
+    AddTextEntry('0x15437F7A','CallSign - 3')   -- NKS_C3
+    AddTextEntry('0xF70042F4','CallSign - 4')   -- NKS_C4
+    AddTextEntry('0xF048B585','CallSign - 5')   -- NKS_C5
+    AddTextEntry('0xE20A9909','CallSign - 6')   -- NKS_C6
+    AddTextEntry('0x11A9F7DF','CallSign - 7')   -- NKS_C7
+    AddTextEntry('0x03E3DC53','CallSign - 8')   -- NKS_C8
+    AddTextEntry('0x3A35C8F6','CallSign - 9')   -- NKS_C9
+
+    AddTextEntry('0x2FB5ABBA','Default Bumper')   -- NKS_RAM_1
+    AddTextEntry('0x22B9078C','Pushbar Bumper')   -- NKS_RAM_1A
+    AddTextEntry('0xCA2892EC','Classic Bumper')   -- NKS_RAM_2
+    AddTextEntry('0x8DB7FD12','Default Bumper with Hook')   -- NKS_RAMHOOK
+
+    AddTextEntry('0x73C47994','Police Laptop')   -- NKS_LAPTOP
+
+    AddTextEntry('0x519333ED','Antenna - 1')   -- NKS_ANTENNA_1
+    AddTextEntry('0x6F446F4F','Antenna - 2')   -- NKS_ANTENNA_2
+    AddTextEntry('0x34D97A7A','Antenna - 3')   -- NKS_ANTENNA_3
+
+    AddTextEntry('0xF78CF9DA','Police Painting')   -- NKS_LIVERY_1
+end)
