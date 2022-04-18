26f94a626dfebf7b52c91e7f25e0cb208be05566
Valentin <valentin@waldeck.dev>
2022-04-18 16:13:35 +0200

    Add Field class
    Former-commit-id: 960f7e6033c01240971e79a28ad7a1251fab95a1

---
+++ resources/[soz]/soz-jobs/server/jobs/food/field.lua
@@ -0,0 +1,44 @@
+Fields = {}
+Field = {}
+
+function Field:new(name)
+    local self = setmetatable({}, {
+        __index = Field,
+        __call = function(self, name)
+            if Fields[name] ~= nil then
+                return Fields[name]
+            end
+            return nil
+        end
+    })
+    self:loadConfig(name)
+
+    self.name = name
+    self.maxQuantity = math.random(self.prodRange.min, self.prodRange.max)
+    self.quantity = self.maxQuantity
+
+    Fields[self.name] = self
+    return Fields[self.name]
+end
+
+Field.loadConfig = function(self, name)
+    if next(FoodConfig.Fields[name]) == nil then
+        error("Invalid Field name")
+    end
+    for k, v in pairs(FoodConfig.Fields[name]) do
+        self[k] = v
+    end
+end
+
+Field.Harvest = function(self)
+    local quantity = math.random(self.harvestRange.min, self.harvestRange.max)
+    if quantity > self.quantity then
+        quantity = self.quantity
+    end
+    self.quantity = self.quantity - quantity
+    return quantity, self.quantity
+end
+
+Field.GetHealth = function(self)
+    return math.ceil(self.quantity * #FoodConfig.FieldHealthStates / self.maxQuantity)
+end
