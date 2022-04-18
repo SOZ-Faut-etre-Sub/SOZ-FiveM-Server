a7aec389eb3f14961f40eb194019471e957809ec
Frozennide <sebastien@sdeu.fr>
2022-04-18 20:11:57 +0200

    feat(weather): use Global state to broadcast weather and time data
    Former-commit-id: 41d0427ce74d20310c6ec711da148a2e1fe712bc

--- resources/[soz]/soz-weather/fxmanifest.lua
+++ resources/[soz]/soz-weather/fxmanifest.lua
@@ -2,6 +2,6 @@ fx_version "cerulean"
 games {"gta5"}
 lua54 "yes"
 
-client_scripts {"client.lua"}
-
-server_scripts {"forecast/*.lua", "server.lua"}
+shared_scripts {"config.lua"}
+client_scripts {"client/*.lua"}
+server_scripts {"forecast/*.lua", "server/main.lua"}
