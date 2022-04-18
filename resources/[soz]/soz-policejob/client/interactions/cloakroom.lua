a7c0b4720f5c06d4621193e6812f0de0794947c5
Frozennide <sebastien@sdeu.fr>
2022-04-18 10:16:22 +0200

    feat: add animation when player change clotheset
    Former-commit-id: 07d6bf3e8cdcd47c419ba038c6251f5c7ae40d5e

--- resources/[soz]/soz-policejob/client/interactions/cloakroom.lua
+++ resources/[soz]/soz-policejob/client/interactions/cloakroom.lua
@@ -4,7 +4,12 @@ RegisterNetEvent("police:client:OpenCloakroomMenu", function()
             label = "Tenue civile",
             value = nil,
             select = function()
-                TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+                QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+                    disableMovement = true,
+                    disableCombat = true,
+                }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
+                    TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+                end)
             end,
         })
 
@@ -13,7 +18,16 @@ RegisterNetEvent("police:client:OpenCloakroomMenu", function()
                 label = name,
                 value = nil,
                 select = function()
-                    TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                    QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+                        disableMovement = true,
+                        disableCombat = true,
+                    }, {
+                        animDict = "anim@mp_yacht@shower@male@",
+                        anim = "male_shower_towel_dry_to_get_dressed",
+                        flags = 16,
+                    }, {}, {}, function() -- Done
+                        TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                    end)
                 end,
             })
         end
@@ -25,10 +39,20 @@ RegisterNetEvent("police:client:SetPrisonerClothes", function()
     local playerPedModel = GetEntityModel(playerPed)
 
     if not LocalPlayer.state.havePrisonerClothes then
-        TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", Config.PrisonerClothes[playerPedModel])
-        LocalPlayer.state:set("havePrisonerClothes", true, true)
+        QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+            disableMovement = true,
+            disableCombat = true,
+        }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
+            TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", Config.PrisonerClothes[playerPedModel])
+            LocalPlayer.state:set("havePrisonerClothes", true, true)
+        end)
     else
-        TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
-        LocalPlayer.state:set("havePrisonerClothes", false, true)
+        QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+            disableMovement = true,
+            disableCombat = true,
+        }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
+            TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+            LocalPlayer.state:set("havePrisonerClothes", false, true)
+        end)
     end
 end)
