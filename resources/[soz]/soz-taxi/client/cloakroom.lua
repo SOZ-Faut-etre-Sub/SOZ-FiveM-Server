b4356f56209068d4770ccf13cee49c2d3a07bd52
Frozennide <sebastien@sdeu.fr>
2022-04-18 10:16:22 +0200

    feat: add animation when player change clotheset
    Former-commit-id: 07d6bf3e8cdcd47c419ba038c6251f5c7ae40d5e

--- resources/[soz]/soz-taxi/client/cloakroom.lua
+++ resources/[soz]/soz-taxi/client/cloakroom.lua
@@ -4,7 +4,12 @@ RegisterNetEvent("taxi:client:OpenCloakroomMenu", function()
             label = "Tenue civile",
             value = nil,
             select = function()
-                TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+                QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+                    disableMovement = true,
+                    disableCombat = true,
+                }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
+                    TriggerEvent("soz-character:Client:ApplyCurrentClothConfig")
+                end)
             end,
         })
 
@@ -13,7 +18,16 @@ RegisterNetEvent("taxi:client:OpenCloakroomMenu", function()
                 label = name,
                 value = nil,
                 select = function()
-                    TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                    QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+                        disableMovement = true,
+                        disableCombat = true,
+                    }, {
+                        animDict = "anim@mp_yacht@shower@male@",
+                        anim = "male_shower_towel_dry_to_get_dressed",
+                        flags = 16,
+                    }, {}, {}, function() -- Done
+                        TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                    end)
                 end,
             })
         end
