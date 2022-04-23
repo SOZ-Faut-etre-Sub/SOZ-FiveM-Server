2e170a80dfe2e9767762b11ff377f1007c4aeba0
rigonkmalk <rigonkmalk@gmail.com>
2022-04-24 01:13:50 +0200

    fix(police6): add more info
    Former-commit-id: 0e94711415503ca20b933e401ad7aad8656c247e

--- resources/[vehicles]/[vehicles-test]/soz-custom-car/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-test]/soz-custom-car/fxmanifest.lua
@@ -15,5 +15,3 @@ data_file "VEHICLE_VARIATION_FILE" "meta/**/carvariations.meta"
 data_file "CARCOLS_FILE" "meta/**/carcols.meta"
 data_file "HANDLING_FILE" "meta/**/handlings.meta"
 data_file "VEHICLE_LAYOUTS_FILE" "meta/**/vehiclelayouts.meta"
--- data_file "AUDIO_GAMEDATA" "audio/buffalo/pbpdc2_game.dat"
-
