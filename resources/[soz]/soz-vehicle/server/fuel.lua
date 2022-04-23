169dbcb9295cf35c63b88183f10ed2a589ff95fc
Joel Wurtz <joel.wurtz@gmail.com>
2022-04-23 21:00:56 +0200

    Store all fuel data into database
    Former-commit-id: 137dde4d0211df0afa6b86f6bd9572f158eeff3b

--- resources/[soz]/soz-vehicle/server/fuel.lua
+++ resources/[soz]/soz-vehicle/server/fuel.lua
@@ -1,4 +1,5 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
+local stations = {}
 
 RegisterNetEvent("fuel:pay", function(price, source)
     local xPlayer = QBCore.Functions.GetPlayer(source)
@@ -8,17 +9,40 @@ RegisterNetEvent("fuel:pay", function(price, source)
     end
 end)
 
-QBCore.Functions.CreateCallback("soz-fuel:server:getfuelstock", function(source, cb)
-    local fuelstock = MySQL.Sync.fetchAll("SELECT * FROM fuel_storage")
-    if fuelstock[1] then
-        cb(fuelstock)
+MySQL.ready(function()
+    local data = MySQL.Sync.fetchAll("SELECT * FROM fuel_storage")
+
+    for _, station in pairs(data) do
+        table.insert(stations, {
+            id = station.id,
+            station = station.station,
+            fuel = station.fuel,
+            stock = station.stock,
+            position = json.decode(station.position),
+            model = station.model,
+            zone = json.decode(station.zone),
+        })
     end
 end)
 
-RegisterNetEvent("soz-fuel:server:setTempFuel", function(int)
-    MySQL.Async.execute("UPDATE fuel_storage SET stock = stock - 100 WHERE station = ?", {int})
+QBCore.Functions.CreateCallback("soz-fuel:server:getStations", function(source, cb)
+    cb(stations)
 end)
 
-RegisterNetEvent("soz-fuel:server:setFinalFuel", function(int, currentFuelAdd)
-    MySQL.Async.execute("UPDATE fuel_storage SET stock = stock + ? WHERE station = ?", {currentFuelAdd, int})
+QBCore.Functions.CreateCallback("soz-fuel:server:getfuelstock", function(source, cb, id)
+    local fuelstock = MySQL.Sync.fetchAll("SELECT * FROM fuel_storage WHERE id = @id", {["@id"] = id})
+
+    if #fuelstock > 0 then
+        cb(fuelstock[1].stock)
+    else
+        cb(0)
+    end
+end)
+
+RegisterNetEvent("soz-fuel:server:setTempFuel", function(id)
+    MySQL.Async.execute("UPDATE fuel_storage SET stock = stock - 100 WHERE id = ?", {id})
+end)
+
+RegisterNetEvent("soz-fuel:server:setFinalFuel", function(id, currentFuelAdd)
+    MySQL.Async.execute("UPDATE fuel_storage SET stock = stock + ? WHERE id = ?", {currentFuelAdd, id})
 end)
