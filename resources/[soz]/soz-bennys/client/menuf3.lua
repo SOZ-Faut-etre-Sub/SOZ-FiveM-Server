d4f1d9506d4a336565540daca6ca5ca8a5867f2c
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-04-23 21:08:14 +0200

    BennysFinalPartie2
    Former-commit-id: 579ea549880368bb0506dfeebb2bf321d6980c56

--- resources/[soz]/soz-bennys/client/menuf3.lua
+++ resources/[soz]/soz-bennys/client/menuf3.lua
@@ -1,38 +1,22 @@
 local BennysF3 = MenuV:CreateMenu(nil, "Services Bennys", "menu_job_bennys", "soz", "bennys:menu:f3")
 
-local function OpenMenu2(menu)
-    menu:AddSlider({
-        icon = "🚧",
-        label = "Poser un objet",
-        value = nil,
-        values = {
-            {label = "Cone de circulation", value = {item = "cone", props = "prop_roadcone02a"}},
-            {label = "Barrière", value = {item = "police_barrier", props = "prop_barrier_work05"}},
-        },
-        select = function(_, value)
-            TriggerServerEvent("job:server:placeProps", value.item, value.props)
-        end,
-    })
-end
-
-local function OpenMenu(menu)
-    local veh = GetVehiclePedIsIn(PlayerPedId(), false)
-    FreezeEntityPosition(veh, true)
-    menu:AddButton({
-        label = "Réparation du véhicule",
-        description = "Réparer les pièces du véhicule",
-        select = function()
-            OpenPartsMenu(Status)
-        end,
-    })
-    menu:AddButton({
-        label = "Customisation du véhicule",
-        description = "Changer les composants du véhicule",
-        select = function()
-            SetVehicleModKit(veh, 0)
-            OpenCustom(VehiculeCustom)
-        end,
-    })
+local function OpenF3Menu(menu)
+    if OnDuty then
+        menu:AddSlider({
+            icon = "🚧",
+            label = "Poser un objet",
+            value = nil,
+            values = {
+                {label = "Cone de circulation", value = {item = "cone", props = "prop_roadcone02a"}},
+                {label = "Barrière", value = {item = "police_barrier", props = "prop_barrier_work05"}},
+            },
+            select = function(_, value)
+                TriggerServerEvent("job:server:placeProps", value.item, value.props)
+            end,
+        })
+    else
+        menu:AddButton({label = "Tu n'es pas en service !", disabled = true})
+    end
 end
 
 local function GenerateF3Menu()
@@ -40,11 +24,11 @@ local function GenerateF3Menu()
         BennysF3:Close()
     else
         BennysF3:ClearItems()
-        OpenMenu(BennysF3)
+        OpenF3Menu(BennysF3)
         BennysF3:Open()
     end
 end
 
-RegisterNetEvent("lsmc:client:OpenSocietyMenu", function()
+RegisterNetEvent("bennys:client:OpenSocietyMenu", function()
     GenerateF3Menu()
 end)
