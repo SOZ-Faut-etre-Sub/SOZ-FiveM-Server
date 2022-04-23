eae5d4fd47b4cf545fe2bea10bc063bb045659ae
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-04-23 21:11:12 +0200

    Format cloakroom
    Former-commit-id: 4b538fff6fb7cf86515b4ddaf13c50ff02eaf763

--- resources/[soz]/soz-bennys/client/menucloakroom.lua
+++ resources/[soz]/soz-bennys/client/menucloakroom.lua
@@ -22,11 +22,7 @@ local function OpenCloakMenu(menu)
                 QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
                     disableMovement = true,
                     disableCombat = true,
-                }, {
-                    animDict = "anim@mp_yacht@shower@male@",
-                    anim = "male_shower_towel_dry_to_get_dressed",
-                    flags = 16,
-                }, {}, {}, function() -- Done
+                }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
                     TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
                 end)
             end,
