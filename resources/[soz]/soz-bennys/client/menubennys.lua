ea2239644900a1f8d6c8bbd39730bf2a4e966dbd
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-04-23 23:40:36 +0200

    Fix plates benys
    Former-commit-id: 4de2a88fe12332174fd06307dafe48911da45422

--- resources/[soz]/soz-bennys/client/menubennys.lua
+++ resources/[soz]/soz-bennys/client/menubennys.lua
@@ -651,9 +651,9 @@ local function OpenPlateIndexMenu(menu)
     local plyVeh = GetVehiclePedIsIn(PlayerPedId(), false)
     local tempPlateIndex = GetVehicleNumberPlateTextIndex(plyVeh)
     local plateTypes = {
-        "Bleu sur Blanc #1",
         "Jaune sur Noir",
         "Jaune sur Bleu",
+        "Bleu sur Blanc #1",
         "Bleu sur Blanc #2",
         "Bleu sur Blanc #3",
         "Yankton Nord",
