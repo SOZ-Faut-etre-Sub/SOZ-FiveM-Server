edb0101127eeb19954743d7eca9b93e88996696b
Valentin <valentin@waldeck.dev>
2022-04-24 09:44:23 +0200

    Changed default F3 menu
    Former-commit-id: b6a42d20d9ad94025abc672bb101eac1622e8dad

--- resources/[soz]/soz-jobs/config.lua
+++ resources/[soz]/soz-jobs/config.lua
@@ -135,7 +135,7 @@ SozJobCore.Jobs = {
     [SozJobCore.JobType.Food] = {
         label = "Food & Drink",
         grades = {},
-        menuCallback = "jobs:client:food:OpenCraftingMenu",
+        menuCallback = "jobs:client:food:OpenSocietyMenu",
         permissions = {
             [SozJobCore.JobPermission.ManageGrade] = {label = "Gestion des grades"},
             [SozJobCore.JobPermission.SocietyBankAccount] = {label = "Accès au compte société"},
