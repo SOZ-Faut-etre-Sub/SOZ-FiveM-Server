7148bd469df4777a0b1729324dec7ef2ee6a0c03
leaudevian <mastersogood@gmail.com>
2022-04-24 16:47:23 +0200

    order
    Former-commit-id: f5602e5ca17109cc7fda4b79f447c00c6059f902

--- resources/[vehicles]/[vehicles-test]/soz-custom-car/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-test]/soz-custom-car/fxmanifest.lua
@@ -10,8 +10,8 @@ client_script {
     'vehicle_names.lua'
 }
 
+data_file "HANDLING_FILE" "meta/**/handlings.meta"
 data_file "VEHICLE_METADATA_FILE" "meta/**/vehicles.meta"
 data_file "VEHICLE_VARIATION_FILE" "meta/**/carvariations.meta"
 data_file "CARCOLS_FILE" "meta/**/carcols.meta"
-data_file "HANDLING_FILE" "meta/**/handlings.meta"
 data_file "VEHICLE_LAYOUTS_FILE" "meta/**/vehiclelayouts.meta"
