9a4035ded45ea01cfd00e58f07c420947beaf648
Frozennide <sebastien@sdeu.fr>
2022-04-26 00:44:29 +0200

    feat(admin): add toggle service option
    Former-commit-id: 8b5c60bf73b67b192bee67f7d7c89b383772d69a

--- resources/[soz]/soz-admin/client/admin_menu/03_jobs.lua
+++ resources/[soz]/soz-admin/client/admin_menu/03_jobs.lua
@@ -2,24 +2,40 @@ local jobMenu = MenuV:InheritMenu(AdminMenu, { subtitle = "Pour se construire un
 
 local JobOption = {JobList = {}}
 
-for _, jobData in pairs(SozJobCore.Jobs) do
-    for _, grade in pairs(jobData.grades) do
-        if grade.owner == 1 then
-            table.insert(JobOption.JobList, {label = jobData.label, value = {label = jobData.label, jobID = grade.jobID, gradeId = grade.id}})
+jobMenu:On("open", function(menu)
+    menu:ClearItems()
+
+    --- Menu entries
+    menu:AddSlider({
+        label = "Changer de métier",
+        value = nil,
+        values = JobOption.JobList,
+        select = function(_, value)
+            TriggerServerEvent("admin:jobs:setjob", value.jobID, value.gradeId)
+            exports["soz-hud"]:DrawNotification("Votre métier est maintenant : ~b~" .. value.label, "info")
+        end,
+    })
+
+    menu:AddCheckbox({
+        label = "Passer en service",
+        value = QBCore.Functions.GetPlayerData().job.onduty,
+        change = function()
+            TriggerServerEvent("QBCore:ToggleDuty")
+        end,
+    })
+end)
+
+RegisterNetEvent("soz-jobs:Client:OnJobSync", function(jobs)
+    JobOption.JobList = {}
+
+    for _, jobData in pairs(jobs) do
+        for _, grade in pairs(jobData.grades) do
+            if grade.owner == 1 then
+                table.insert(JobOption.JobList, {label = jobData.label, value = {label = jobData.label, jobID = grade.jobID, gradeId = grade.id}})
+            end
         end
     end
-end
-
---- Menu entries
-jobMenu:AddSlider({
-    label = "Changer de métier",
-    value = nil,
-    values = JobOption.JobList,
-    select = function(_, value)
-        TriggerServerEvent("admin:jobs:setjob", value.jobID, value.gradeId)
-        exports["soz-hud"]:DrawNotification("Votre métier est maintenant : ~b~" .. value.label, "info")
-    end,
-})
+end)
 
 --- Add to main menu
-AdminMenu:AddButton({icon = "⛑", label = "Gestion métier", value = jobMenu })
+AdminMenu:AddButton({icon = "⛑", label = "Gestion métier", value = jobMenu})
