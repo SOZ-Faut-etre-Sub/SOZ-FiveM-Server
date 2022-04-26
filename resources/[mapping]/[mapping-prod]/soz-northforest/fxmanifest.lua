fa84e55c829cf44cf2ba7602f0e19a31ee710c60
rigonkmalk <rigonkmalk@gmail.com>
2022-04-26 16:10:54 +0200

    refactor(mapping): move mapping-test to mapping-prod
    Former-commit-id: 0f166a66c022e67ddebfe9150ccd97596397533a

---
+++ resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- This mod was made for free by JRod and Larcius and the FiveM resource bundle was made by leonard1125.
+-- For more information check out https://www.gta5-mods.com/maps/forests-of-san-andreas-revised
+
+fx_version "adamant"
+game "gta5"
+
+author 'Larcius'
+description 'Forests of San Andreas: Revised'
+version '3.2'
+
+data_file('DLC_ITYP_REQUEST')('stream/forest_lod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/forest_n_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/forest_s_slod.ytyp')
