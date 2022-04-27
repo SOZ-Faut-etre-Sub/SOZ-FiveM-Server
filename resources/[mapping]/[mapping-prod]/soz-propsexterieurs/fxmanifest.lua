d28c00d295971fc02cad51000b9b624ae267a858
Frozennide <sebastien@sdeu.fr>
2022-04-27 10:44:17 +0200

    fix(prop): make prop_barrier_work05 invincible
    Former-commit-id: 53002f57a113c580d84f46fe665044e987eb2995

--- resources/[mapping]/[mapping-prod]/soz-propsexterieurs/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-propsexterieurs/fxmanifest.lua
@@ -7,4 +7,5 @@ files {"interiorproxies.meta"}
 data_file 'DLC_ITYP_REQUEST' 'stream/prop_z_park.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_prop_radar.ytyp'
 data_file 'DLC_ITYP_REQUEST' "stream/soz_prop_paystation.ytyp"
+data_file 'DLC_ITYP_REQUEST' "stream/ytyp/barrier.ytyp"
 data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
