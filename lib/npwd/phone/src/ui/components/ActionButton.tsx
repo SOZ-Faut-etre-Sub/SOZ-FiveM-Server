ee1c6ce8d21dac04de48e0e5886a9269ac96affc
Frozennide <sebastien@sdeu.fr>
2022-04-27 22:04:04 +0200

    fix(phone): block society message transfert if message is empty
    Former-commit-id: 6a6589ba981bc2d828a213a47d35833425e70632

--- lib/npwd/phone/src/ui/components/ActionButton.tsx
+++ lib/npwd/phone/src/ui/components/ActionButton.tsx
@@ -1,11 +1,17 @@
 import React, {useContext} from 'react';
 import {ThemeContext} from "../../styles/themeProvider";
+import cn from 'classnames';
 
 export const ActionButton: React.FC<any> = ({ ...props }) => {
     const {theme} = useContext(ThemeContext);
 
     return (
-        <div className={`flex flex-col justify-center items-center ${theme === 'dark' ? 'bg-[#1C1C1E] text-[#347DD9]': 'bg-white text-gray-700'} rounded-xl p-3 cursor-pointer`} {...props}>
+        <div {...props} className={cn('flex flex-col justify-center items-center rounded-xl p-3', {
+            'bg-[#1C1C1E] text-[#347DD9]': theme === 'dark',
+            'bg-white text-gray-700': theme === 'light',
+            'bg-opacity-50 cursor-not-allowed': props.disabled,
+            'cursor-pointer': !props.disabled,
+        })}>
             {props.children}
         </div>
     )
