88b328cde2fe42d51ef293642c1f14c96a0e65ae
Frozennide <sebastien@sdeu.fr>
2022-04-28 22:52:05 +0200

    feat(mtp): add storages
    Former-commit-id: 7a162f96e4d95a0f48988966081f655ef483013c

---
+++ resources/[soz]/soz-inventory/storages/oil.lua
@@ -0,0 +1,29 @@
+Config.Storages["oil_fridge"] = {
+    label = "Frigo MTP",
+    type = "fridge",
+    owner = "oil",
+    position = vector3(-232.84, 6080.73, 32.26),
+    size = vec2(0.8, 1.6),
+    heading = 45,
+    offsetUpZ = 1.5,
+}
+
+Config.Storages["oil_boss_storage"] = {
+    label = "Coffre patron MTP",
+    type = "boss_storage",
+    owner = "oil",
+    position = vector3(-232.77, 6092.4, 32.26),
+    size = vec2(0.8, 2.6),
+    heading = 45,
+    offsetUpZ = 1.1,
+}
+
+Config.Storages["oil_storage"] = {
+    label = "Coffre MTP",
+    type = "storage",
+    owner = "oil",
+    position = vector3(-251.87, 6083.44, 32.26),
+    size = vec2(3.0, 12.4),
+    heading = 45,
+    offsetUpZ = 4.5,
+}
