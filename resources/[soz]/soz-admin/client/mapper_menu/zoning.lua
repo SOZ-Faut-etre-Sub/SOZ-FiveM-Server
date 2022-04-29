df70e273543d95879f16e6ab6b1ab6f94ca7a76a
leaudevian <mastersogood@gmail.com>
2022-04-29 20:02:24 +0200

    lint
    Former-commit-id: afc4a6a6414d92b46bd38cc9c2994c3d9a7b6393

--- resources/[soz]/soz-admin/client/mapper_menu/zoning.lua
+++ resources/[soz]/soz-admin/client/mapper_menu/zoning.lua
@@ -10,7 +10,7 @@ local function _calculatePoints(center, length, width)
     local halfLength, halfWidth = length / 2, width / 2
     local min = vector2(-halfWidth, -halfLength)
     local max = vector2(halfWidth, halfLength)
-    
+
     -- Box vertices
     p1 = center.xy + vector2(min.x, min.y)
     p2 = center.xy + vector2(min.x, max.y)
@@ -27,10 +27,14 @@ function DisplayZone(zone)
     local angle = zone.heading * (2 * math.pi) / 360
 
     _calculatePoints(vector3(zone.x, zone.y, zone.z), zone.sx, zone.sy)
-    p1 = vector2((math.cos(angle) * (p1.x - zone.x) - math.sin(angle) * (p1.y - zone.y) + zone.x),(math.sin(angle) * (p1.x - zone.x) + math.cos(angle) * (p1.y - zone.y) + zone.y))
-    p2 = vector2((math.cos(angle) * (p2.x - zone.x) - math.sin(angle) * (p2.y - zone.y) + zone.x),(math.sin(angle) * (p2.x - zone.x) + math.cos(angle) * (p2.y - zone.y) + zone.y))
-    p3 = vector2((math.cos(angle) * (p3.x - zone.x) - math.sin(angle) * (p3.y - zone.y) + zone.x),(math.sin(angle) * (p3.x - zone.x) + math.cos(angle) * (p3.y - zone.y) + zone.y))
-    p4 = vector2((math.cos(angle) * (p4.x - zone.x) - math.sin(angle) * (p4.y - zone.y) + zone.x),(math.sin(angle) * (p4.x - zone.x) + math.cos(angle) * (p4.y - zone.y) + zone.y))
+    p1 = vector2((math.cos(angle) * (p1.x - zone.x) - math.sin(angle) * (p1.y - zone.y) + zone.x),
+                 (math.sin(angle) * (p1.x - zone.x) + math.cos(angle) * (p1.y - zone.y) + zone.y))
+    p2 = vector2((math.cos(angle) * (p2.x - zone.x) - math.sin(angle) * (p2.y - zone.y) + zone.x),
+                 (math.sin(angle) * (p2.x - zone.x) + math.cos(angle) * (p2.y - zone.y) + zone.y))
+    p3 = vector2((math.cos(angle) * (p3.x - zone.x) - math.sin(angle) * (p3.y - zone.y) + zone.x),
+                 (math.sin(angle) * (p3.x - zone.x) + math.cos(angle) * (p3.y - zone.y) + zone.y))
+    p4 = vector2((math.cos(angle) * (p4.x - zone.x) - math.sin(angle) * (p4.y - zone.y) + zone.x),
+                 (math.sin(angle) * (p4.x - zone.x) + math.cos(angle) * (p4.y - zone.y) + zone.y))
 
     local wall1 = {
         bottomLeft = vector3(p1.x, p1.y, minZ),
@@ -84,17 +88,17 @@ function DisplayZone(zone)
         DrawPoly(wall2.bottomLeft, wall2.topLeft, wall2.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall2.topLeft, wall2.topRight, wall2.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall2.bottomRight, wall2.topRight, wall2.topLeft, 93, 173, 226, 75)
-        DrawPoly(wall2.bottomRight, wall2.topLeft, wall2.bottomLeft, 93, 173, 226, 75)        
+        DrawPoly(wall2.bottomRight, wall2.topLeft, wall2.bottomLeft, 93, 173, 226, 75)
         --       
         DrawPoly(wall3.bottomLeft, wall3.topLeft, wall3.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall3.topLeft, wall3.topRight, wall3.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall3.bottomRight, wall3.topRight, wall3.topLeft, 93, 173, 226, 75)
-        DrawPoly(wall3.bottomRight, wall3.topLeft, wall3.bottomLeft, 93, 173, 226, 75) 
+        DrawPoly(wall3.bottomRight, wall3.topLeft, wall3.bottomLeft, 93, 173, 226, 75)
         --       
         DrawPoly(wall4.bottomLeft, wall4.topLeft, wall4.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall4.topLeft, wall4.topRight, wall4.bottomRight, 93, 173, 226, 75)
         DrawPoly(wall4.bottomRight, wall4.topRight, wall4.topLeft, 93, 173, 226, 75)
-        DrawPoly(wall4.bottomRight, wall4.topLeft, wall4.bottomLeft, 93, 173, 226, 75) 
+        DrawPoly(wall4.bottomRight, wall4.topLeft, wall4.bottomLeft, 93, 173, 226, 75)
 
         Wait(1)
     end
