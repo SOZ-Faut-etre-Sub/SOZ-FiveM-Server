8bf0ad2bb108198315fa46fb6f882ae1b50a407e
Frozennide <sebastien@sdeu.fr>
2022-04-29 15:53:41 +0200

    feat(mtp): setup blip
    Former-commit-id: 98fe8292cebefa42ad2c9fa1384a0134ab2ab1e6

---
+++ resources/[soz]/soz-jobs/client/jobs/oil.lua
@@ -0,0 +1,25 @@
+local societyMenu = MenuV:CreateMenu(nil, "", "menu_job_fueler", "soz", "fueler:menu")
+
+--- Targets
+CreateThread(function()
+    exports["qb-target"]:AddGlobalPlayer({
+        options = {
+            {label = "Facturer", icon = "c:jobs/facture.png", event = "jobs:client:news:InvoicePlayer", job = "news"},
+        },
+        distance = 1.5,
+    })
+
+    exports["qb-target"]:AddBoxZone("mtp:duty", vector3(-246.62, 6090.77, 32.25), 0.15, 1.2,
+        {name = "mtp:duty", heading = 45, minZ = 32.1, maxZ = 33.4,debugPoly=true},
+        {options = SozJobCore.Functions.GetDutyActions("news"), distance = 2.5})
+end)
+
+--- Threads
+CreateThread(function()
+    QBCore.Functions.CreateBlip("jobs:oil", {
+        name = "Michel Transport Petrol",
+        coords = vector3(-243.97, 6071.59, 32.32),
+        sprite = 436,
+        scale = 1.0,
+    })
+end)
