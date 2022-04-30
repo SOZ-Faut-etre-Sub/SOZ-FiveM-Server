cc42335a1d2316c319a2ebfff8a36068e97a9114
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-04-30 21:34:51 +0200

    Bloquer_sortirvoiture_lscustom_bennys
    Former-commit-id: efe6b03654301c740e0cb5a3ec4ae8471a342d93

--- resources/[soz]/soz-vehicle/client/keep_state.lua
+++ resources/[soz]/soz-vehicle/client/keep_state.lua
@@ -3,9 +3,9 @@ CreateThread(function()
         local ped = PlayerPedId()
         local vehicle = GetVehiclePedIsIn(ped, false)
 
-        if IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) then
+        if IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) and (GetVehicleDoorLockStatus(vehicle) == 1) then
             Wait(150)
-            if IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) then
+            if IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) and (GetVehicleDoorLockStatus(vehicle) == 1) then
                 SetVehicleEngineOn(vehicle, true, true, false)
                 TaskLeaveVehicle(ped, vehicle, 0)
             end
