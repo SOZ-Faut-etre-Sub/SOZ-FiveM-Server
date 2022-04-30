82fad814ea1a65fe093bf8f91a370e28a70d51d8
Frozennide <sebastien@sdeu.fr>
2022-04-30 10:38:39 +0200

    feat(mtp): limit interaction to farming zone
    Former-commit-id: 5c1dfe2f89e3ce32547a8fad4e23f05725505d58

---
+++ resources/[soz]/soz-locations/client/location/fueler.lua
@@ -0,0 +1,17 @@
+-- Fueler PolyZone Locations
+Locations["fueler_petrol_farm"] = {
+    [1] = PolyZone:Create({
+        vector2(476.27612304688, 2986.7197265625),
+        vector2(483.51190185546, 2926.3686523438),
+        vector2(509.33477783204, 2909.2048339844),
+        vector2(533.97302246094, 2839.9375),
+        vector2(727.41925048828, 2845.6423339844),
+        vector2(665.96124267578, 3053.6645507812),
+        vector2(588.5396118164, 3042.2253417968)
+    }, {
+        name="fueler_farm",
+        minZ = 37.55,
+        maxZ = 55.55,
+        debugPoly = LocationDebugPoly,
+    }),
+}
