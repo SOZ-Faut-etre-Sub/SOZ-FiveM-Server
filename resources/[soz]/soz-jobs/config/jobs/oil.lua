502c2fe70d13edd26a06fdbdef9aaa1e22260884
Frozennide <sebastien@sdeu.fr>
2022-04-30 22:32:56 +0200

    feat(mtp): add cloakroom
    Former-commit-id: 40441705021621b4909dd4476ae742fbe03328ed

--- resources/[soz]/soz-jobs/config/jobs/oil.lua
+++ resources/[soz]/soz-jobs/config/jobs/oil.lua
@@ -1,3 +1,36 @@
 FuelerConfig = {}
 
 FuelerConfig.SellPrice = 8
+
+FuelerConfig.Cloakroom = {
+    [GetHashKey("mp_m_freemode_01")] = {
+        Components = {
+            [1] = {Drawable = 0, Texture = 0, Palette = 0},
+            [3] = {Drawable = 6, Texture = 0, Palette = 0},
+            [4] = {Drawable = 9, Texture = 7, Palette = 0},
+            [5] = {Drawable = 0, Texture = 0, Palette = 0},
+            [6] = {Drawable = 25, Texture = 0, Palette = 0},
+            [7] = {Drawable = 0, Texture = 0, Palette = 0},
+            [8] = {Drawable = 15, Texture = 0, Palette = 0},
+            [9] = {Drawable = 0, Texture = 0, Palette = 0},
+            [10] = {Drawable = 0, Texture = 0, Palette = 0},
+            [11] = {Drawable = 378, Texture = 0, Palette = 0},
+        },
+        Props = {},
+    },
+    [GetHashKey("mp_f_freemode_01")] = {
+        Components = {
+            [1] = {Drawable = 0, Texture = 0, Palette = 0},
+            [3] = {Drawable = 0, Texture = 0, Palette = 0},
+            [4] = {Drawable = 39, Texture = 1, Palette = 0},
+            [5] = {Drawable = 0, Texture = 0, Palette = 0},
+            [6] = {Drawable = 25, Texture = 0, Palette = 0},
+            [7] = {Drawable = 0, Texture = 0, Palette = 0},
+            [8] = {Drawable = 3, Texture = 0, Palette = 0},
+            [9] = {Drawable = 0, Texture = 0, Palette = 0},
+            [10] = {Drawable = 0, Texture = 0, Palette = 0},
+            [11] = {Drawable = 396, Texture = 0, Palette = 0},
+        },
+        Props = {},
+    },
+}
