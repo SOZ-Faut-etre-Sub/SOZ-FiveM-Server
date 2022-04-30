c6d53470b52fc5d18b6a92ab33b188019233cbf3
Frozennide <sebastien@sdeu.fr>
2022-04-30 21:26:50 +0200

    fix(identity): update job when player switch
    Former-commit-id: 1b98bd654cc09b9ebecd320796589d699d1a099c

--- resources/[soz]/soz-identity/client/events.lua
+++ resources/[soz]/soz-identity/client/events.lua
@@ -1,6 +1,18 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 local PlayerData = QBCore.Functions.GetPlayerData()
 
+RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent("QBCore:Player:SetPlayerData", function(data)
+    PlayerData = data
+end)
+
+RegisterNetEvent("QBCore:Client:OnPlayerUnload", function()
+    PlayerData = {}
+end)
+
 --
 -- NUI related events
 --
