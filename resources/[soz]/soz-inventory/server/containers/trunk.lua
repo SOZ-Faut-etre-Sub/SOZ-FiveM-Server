19124759b8499fd7e7e750ccf4635a5f1efe8eef
Frozennide <sebastien@sdeu.fr>
2022-05-01 21:17:36 +0200

    fix(inventory): disable inventory slots
    Former-commit-id: 6d8b19614006d02cf1aa959053b7d54a05e962e1

--- resources/[soz]/soz-inventory/server/containers/trunk.lua
+++ resources/[soz]/soz-inventory/server/containers/trunk.lua
@@ -41,7 +41,7 @@ end
 --- @param item table
 --- @return boolean
 function TrunkInventory:AllowedItems(item)
-    local typeAllowed = {["item"] = true, ["drug"] = true}
+    local typeAllowed = {["weapon"] = true, ["weapon_ammo"] = true, ["item"] = true, ["drug"] = true}
     return typeAllowed[item.type or ""] or false
 end
 
