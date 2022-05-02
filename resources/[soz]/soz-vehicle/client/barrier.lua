06a3bfaaa926a5b2b482568dddd74cef3400f247
Frozennide <sebastien@sdeu.fr>
2022-05-02 10:27:37 +0200

    fix(mtp): use new door system for barrier
    Former-commit-id: 00dbee09a8bac581168db94b8cbdd35961150fbe

--- resources/[soz]/soz-vehicle/client/barrier.lua
+++ resources/[soz]/soz-vehicle/client/barrier.lua
@@ -1,7 +1,5 @@
 local gates = {[GetHashKey("prop_sec_barrier_ld_01a")] = true, [GetHashKey("prop_sec_barrier_ld_02a")] = true}
 
-local closestGate = {}
-
 Citizen.CreateThread(function()
     while true do
         local coords = GetEntityCoords(PlayerPedId())
@@ -10,63 +8,22 @@ Citizen.CreateThread(function()
         for i = 1, #objects, 1 do
             local objectModel = GetEntityModel(objects[i])
             local objectCoords = GetEntityCoords(objects[i])
+            local distance = #(objectCoords - coords)
+            local identifier = "b_" .. objectCoords.x .. objectCoords.y .. objectCoords.z
 
-            if gates[objectModel] and #(objectCoords - coords) <= 15.0 then
-                closestGate[objects[i]] = false
+            if gates[objectModel] and distance <= 20.0 then
+                if not IsDoorRegisteredWithSystem(identifier) then
+                    AddDoorToSystem(identifier, objectModel, objectCoords.x, objectCoords.y, objectCoords.z, true, true, false);
+                    DoorSystemSetOpenRatio(identifier, 1.0, false, true);
+                end
+                DoorSystemSetAutomaticDistance(identifier, 5.0, false, true)
+            elseif gates[objectModel] and distance >= 40.0 then
+                if IsDoorRegisteredWithSystem(identifier) then
+                    RemoveDoorFromSystem(identifier)
+                end
             end
         end
 
         Wait(1000)
     end
 end)
-
-Citizen.CreateThread(function()
-    while true do
-        local coords = GetEntityCoords(PlayerPedId())
-
-        for gate, state in pairs(closestGate) do
-            CreateThread(function()
-                if state then
-                    return
-                end
-
-                closestGate[gate] = true
-                if DoesEntityExist(gate) then
-                    local gateRotation = GetEntityRotation(gate, 0)
-
-                    if #(GetEntityCoords(gate) - coords) <= 15.0 then
-                        while gateRotation.y >= -89.5 do
-                            SetEntityRotation(gate, gateRotation.x, gateRotation.y - 1.0, gateRotation.z, 0, true)
-                            gateRotation = GetEntityRotation(gate, 0)
-
-                            if not DoesEntityExist(gate) then
-                                break
-                            end
-
-                            Wait(0)
-                        end
-                    else
-                        while gateRotation.y <= -0.5 do
-                            SetEntityRotation(gate, gateRotation.x, gateRotation.y + 1.0, gateRotation.z, 0, true)
-                            gateRotation = GetEntityRotation(gate, 0)
-
-                            if not DoesEntityExist(gate) then
-                                break
-                            end
-
-                            Wait(0)
-                        end
-                        closestGate[gate] = nil
-                    end
-                else
-                    closestGate[gate] = nil
-                end
-
-                closestGate[gate] = false
-            end)
-        end
-
-        Wait(500)
-    end
-end)
-
