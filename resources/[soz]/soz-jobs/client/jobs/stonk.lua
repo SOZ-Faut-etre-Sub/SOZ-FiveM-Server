c1802c3c60f3e98191a39c578139fd3903891d5c
Frozennide <sebastien@sdeu.fr>
2022-05-03 22:26:08 +0200

    fix(stonk): use new clothes
    Former-commit-id: 7e91de5a526009d3cbd24e00921b907c9a568135

--- resources/[soz]/soz-jobs/client/jobs/stonk.lua
+++ resources/[soz]/soz-jobs/client/jobs/stonk.lua
@@ -175,18 +175,20 @@ RegisterNetEvent("jobs:client:stonk:OpenCloakroomMenu", function()
         end,
     })
 
-    menu:AddButton({
-        label = "Tenue de travail",
-        value = nil,
-        select = function()
-            QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
-                disableMovement = true,
-                disableCombat = true,
-            }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
-                TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", StonkConfig.Cloakroom[PlayerData.skin.Model.Hash])
-            end)
-        end,
-    })
+    for name, skin in pairs(StonkConfig.Cloakroom[PlayerData.skin.Model.Hash]) do
+        menu:AddButton({
+            label = name,
+            value = nil,
+            select = function()
+                QBCore.Functions.Progressbar("switch_clothes", "Changement d'habits...", 5000, false, true, {
+                    disableMovement = true,
+                    disableCombat = true,
+                }, {animDict = "anim@mp_yacht@shower@male@", anim = "male_shower_towel_dry_to_get_dressed", flags = 16}, {}, {}, function() -- Done
+                    TriggerEvent("soz-character:Client:ApplyTemporaryClothSet", skin)
+                end)
+            end,
+        })
+    end
 
     menu:Open()
 end)
