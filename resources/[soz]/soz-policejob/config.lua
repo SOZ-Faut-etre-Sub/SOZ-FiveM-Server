e804c4d0355109f6e136c8ba5a453526a00b8d83
Frozennide <sebastien@sdeu.fr>
2022-05-03 15:09:14 +0200

    fix(police): whitelist all new police vehicles
    Former-commit-id: 3acc5d034578c28163269efeeecf6345709908a5

--- resources/[soz]/soz-policejob/config.lua
+++ resources/[soz]/soz-policejob/config.lua
@@ -491,10 +491,16 @@ Config.RadarAllowedVehicle = {
     [GetHashKey("police")] = true,
     [GetHashKey("police2")] = true,
     [GetHashKey("police3")] = true,
+    [GetHashKey("police4")] = true,
+    [GetHashKey("police5")] = true,
+    [GetHashKey("police6")] = true,
     [GetHashKey("policeb")] = true,
     [GetHashKey("pbus")] = true,
     [GetHashKey("sheriff")] = true,
     [GetHashKey("sheriff2")] = true,
+    [GetHashKey("sheriff3")] = true,
+    [GetHashKey("sheriff4")] = true,
+    [GetHashKey("sheriffb")] = true,
 }
 --- Fines
 Config.Fines = {
