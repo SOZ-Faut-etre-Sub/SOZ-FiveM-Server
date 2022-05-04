952f0b32dd2197717e05c281ef27fafc9bd58685
leaudevian <mastersogood@gmail.com>
2022-05-04 16:16:55 +0200

    payment taxi pnj
    Former-commit-id: b8929f4c1b5cc9d48a629b65c074244851b43485

--- resources/[soz]/soz-taxi/server/server.lua
+++ resources/[soz]/soz-taxi/server/server.lua
@@ -3,6 +3,6 @@ local QBCore = exports["qb-core"]:GetCoreObject()
 RegisterNetEvent("taxi:server:NpcPay", function(Payment)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.AddMoney("money", Payment)
-    TriggerClientEvent("hud:client:DrawNotification", source, string.format("Vous recevez: %s $ pour votre mission", Payment))
+    TriggerServerEvent("banking:server:TransfertMoney", NPCpay, "taxi", Payment)
+    TriggerClientEvent("hud:client:DrawNotification", source, string.format("L'entreprise à reçu: %s $ pour votre mission", Payment))
 end)
