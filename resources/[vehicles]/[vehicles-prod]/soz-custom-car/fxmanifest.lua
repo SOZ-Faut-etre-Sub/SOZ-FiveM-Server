19b210128df6f2da7bca5485704d9acd636bd476
rigonkmalk <rigonkmalk@gmail.com>
2022-05-06 21:47:26 +0200

    chore(sound): converge two sound for paragonfbi and cogfbi in one file
    Former-commit-id: 4b322c4b53660e023016d4d4dac9d092c807bc14

--- resources/[vehicles]/[vehicles-prod]/soz-custom-car/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-custom-car/fxmanifest.lua
@@ -13,6 +13,6 @@ client_script {
 data_file "HANDLING_FILE" "meta/**/handling.meta"
 data_file "VEHICLE_METADATA_FILE" "meta/**/vehicles.meta"
 data_file "VEHICLE_VARIATION_FILE" "meta/**/carvariations.meta"
-data_file "AUDIO_GAMEDATA" "audio/cogfbi_game.dat"
+data_file "AUDIO_GAMEDATA" "audio/sozfbi_game.dat"
 data_file "CARCOLS_FILE" "meta/**/carcols.meta"
 data_file "VEHICLE_LAYOUTS_FILE" "meta/**/vehiclelayouts.meta"
