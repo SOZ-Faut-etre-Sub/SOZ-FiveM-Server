b0d05948a215af253941a8694a7a0d0587ee7fc5
Joel Wurtz <jwurtz@jolicode.com>
2022-05-06 22:21:32 +0000

    Revert "Merge branch 'feature/disallow-vehicle' into 'main'"
    This reverts merge request !630
    
    Former-commit-id: bfe3d015aff32100b820fbd2afb520545883198d

--- resources/[soz]/soz-utils/server/entities.lua
+++ resources/[soz]/soz-utils/server/entities.lua
@@ -1,7 +1,9 @@
+-- Blacklisting entities can just be handled entirely server side with onesync events
+-- No need to run coroutines to supress or delete these when we can simply delete them before they spawn
 AddEventHandler("entityCreating", function(handle)
     local entityModel = GetEntityModel(handle)
 
-    if Config.DisabledVehicle[entityModel] or Config.DisabledPeds[entityModel] then
+    if Config.BlacklistedVehs[entityModel] or Config.BlacklistedPeds[entityModel] then
         CancelEvent()
     end
 end)
