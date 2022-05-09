47ab335d3a4218f4de627debf7a70a3e601f8bb7
Frozennide <sebastien@sdeu.fr>
2022-05-10 00:08:38 +0200

    fix(taxi): clean pay function
    Former-commit-id: b72e674413a0092d4852b84ded9af2c1d57107e7

--- resources/[soz]/soz-taxi/server/server.lua
+++ resources/[soz]/soz-taxi/server/server.lua
@@ -4,9 +4,7 @@ RegisterNetEvent("taxi:server:NpcPay", function(Payment)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
-    TriggerServerEvent("banking:server:TransferMoney", "farm_taxi", "safe_taxi", Payment)
-    TriggerClientEvent("hud:client:DrawNotification", source, string.format("L'entreprise à reçu: %s $ pour votre mission", Payment))
-
+    TriggerEvent("banking:server:TransferMoney", "farm_taxi", "safe_taxi", Payment)
     TriggerEvent("monitor:server:event", "job_carljr_npc_course", {player_source = Player.PlayerData.source},
                  {amount = tonumber(Payment), position = GetEntityCoords(GetPlayerPed(Player.PlayerData.source))})
 end)
