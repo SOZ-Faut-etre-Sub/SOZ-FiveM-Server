8c10e5a0d0983998583e51ac37566ed2cd0f6cb1
Valentin <valentin@waldeck.dev>
2022-05-15 07:54:29 +0200

    Add menu entry
    Former-commit-id: f17cd96b27dcf3bbc7d98944c370fb971667560a

---
+++ resources/[soz]/soz-personal-menu/client/components/welcome.lua
@@ -0,0 +1,9 @@
+function WelcomeEntry(menu)
+    menu:AddButton({
+        label = "Guide de bienvenue",
+        description = "Bienvenue à Los Santos !",
+        select = function()
+            -- TODO: Open NUI
+        end
+    })
+end
