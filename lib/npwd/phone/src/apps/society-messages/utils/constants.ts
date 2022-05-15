0122ce529765f33b5765a523063ed94618038a8e
Frozennide <sebastien@sdeu.fr>
2022-05-15 18:43:11 +0200

    fix(phone): open message conversation when "Message" option is selected in society app
    Former-commit-id: 3cfb4ac0f22abf718801fd96355d1df95932dbda

--- lib/npwd/phone/src/apps/society-messages/utils/constants.ts
+++ lib/npwd/phone/src/apps/society-messages/utils/constants.ts
@@ -4,7 +4,7 @@ export const MockSocietyMessages: SocietyMessage[] = [
     {
         id: 1,
         conversation_id: '555-POLICE',
-        source_phone: '555-1234',
+        source_phone: '603-275-8373',
         message: 'Lorem ipsum dolor sit amet, conse ctetur adip iscing elit.',
         position: '{"x":205.12088012695312,"y":1160.4395751953125,"z":226.99560546875}',
         isTaken: false,
