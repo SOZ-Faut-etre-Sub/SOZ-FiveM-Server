4766e22739e7deb260fbd92a1a531e089c5a1ad0
Frozennide <sebastien@sdeu.fr>
2022-05-16 22:25:01 +0200

    fix(news): rename migration name
    Former-commit-id: 73d174f8c77a8b9ca9dd41aff9789fb5d1def361

--- resources/[soz]/mysql-migration/migrations/2022-05-16-17-10-add-reporter-id.lua
+++ resources/[soz]/mysql-migration/migrations/2022-05-16-17-10-add-reporter-id.lua
@@ -1,5 +1,5 @@
 table.insert(migrations, {
-    name = "add-society-message-taken-by-username",
+    name = "add-reporter-id",
     queries = {
         [[
             alter table phone_twitch_news add reporterId varchar(70) null after reporter;
