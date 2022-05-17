e1c136cbb6aebf6cfdba75df25ded17b5c7ac6af
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-05-17 15:32:18 +0200

    fix carwash et nettoyer
    Former-commit-id: e0c9728061ad93d6e0d5de9de9edec512e732346

--- resources/[soz]/soz-vehicle/server/carwash.lua
+++ resources/[soz]/soz-vehicle/server/carwash.lua
@@ -4,7 +4,7 @@ RegisterNetEvent("qb-carwash:server:washCar", function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
-    if Player.Functions.RemoveMoney("money", Config.DefaultPrice, "car-washed") then
+    if Player.Functions.RemoveMoney("money", 50, "car-washed") then
         TriggerClientEvent("qb-carwash:client:washCar", src)
     else
         TriggerClientEvent("hud:client:DrawNotification", src, "Vous n'avez pas assez d'argent", "error")
