60bb7000cbd9b22acdd71961901599d372c8b554
leaudevian <leaudevian@dt.com>
2022-05-18 19:36:45 +0200

    hazmat
    Former-commit-id: 50c32497dc9e6d20b235452aff8c3fc7d0a59a0c

--- resources/[soz]/soz-ems/config.lua
+++ resources/[soz]/soz-ems/config.lua
@@ -35,6 +35,21 @@ Config.Cloakroom = {
                 },
                 Props = {[0] = {Drawable = 45, Texture = 0, Palette = 0}},
             },
+            ["Tenue hazmat"] = {
+                Components = {
+                    [1] = {Drawable = 129, Texture = 12, Palette = 0},
+                    [3] = {Drawable = 86, Texture = 0, Palette = 0},
+                    [4] = {Drawable = 40, Texture = 2, Palette = 0},
+                    [5] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [6] = {Drawable = 25, Texture = 0, Palette = 0},
+                    [7] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [8] = {Drawable = 14, Texture = 0, Palette = 0},
+                    [9] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [10] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [11] = {Drawable = 67, Texture = 2, Palette = 0},
+                },
+                Props = {},
+            },
         },
         [GetHashKey("mp_f_freemode_01")] = {
             ["Tenue incendie"] = {
@@ -51,6 +66,21 @@ Config.Cloakroom = {
                     [11] = {Drawable = 325, Texture = 0, Palette = 0},
                 },
                 Props = {[0] = {Drawable = 44, Texture = 0, Palette = 0}},
+            },
+            ["Tenue hazmat"] = {
+                Components = {
+                    [1] = {Drawable = 129, Texture = 12, Palette = 0},
+                    [3] = {Drawable = 101, Texture = 0, Palette = 0},
+                    [4] = {Drawable = 40, Texture = 2, Palette = 0},
+                    [5] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [6] = {Drawable = 25, Texture = 0, Palette = 0},
+                    [7] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [8] = {Drawable = 14, Texture = 0, Palette = 0},
+                    [9] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [10] = {Drawable = 0, Texture = 0, Palette = 0},
+                    [11] = {Drawable = 61, Texture = 2, Palette = 0},
+                },
+                Props = {},
             },
         },
     },
