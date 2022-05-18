c1a05a1b7a6a02b7efd3de1a71806caa02c313dc
Joel Wurtz <jwurtz@jolicode.com>
2022-05-18 18:14:34 +0000

    Revert "Merge branch 'fix/fix-vehicules-seats' into 'main'"
    This reverts merge request !869
    
    Former-commit-id: 3689c8c8a6dc5a6c3111718ff41e50bca22c47c9

--- resources/[soz]/soz-vehicle/client/VehSeat.lua
+++ resources/[soz]/soz-vehicle/client/VehSeat.lua
@@ -1,94 +1,32 @@
-local DOOR_INDEX_CONFIG = {
-    ["seat_dside_f"] = -1,
-    ["seat_pside_f"] = 0,
-    ["seat_dside_r"] = 1,
-    ["seat_pside_r"] = 2,
-    ["door_dside_f"] = -1,
-    ["door_pside_f"] = 0,
-    ["door_dside_r"] = 1,
-    ["door_pside_r"] = 2,
-    ["wheel_lr"] = {3, 5},
-    ["wheel_rr"] = {4, 6},
-}
+delay = 800
 
 Citizen.CreateThread(function()
     while true do
-        local playerPed = GetPlayerPed(-1)
-        SetPedConfigFlag(playerPed, 220, true)
+        Citizen.Wait(delay)
+        delay = 800
 
-        local vehicle = GetVehiclePedIsTryingToEnter(playerPed)
-        local IsInVehicle = IsPedInAnyVehicle(playerPed, false)
-
-        -- Enter only if there is a vehicle, ped is not in vehicle and lock status is ok
-        if vehicle ~= 0 and not IsInVehicle and GetVehicleDoorLockStatus(vehicle) ~= 2 then
-            local max = GetVehicleMaxNumberOfPassengers(vehicle)
-            local playerPos = GetEntityCoords(playerPed, false)
-            local minDistance = 2.0
-            local closestDoor = nil
-
-            for bone, seatIndex in pairs(DOOR_INDEX_CONFIG) do
-                -- Avoid using same seat as an existing ped
-                local useSeat = false
-                local availableSeatIndex = seatIndex
-
-                -- Get available seat (skip seat where there is an existing ped)
-                if type(seatIndex) == "number" then
-                    useSeat = GetPedInVehicleSeat(vehicle, seatIndex) == 0
-                else
-                    for _, index in pairs(seatIndex) do
-                        if not useSeat and GetPedInVehicleSeat(vehicle, index) == 0 then
-                            useSeat = true
-                            availableSeatIndex = index
-                        end
-                    end
+        local ped = PlayerPedId()
+        local pco = GetEntityCoords(ped)
+        local vehicle = GetClosestVehicle(pco, 5.0, 0, 71)
+        if DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) and (IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1)) or GetPedInVehicleSeat(vehicle, -1) == 0) then
+            delay = 25
+            local bldoor = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "window_lr"))
+            local brdoor = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "window_rr"))
+            local playerpos = GetEntityCoords(GetPlayerPed(-1), 1)
+            local distanceToBrdoor = GetDistanceBetweenCoords(brdoor, playerpos, 1)
+            local distanceToBldoor = GetDistanceBetweenCoords(bldoor, playerpos, 1)
+            if distanceToBrdoor < 1.1 and DoesVehicleHaveDoor(vehicle, 3) and not DoesEntityExist(GetPedInVehicleSeat(vehicle, 2)) and
+                GetVehicleDoorLockStatus(vehicle) ~= 2 then
+                if (IsControlJustPressed(1, 49)) then
+                    TaskEnterVehicle(PlayerPedId(), vehicle, 10000, 2, 1.0, 1, 0)
                 end
-
-                -- Check available seat is not outbound
-                if availableSeatIndex > max - 1 then
-                    useSeat = false
-                end
-
-                if useSeat then
-                    local bonePos = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, bone))
-                    local distance = #(playerPos - bonePos)
-
-                    if closestDoor == nil then
-                        if distance <= minDistance then
-                            closestDoor = {
-                                bone = bone,
-                                boneDist = distance,
-                                bonePos = bonePos,
-                                seatIndex = availableSeatIndex,
-                            }
-                        end
-                    else
-                        if distance < closestDoor.boneDist then
-                            closestDoor = {
-                                bone = bone,
-                                boneDist = distance,
-                                bonePos = bonePos,
-                                seatIndex = availableSeatIndex,
-                            }
-                        end
-                    end
-                end
-            end
-
-            if closestDoor ~= nil then
-                -- ClearPedTasksImmediately(playerPed)
-                TaskEnterVehicle(playerPed, vehicle, -1, closestDoor.seatIndex, 1.0, 1, 0)
-                Wait(200)
-
-                local enteringVehicle = GetVehiclePedIsEntering(playerPed) or GetVehiclePedIsTryingToEnter(playerPed)
-
-                while enteringVehicle ~= 0 do
-                    Wait(200)
-
-                    enteringVehicle = GetVehiclePedIsEntering(playerPed) or GetVehiclePedIsTryingToEnter(playerPed)
+            elseif distanceToBldoor < 1.1 and DoesVehicleHaveDoor(vehicle, 2) and not DoesEntityExist(GetPedInVehicleSeat(vehicle, 1)) and
+                GetVehicleDoorLockStatus(vehicle) ~= 2 then
+                if (IsControlJustPressed(1, 49)) then
+                    TaskEnterVehicle(PlayerPedId(), vehicle, 10000, 1, 1.0, 1, 0)
                 end
             end
         end
 
-        Wait(0)
     end
 end)
