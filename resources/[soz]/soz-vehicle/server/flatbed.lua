d7474526a5f597554500b6092a42dbdcaeee5ebc
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-05-19 20:18:37 +0200

    Rework flatbed
    Former-commit-id: 50e9cfba3325bc2d0acec86947b686f2071f1ccb

--- resources/[soz]/soz-vehicle/server/flatbed.lua
+++ resources/[soz]/soz-vehicle/server/flatbed.lua
@@ -19,13 +19,26 @@ AddEventHandler("soz-flatbed:server:getProp", function(VehicleId)
 end)
 
 RegisterServerEvent("soz-flatbed:server:action")
-AddEventHandler("soz-flatbed:server:action", function(VehicleId, Action)
-    TriggerClientEvent("soz-flatbed:client:action", source, FlatbedProps[VehicleId], Action)
+AddEventHandler("soz-flatbed:server:action", function(VehicleId, Action, owner)
+    TriggerClientEvent("soz-flatbed:client:action", source, FlatbedProps[VehicleId], Action, owner)
 end)
 
 RegisterServerEvent("soz-flatbed:server:tpaction")
 AddEventHandler("soz-flatbed:server:tpaction", function(VehicleId, lastveh, entity)
     TriggerClientEvent("soz-flatbed:client:tpaction", source, FlatbedProps[VehicleId], lastveh, entity)
+end)
+
+RegisterServerEvent("soz-flatbed:server:actionowner")
+AddEventHandler("soz-flatbed:server:actionowner", function(BedInfo, Action, LastVehicle, owner)
+    if Action == "lower" then
+        TriggerClientEvent("soz-flatbed:client:actionlower", owner, BedInfo, Action, LastVehicle)
+    elseif Action == "raise" then
+        TriggerClientEvent("soz-flatbed:client:actionraise", owner, BedInfo, Action, LastVehicle)
+    elseif Action == "attach" then
+        TriggerClientEvent("soz-flatbed:client:actionattach", owner, BedInfo, Action, LastVehicle)
+    elseif Action == "detach" then
+        TriggerClientEvent("soz-flatbed:client:actiondettach", owner, BedInfo, Action, LastVehicle)
+    end
 end)
 
 Citizen.CreateThread(function()
