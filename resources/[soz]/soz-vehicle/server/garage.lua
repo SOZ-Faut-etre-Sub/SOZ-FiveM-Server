e6038b1e1d9fa94effcf7daf04ffeb63a4c47fe8
Joel Wurtz <joel.wurtz@gmail.com>
2022-05-20 09:23:18 +0200

    Fix vehicle list in pound for job and duplication
    Former-commit-id: a383f29615484a2adb32302e28484abb504b7dfd

--- resources/[soz]/soz-vehicle/server/garage.lua
+++ resources/[soz]/soz-vehicle/server/garage.lua
@@ -25,9 +25,9 @@ QBCore.Functions.CreateCallback("qb-garage:server:GetGarageVehicles", function(s
             end
         end)
     elseif type == "depot" then -- Depot give player cars that are not in garage only
-        MySQL.Async.fetchAll("SELECT * FROM player_vehicles WHERE citizenid = ? AND (state = ? OR state = ?)", {
+        MySQL.Async.fetchAll("SELECT * FROM player_vehicles WHERE (citizenid = ? OR job = ?) AND state = ?", {
+            pData.PlayerData.job.id,
             pData.PlayerData.citizenid,
-            0,
             2,
         }, function(result)
             local tosend = {}
