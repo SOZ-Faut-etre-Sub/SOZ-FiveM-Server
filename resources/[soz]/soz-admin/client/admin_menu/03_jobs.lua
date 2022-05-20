dd1847e1d2b69d4361b7b01ebe5c25ef83fe4413
Joel Wurtz <joel.wurtz@gmail.com>
2022-05-20 12:43:32 +0200

    Add job grade menu in admin, add grade to take out vehicle from pound
    Former-commit-id: c831f9ad4e8d9ea1aba57ab3f1b2eaf02f688345

--- resources/[soz]/soz-admin/client/admin_menu/03_jobs.lua
+++ resources/[soz]/soz-admin/client/admin_menu/03_jobs.lua
@@ -1,4 +1,44 @@
-local jobMenu, JobOption = nil, {JobList = {}}
+local jobMenu
+local jobGradeSlider
+
+function BuildCurrentGradesValues(jobId)
+    local SozJobCore = exports["soz-jobs"]:GetCoreObject()
+    local job = SozJobCore.Jobs[jobId]
+    local values = {}
+
+    if not job then
+        return values
+    end
+
+    for gradeId, grade in pairs(job.grades) do
+        table.insert(values, { label = grade.name,
+                               value = {label = grade.name, jobID = jobId, gradeId = gradeId},})
+    end
+
+    return values
+end
+
+function BuildJobList()
+    local SozJobCore = exports["soz-jobs"]:GetCoreObject()
+    local JobList = {}
+
+    for jobID, jobData in pairs(SozJobCore.Jobs) do
+        for _, grade in pairs(jobData.grades) do
+            if grade.owner == 1 then
+                table.insert(JobList, {
+                    label = jobData.label,
+                    value = {label = jobData.label, jobID = jobID, gradeId = grade.id},
+                })
+            end
+        end
+    end
+
+    table.sort(JobList, function(a, b)
+        return a.label < b.label
+    end)
+
+    return JobList
+end
 
 function AdminMenuJob(menu, permission)
     if jobMenu == nil then
@@ -6,24 +46,39 @@ function AdminMenuJob(menu, permission)
     end
 
     jobMenu:ClearItems()
+    jobGradeSlider = nil
 
     jobMenu:On("open", function(m)
+        local playerData = QBCore.Functions.GetPlayerData()
+
         m:ClearItems()
 
-        --- Menu entries
         m:AddSlider({
             label = "Changer de métier",
             value = nil,
-            values = JobOption.JobList,
+            values = BuildJobList(),
             select = function(_, value)
                 TriggerServerEvent("admin:jobs:setjob", value.jobID, value.gradeId)
                 exports["soz-hud"]:DrawNotification("Votre métier est maintenant : ~b~" .. value.label, "info")
+
+                jobGradeSlider:ClearValues()
+                jobGradeSlider:AddValues(BuildCurrentGradesValues(value.jobID))
+            end,
+        })
+
+        jobGradeSlider = m:AddSlider({
+            label = "Changer de grade",
+            value = nil,
+            values = BuildCurrentGradesValues(playerData.job.id),
+            select = function(_, value)
+                TriggerServerEvent("admin:jobs:setjob", value.jobID, value.gradeId)
+                exports["soz-hud"]:DrawNotification("Votre grade est maintenant : ~b~" .. value.label, "info")
             end,
         })
 
         m:AddCheckbox({
             label = "Passer en service",
-            value = QBCore.Functions.GetPlayerData().job.onduty,
+            value = playerData.job.onduty,
             change = function()
                 TriggerServerEvent("QBCore:ToggleDuty")
             end,
@@ -33,22 +88,3 @@ function AdminMenuJob(menu, permission)
     --- Add to main menu
     menu:AddButton({icon = "⛑", label = "Gestion métier", value = jobMenu, disabled = permission ~= "admin"})
 end
-
-RegisterNetEvent("soz-jobs:Client:OnJobSync", function(jobs)
-    JobOption.JobList = {}
-
-    for jobID, jobData in pairs(jobs) do
-        for _, grade in pairs(jobData.grades) do
-            if grade.owner == 1 then
-                table.insert(JobOption.JobList, {
-                    label = jobData.label,
-                    value = {label = jobData.label, jobID = jobID, gradeId = grade.id},
-                })
-            end
-        end
-    end
-
-    table.sort(JobOption.JobList, function(a, b)
-        return a.label < b.label
-    end)
-end)
