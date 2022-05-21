341abf920aa4bef94d1ea76d33f2d86022fcc728
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-05-21 19:10:59 +0200

    Big clean bennys et vehicle et delete vehicle status
    Former-commit-id: 0aa702275057fd4752af4cfafb74e247aae8a6e5

--- resources/[soz]/soz-bennys/config.lua
+++ resources/[soz]/soz-bennys/config.lua
@@ -11,20 +11,12 @@ Config.Locations = {
 Config.MaxStatusValues = {
     ["engine"] = 1000.0,
     ["body"] = 1000.0,
-    ["radiator"] = 100,
-    ["axle"] = 100,
-    ["brakes"] = 100,
-    ["clutch"] = 100,
-    ["fuel"] = 100,
+    ["fuel"] = 1000.0,
 }
 
 Config.ValuesLabels = {
     ["engine"] = "Moteur",
     ["body"] = "Carrosserie",
-    ["radiator"] = "Radiateur",
-    ["axle"] = "Transmission",
-    ["brakes"] = "Freins",
-    ["clutch"] = "Embrayage",
     ["fuel"] = "Réservoir",
 }
 
