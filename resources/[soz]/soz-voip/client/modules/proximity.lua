3d86bb3fef2381e5746d4f71253fbccc88e316f5
Frozennide <sebastien@sdeu.fr>
2022-05-24 17:49:35 +0200

    Revert "Revert "Merge branch 'fix/voip-proximity' into 'main'""
    This reverts commit 8eecc4e5fdb71ffcf95257f0f59a9973b70efd27 [formerly ba95bc793bcd053bec0798b040c4d1d358a473cd].
    
    
    Former-commit-id: 019efc165d72333ac1c943f3a6512fb36a619b5f

--- resources/[soz]/soz-voip/client/modules/proximity.lua
+++ resources/[soz]/soz-voip/client/modules/proximity.lua
@@ -1,3 +1,73 @@
+local GridSize, EdgeSize = Config.gridSize, Config.gridEdge
+
+local CurrentGrids, PreviousGrid = {}, 0
+
+local deltas = {
+    vector2(-1, -1),
+    vector2(-1, 0),
+    vector2(-1, 1),
+    vector2(0, -1),
+    vector2(1, -1),
+    vector2(1, 0),
+    vector2(1, 1),
+    vector2(0, 1),
+}
+
+function GridToChannel(vectors)
+    return (vectors.x << 8) | vectors.y
+end
+
+function GetGridChunk(coords)
+    return math.floor((coords + 8192) / GridSize)
+end
+
+function GetGridChannel(coords, intact)
+    local grid = vector2(GetGridChunk(coords.x), GetGridChunk(coords.y))
+    local channel = GridToChannel(grid)
+
+    if not intact and PlayerData.CurrentInstance ~= 0 then
+        channel = tonumber(("%s0%s"):format(channel, PlayerData.CurrentInstance))
+    end
+
+    return channel
+end
+
+function GetTargetChannels(coords, edge)
+    local targets = {}
+
+    for _, delta in ipairs(deltas) do
+        local vectors = vector2(coords.x + delta.x * edge, coords.y + delta.y * edge)
+        local channel = GetGridChannel(vectors)
+
+        if not table.exist(targets, channel) then
+            table.insert(targets, channel)
+        end
+    end
+
+    return targets
+end
+
+function SetGridChannels(current, previous)
+    local toRemove = {}
+
+    for _, grid in ipairs(previous) do
+        if not table.exist(current, grid) then
+            toRemove[#toRemove + 1] = grid
+        end
+    end
+
+    AddChannelGroupToTargetList(current, "proximity")
+    RemoveChannelGroupFromTargetList(toRemove, "proximity")
+
+    CurrentGrids = current
+end
+
+function SetVoiceChannel(channelID)
+    NetworkSetVoiceChannel(channelID)
+    console.debug("[Main] Current Channel: %s", channelID)
+    PlayerData.CurrentVoiceChannel = channelID
+end
+
 --- Private functions
 local function MutePlayer(state)
     if state and type(state) == "boolean" then
@@ -44,6 +114,28 @@ function RegisterProximityModule()
     console.debug("Proximity module registering...")
 
     RegisterModuleContext("proximity", 0)
+
+    Citizen.CreateThread(function()
+        while true do
+            local idle = 100
+
+            local currentGrid = GetGridChannel(PlayerData.PlayerCoords)
+            local edgeGrids = GetTargetChannels(PlayerData.PlayerCoords, EdgeSize)
+
+            if IsDifferent(edgeGrids, CurrentGrids) then
+                SetGridChannels(edgeGrids, CurrentGrids)
+                console.debug("Grid changed: %s", table.concat(edgeGrids, ", "))
+            end
+
+            if currentGrid ~= PreviousGrid then
+                SetVoiceChannel(currentGrid)
+            end
+
+            PreviousGrid = currentGrid
+
+            Citizen.Wait(idle)
+        end
+    end)
 
     --- Keybindings
     RegisterCommand("voip-voice_up", ProximityVoiceIncrease, false)
