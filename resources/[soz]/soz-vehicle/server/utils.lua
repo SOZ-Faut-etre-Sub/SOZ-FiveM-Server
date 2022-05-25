fc08296db6568a8a1a81fc07a3021c081c7a52ea
Valentin <valentin@waldeck.dev>
2022-05-25 21:42:45 +0200

    Perform Spawn logic server side
    Former-commit-id: bd76719a62b5c7db51d94a58bc2990c6584aba47

--- resources/[soz]/soz-vehicle/server/utils.lua
+++ resources/[soz]/soz-vehicle/server/utils.lua
@@ -4,3 +4,22 @@ function DespawnVehicle(vehicleNetId)
     DeleteEntity(NetworkGetEntityFromNetworkId(vehicleNetId))
 end
 exports("DespawnVehicle", DespawnVehicle)
+
+
+--- Make sure model is loaded on client
+function SpawnVehicle(modelName, coords, mods)
+    local veh = CreateVehicle(GetHashKey(modelName), coords, true, true)
+    while not DoesEntityExist(veh) do
+        Citizen.Wait(0)
+    end
+
+    SetVehicleNumberPlateText(veh, mods.plate)
+
+    -- Send event to entity owner (even if this is not the player that is spawning vehicle)
+    local owner = NetworkGetEntityOwner(veh)
+    local vehNetId = NetworkGetNetworkIdFromEntity(veh)
+    TriggerClientEvent("soz-garage:client:SetVehicleProperties", owner, vehNetId, mods)
+
+    return veh
+end
+exports("SpawnVehicle", SpawnVehicle)
