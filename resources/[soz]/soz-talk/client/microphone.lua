33e791c74b2a68b926df7e86d694362a0e58862f
Frozennide <sebastien@sdeu.fr>
2022-05-25 09:48:52 +0200

    fix(props): disable prop entity migration
    Former-commit-id: 152be1d7daf42a2e0c8193dd751621667783f9aa

--- resources/[soz]/soz-talk/client/microphone.lua
+++ resources/[soz]/soz-talk/client/microphone.lua
@@ -6,6 +6,7 @@ local function toggleMicrophoneAnimation(pState)
     if pState then
         TaskPlayAnim(PlayerPedId(), "anim@random@shop_clothes@watches", "base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
         microphoneProp = CreateObject(GetHashKey("prop_microphone_02"), 1.0, 1.0, 1.0, 1, 1, 0)
+        SetNetworkIdCanMigrate(ObjToNet(microphoneProp), false)
         AttachEntityToEntity(microphoneProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 60309), 0.10, 0.0, 0.012, 20.0, 110.0, 70.0, 1, 0, 0, 0, 2, 1)
     else
         StopAnimTask(PlayerPedId(), "anim@random@shop_clothes@watches", "base", 1.0)
