c7522c3cee28314440e07ed49963e7a952831716
leaudevian <leaudevian@dt.com>
2022-05-27 23:14:05 +0200

    storages housing
    Former-commit-id: 7061f6a44a28fab8597a524a91dec57dcbfc25fd

--- resources/[soz]/soz-inventory/server/events.lua
+++ resources/[soz]/soz-inventory/server/events.lua
@@ -14,6 +14,18 @@ RegisterServerEvent("inventory:server:openInventory", function(storageType, invI
     end
 end)
 
+RegisterServerEvent("inventory:server:openHousingInventory", function(storageType, invID, ctx)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    local sourceInv = Inventory(source)
+    local targetInv = GetOrCreateInventory(storageType, invID, ctx)
+
+    targetInv.users[Player.PlayerData.source] = true
+
+    TriggerClientEvent("inventory:client:openInventory", Player.PlayerData.source, Inventory.FilterItems(sourceInv, targetInv.type),
+        Inventory.FilterItems(targetInv, sourceInv.type))
+end)
+
 QBCore.Functions.CreateCallback("inventory:server:TransfertItem", function(source, cb, inventorySource, inventoryTarget, item, amount, metadata, slot)
     Inventory.TransfertItem(inventorySource, inventoryTarget, item, amount, metadata, slot, function(success, reason)
         local sourceInv = Inventory(inventorySource)
