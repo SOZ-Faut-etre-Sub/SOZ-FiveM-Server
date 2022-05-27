faa5506c12209b963190210908ba82d622f6f881
Frozennide <sebastien@sdeu.fr>
2022-05-27 11:15:28 +0200

    fix(phone): convert object to array for items
    Former-commit-id: 3f3d74b4e9ce0eab3c3725cebb8a6d5dc7eb263c

--- lib/npwd/resources/client/cl_main.ts
+++ lib/npwd/resources/client/cl_main.ts
@@ -162,7 +162,8 @@ onNet('QBCore:Client:OnPlayerLoaded', async () => {
 });
 
 onNet('QBCore:Player:SetPlayerData', async (playerData: PlayerData) => {
-    const hasItem = playerData.items.find(item => item.name === 'phone')
+    if (typeof playerData.items === 'object') playerData.items = Object.values(playerData.items);
+    const hasItem = playerData.items.find(item => item.name === 'phone');
     sendMessage('PHONE', PhoneEvents.SET_AVAILABILITY, !!hasItem);
 });
 
