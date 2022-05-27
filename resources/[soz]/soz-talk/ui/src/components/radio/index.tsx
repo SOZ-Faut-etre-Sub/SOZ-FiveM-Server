df1f26e58ba1ad0e2ed59ca60a7a24bfd333d44d
Frozennide <sebastien@sdeu.fr>
2022-05-27 18:56:00 +0200

    fix(radio): reset radio configuration on item drop
    Former-commit-id: d814085fc88dad106603733c3c3142e82598bda3

--- resources/[soz]/soz-talk/ui/src/components/radio/index.tsx
+++ resources/[soz]/soz-talk/ui/src/components/radio/index.tsx
@@ -113,7 +113,15 @@ const Radio: React.FC<{type: 'radio' | 'cibi'}> = (props) => {
         const {type, action, frequency, volume, ear, isPrimary, isEnabled} = event.data as TalkMessageData;
 
         if (type === props.type) {
-            if (action === 'open') {
+            if (action === 'reset') {
+                setDisplay(false)
+                setEnabled(false)
+                setCurrentFrequency('primary')
+                setPrimaryFrequency({frequency: 0.0, volume: 100, ear: Ear.Both})
+                setSecondaryFrequency({frequency: 0.0, volume: 100, ear: Ear.Both})
+                setValue('primaryFrequency', null)
+                setValue('secondaryFrequency', null)
+            } else if (action === 'open') {
                 setDisplay(true)
             } else if (action === 'close') {
                 setDisplay(false)
--- resources/[soz]/soz-talk/ui/src/types/TalkMessageEvent.ts
+++ resources/[soz]/soz-talk/ui/src/types/TalkMessageEvent.ts
@@ -1,7 +1,7 @@
 import {Ear} from "./RadioScreen";
 
 export type TalkMessageType = 'radio' | 'cibi'
-export type TalkMessageAction = 'open' | 'close' | 'enabled' | 'frequency_change' | 'volume_change' | 'ear_change'
+export type TalkMessageAction = 'open' | 'close' | 'enabled' | 'frequency_change' | 'volume_change' | 'ear_change' | 'reset'
 
 export interface TalkMessageData {
     type: TalkMessageType
