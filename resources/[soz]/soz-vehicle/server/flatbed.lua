f34c26a5260e78daba5d7dd5624b40bce3500a7d
DarkEagle <10519166-DarkEagl@users.noreply.gitlab.com>
2022-05-28 02:12:48 +0200

    FlatbedrevampPartie6
    Former-commit-id: 5d9893102681127fef8ad9c4e1ba6a935184cbd3

--- resources/[soz]/soz-vehicle/server/flatbed.lua
+++ resources/[soz]/soz-vehicle/server/flatbed.lua
@@ -20,3 +20,17 @@ AddEventHandler("onResourceStop", function(ResName)
         end
     end
 end)
+
+RegisterNetEvent("soz-flatbed:server:setupstate", function(entity, name, value)
+    local Vehicle = NetworkGetEntityFromNetworkId(entity)
+    if name == "prop" then
+        Entity(Vehicle).state.prop = value
+        print(Entity(Vehicle).state.prop )
+    elseif name == "busy" then
+        Entity(Vehicle).state.busy = value
+    elseif name == "status" then
+        Entity(Vehicle).state.status = value
+    elseif name == "towedVehicle" then
+        Entity(Vehicle).state.towedVehicle = value
+    end
+end)
