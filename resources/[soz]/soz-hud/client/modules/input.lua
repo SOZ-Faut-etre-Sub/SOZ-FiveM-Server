85809de37555ec4e388efef855152d8ef23f37c3
Frozennide <sebastien@sdeu.fr>
2022-05-28 20:54:20 +0200

    fix(hud): better handle of error on input
    Former-commit-id: a07309e1d629e90f1a05a8fbd8e30e37a1d99589

--- resources/[soz]/soz-hud/client/modules/input.lua
+++ resources/[soz]/soz-hud/client/modules/input.lua
@@ -2,29 +2,34 @@ local properties = nil
 
 RegisterNUICallback("input/submit", function(data, cb)
     SetNuiFocus(false, false)
-    properties:resolve(data.data)
+    if properties then
+        properties:resolve(data.data)
+    end
     properties = nil
     cb("ok")
 end)
 
 RegisterNUICallback("input/close", function(data, cb)
     SetNuiFocus(false, false)
-    properties:resolve(nil)
+    if properties then
+        properties:resolve(nil)
+    end
     properties = nil
     cb("ok")
 end)
 
 exports("Input", function(title, maxChar, content)
     SetNuiFocus(true, true)
-    Wait(150)
+    Wait(50)
 
-    if properties ~= nil then
+    if properties then
         SetNuiFocus(false, false)
+        properties:resolve(nil)
+        properties = nil
         return
     end
 
     properties = promise.new()
-
     SendNUIMessage({action = "draw_input", title = title, maxChar = maxChar, content = content})
 
     return Citizen.Await(properties)
