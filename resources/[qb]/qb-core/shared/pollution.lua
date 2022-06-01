071fe49ff9cc9e84c5b5f04cd9658528342553a3
Valentin <valentin@waldeck.dev>
2022-06-01 18:56:05 +0200

    Add pollution thresholds
    Former-commit-id: 36123e0bfbe291c09d6e1b0f8d1ca02cae4e476a

--- resources/[qb]/qb-core/shared/pollution.lua
+++ resources/[qb]/qb-core/shared/pollution.lua
@@ -8,3 +8,9 @@ QBShared.Pollution.Level = {
     High = 3
 }
 
+QBShared.Pollution.Threshold = {
+    [QBShared.Pollution.Level.Low] = { min = 0, max = 11 },
+    [QBShared.Pollution.Level.Neutral] = { min = 11, max = 70 },
+    [QBShared.Pollution.Level.High] = { min = 70, max = 100 },
+}
+
--- resources/[soz]/soz-upw/server/pollution.lua
+++ resources/[soz]/soz-upw/server/pollution.lua
@@ -1,6 +1,14 @@
 local function GetPollutionLevel()
-    -- TEMP set to Neutral level for now
-    return QBCore.Shared.PollutionLevel.Neutral
+    local currentPollution = 50 -- TEMP set to Neutral level for now
+    if currentPollution >= 100 then
+        return QBShared.Pollution.Level.High
+    end
+
+    for level, range in pairs(QBShared.Pollution.Threshold) do
+        if currentPollution >= range.min and currentPollution < range.max then
+            return QBShared.Pollution.Level[level]
+        end
+    end
 end
 
 QBCore.Functions.CreateCallback("soz-upw:server:GetPollutionLevel", function (source, cb)
