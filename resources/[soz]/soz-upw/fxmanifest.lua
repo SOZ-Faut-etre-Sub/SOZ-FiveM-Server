fd5b22b725b6118232b3bd1f56ace7e355d25313
Valentin <valentin@waldeck.dev>
2022-06-01 18:41:08 +0200

    Add soz-upw resource
    Former-commit-id: c85f432718b6da13f6e71ffa76928b8455dd936b

---
+++ resources/[soz]/soz-upw/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+games {"gta5"}
+lua54 "yes"
+
+shared_scripts {"config.lua"}
+
+client_scripts {"client/*.lua"}
+
+server_scripts {"@oxmysql/lib/MySQL.lua", "server/*.lua"}
+
+dependencies {"oxmysql", "qb-core"}
