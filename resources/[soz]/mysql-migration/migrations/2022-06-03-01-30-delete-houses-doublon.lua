23892ffff40c3c25c2cedd5a411642911af5ab08
leaudevian <leaudevian@dt.com>
2022-06-03 01:32:17 +0200

    Clean housing Doublon
    Former-commit-id: 4fce8df5af6228bb1b76b94d74655b2081dab2f9

---
+++ resources/[soz]/mysql-migration/migrations/2022-06-03-01-30-delete-houses-doublon.lua
@@ -0,0 +1,11 @@
+table.insert(migrations, {
+    name = "delete-housing-doublon"
+    queries = {
+        [[
+            DELETE FROM player_house WHERE id = 109;
+        ]],
+        [[
+            DELETE FROM player_house WHERE id = 110;
+        ]],
+    },
+});
