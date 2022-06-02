af1f5e38fbd38c88ee092ffe6d321cecc652efac
leaudevian <leaudevian@dt.com>
2022-06-03 01:51:12 +0200

    typo
    Former-commit-id: e2d58d28554af8d0092d3c4b39249e658f457596

--- resources/[soz]/mysql-migration/migrations/2022-06-03-01-30-delete-houses-doublon.lua
+++ resources/[soz]/mysql-migration/migrations/2022-06-03-01-30-delete-houses-doublon.lua
@@ -1,5 +1,5 @@
 table.insert(migrations, {
-    name = "delete-housing-doublon"
+    name = "delete-housing-doublon",
     queries = {
         [[
             DELETE FROM player_house WHERE id = 109;
