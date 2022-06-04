028bfc39d8427b5ada9856aa8f7a0154f769e8ea
Frozennide <sebastien@sdeu.fr>
2022-06-04 13:43:59 +0200

    feat(upw): add blip
    Former-commit-id: e2230b0f5f12b37c913e7612f8ad2f76ac895c77

--- resources/[soz]/soz-upw/client/main.lua
+++ resources/[soz]/soz-upw/client/main.lua
@@ -1,1 +1,12 @@
 QBCore = exports["qb-core"]:GetCoreObject()
+
+Citizen.CreateThread(function()
+    -- Blip
+    if not QBCore.Functions.GetBlip("job_upw") then
+        QBCore.Functions.CreateBlip("job_upw", {
+            name = Config.Blip.Name,
+            coords = Config.Blip.Coords,
+            sprite = Config.Blip.Sprite,
+        })
+    end
+end)
