d18537c1f625a5f5c5a47e79775177df2a5e6623
Valentin <valentin@waldeck.dev>
2022-06-05 11:49:20 +0200

    Add Waste production
    Former-commit-id: 2b966066488d6f7ab4d481437f62a360363add3e

--- resources/[soz]/soz-upw/server/facilities/plant.lua
+++ resources/[soz]/soz-upw/server/facilities/plant.lua
@@ -25,19 +25,54 @@ function Plant:Save()
 end
 
 --
--- PRODUCTION
+-- ENERGY PRODUCTION
 --
 function Plant:CanProduce()
     return self.active and self.capacity < self.maxCapacity
 end
 
 function Plant:Produce()
+    -- Produce energy
     local prod = self.productionPerMinute
+
     if type(self.productionPerMinute) == "table" then
         prod = math.random(self.productionPerMinute.min, self.productionPerMinute.max)
     end
 
     self.capacity = self.capacity + prod
 
+    -- Produce waste
+    local wasteMulitplier = self.GetWasteMultiplier(self.waste)
+    self:ProduceWaste(wasteMulitplier)
+
     return prod
+end
+
+--
+-- WASTE PRODUCTION
+--
+function Plant:GetWasteMultiplier(w)
+    local waste = (w or self.waste) / self.maxWaste
+
+    for wasteMultiplier, wasteRange in pairs(self.Config.WasteMultiplier) do
+        if wasteRange.min == nil and waste < wasteRange.max then
+            return wasteMultiplier
+
+        elseif wasteRange.max == nil and waste >= wasteRange.min then
+            return wasteMultiplier
+
+        elseif waste >= wasteRange.min and waste < wasteRange.max then
+            return wasteMultiplier
+        end
+    end
+end
+
+function Plant:ProduceWaste(wasteMultiplier)
+    local waste = self.wastePerMinute
+
+    if type(self.wastePerMinute) == "table" then
+        waste = math.random(self.wastePerMinute.min, self.wastePerMinute.max)
+    end
+
+    self.waste = self.waste + waste * wasteMultiplier
 end
