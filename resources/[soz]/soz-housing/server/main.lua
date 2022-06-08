f750e2931e09b966d0ebbd4d26eb067167951ddf
Frozennide <sebastien@sdeu.fr>
2022-06-09 00:42:02 +0200

    feat(housing): add cloakroom in house
    Former-commit-id: 1d4a78cbba3cbacbe6864a3ac40ba47d72a5a803

--- resources/[soz]/soz-housing/server/main.lua
+++ resources/[soz]/soz-housing/server/main.lua
@@ -15,6 +15,7 @@ AddEventHandler("soz-housing:server:SetZone", function()
     TriggerClientEvent("soz-housing:client:SetEntry", Player.PlayerData.source, GlobalZone)
     TriggerClientEvent("soz-housing:client:SetExit", Player.PlayerData.source, GlobalZone)
     TriggerClientEvent("soz-housing:client:SetStorage", Player.PlayerData.source, GlobalZone)
+    TriggerClientEvent("soz-housing:client:SetCloakroom", Player.PlayerData.source, GlobalZone)
 end)
 
 RegisterNetEvent("soz-housing:server:isOwned")
