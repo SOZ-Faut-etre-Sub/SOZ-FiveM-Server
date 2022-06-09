d10384d72446f0a9cea43aa6d1a1731b5ef83f37
Valentin <valentin@waldeck.dev>
2022-06-09 22:16:00 +0200

    Add energy harvest logic
    Former-commit-id: 6269085218b0a00a272616b5c33e681f561ecdb8

--- resources/[soz]/soz-upw/server/production.lua
+++ resources/[soz]/soz-upw/server/production.lua
@@ -51,3 +51,51 @@ RegisterNetEvent("soz-upw:server:TooglePlantActive", function()
         plant:ToogleActive()
     end
 end)
+
+local function GetItem(identifier, type)
+    if type == "energy" then
+        return Config.Plants[identifier].items.energy
+    elseif type == "waste" then
+        return Config.Plants[identifier].items.energy
+    end
+
+    return nil
+end
+
+QBCore.Functions.CreateCallback("soz-upw:server:PrecheckHarvest", function(source, cb, identifier)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    local item = GetItem(identifier, "energy")
+    if not item then
+        cb(false, "invalid item")
+        return
+    end
+
+    local canCarry = exports["soz-inventory"]:CanCarryItem(Player.PlayerData.source, item, 1)
+    if not canCarry then
+        cb(false, "Vos poches sont pleines...")
+        return
+    end
+
+    local plant = GetPlant(identifier)
+
+    if plant.capacity < Config.Production.EnergyPerCell then
+        cb(false, "Pénurie d'énergie")
+        return
+    end
+
+    cb(true)
+end)
+
+QBCore.Functions.CreateCallback("soz-upw:server:PrecheckHarvest", function(source, cb, identifier)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    local item = GetItem(identifier, "energy")
+    exports['soz-inventory']:AddItem(Player.PlayerData.source, item, 1)
+
+    local plant = GetPlant(identifier)
+
+    plant.capacity = plant.capacity - Config.Production.EnergyPerCell
+
+    cb(true)
+end)
