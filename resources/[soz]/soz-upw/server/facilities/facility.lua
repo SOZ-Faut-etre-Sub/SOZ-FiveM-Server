a4c442d9561d5b90b7a5a257dd19818aaa7f0e95
Valentin <valentin@waldeck.dev>
2022-06-09 07:04:19 +0200

    Revert "Revert "Merge branch 'feat/dlc1-upw' into 'main'""
    This reverts commit 27b1c72be41da219ddcabd21b6d6007a0dc5fff9 [formerly e17247f8be434137933b150a00b394474487e4bb].
    
    
    Former-commit-id: 16dd56db52e5257f364e8dddb75daf06af263cb6

---
+++ resources/[soz]/soz-upw/server/facilities/facility.lua
@@ -0,0 +1,82 @@
+Facility = InheritsFrom(nil)
+
+function Facility:new(identifier, data)
+    local self = setmetatable({}, {__index = Facility})
+
+    self.identifier = identifier
+    if type(identifier) ~= "string" and #identifier == 0 then
+        error("'identifier' should be a string")
+    end
+
+    self:init(identifier, data)
+
+    if not self.type then
+        error("Type of Facility (self.type) is required")
+    end
+
+    return self
+end
+
+function Facility:init(identifier, data)
+    local res = self:select(identifier)
+    if res then
+        self:load(res.data)
+    else
+        self:insert(data)
+    end
+end
+
+function Facility:load(data)
+    if type(data) == "string" then
+        data = json.decode(data)
+    end
+
+    for key, value in pairs(data) do
+        self[key] = value
+    end
+end
+
+function Facility:select(identifier)
+    return MySQL.Sync.fetchSingle("SELECT * FROM upw_facility WHERE identifier = @identifier", {
+        ["@identifier"] = identifier,
+    })
+end
+
+function Facility:insert(data)
+    local res = MySQL.Sync.execute("INSERT INTO upw_facility (type, identifier, data) VALUES(@type, @identifier, @data)",
+                                   {
+        ["@type"] = data.type,
+        ["@identifier"] = self.identifier,
+        ["@data"] = json.encode(data),
+    })
+
+    if res then
+        local facility = self:select(self.identifier)
+        self:load(facility.data)
+    end
+end
+
+function Facility:get_data()
+    if not self.fields_to_save then
+        error("self.fields_to_save is not defined")
+    end
+
+    local data = {}
+
+    for _, field in ipairs(self.fields_to_save) do
+        data[field] = self[field]
+    end
+
+    return data
+end
+
+function Facility:save()
+    local data = self:get_data()
+
+    local res = MySQL.Sync.execute("UPDATE upw_facility SET `data` = @data WHERE identifier = @identifier",
+                                   {["@identifier"] = self.identifier, ["@data"] = json.encode(data)})
+
+    if res == 1 then
+        self:load(data)
+    end
+end
--- resources/[soz]/soz-upw/server/pollution.lua
+++ resources/[soz]/soz-upw/server/pollution.lua
@@ -1,16 +1,3 @@
-local function GetPollutionLevel()
-    local currentPollution = 50 -- TEMP set to Neutral level for now
-    if currentPollution >= 100 then
-        return QBCore.Shared.Pollution.Level.High
-    end
-
-    for level, range in pairs(QBCore.Shared.Pollution.Threshold) do
-        if currentPollution >= range.min and currentPollution < range.max then
-            return level
-        end
-    end
-end
-
 QBCore.Functions.CreateCallback("soz-upw:server:GetPollutionLevel", function(source, cb)
-    return cb(GetPollutionLevel())
+    return cb(Pm:GetPollutionLevel())
 end)
