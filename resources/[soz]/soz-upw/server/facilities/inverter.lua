90f533e171868fbe38c1a139842fbd7251eeba39
Valentin <valentin@waldeck.dev>
2022-06-11 09:36:33 +0200

    Add Inverter class
    Former-commit-id: b27eb0ece40b3bafb8e106900afae9bbe9c20454

---
+++ resources/[soz]/soz-upw/server/facilities/inverter.lua
@@ -0,0 +1,27 @@
+Inverter = InheritFrom(Facility)
+
+function Inverter:new(identifier, options)
+    options.type = "inverter"
+
+    local self = Inverter:Super():new(identifier, options)
+
+    self.fields_to_save = {"type", "capacity", "maxCapacity"}
+
+    setmetatable(self, {__index = Inverter})
+
+    return self
+end
+
+function Inverter:CanStoreEnergy()
+    return self.capacity < self.maxCapacity
+end
+
+function Inverter:StoreEnergy()
+    self.capacity = self.capacity + Config.Production.EnergyPerCell
+
+    if self.capacity > self.maxCapacity then
+        self.capacity = self.maxCapacity
+    end
+
+    return self.capacity
+end
