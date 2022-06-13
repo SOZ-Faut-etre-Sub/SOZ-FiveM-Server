be906708386b422f3e297a795adc5f254d5cbe4e
Valentin <valentin@waldeck.dev>
2022-06-13 22:04:05 +0200

    Add terminal class
    Former-commit-id: 0f21b05c4ab01423fcdc5a3c6734ac8be633c443

---
+++ resources/[soz]/soz-upw/server/facilities/terminal.lua
@@ -0,0 +1,13 @@
+Terminal = InheritsFrom(Facility)
+
+function Terminal:new(identifier, options)
+    options.type = "terminal"
+
+    local self = Terminal:Super():new(identifier, options)
+
+    self.fields_to_save = {"type", "scope", "capacity", "maxCapacity"}
+
+    setmetatable(self, {__index = Terminal})
+
+    return self
+end
