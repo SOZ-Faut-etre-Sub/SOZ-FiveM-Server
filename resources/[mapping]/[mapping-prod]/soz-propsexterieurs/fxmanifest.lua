9289e30de1ba676dc1aa75c0f3eac568dcbfd333
blaqqendwhyt <57453491+blaqqendwhyt@users.noreply.github.com>
2022-06-13 12:31:42 +0200

    Trash Bin
    Former-commit-id: 3f44a2baa1109b2469d1ebe6b0204051f5ae23b9

--- resources/[mapping]/[mapping-prod]/soz-propsexterieurs/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-propsexterieurs/fxmanifest.lua
@@ -2,10 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'
 
-files {"interiorproxies.meta"}
 
 data_file 'DLC_ITYP_REQUEST' 'stream/prop_z_park.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_prop_radar.ytyp'
 data_file 'DLC_ITYP_REQUEST' "stream/soz_prop_paystation.ytyp"
 data_file 'DLC_ITYP_REQUEST' "stream/ytyp/barrier.ytyp"
-data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+data_file 'DLC_ITYP_REQUEST' "stream/soz_prop_bb_bin.ytyp"
