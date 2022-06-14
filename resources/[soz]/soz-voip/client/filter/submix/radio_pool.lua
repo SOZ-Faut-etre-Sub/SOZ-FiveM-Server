08593f3555ddb4cfcf2d26890e8ccd1265bd071f
Joel Wurtz <joel.wurtz@gmail.com>
2022-06-15 00:12:25 +0200

    fix(voip): remove useless effect, disable radio if no used, limit pool to working effects
    Former-commit-id: a9da41b683d88932e1f7a7055424b07356ea4e17

--- resources/[soz]/soz-voip/client/filter/submix/radio_pool.lua
+++ resources/[soz]/soz-voip/client/filter/submix/radio_pool.lua
@@ -3,7 +3,16 @@ RadioPoolSubmix = {}
 function RadioPoolSubmix:new(factory, limit)
     self.__index = self
 
-    return setmetatable({factory = factory, limit = limit, available = {}, used = {}}, self)
+    return setmetatable({factory = factory, max = limit, limit = limit, available = {}, used = {}}, self)
+end
+
+function RadioPoolSubmix:init()
+    while self.limit > 0 do
+        local submix = self.factory(self.max - self.limit)
+        table.insert(self.available, submix)
+        self.limit = self.limit - 1
+        Wait(100)
+    end
 end
 
 function RadioPoolSubmix:acquire(serverId)
@@ -17,6 +26,7 @@ function RadioPoolSubmix:acquire(serverId)
     end
 
     if self.limit <= 0 then
+        print("no more radio submixes available")
         return nil
     end
 
@@ -34,6 +44,6 @@ function RadioPoolSubmix:release(serverId)
         return
     end
 
-    table.insert(self.available, self.used[id])
+    table.insert(self.available, 1, self.used[id])
     self.used[id] = nil
 end
