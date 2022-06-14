27899aa75214383f22ba4ad0c6c3d562f69e10be
Valentin <valentin@waldeck.dev>
2022-06-14 21:36:28 +0200

    Add storage interaction
    Former-commit-id: 40f6749b6bd1db8abee16267bb76119da2e74523

--- resources/[soz]/soz-upw/server/storage.lua
+++ resources/[soz]/soz-upw/server/storage.lua
@@ -1,11 +1,14 @@
---
--- INVERTERS
---
-RegisterNetEvent("soz-upw:client:InverterCapacity", function(data)
-    local inverter = GetInverter(data.identifier)
+local facilities = {["inverter"] = GetInverter, ["terminal"] = GetTerminal}
 
-    if inverter then
+RegisterNetEvent("soz-upw:server:FacilityCapacity", function(data)
+    local getter = facilities[data.facility]
+    if not getter then
+        return
+    end
+
+    local facility = getter(data.identifier)
+    if facility then
         TriggerClientEvent("hud:client:DrawNotification", source,
-                           string.format("Remplissage : %s%%", math.floor(inverter.capacity / inverter.maxCapacity * 100)))
+                           string.format("Remplissage : %s%%", math.floor(facility.capacity / facility.maxCapacity * 100)))
     end
 end)
