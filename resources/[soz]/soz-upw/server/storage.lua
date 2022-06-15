db1a785c50ad7df6ba3d2bd9e0515f68968e73ce
Valentin <valentin@waldeck.dev>
2022-06-15 21:50:04 +0200

    Add callback to get blackout level
    Former-commit-id: a92ea6dac0063b2e978838adbddf0ecd3dd0529f

--- resources/[soz]/soz-upw/server/storage.lua
+++ resources/[soz]/soz-upw/server/storage.lua
@@ -12,3 +12,34 @@ RegisterNetEvent("soz-upw:server:FacilityCapacity", function(data)
                            string.format("Remplissage : %s%%", math.floor(facility.capacity / facility.maxCapacity * 100)))
     end
 end)
+
+local function GetTerminalCapacities(scope)
+    if not scope then
+        scope = "default"
+    end
+
+    local capacity, maxCapacity = 0, 0
+
+    for _, terminal in pairs(Terminals) do
+        if terminal.scope == scope then
+            capacity = capacity + terminal.capacity
+            maxCapacity = maxCapacity + terminal.maxCapacity
+        end
+    end
+end
+
+QBCore.Functions.CreateCallback("soz-upw:server:GetBlackoutLevel", function(source, cb)
+    local capacity, maxCapacity = GetTerminalCapacities("default")
+    local percent = math.ceil(capacity / maxCapacity * 100)
+
+    if percent >= 100 then
+        cb(QBCore.Shared.Blackout.Level.Zero)
+        return
+    end
+
+    for level, range in pairs(Config.Blackout.Threshold) do
+        if percent >= range.min and percent < range.max then
+            cb(level)
+        end
+    end
+end)
