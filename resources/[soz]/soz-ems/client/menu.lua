ed63eb5d72bab4c5f3bb608747d45065e857b7d8
Lucas Perreau <lucasperreau@gmail.com>
2022-06-16 23:39:02 +0200

    refacto: First check for closing before calling the cb

--- resources/[soz]/soz-ems/client/menu.lua
+++ resources/[soz]/soz-ems/client/menu.lua
@@ -30,8 +30,6 @@ EmsJob.Functions.Menu.GenerateMenu = function(job, cb)
     local menu = EmsJob.Menus[job].menu
     menu:ClearItems()
 
-    cb(menu)
-
     if menu.IsOpen then
         MenuV:CloseAll(function()
             menu:Close()
@@ -41,6 +39,8 @@ EmsJob.Functions.Menu.GenerateMenu = function(job, cb)
             menu:Open()
         end)
     end
+
+    cb(menu)
 end
 
 EmsJob.Functions.Menu.GenerateJobMenu = function(job)
