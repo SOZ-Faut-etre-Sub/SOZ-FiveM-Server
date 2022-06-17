854fda97ab26ca15ee5dc6f05c5dea5cce175a65
rigonkmalk <rigonkmalk@gmail.com>
2022-06-17 19:36:29 +0200

    perf(northforest): update 3.2 to 3.3 North Forest

--- resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
@@ -6,8 +6,7 @@ game "gta5"
 
 author 'Larcius'
 description 'Forests of San Andreas: Revised'
-version '3.2'
+version '3.3'
 
-data_file('DLC_ITYP_REQUEST')('stream/forest_lod.ytyp')
-data_file('DLC_ITYP_REQUEST')('stream/forest_n_slod.ytyp')
-data_file('DLC_ITYP_REQUEST')('stream/forest_s_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/ForestsOfSA_N/forest_n_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/ForestsOfSA_S/forest_s_slod.ytyp')
