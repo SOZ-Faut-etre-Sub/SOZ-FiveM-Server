d9fc9064e38462f7dc1ce0b9de4d44c4cfdd039d
Lanahrone <lanahrone@gmail.com>
2022-06-18 09:10:11 +0200

    Make sure key matches model name

--- resources/[qb]/qb-core/shared/vehicles.lua
+++ resources/[qb]/qb-core/shared/vehicles.lua
@@ -763,7 +763,7 @@ QBShared.Vehicles = {
         ['hash']     = GetHashKey('ruffian'),
         ['shop']     = 'pdm',
     },
-    ['sanchez01']      = {
+    ['sanchez']      = {
         ['name']     = 'Sanchez Livery',
         ['brand']    = 'Maibatsu',
         ['model']    = 'sanchez',
@@ -772,7 +772,7 @@ QBShared.Vehicles = {
         ['hash']     = GetHashKey('sanchez'),
         ['shop']     = 'pdm',
     },
-    ['sanchez02']     = {
+    ['sanchez2']     = {
         ['name']     = 'Sanchez',
         ['brand']    = 'Maibatsu',
         ['model']    = 'sanchez2',
