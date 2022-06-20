4c090562d781bd21688bcae86141a546f2ec535c
Frozennide <sebastien@sdeu.fr>
2022-06-20 22:11:11 +0200

    feat(pawl): add crafting

--- resources/[soz]/soz-pawl/client/main.lua
+++ resources/[soz]/soz-pawl/client/main.lua
@@ -40,7 +40,7 @@ Citizen.CreateThread(function()
         heading = 72,
         minZ = 73.74,
         maxZ = 76.54,
-        debugPoly = true,
+        debugPoly = false,
     }, {
         options = {
             {
@@ -48,8 +48,43 @@ Citizen.CreateThread(function()
                 label = "Transformer",
                 icon = "c:inventory/ouvrir_le_stockage.png",
                 event = "pawl:server:processingTree",
+                canInteract = function()
+                    return PlayerData.job.onduty
+                end,
             },
         },
         distance = 2.5,
     })
+
+    -- Craft
+    local craftOptions = {}
+    for craftId, craft in pairs(Config.Craft) do
+        craftOptions[#craftOptions + 1] = {
+            label = craft.Name,
+            icon = "c:inventory/ouvrir_le_stockage.png",
+            event = "pawl:client:craft",
+            canInteract = function()
+                return PlayerData.job.onduty
+            end,
+            identifier = craftId,
+        }
+    end
+
+    exports["qb-target"]:RemoveZone("pawl:crafting")
+    exports["qb-target"]:AddBoxZone("pawl:crafting", vector3(-533.23, 5293.28, 74.2), 1.8, 1.0,
+                                    {name = "pawl:crafting", heading = 0, minZ = 73.2, maxZ = 76.0, debugPoly = false}, {
+        options = craftOptions,
+        distance = 2.5,
+    })
+end)
+
+RegisterNetEvent("pawl:client:craft", function(data)
+    local success, _ = exports["soz-utils"]:Progressbar("craft", "", Config.CraftDuration, false, true, {
+        disableMovement = true,
+        disableCombat = true,
+    }, {}, {}, {})
+
+    if success then
+        TriggerServerEvent("pawl:server:craft", data.identifier)
+    end
 end)
