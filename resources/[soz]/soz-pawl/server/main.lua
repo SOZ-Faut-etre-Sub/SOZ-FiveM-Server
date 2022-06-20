e11a7045e0fff4bb2a7cfd58483f0c6299e3e963
Frozennide <sebastien@sdeu.fr>
2022-06-20 22:11:11 +0200

    feat(pawl): add crafting

--- resources/[soz]/soz-pawl/server/main.lua
+++ resources/[soz]/soz-pawl/server/main.lua
@@ -96,3 +96,69 @@ RegisterNetEvent("pawl:server:processingTree", function(data)
         end
     end)
 end)
+
+--- Crafting
+local function pairsByKeys(t)
+    local a = {}
+    for n in pairs(t) do
+        table.insert(a, n)
+    end
+    table.sort(a)
+    local i = 0
+    local iter = function()
+        i = i + 1
+        if a[i] == nil then
+            return nil
+        else
+            return a[i], t[a[i]]
+        end
+    end
+    return iter
+end
+
+RegisterNetEvent("pawl:server:craft", function(identifier)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player == nil then
+        return
+    end
+
+    local craft = Config.Craft[identifier]
+    if craft == nil then
+        return
+    end
+
+    if not exports["soz-inventory"]:CanCarryItem(Player.PlayerData.source, craft.RewardItem, craft.RewardAmount) then
+        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Vous ne pouvez pas fabriquer d'objet en ce moment.", "error")
+        return
+    end
+
+    local craftItemInventory = exports["soz-inventory"]:GetItem(Config.CraftStorage, craft.SourceItem, nil, true)
+
+    if craftItemInventory < (craft.SourceItemAmount or 1) then
+        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Le stockage n'a pas assez de planche !", "error")
+        return
+    end
+
+    local metadata = {}
+    if craft.RewardTier then
+        local random = math.random(1, 100)
+
+        for tierId, tier in pairsByKeys(craft.RewardTier) do
+            if random >= tier.Chance then
+                metadata.tier = tierId
+                metadata.label = tier.Name
+                break
+            end
+        end
+    end
+
+    if exports["soz-inventory"]:RemoveItem(Config.CraftStorage, craft.SourceItem, craft.SourceItemAmount or 1) then
+        exports["soz-inventory"]:AddItem(Player.PlayerData.source, craft.RewardItem, craft.RewardAmount, metadata, nil, function(success, reason)
+            if success then
+                TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Vous avez récupéré ~g~" .. craft.Name .. "~s~ !", "success")
+            else
+                TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Vous ne pouvez pas récupérer d'objet !", "error")
+            end
+        end)
+    end
+end)
