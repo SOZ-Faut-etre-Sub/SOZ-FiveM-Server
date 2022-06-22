00473e93c26ef26acca19905aca4fb453ba0efdf
Lucas Perreau <Mcfloy@users.noreply.github.com>
2022-06-22 09:35:15 +0200

    Update gta-server/resources/[soz]/mysql-migration/migrations/2022-06-22-06-53-add-inpound-counter.lua
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- resources/[soz]/mysql-migration/migrations/2022-06-22-06-53-add-inpound-counter.lua
+++ resources/[soz]/mysql-migration/migrations/2022-06-22-06-53-add-inpound-counter.lua
@@ -3,6 +3,6 @@ table.insert(migrations, {
     queries = {
         [[
             ALTER TABLE `player_vehicles` ADD COLUMN `life_counter` INT(1) NOT NULL DEFAULT 4 AFTER state;
-        ]]
-    }
+        ]],
+    },
 })
