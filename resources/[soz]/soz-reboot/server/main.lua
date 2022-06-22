05f4c08c1ffad1d56e366e5c823569b3049933d3
Lucas Perreau <lucasperreau@gmail.com>
2022-06-22 10:59:34 +0200

    chore: use the typo fixer

--- resources/[soz]/soz-reboot/server/main.lua
+++ resources/[soz]/soz-reboot/server/main.lua
@@ -26,10 +26,13 @@ local reboot = function()
 
     local parkingtime = os.time()
     print("[soz-reboot] Mise des véhicules à la fourrière")
-    MySQL.Sync.execute("UPDATE player_vehicles SET state = 2, garage = 'fourriere', parkingtime = ?, life_counter = life_counter - 1 WHERE state = 0", {parkingtime})
+    MySQL.Sync.execute("UPDATE player_vehicles SET state = 2, garage = 'fourriere', parkingtime = ?, life_counter = life_counter - 1 WHERE state = 0",
+                       {parkingtime})
 
     print("[soz-reboot] Mise des véhicules dans le void...")
-    MySQL.Sync.execute("UPDATE player_vehicles SET state = 4, parkingtime = ? WHERE life_counter = 0 AND state != 4", {parkingtime})
+    MySQL.Sync.execute("UPDATE player_vehicles SET state = 4, parkingtime = ? WHERE life_counter = 0 AND state != 4", {
+        parkingtime,
+    })
 
     print("[soz-reboot] Sauvegarde des comptes bancaires...")
     exports["soz-bank"]:saveAccounts()
