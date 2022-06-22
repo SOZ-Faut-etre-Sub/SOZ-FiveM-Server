c49613068d4bf578fc100f7b8a930292dc8e35ee
Lucas Perreau <Mcfloy@users.noreply.github.com>
2022-06-22 09:35:19 +0200

    Update gta-server/resources/[soz]/soz-reboot/server/main.lua
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- resources/[soz]/soz-reboot/server/main.lua
+++ resources/[soz]/soz-reboot/server/main.lua
@@ -26,7 +26,8 @@ local reboot = function()
 
     local parkingtime = os.time()
     print("[soz-reboot] Mise des véhicules à la fourrière")
-    MySQL.Sync.execute("UPDATE player_vehicles SET state = 2, garage = 'fourriere', parkingtime = ?, life_counter = life_counter - 1 WHERE state = 0", {parkingtime})
+    MySQL.Sync.execute("UPDATE player_vehicles SET state = 2, garage = 'fourriere', parkingtime = ?, life_counter = life_counter - 1 WHERE state = 0",
+                       {parkingtime})
 
     print("[soz-reboot] Mise des véhicules dans le void...")
     MySQL.Sync.execute("UPDATE player_vehicles SET state = 4, parkingtime = ? WHERE life_counter = 0 AND state != 4", {parkingtime})
