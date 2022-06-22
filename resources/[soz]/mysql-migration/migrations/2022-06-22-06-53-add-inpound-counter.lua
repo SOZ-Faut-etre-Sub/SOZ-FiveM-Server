16cb1cb2dbed87d798f280f43d9038b8be028941
Lucas Perreau <lucasperreau@gmail.com>
2022-06-22 09:21:05 +0200

    feat(DLC1): put the vehicles inside the depot 3 times then into the void

---
+++ resources/[soz]/mysql-migration/migrations/2022-06-22-06-53-add-inpound-counter.lua
@@ -0,0 +1,8 @@
+table.insert(migrations, {
+    name = "add-impounded-counter",
+    queries = {
+        [[
+            ALTER TABLE `player_vehicles` ADD COLUMN `life_counter` INT(1) NOT NULL DEFAULT 3 AFTER state;
+        ]]
+    }
+})
