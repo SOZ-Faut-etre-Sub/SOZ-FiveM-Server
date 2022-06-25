7cf9dfb10e1786f4d7fd6150e841c3346e98c13d
Lucas Perreau <lucasperreau@gmail.com>
2022-06-25 07:53:43 +0200

    add citizenid in parameter

--- resources/[soz]/soz-housing/client/components/property.lua
+++ resources/[soz]/soz-housing/client/components/property.lua
@@ -37,7 +37,7 @@ Housing.Functions.Components.SetupEntryInteraction = function(propertyId, proper
             label = "Sonner",
             icon = "c:housing/bell.png",
             canInteract = function()
-                return property:HasRentedApartmentBesidesForCitizenId() and not Housing.Functions.IsInsideApartment()
+                return property:HasRentedApartmentBesidesForCitizenId(PlayerData.citizenid) and not Housing.Functions.IsInsideApartment()
             end,
             action = function()
                 TriggerEvent("housing:client:ShowBellMenu", propertyId)
