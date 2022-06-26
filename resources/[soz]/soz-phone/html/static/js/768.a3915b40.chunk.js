a20951f526803b223ddbce83393d08d1444030d0
Frozennide <sebastien@sdeu.fr>
2022-06-27 00:06:24 +0200

    fix(phone): keep phone closed on notification when player is in water

---
+++ resources/[soz]/soz-phone/html/static/js/768.a3915b40.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknpwd_nui=self.webpackChunknpwd_nui||[]).push([[768],{4768:function(n,u,e){e.r(u);e(776);var i=e(5430);u.default=function(n){return(0,i.jsx)("div",{})}}}]);
