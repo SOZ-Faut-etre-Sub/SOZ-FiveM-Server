db9124815b5a713729894ad2646672576460f8fb
Lanahrone <lanahrone@gmail.com>
2022-06-27 21:02:58 +0200

    Remove previous logs

--- resources/[soz]/soz-upw/server/storage.lua
+++ resources/[soz]/soz-upw/server/storage.lua
@@ -95,7 +95,6 @@ function StartConsumptionLoop()
             end
 
             local newBlackoutLevel = GetBlackoutLevel()
-            exports["soz-monitor"]:Log("INFO", "Blackout level updated: " .. newBlackoutLevel)
 
             -- Blackout level has changed
             if currentBlackoutLevel ~= newBlackoutLevel then
