e8fffd32e3f87f8e7fdaec7c036b0bc16bf20d5d
Frozennide <sebastien@sdeu.fr>
2022-07-03 19:07:53 +0200

    chore: lint files

--- resources/[soz]/soz-jobs/config/jobs/pawl.lua
+++ resources/[soz]/soz-jobs/config/jobs/pawl.lua
@@ -1,5 +1,3 @@
 PawlConfig = {}
 
-PawlConfig.BossShop = {
-    [1] = {name = "weapon_hatchet", metadata = {}, price = 100, amount = 1},
-}
+PawlConfig.BossShop = {[1] = {name = "weapon_hatchet", metadata = {}, price = 100, amount = 1}}
