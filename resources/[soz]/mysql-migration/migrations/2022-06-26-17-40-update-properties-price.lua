f2de2fb646e2501c61405ea431848dd3b0a578ae
Lucas Perreau <lucasperreau@gmail.com>
2022-07-04 06:36:34 +0200

    fix: use update key correctly in migration

--- resources/[soz]/mysql-migration/migrations/2022-06-26-17-40-update-properties-price.lua
+++ resources/[soz]/mysql-migration/migrations/2022-06-26-17-40-update-properties-price.lua
@@ -2,16 +2,16 @@ table.insert(migrations, {
     name = "update-properties-price",
     queries = {
         [[
-            alter table housing_apartment set price = 72000 where identifier LIKE 'south_house_low%';
+            UPDATE housing_apartment SET price = 72000 WHERE identifier LIKE 'south_house_low%';
         ]],
         [[
-            alter table housing_apartment set price = 90000 where identifier LIKE 'south_house_mid%';
+            UPDATE housing_apartment SET price = 90000 WHERE identifier LIKE 'south_house_mid%';
         ]],
         [[
-            alter table housing_apartment set price = 116000 where identifier LIKE 'soz_appartements%';
+            UPDATE housing_apartment SET price = 116000 WHERE identifier LIKE 'soz_appartements%';
         ]],
         [[
-            alter table housing_apartment set price = 400000 where identifier LIKE 'soz_villa%';
+            UPDATE housing_apartment SET price = 400000 WHERE identifier LIKE 'soz_villa%';
         ]],
     },
 })
