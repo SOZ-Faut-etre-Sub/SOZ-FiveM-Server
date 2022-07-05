d391f443eeb375af215c86e79b5ebec29e73c16c
blaqqendwhyt <57453491+blaqqendwhyt@users.noreply.github.com>
2022-07-06 01:45:21 +0200

    fix 2

--- resources/[mapping]/[mapping-prod]/soz-lsmc/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-lsmc/fxmanifest.lua
@@ -5,10 +5,8 @@ this_is_a_map 'yes'
 
 files {
 
-    "interiorproxies.meta",
-	"strawberry.ymt"
+    "interiorproxies.meta"
 	
 }
 
-data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "strawberry.ymt"
 data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
