9e3c7179d27fa5732501b83dd8e890b88ec70d78
Lucas Perreau <lucasperreau@gmail.com>
2022-07-07 17:58:25 +0200

    feat: move upw parking

--- resources/[mapping]/[mapping-prod]/soz-map-upw/stream/client.lua
+++
@@ -1,7 +0,0 @@
-Citizen.CreateThread(function()
-    while (true) do
-        ClearAreaOfPeds(646.7, 2752.76, 41.86, 10.0, 1)
-        ClearAreaOfPeds(610.31, 2745.79, 42.39, 10.0, 1)
-        Citizen.Wait(100)
-    end
-end)
