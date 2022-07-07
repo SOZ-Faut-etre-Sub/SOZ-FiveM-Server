29f27659b3645597dbae0c2d5eb94b4c90d70873
Lucas Perreau <lucasperreau@gmail.com>
2022-07-07 13:33:19 +0200

    change price

--- resources/[soz]/mysql-migration/migrations/2022-07-07-12-24-add-upw-vehicles.lua
+++ resources/[soz]/mysql-migration/migrations/2022-07-07-12-24-add-upw-vehicles.lua
@@ -4,7 +4,7 @@ table.insert(migrations, {
         [[
             INSERT INTO concess_entreprise (job, vehicle, price, liverytype) VALUES
                 ('upw', 'brickade1', 35000, 1),
-                ('upw', 'boxville', 25000, 1)
+                ('upw', 'boxville', 24000, 1)
             ;
         ]],
         [[
