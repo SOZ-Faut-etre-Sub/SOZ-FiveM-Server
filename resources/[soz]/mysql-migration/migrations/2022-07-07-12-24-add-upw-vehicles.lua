55be9d797c5e953ce07999e9f10060e1c7075b27
Lucas Perreau <lucasperreau@gmail.com>
2022-07-07 13:19:37 +0200

    feat: add upw vehicles in job dealership

---
+++ resources/[soz]/mysql-migration/migrations/2022-07-07-12-24-add-upw-vehicles.lua
@@ -0,0 +1,18 @@
+table.insert(migrations, {
+    name = "add-upw-vehicles",
+    queries = {
+        [[
+            INSERT INTO concess_entreprise (job, vehicle, price, liverytype) VALUES
+                ('upw', 'brickade1', 35000, 1),
+                ('upw', 'boxville', 25000, 1)
+            ;
+        ]],
+        [[
+            UPDATE concess_entreprise SET vehicle = 'sadler1' WHERE vehicle = 'sadler' AND job = 'pawl';
+        ]],
+        [[
+            UPDATE concess_entreprise SET vehicle = 'hauler1' WHERE vehicle = 'hauler' AND job = 'pawl';
+        ]],
+
+    },
+})
