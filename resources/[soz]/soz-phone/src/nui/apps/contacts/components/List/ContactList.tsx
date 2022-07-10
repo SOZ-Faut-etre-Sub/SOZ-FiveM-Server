600c543598c69fb01565a692d322da2fe7633447
Frozennide <sebastien@sdeu.fr>
2022-07-10 18:05:03 +0200

    fix(phone): set context menu to position absolute

--- resources/[soz]/soz-phone/src/nui/apps/contacts/components/List/ContactList.tsx
+++ resources/[soz]/soz-phone/src/nui/apps/contacts/components/List/ContactList.tsx
@@ -99,8 +99,9 @@ export const ContactList: React.FC = () => {
                                             leave="transition duration-75 ease-out"
                                             leaveFrom="transform scale-100 opacity-100"
                                             leaveTo="transform scale-95 opacity-0"
+                                            className="absolute z-50 right-0 w-56"
                                         >
-                                            <Menu.Items className="absolute z-30 right-0 w-56 mt-2 origin-top-right bg-black bg-opacity-70 divide-y divide-gray-600 divide-opacity-50 rounded-md shadow-lg focus:outline-none">
+                                            <Menu.Items className="mt-2 origin-top-right bg-black bg-opacity-70 divide-y divide-gray-600 divide-opacity-50 rounded-md shadow-lg focus:outline-none">
                                                 <Menu.Item>
                                                     <Button
                                                         className="flex items-center w-full text-white px-2 py-2 hover:text-gray-300"
