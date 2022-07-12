eeeb7982f7c6de25bf5459575e2bdf50e710cd7f
Lucas Perreau <Mcfloy@users.noreply.github.com>
2022-07-12 20:06:03 +0200

    Update gta-server/resources/[soz]/mysql-migration/migrations/2022-07-12-18-33-move-bin-prop.lua
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- resources/[soz]/mysql-migration/migrations/2022-07-12-18-33-move-bin-prop.lua
+++ resources/[soz]/mysql-migration/migrations/2022-07-12-18-33-move-bin-prop.lua
@@ -3,6 +3,6 @@ table.insert(migrations, {
     queries = {
         [[
             UPDATE persistent_prop SET position = '{"x":193.33306884765626,"y":3041.50732421875,"z":42.87635040283203,"w":0.0}' WHERE id = 50;
-        ]]
+        ]],
     },
 });
