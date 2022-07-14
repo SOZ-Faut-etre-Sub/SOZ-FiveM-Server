66ef5454c3a8a11088f74670884661cb527cf01c
Lucas Perreau <lucasperreau@gmail.com>
2022-07-14 15:37:58 +0200

    feat: rework dealership

--- resources/[soz]/soz-vehicle/fxmanifest.lua
+++ resources/[soz]/soz-vehicle/fxmanifest.lua
@@ -3,13 +3,20 @@ games {"gta5"}
 
 shared_scripts {
     "config.lua",
+    "config/*.lua",
     "shared/utils.lua",
     "shared/fuel-station.lua",
     "@qb-core/shared/locale.lua",
     "locales/fr.lua",
 }
 
-client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@menuv/menuv.lua", "client/*.lua"}
+client_scripts {
+    "@PolyZone/client.lua",
+    "@PolyZone/BoxZone.lua",
+    "@menuv/menuv.lua",
+    "client/dealership/*.lua",
+    "client/*.lua"
+}
 
 server_scripts {"@oxmysql/lib/MySQL.lua", "server/*.lua"}
 
