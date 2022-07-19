7a91e27116d0ca2549e68d657c8e26bb39cbd633
Lucas Perreau <lucasperreau@gmail.com>
2022-07-19 11:25:30 +0200

    fix helmet

--- resources/[soz]/soz-character/client/skin/apply.lua
+++ resources/[soz]/soz-character/client/skin/apply.lua
@@ -126,10 +126,13 @@ local function ApplyPedClothSet(ped, clothSet)
 
     for _, propId in pairs(PropType) do
         local prop = clothSet.Props[tostring(propId)]
-        if prop == nil or prop.Clear == true then
-            ClearPedProp(ped, tonumber(propId))
-        elseif tonumber(propId) ~= nil then
-            SetPedPropIndex(ped, tonumber(propId), prop.Drawable, prop.Texture or 0, prop.Palette or 0)
+        if tonumber(propId) ~= nil then
+            if prop == nil or prop.Clear == true then
+                ClearPedProp(ped, tonumber(propId))
+            else
+                SetPedPropIndex(ped, tonumber(propId), prop.Drawable, prop.Texture or 0, prop.Palette or 0)
+            end
+
         end
     end
 end
