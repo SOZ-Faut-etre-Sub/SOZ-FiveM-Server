8fd5437d72e74308a88a76ae899d76c2a3402323
Lucas Perreau <lucasperreau@gmail.com>
2022-07-19 10:01:21 +0200

    handle the case where you leave a vehicle

--- resources/[soz]/soz-vehicle/client/keep_state.lua
+++ resources/[soz]/soz-vehicle/client/keep_state.lua
@@ -15,19 +15,54 @@ CreateThread(function()
     end
 end)
 
+local lastVehicle = nil
+
+local function ShouldThisModelBeTagged(vehicle)
+    local hash = GetEntityModel(vehicle)
+    return IsThisModelABicycle(hash) or IsThisModelABike(hash) or IsThisModelACar(hash) or IsThisModelAHeli(hash) or IsThisModelAPlane(hash) or
+               IsThisModelAQuadbike(hash) or IsThisModelATrain(hash)
+end
+
+local function CheckIsVehicleDead(vehicle)
+    if ShouldThisModelBeTagged(vehicle) ~= 1 then
+        return
+    end
+    local state = Entity(vehicle).state
+    if state.isDead then
+        return
+    end
+    if (state.deadCounter or 0) < 3 then
+        if IsEntityDead(vehicle) then
+            state.deadCounter = 3
+        elseif IsEntityInWater(vehicle) and not GetIsVehicleEngineRunning(vehicle) then
+            state.deadCounter = state.deadCounter + 1
+        end
+    else
+        if vehicle == lastVehicle then
+            lastVehicle = nil
+        end
+        state.isDead = true
+        TriggerServerEvent("qb-garage:server:setVehicleDestroy", GetVehicleNumberPlateText(vehicle))
+    end
+end
+
 CreateThread(function()
     while true do
         local ped = PlayerPedId()
         local vehicle = GetVehiclePedIsIn(ped, false)
-        if IsPedInAnyVehicle(ped, false) and GetPedInVehicleSeat(vehicle, -1) == ped then
-            local state = Entity(vehicle).state
-            if not state.isDead then
-                if IsEntityDead(vehicle) then
-                    state.isDead = true
-                    TriggerServerEvent("qb-garage:server:setVehicleDestroy", GetVehicleNumberPlateText(vehicle))
-                end
+        if (vehicle ~= 0 and GetPedInVehicleSeat(vehicle, -1) == ped) then
+            lastVehicle = vehicle
+            CheckIsVehicleDead(vehicle)
+        elseif lastVehicle ~= nil then
+            if not DoesEntityExist(lastVehicle) then
+                lastVehicle = nil
+            elseif GetPedInVehicleSeat(lastVehicle, -1) == 0 then
+                -- No driver in the last vehicle entered, so we keep checking the state
+                CheckIsVehicleDead(lastVehicle)
+            elseif GetPedInVehicleSeat(lastVehicle, -1) ~= ped then
+                lastVehicle = nil
             end
         end
-        Wait(100)
+        Wait(1000)
     end
 end)
