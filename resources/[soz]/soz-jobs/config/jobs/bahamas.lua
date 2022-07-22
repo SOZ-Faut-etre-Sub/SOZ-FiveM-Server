94b3fe5b25302f186cc870551d71719b614550d3
Lucas Perreau <lucasperreau@gmail.com>
2022-07-22 18:36:39 +0200

    feat: finish the recipes book

--- resources/[soz]/soz-jobs/config/jobs/bahamas.lua
+++ resources/[soz]/soz-jobs/config/jobs/bahamas.lua
@@ -12,29 +12,29 @@ BahamasConfig.Cloakroom = {
     [GetHashKey("mp_m_freemode_01")] = {
         ["Tenue de patron"] = {
             Components = {
-                [1] = {Drawable = 0, Texture = 0, Palette = 0},
-                [3] = {Drawable = 0, Texture = 0, Palette = 0},
-                [4] = {Drawable = 90, Texture = 0, Palette = 0},
-                [6] = {Drawable = 51, Texture = 0, Palette = 0},
-                [7] = {Drawable = 0, Texture = 0, Palette = 0},
-                [8] = {Drawable = 15, Texture = 0, Palette = 0},
-                [9] = {Drawable = 0, Texture = 0, Palette = 0},
-                [10] = {Drawable = 0, Texture = 0, Palette = 0},
-                [11] = {Drawable = 0, Texture = 2, Palette = 0},
+                [1] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [3] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [4] = { Drawable = 90, Texture = 0, Palette = 0 },
+                [6] = { Drawable = 51, Texture = 0, Palette = 0 },
+                [7] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [8] = { Drawable = 15, Texture = 0, Palette = 0 },
+                [9] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [10] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [11] = { Drawable = 0, Texture = 2, Palette = 0 },
             },
             Props = {},
         },
         ["Tenue de serveur"] = {
             Components = {
-                [1] = {Drawable = 0, Texture = 0, Palette = 0},
-                [3] = {Drawable = 14, Texture = 0, Palette = 0},
-                [4] = {Drawable = 35, Texture = 0, Palette = 0},
-                [6] = {Drawable = 20, Texture = 3, Palette = 0},
-                [7] = {Drawable = 0, Texture = 0, Palette = 0},
-                [8] = {Drawable = 25, Texture = 3, Palette = 0},
-                [9] = {Drawable = 0, Texture = 0, Palette = 0},
-                [10] = {Drawable = 0, Texture = 0, Palette = 0},
-                [11] = {Drawable = 4, Texture = 0, Palette = 0},
+                [1] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [3] = { Drawable = 14, Texture = 0, Palette = 0 },
+                [4] = { Drawable = 35, Texture = 0, Palette = 0 },
+                [6] = { Drawable = 20, Texture = 3, Palette = 0 },
+                [7] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [8] = { Drawable = 25, Texture = 3, Palette = 0 },
+                [9] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [10] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [11] = { Drawable = 4, Texture = 0, Palette = 0 },
             },
             Props = {},
         },
@@ -42,29 +42,29 @@ BahamasConfig.Cloakroom = {
     [GetHashKey("mp_f_freemode_01")] = {
         ["Tenue de travail"] = {
             Components = {
-                [1] = {Drawable = 0, Texture = 0, Palette = 0},
-                [3] = {Drawable = 0, Texture = 0, Palette = 0},
-                [4] = {Drawable = 93, Texture = 0, Palette = 0},
-                [6] = {Drawable = 52, Texture = 0, Palette = 0},
-                [7] = {Drawable = 0, Texture = 0, Palette = 0},
-                [8] = {Drawable = 1, Texture = 0, Palette = 0},
-                [9] = {Drawable = 0, Texture = 0, Palette = 0},
-                [10] = {Drawable = 0, Texture = 0, Palette = 0},
-                [11] = {Drawable = 73, Texture = 1, Palette = 0},
+                [1] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [3] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [4] = { Drawable = 93, Texture = 0, Palette = 0 },
+                [6] = { Drawable = 52, Texture = 0, Palette = 0 },
+                [7] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [8] = { Drawable = 1, Texture = 0, Palette = 0 },
+                [9] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [10] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [11] = { Drawable = 73, Texture = 1, Palette = 0 },
             },
             Props = {},
         },
         ["Tenue de prestation"] = {
             Components = {
-                [1] = {Drawable = 0, Texture = 0, Palette = 0},
-                [3] = {Drawable = 3, Texture = 0, Palette = 0},
-                [4] = {Drawable = 34, Texture = 0, Palette = 0},
-                [6] = {Drawable = 29, Texture = 1, Palette = 0},
-                [7] = {Drawable = 0, Texture = 0, Palette = 0},
-                [8] = {Drawable = 40, Texture = 7, Palette = 0},
-                [9] = {Drawable = 0, Texture = 0, Palette = 0},
-                [10] = {Drawable = 0, Texture = 0, Palette = 0},
-                [11] = {Drawable = 7, Texture = 0, Palette = 0},
+                [1] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [3] = { Drawable = 3, Texture = 0, Palette = 0 },
+                [4] = { Drawable = 34, Texture = 0, Palette = 0 },
+                [6] = { Drawable = 29, Texture = 1, Palette = 0 },
+                [7] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [8] = { Drawable = 40, Texture = 7, Palette = 0 },
+                [9] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [10] = { Drawable = 0, Texture = 0, Palette = 0 },
+                [11] = { Drawable = 7, Texture = 0, Palette = 0 },
             },
             Props = {},
         },
@@ -79,5 +79,14 @@ BahamasConfig.Recipes = {
         { itemId = "kurkakola", quantity = 1},
         { itemId = "lemon", quantity = 1},
         { itemId = "mint", quantity = 1 },
+    },
+    limonade = {
+        { itemId = "water_bottle", quantity = 1},
+        { itemId = "lemon", quantity = 1}
     }
+}
+
+BahamasConfig.Durations = {
+    Crafting = 5000, -- in ms
+    Harvesting = 3000, -- in ms
 }
