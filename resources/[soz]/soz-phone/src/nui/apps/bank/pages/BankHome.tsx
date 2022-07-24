ada21d155d25955ff1213f73fd6761108e244cba
Frozennide <sebastien@sdeu.fr>
2022-07-24 16:02:00 +0200

    fix(app-bank): clean icons

--- resources/[soz]/soz-phone/src/nui/apps/bank/pages/BankHome.tsx
+++ resources/[soz]/soz-phone/src/nui/apps/bank/pages/BankHome.tsx
@@ -1,3 +1,4 @@
+import { CogIcon, EyeIcon, LockClosedIcon } from '@heroicons/react/outline';
 import cn from 'classnames';
 import React, { memo, useContext } from 'react';
 import { useSelector } from 'react-redux';
@@ -7,89 +8,84 @@ import { ThemeContext } from '../../../styles/themeProvider';
 import { AppContent } from '../../../ui/components/AppContent';
 import { BankCard } from '../components/BankCard';
 import { Checkbox } from '../components/Checkbox';
-import { CogIcon } from '../components/cogIcon';
-import { EyeIcon } from '../components/eyeIcon';
-import { LockIcon } from '../components/lockIcon';
 
 export const BankHome = memo(() => {
     const credentials = useSelector((state: RootState) => state.appBank);
     const { theme } = useContext(ThemeContext);
 
-    return (
-        <>
-            {credentials ? (
-                <AppContent>
-                    <div className="m-auto pt-1 pb-3 flex flex-col w-5/6">
-                        <h2
-                            className={cn('text-3xl', {
-                                'text-red-500': credentials.balance <= 0,
-                                'text-emerald-500': credentials.balance > 0,
-                            })}
-                        >
-                            {credentials.balance.toLocaleString('en-US', {
-                                style: 'currency',
-                                currency: 'USD',
-                                maximumFractionDigits: 0,
-                            })}
-                        </h2>
-                        <h5
-                            className={cn('text-xs uppercase font-light', {
-                                'text-white': theme === 'dark',
-                                'text-black': theme === 'light',
-                            })}
-                        >
-                            Solde actuel
-                        </h5>
-                    </div>
-                    <BankCard name={credentials.name} account={credentials.account} />
-                    <div className="flex pt-10 p-6 justify-around">
-                        <div
-                            className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
-                                'text-white border-white': theme === 'dark',
-                                'text-black border-black': theme === 'light',
-                            })}
-                        >
-                            <EyeIcon />
-                        </div>
-                        <div
-                            className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
-                                'text-white border-white': theme === 'dark',
-                                'text-black border-black': theme === 'light',
-                            })}
-                        >
-                            <LockIcon />
-                        </div>
-                        <div
-                            className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
-                                'text-white border-white': theme === 'dark',
-                                'text-black border-black': theme === 'light',
-                            })}
-                        >
-                            <CogIcon />
-                        </div>
-                    </div>
-                    <div className="p-6">
-                        <h2
-                            className={cn('text-2xl', {
-                                'text-white': theme === 'dark',
-                                'text-black': theme === 'light',
-                            })}
-                        >
-                            Paramètre de la carte
-                        </h2>
+    if (!credentials) {
+        return (
+            <AppContent className={`flex justify-center items-center ${theme === 'dark' ? 'text-white' : 'text-dark'}`}>
+                Information non disponible
+            </AppContent>
+        );
+    }
 
-                        <Checkbox title="Activer les retaits distributeurs" enabled={true} />
-                        <Checkbox title="Paiement en ligne" enabled={false} />
-                        <Checkbox title="Stonks Gold+" enabled={false} />
-                    </div>
-                </AppContent>
-            ) : (
-                <AppContent
-                    className={`flex justify-center items-center ${theme === 'dark' ? 'text-white' : 'text-dark'}`}
+    return (
+        <AppContent>
+            <div className="m-auto pt-1 pb-3 flex flex-col w-5/6">
+                <h2
+                    className={cn('text-3xl', {
+                        'text-red-500': credentials.balance <= 0,
+                        'text-emerald-500': credentials.balance > 0,
+                    })}
                 >
-                    Information non disponible
-                </AppContent>
-            )}
-        </>
+                    {credentials.balance.toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: 'USD',
+                        maximumFractionDigits: 0,
+                    })}
+                </h2>
+                <h5
+                    className={cn('text-xs uppercase font-light', {
+                        'text-white': theme === 'dark',
+                        'text-black': theme === 'light',
+                    })}
+                >
+                    Solde actuel
+                </h5>
+            </div>
+            <BankCard name={credentials.name} account={credentials.account} />
+            <div className="flex pt-10 p-6 justify-around">
+                <div
+                    className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
+                        'text-white border-white': theme === 'dark',
+                        'text-black border-black': theme === 'light',
+                    })}
+                >
+                    <EyeIcon className="h-7 w-7" />
+                </div>
+                <div
+                    className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
+                        'text-white border-white': theme === 'dark',
+                        'text-black border-black': theme === 'light',
+                    })}
+                >
+                    <LockClosedIcon className="h-7 w-7" />
+                </div>
+                <div
+                    className={cn('p-3 rounded-full text-dark/50 border border-opacity-50', {
+                        'text-white border-white': theme === 'dark',
+                        'text-black border-black': theme === 'light',
+                    })}
+                >
+                    <CogIcon className="h-7 w-7" />
+                </div>
+            </div>
+            <div className="p-6">
+                <h2
+                    className={cn('text-2xl', {
+                        'text-white': theme === 'dark',
+                        'text-black': theme === 'light',
+                    })}
+                >
+                    Paramètre de la carte
+                </h2>
+
+                <Checkbox title="Activer les retaits distributeurs" enabled={true} />
+                <Checkbox title="Paiement en ligne" enabled={false} />
+                <Checkbox title="Stonks Gold+" enabled={false} />
+            </div>
+        </AppContent>
     );
 });
