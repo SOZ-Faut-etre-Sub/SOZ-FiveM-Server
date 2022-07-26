c79d2c3e6004dcb3ca6681c770384b6e898c23ad
Lucas Perreau <lucasperreau@gmail.com>
2022-07-26 22:46:59 +0200

    feat: add a 'give licence' cheat for the admins

--- resources/[soz]/soz-admin/server/module/gamemaster.lua
+++ resources/[soz]/soz-admin/server/module/gamemaster.lua
@@ -9,6 +9,17 @@ RegisterNetEvent("admin:gamemaster:giveMoney", function(moneyType, amount)
     end
 end)
 
+RegisterNetEvent("admin:gamemaster:giveLicence", function(licence)
+    if not SozAdmin.Functions.IsPlayerAdmin(source) then
+        return
+    end
+
+    local player = QBCore.Functions.GetPlayer(source)
+    if player and licence then
+        player.Functions.SetLicence(licence, 12)
+    end
+end)
+
 RegisterNetEvent("admin:gamemaster:unCuff", function(moneyType, amount)
     if not SozAdmin.Functions.IsPlayerHelper(source) then
         return
