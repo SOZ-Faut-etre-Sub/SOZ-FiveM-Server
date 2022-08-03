c46198f8e37e975289b736947b1a0b1e12387ebd
Frozennide <sebastien@sdeu.fr>
2022-08-03 23:25:14 +0200

    feat: allow sprint for escort when player is in water

--- resources/[soz]/soz-policejob/client/interactions/player.lua
+++ resources/[soz]/soz-policejob/client/interactions/player.lua
@@ -204,7 +204,9 @@ RegisterNetEvent("police:client:SetEscorting", function()
         Wait(1000)
 
         while LocalPlayer.state.isEscorting do
-            DisableControlAction(0, 21, true)
+            if not IsPedSwimming(PlayerPedId()) then
+                DisableControlAction(0, 21, true)
+            end
             QBCore.Functions.ShowHelpNotification("~INPUT_FRONTEND_RRIGHT~ Pour lâcher")
             if IsControlJustReleased(0, 194) or IsControlJustReleased(0, 225) then
                 local player, distance = QBCore.Functions.GetClosestPlayer()
