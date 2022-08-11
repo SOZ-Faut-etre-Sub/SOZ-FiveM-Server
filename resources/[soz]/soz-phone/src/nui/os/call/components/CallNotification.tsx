857c4afa965174ade74428c8f9604c5725faab83
Frozennide <sebastien@sdeu.fr>
2022-08-12 01:11:43 +0200

    fix(phone): use startedAt data to create call timer

--- resources/[soz]/soz-phone/src/nui/os/call/components/CallNotification.tsx
+++ resources/[soz]/soz-phone/src/nui/os/call/components/CallNotification.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
-import { CallControls } from './CallControls';
-
 export const CallNotification = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div>
-            <div>{children}</div>
-            <div>
-                <CallControls />
-            </div>
-        </div>
-    );
+    return <div>{children}</div>;
 };
