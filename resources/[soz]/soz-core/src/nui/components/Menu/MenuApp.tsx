88de73eb7b1201a05573900bb3ba090c0646a9b4
Joel Wurtz <joel.wurtz@gmail.com>
2022-08-14 11:16:30 +0200

    feat(core): use router for menu

--- resources/[soz]/soz-core/src/nui/components/Menu/MenuApp.tsx
+++ resources/[soz]/soz-core/src/nui/components/Menu/MenuApp.tsx
@@ -1,23 +1,33 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, PropsWithChildren, useEffect } from 'react';
+import { MemoryRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
 import { MenuType } from '../../../shared/menu';
 import { useSozCoreNuiEvent } from '../Nui/hooks/useNuiEvent';
 import { MenuSetHealthState } from './MenuSetHealthState';
 
 export const MenuApp: FunctionComponent = () => {
-    const [currentMenu, setCurrentMenu] = useState<MenuType | null>(MenuType.SetHealthState);
+    return (
+        <MemoryRouter>
+            <MenuRouteControl>
+                <Routes>
+                    <Route path="/" element={null} />
+                    <Route path={`/${MenuType.SetHealthState}`} element={<MenuSetHealthState />} />
+                </Routes>
+            </MenuRouteControl>
+        </MemoryRouter>
+    );
+};
+
+const MenuRouteControl: FunctionComponent<PropsWithChildren> = ({ children }) => {
+    const navigate = useNavigate();
 
     useSozCoreNuiEvent<MenuType | null>('SetMenuType', menutype => {
-        setCurrentMenu(menutype);
+        navigate(menutype ? `/${menutype}` : '/');
     });
 
     useSozCoreNuiEvent<never>('CloseMenu', () => {
-        setCurrentMenu(null);
+        navigate('/');
     });
 
-    if (currentMenu === MenuType.SetHealthState) {
-        return <MenuSetHealthState />;
-    }
-
-    return null;
+    return <>{children}</>;
 };
