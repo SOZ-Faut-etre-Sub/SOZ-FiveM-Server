b8d40746dedd4805ce28121ee73ea50c22197c27
Joel Wurtz <joel.wurtz@gmail.com>
2022-08-15 20:54:57 +0200

    feat(mbs): add chin ups / free weight animation and position

--- resources/[soz]/soz-core/src/client/player/player.health.provider.ts
+++ resources/[soz]/soz-core/src/client/player/player.health.provider.ts
@@ -5,9 +5,51 @@ import { Tick, TickInterval } from '../../core/decorators/tick';
 import { ServerEvent } from '../../shared/event';
 import { Feature, isFeatureEnabled } from '../../shared/features';
 import { PlayerData } from '../../shared/player';
+import { Vector3, Vector4 } from '../../shared/polyzone/vector';
 import { AnimationService } from '../animation/animation.service';
 import { TargetFactory } from '../target/target.factory';
 import { PlayerService } from './player.service';
+
+const CHIN_UPS_COORDS = [
+    {
+        name: 'chin_ups_1',
+        zone: {
+            center: [-1205.14, -1563.84, 4.61] as Vector3,
+            heading: 35,
+            width: 1.0,
+            length: 1.2,
+            minZ: 3.61,
+            maxZ: 6.61,
+        },
+        coords: [-1204.74, -1564.26, 4.61, 30.12] as Vector4,
+    },
+    {
+        name: 'chin_ups_2',
+        zone: {
+            center: [-1199.66, -1571.64, 4.61] as Vector3,
+            heading: 305,
+            width: 1.4,
+            length: 1.0,
+            minZ: 3.61,
+            maxZ: 6.61,
+        },
+        coords: [-1199.98, -1571.16, 4.61, 215.66] as Vector4,
+    },
+];
+
+const FREE_WEIGHT_COORDS = [
+    {
+        name: 'free_weight_1',
+        zone: {
+            center: [-1203.27, -1573.62, 4.61] as Vector3,
+            length: 1.8,
+            minZ: 3.61,
+            maxZ: 6.61,
+            width: 1,
+            heading: 35,
+        },
+    },
+];
 
 @Provider()
 export class PlayerHealthProvider {
@@ -42,7 +84,7 @@ export class PlayerHealthProvider {
         // @TODO set player animation and task to do footing
     }
 
-    private async doFreeWeight(): Promise<void> {
+    private async doPushUps(): Promise<void> {
         await this.animationService.playAnimation({
             enter: {
                 dictionary: 'amb@world_human_push_ups@male@enter',
@@ -64,33 +106,77 @@ export class PlayerHealthProvider {
         TriggerServerEvent(ServerEvent.PLAYER_INCREASE_STRENGTH);
     }
 
+    private async doSitUps(): Promise<void> {
+        await this.animationService.playAnimation({
+            enter: {
+                dictionary: 'amb@world_human_sit_ups@male@enter',
+                name: 'enter',
+                duration: 3050,
+            },
+            base: {
+                dictionary: 'amb@world_human_sit_ups@male@base',
+                name: 'base',
+                duration: 4000,
+            },
+            exit: {
+                dictionary: 'amb@world_human_sit_ups@male@exit',
+                name: 'exit',
+                duration: 3400,
+            },
+        });
+
+        TriggerServerEvent(ServerEvent.PLAYER_INCREASE_STRENGTH);
+    }
+
+    private async doFreeWeight(): Promise<void> {
+        await this.animationService.playScenario({
+            name: 'world_human_muscle_free_weights',
+            duration: 10000,
+        });
+
+        TriggerServerEvent(ServerEvent.PLAYER_INCREASE_STRENGTH);
+    }
+
+    private async doChinUps(coords: Vector4): Promise<void> {
+        await this.animationService.walkToCoords(coords, 2000);
+
+        await this.animationService.playScenario({
+            name: 'prop_human_muscle_chin_ups',
+            duration: 10000,
+        });
+
+        TriggerServerEvent(ServerEvent.PLAYER_INCREASE_STRENGTH);
+    }
+
     @Once()
     async onStart(): Promise<void> {
         if (!isFeatureEnabled(Feature.MyBodySummer)) {
             return;
         }
 
-        this.targetFactory.createForBoxZone(
-            'do_free_weight',
-            {
-                center: [-1203.27, -1573.62, 4.61],
-                length: 1.8,
-                minZ: 3.61,
-                maxZ: 6.61,
-                width: 1,
-                heading: 35,
-            },
-            [
+        for (const { name, zone, coords } of CHIN_UPS_COORDS) {
+            this.targetFactory.createForBoxZone(name, zone, [
                 {
-                    label: 'Soulever de la fonte',
-                    canInteract: () => {
-                        return true;
+                    label: 'Faire des tractions',
+                    canInteract: () => true,
+                    action: () => {
+                        this.doChinUps(coords);
                     },
-                    action: this.doFreeWeight.bind(this),
                 },
-            ],
-            2.5
-        );
+            ]);
+        }
+
+        for (const { name, zone } of FREE_WEIGHT_COORDS) {
+            this.targetFactory.createForBoxZone(name, zone, [
+                {
+                    label: 'Soulever le poids',
+                    canInteract: () => true,
+                    action: () => {
+                        this.doFreeWeight();
+                    },
+                },
+            ]);
+        }
     }
 
     @Once(OnceStep.PlayerLoaded)
