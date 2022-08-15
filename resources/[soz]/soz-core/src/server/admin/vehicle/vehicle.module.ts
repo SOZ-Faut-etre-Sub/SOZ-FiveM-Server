393b6c0203249360c55f602dc0385083bfab42a3
Lucas Perreau <lucasperreau@gmail.com>
2022-08-15 18:04:26 +0200

    remove unnecessary import

--- resources/[soz]/soz-core/src/server/admin/vehicle/vehicle.module.ts
+++ resources/[soz]/soz-core/src/server/admin/vehicle/vehicle.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '../../../core/decorators/module';
 import { VehicleProvider } from './vehicle.provider';
-import {DatabaseModule} from "../../database/database.module";
 
 @Module({
-    imports: [DatabaseModule],
     providers: [VehicleProvider],
 })
 export class VehicleModule {}
