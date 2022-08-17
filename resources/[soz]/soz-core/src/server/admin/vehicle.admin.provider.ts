0feb3840c5ca082352830de02c07c9c806f822cf
Lucas Perreau <lucasperreau@gmail.com>
2022-08-17 19:46:47 +0200

    update from feedback

--- resources/[soz]/soz-core/src/server/admin/vehicle.admin.provider.ts
+++ resources/[soz]/soz-core/src/server/admin/vehicle.admin.provider.ts
@@ -1,21 +1,25 @@
-import { OnEvent } from '../../../core/decorators/event';
-import { Inject } from '../../../core/decorators/injectable';
-import { Provider } from '../../../core/decorators/provider';
-import { ClientEvent, ServerEvent } from '../../../shared/event';
-import { PrismaService } from '../../database/prisma.service';
-import { QBCore } from '../../qbcore';
+import { OnEvent } from '../../core/decorators/event';
+import { Inject } from '../../core/decorators/injectable';
+import { Provider } from '../../core/decorators/provider';
+import { ServerEvent } from '../../shared/event';
+import { PrismaService } from '../database/prisma.service';
+import { Notifier } from '../notifier';
+import { PlayerService } from '../player/player.service';
 
 @Provider()
-export class VehicleProvider {
-    @Inject(QBCore)
-    private qbCore: QBCore;
+export class VehicleAdminProvider {
+    @Inject(PlayerService)
+    private playerService: PlayerService;
+
+    @Inject(Notifier)
+    private notifier: Notifier;
 
     @Inject(PrismaService)
     private prisma: PrismaService;
 
     @OnEvent(ServerEvent.ADMIN_VEHICLE_SEE_CAR_PRICE)
     public async seeCarPrice(source: number, modelName: string): Promise<void> {
-        if (!this.qbCore.hasPermission(source, 'admin')) {
+        if (!this.playerService.hasPermission(source, 'admin')) {
             return;
         }
         const vehicle = await this.prisma.vehicles.findFirst({
@@ -25,12 +29,12 @@ export class VehicleProvider {
         });
 
         const message = `Prix du ~b~${modelName}~s~: ~g~${vehicle.price}`;
-        TriggerClientEvent(ClientEvent.DRAW_NOTIFICATION, source, message);
+        this.notifier.notify(source, message, 'success');
     }
 
     @OnEvent(ServerEvent.ADMIN_VEHICLE_CHANGE_CAR_PRICE)
     public async changeCarPrice(source: number, modelName: string, price: number): Promise<void> {
-        if (!this.qbCore.hasPermission(source, 'admin')) {
+        if (!this.playerService.hasPermission(source, 'admin')) {
             return;
         }
         await this.prisma.vehicles.update({
@@ -43,6 +47,6 @@ export class VehicleProvider {
         });
 
         const message = `Nouveau prix du ~b~${modelName}~s~: ~g~${price}`;
-        TriggerClientEvent(ClientEvent.DRAW_NOTIFICATION, source, message);
+        this.notifier.notify(source, message, 'success');
     }
 }
--- resources/[soz]/soz-core/src/server/admin/vehicle/vehicle.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '../../../core/decorators/module';
-import { VehicleProvider } from './vehicle.provider';
-
-@Module({
-    providers: [VehicleProvider],
-})
-export class VehicleModule {}
