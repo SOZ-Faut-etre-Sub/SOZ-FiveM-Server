a008522435dbf7f74e4aa10a650e2fe1b977f829
Frozennide <sebastien@sdeu.fr>
2022-08-20 12:49:21 +0200

    feat(vehicle): replace policeb with policeb2

--- resources/[soz]/soz-policejob/config.lua
+++ resources/[soz]/soz-policejob/config.lua
@@ -483,7 +483,7 @@ Config.RadarAllowedVehicle = {
     [GetHashKey("police4")] = true,
     [GetHashKey("police5")] = true,
     [GetHashKey("police6")] = true,
-    [GetHashKey("policeb")] = true,
+    [GetHashKey("policeb2")] = true,
     --- BCSO
     [GetHashKey("sheriff")] = true,
     [GetHashKey("sheriff2")] = true,
