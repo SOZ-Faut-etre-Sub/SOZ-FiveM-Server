8391ef72d764ff8a6871abbff1672027d2318697
Frozennide <sebastien@sdeu.fr>
2022-08-26 19:27:19 +0200

    feat(fbi): add blip

---
+++ resources/[soz]/soz-jobs/client/jobs/fbi.lua
@@ -0,0 +1,9 @@
+Citizen.CreateThread(function()
+    -- BLIP
+    QBCore.Functions.CreateBlip("fbi", {
+        name = "FBI",
+        coords = vector3(112.25, -749.32, 46.05),
+        sprite = 421,
+        scale = 1.0,
+    })
+end)
