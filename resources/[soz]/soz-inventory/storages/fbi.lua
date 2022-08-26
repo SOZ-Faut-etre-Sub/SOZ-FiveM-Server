b575161867b13e9fb2074ec1205630e761381593
Frozennide <sebastien@sdeu.fr>
2022-08-26 19:27:39 +0200

    feat(fbi): add stockage

---
+++ resources/[soz]/soz-inventory/storages/fbi.lua
@@ -0,0 +1,32 @@
+Config.Storages["fbi_storage"] = {
+    label = "Coffre FBI",
+    type = "storage",
+    owner = "fbi",
+    position = vector3(134.23, -770.05, 242.15),
+    size = vec2(0.6, 2.8),
+    heading = 250,
+    minZ = 241.15,
+    maxZ = 243.55,
+}
+
+Config.Storages["fbi_armory"] = {
+    label = "Armurerie FBI",
+    type = "armory",
+    owner = "fbi",
+    position = vector3(144.18, -764.65, 242.15),
+    size = vec2(2.8, 0.4),
+    heading = 340,
+    minZ = 241.15,
+    maxZ = 243.7,
+}
+
+Config.Storages["fbi_ammo"] = {
+    label = "Munition FBI",
+    type = "ammo",
+    owner = "fbi",
+    position = vector3(145.13, -762.08, 242.15),
+    size = vec2(2.7, 0.4),
+    heading = 340,
+    minZ = 241.15,
+    maxZ = 243.65,
+}
