80bc24eb9c3143e005ccbeb78e9a2b4fe4ea0e53
Lucas Perreau <lucasperreau@gmail.com>
2022-09-02 08:53:24 +0200

    hide luxury dealership behind feature flag

--- resources/[soz]/soz-vehicle/client/dealership/luxury_dealership.lua
+++ resources/[soz]/soz-vehicle/client/dealership/luxury_dealership.lua
@@ -108,14 +108,16 @@ AddEventHandler("onClientResourceStart", function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
     end
-    QBCore.Functions.CreateBlip("luxury:dealership", {
-        name = LuxuryDealershipConfig.blip.name,
-        coords = LuxuryDealershipConfig.blip.coords,
-        sprite = LuxuryDealershipConfig.blip.sprite,
-        color = LuxuryDealershipConfig.blip.color,
-    })
-    local vehicles = QBCore.Functions.TriggerRpc("soz-dealership:server:GetAuctions")
-    for _, vehicle in pairs(vehicles) do
-        spawnVehicle(vehicle)
+    if GetConvarInt("feature_dlc2_luxury", 0) == 1 then
+        QBCore.Functions.CreateBlip("luxury:dealership", {
+            name = LuxuryDealershipConfig.blip.name,
+            coords = LuxuryDealershipConfig.blip.coords,
+            sprite = LuxuryDealershipConfig.blip.sprite,
+            color = LuxuryDealershipConfig.blip.color,
+        })
+        local vehicles = QBCore.Functions.TriggerRpc("soz-dealership:server:GetAuctions")
+        for _, vehicle in pairs(vehicles) do
+            spawnVehicle(vehicle)
+        end
     end
 end)
