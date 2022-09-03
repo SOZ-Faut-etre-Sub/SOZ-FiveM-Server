c9600247bd8c392c6f87fdfc9acd3ecb70f64995
Frozennide <sebastien@sdeu.fr>
2022-09-04 00:42:24 +0200

    feat(bennys): use new flatbed

--- resources/[vehicles]/[vehicles-prod]/soz-flatbed3/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-flatbed3/fxmanifest.lua
@@ -1,8 +1,5 @@
 fx_version "cerulean"
 game "gta5"
 
-files {
-    "stream/def_flatbed3_props.ytyp"
-}
-
-data_file "DLC_ITYP_REQUEST" "stream/def_flatbed3_props.ytyp"
+data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
