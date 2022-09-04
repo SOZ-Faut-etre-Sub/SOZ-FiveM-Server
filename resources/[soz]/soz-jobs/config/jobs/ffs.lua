fc5100bd2116bd5b5f6bb8110458dab2edbf250d
Lucas Perreau <lucasperreau@gmail.com>
2022-09-04 21:50:49 +0200

    add employees clothes

--- resources/[soz]/soz-jobs/config/jobs/ffs.lua
+++ resources/[soz]/soz-jobs/config/jobs/ffs.lua
@@ -2,6 +2,20 @@ FfsConfig = {}
 
 FfsConfig.Cloakroom = {
     [GetHashKey("mp_m_freemode_01")] = {
+        ["Tenue Employé"] = {
+            Components = {
+                [1] = {Drawable = 0, Texture = 0, Palette = 0},
+                [3] = {Drawable = 4, Texture = 0, Palette = 0},
+                [4] = {Drawable = 53, Texture = 0, Palette = 0},
+                [6] = {Drawable = 20, Texture = 0, Palette = 0},
+                [7] = {Drawable = 0, Texture = 0, Palette = 0},
+                [8] = {Drawable = 31, Texture = 0, Palette = 0},
+                [9] = {Drawable = 0, Texture = 0, Palette = 0},
+                [10] = {Drawable = 0, Texture = 0, Palette = 0},
+                [11] = {Drawable = 303, Texture = 3, Palette = 0},
+            },
+            Props = {},
+        },
         ["Tenue Direction"] = {
             Components = {
                 [1] = {Drawable = 0, Texture = 0, Palette = 0},
@@ -18,6 +32,20 @@ FfsConfig.Cloakroom = {
         },
     },
     [GetHashKey("mp_f_freemode_01")] = {
+        ["Tenue Employé"] = {
+            Components = {
+                [1] = {Drawable = 0, Texture = 0, Palette = 0},
+                [3] = {Drawable = 3, Texture = 0, Palette = 0},
+                [4] = {Drawable = 55, Texture = 0, Palette = 0},
+                [6] = {Drawable = 6, Texture = 0, Palette = 0},
+                [7] = {Drawable = 0, Texture = 0, Palette = 0},
+                [8] = {Drawable = 38, Texture = 0, Palette = 0},
+                [9] = {Drawable = 0, Texture = 0, Palette = 0},
+                [10] = {Drawable = 0, Texture = 0, Palette = 0},
+                [11] = {Drawable = 94, Texture = 0, Palette = 0},
+            },
+            Props = {},
+        },
         ["Tenue Direction"] = {
             Components = {
                 [1] = {Drawable = 0, Texture = 0, Palette = 0},
