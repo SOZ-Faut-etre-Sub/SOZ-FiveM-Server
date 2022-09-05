68d89df8b0a4140d70095248ea60207d9840787f
Joel Wurtz <joel.wurtz@gmail.com>
2022-09-05 11:55:57 +0200

    feat(cinema): support multiple stream screen

--- resources/[soz]/soz-core/src/server/stream/stream.provider.ts
+++ resources/[soz]/soz-core/src/server/stream/stream.provider.ts
@@ -1,15 +1,40 @@
 import { Command } from '../../core/decorators/command';
+import { Once } from '../../core/decorators/event';
 import { Provider } from '../../core/decorators/provider';
+
+const BLACK_SCREEN_URL = 'nui://soz-core/public/dui_twitch_stream.html';
 
 @Provider()
 export class StreamProvider {
-    @Command('cinema-url', { role: 'admin' })
-    setCinemaUrlCommand(source: number, url = ''): void {
-        GlobalState.cinema_url = url;
+    @Once()
+    onStart(): void {
+        GlobalState.stream_url_bennys ||= BLACK_SCREEN_URL;
+        GlobalState.stream_url_cinema ||= BLACK_SCREEN_URL;
     }
 
-    @Command('cinema-stop', { role: 'admin' })
-    stopCinemaCommand(): void {
-        GlobalState.cinema_url = 'nui://soz-core/public/dui_twitch_stream.html';
+    @Command('stream-url', { role: 'admin' })
+    setStreamUrlCommand(source: number, name: string, url = ''): void {
+        name = name.toLowerCase();
+
+        if (name === 'bennys') {
+            GlobalState.stream_url_bennys = url;
+        } else if (name === 'cinema') {
+            GlobalState.stream_url_cinema = url;
+        } else {
+            console.log(`Stream inconnu: ${name}`);
+        }
+    }
+
+    @Command('stream-stop', { role: 'admin' })
+    stopStreamCommand(source: number, name: string): void {
+        name = name.toLowerCase();
+
+        if (name === 'bennys') {
+            GlobalState.stream_url_bennys = BLACK_SCREEN_URL;
+        } else if (name === 'cinema') {
+            GlobalState.stream_url_cinema = BLACK_SCREEN_URL;
+        } else {
+            console.log(`Stream inconnu: ${name}`);
+        }
     }
 }
