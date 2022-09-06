08e4ee3a7df4ddc42bb727dcb37a90b7f719cfa8
Joel Wurtz <joel.wurtz@gmail.com>
2022-09-06 20:44:43 +0200

    fix(mbs): disallow some animations while there is a progress bar, fix #1234

--- resources/[soz]/soz-personal-menu/client/components/animations.lua
+++ resources/[soz]/soz-personal-menu/client/components/animations.lua
@@ -39,7 +39,7 @@ local PlayEmote = function(animation)
     local ped = PlayerPedId()
 
     if IsNuiFocused() or IsPedSittingInAnyVehicle(ped) or LocalPlayer.state.isEscorted or LocalPlayer.state.isEscorting or PlayerData.metadata["isdead"] or
-        PlayerData.metadata["ishandcuffed"] or PlayerData.metadata["inlaststand"] then
+        PlayerData.metadata["ishandcuffed"] or PlayerData.metadata["inlaststand"] or exports["progressbar"]:IsDoingAction() then
         return
     end
 
