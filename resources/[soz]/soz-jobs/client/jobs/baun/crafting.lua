e08e2fb8443342199a978eb90f267f29c93cae19
Lucas Perreau <lucasperreau@gmail.com>
2022-09-07 13:24:07 +0200

    remove unnecessary log

--- resources/[soz]/soz-jobs/client/jobs/baun/crafting.lua
+++ resources/[soz]/soz-jobs/client/jobs/baun/crafting.lua
@@ -78,7 +78,6 @@ CreateThread(function()
 end)
 
 RegisterNetEvent("soz-jobs:client:baun:craft", function(cocktailId)
-    print("Crafting " .. cocktailId)
     local item = QBCore.Shared.Items[cocktailId]
     local action_message = string.format("Vous confectionnez 1 %s", item.label)
     local finished_message = string.format("Vous avez terminé de mélanger.", item.pluralLabel)
