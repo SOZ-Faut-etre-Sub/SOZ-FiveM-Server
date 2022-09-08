88db54fe83910e2aa4e99556048892a53a038443
Rigonkmalk <rigonkmalk@gmail.com>
2022-09-08 13:55:12 +0200

    feat(bhm): add files on fxmanifest

--- resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
@@ -1,3 +1,11 @@
 fx_version 'bodacious'
 games { 'gta5' }
 this_is_a_map 'yes'
+
+files {
+    audio/v_bahama_game.dat151.rel,
+    audio/v_bahama_mix.dat15.rel,
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/v_bahama_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_bahama_mix.dat'
