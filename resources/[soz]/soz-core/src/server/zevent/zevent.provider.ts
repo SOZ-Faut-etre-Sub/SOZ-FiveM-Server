2456099d5aab9ac812a17d8cb4a09d86729abb95
Lucas Perreau <lucasperreau@gmail.com>
2022-09-09 22:58:23 +0200

    remove imports

--- resources/[soz]/soz-core/src/server/zevent/zevent.provider.ts
+++ resources/[soz]/soz-core/src/server/zevent/zevent.provider.ts
@@ -2,7 +2,6 @@ import { Once, OnEvent } from '../../core/decorators/event';
 import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
 import { ClientEvent, ServerEvent } from '../../shared/event';
-import { CommonItem, InventoryItem } from '../../shared/item';
 import { InventoryManager } from '../item/inventory.manager';
 import { ItemService } from '../item/item.service';
 import { Notifier } from '../notifier';
