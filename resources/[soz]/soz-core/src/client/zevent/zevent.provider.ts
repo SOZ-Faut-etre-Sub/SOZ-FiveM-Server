0a06862b312de3d2c80751247403674cac9e4624
Lucas Perreau <lucasperreau@gmail.com>
2022-09-09 22:36:38 +0200

    feat(ZEvent): add tshirt

--- resources/[soz]/soz-core/src/client/zevent/zevent.provider.ts
+++ resources/[soz]/soz-core/src/client/zevent/zevent.provider.ts
@@ -1,13 +1,17 @@
-import { Once, OnceStep } from '../../core/decorators/event';
+import { Once, OnceStep, OnEvent } from '../../core/decorators/event';
 import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
-import { ServerEvent } from '../../shared/event';
+import { ClientEvent, ServerEvent } from '../../shared/event';
+import { PlayerService } from '../player/player.service';
 import { TargetFactory } from '../target/target.factory';
 
 @Provider()
 export class ZEventProvider {
     @Inject(TargetFactory)
     private targetFactory: TargetFactory;
+
+    @Inject(PlayerService)
+    private playerService: PlayerService;
 
     @Once(OnceStep.PlayerLoaded)
     public onPlayerLoaded() {
@@ -31,5 +35,40 @@ export class ZEventProvider {
                 },
             ]
         );
+
+        const player = this.playerService.getPlayer();
+
+        if (player.metadata.isWearingItem == 'zevent2022_tshirt') {
+            this.playerService.setJobClothes(this.skin[player.skin.Model.Hash]);
+        }
+    }
+
+    private readonly skin: any = {
+        [GetHashKey('mp_m_freemode_01')]: {
+            Components: {
+                [3]: { Drawable: 0, Texture: 0, Palette: 0 },
+                [8]: { Drawable: 2, Texture: 0, Palette: 0 },
+                [11]: { Drawable: 44, Texture: 1, Palette: 0 },
+            },
+        },
+        [GetHashKey('mp_f_freemode_01')]: {
+            Components: {
+                [3]: { Drawable: 0, Texture: 0, Palette: 0 },
+                [8]: { Drawable: 15, Texture: 0, Palette: 0 },
+                [11]: { Drawable: 335, Texture: 19, Palette: 0 },
+            },
+        },
+    };
+
+    @OnEvent(ClientEvent.ZEVENT_TOGGLE_TSHIRT)
+    public onToggleTShirt() {
+        const player = this.playerService.getPlayer();
+        if (player.metadata.isWearingItem == 'zevent2022_tshirt') {
+            player.metadata.isWearingItem = null;
+            this.playerService.setJobClothes(null);
+        } else {
+            player.metadata.isWearingItem = 'zevent2022_tshirt';
+            this.playerService.setJobClothes(this.skin[player.skin.Model.Hash]);
+        }
     }
 }
--- resources/[soz]/soz-core/src/server/zevent/zevent.provider.ts
+++ resources/[soz]/soz-core/src/server/zevent/zevent.provider.ts
@@ -1,16 +1,26 @@
-import { OnEvent } from '../../core/decorators/event';
+import { Once, OnEvent } from '../../core/decorators/event';
 import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
-import { ServerEvent } from '../../shared/event';
+import { ClientEvent, ServerEvent } from '../../shared/event';
+import { CommonItem, InventoryItem } from '../../shared/item';
 import { InventoryManager } from '../item/inventory.manager';
+import { ItemService } from '../item/item.service';
 import { Notifier } from '../notifier';
+import { ProgressService } from '../player/progress.service';
 
 @Provider()
 export class ZEventProvider {
     private readonly ITEM_ID = 'zevent2022_popcorn';
     private readonly AMOUNT_TO_GIVE = 1;
+
     @Inject(InventoryManager)
     private inventoryManager: InventoryManager;
+
+    @Inject(ItemService)
+    private itemService: ItemService;
+
+    @Inject(ProgressService)
+    private progressService: ProgressService;
 
     @Inject(Notifier)
     private notifier: Notifier;
@@ -23,5 +33,24 @@ export class ZEventProvider {
         }
         this.inventoryManager.addItemToInventory(source, this.ITEM_ID, this.AMOUNT_TO_GIVE);
         this.notifier.notify(source, `Vous avez pris du pop-corn. Bon appétit !`, 'success');
+    }
+
+    private async useTShirt(source: number, _item: CommonItem, _inventoryItem: InventoryItem) {
+        const { completed } = await this.progressService.progress(source, 'wear_tshirt', '', 5000, {
+            dictionary: 'anim@mp_yacht@shower@male@',
+            name: 'male_shower_towel_dry_to_get_dressed',
+            flags: 15,
+        });
+
+        if (!completed) {
+            return;
+        }
+
+        TriggerClientEvent(ClientEvent.ZEVENT_TOGGLE_TSHIRT, source);
+    }
+
+    @Once()
+    public onStart() {
+        this.itemService.setItemUseCallback('zevent2022_tshirt', this.useTShirt.bind(this));
     }
 }
