d5de8857632e45e6c15aaa34d255431b3354f0e4
Sébastien D <1666044+sebastiendeu@users.noreply.github.com>
2022-09-14 21:27:18 +0200

    feat(msb): reset player metadata

---
+++ resources/[soz]/mysql-migration/migrations/2022-09-14-21-30-reset-msb-metadata.lua
@@ -0,0 +1,13 @@
+table.insert(migrations, {
+    name = "reset-msb-metadata",
+    queries = {
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.fiber', 70) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.lipid', 70) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.sugar', 70) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.protein', 70) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.max_stamina', 100) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.strength', 100) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.stress_level', 0) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.health_level', 100) ]],
+    },
+})
