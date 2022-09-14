40232a682c61f5d109e1c5a8804a9c9dd4080611
Sébastien D <1666044+sebastiendeu@users.noreply.github.com>
2022-09-14 22:42:24 +0200

    fix strength value

--- resources/[soz]/mysql-migration/migrations/2022-09-14-21-30-reset-msb-metadata.lua
+++ resources/[soz]/mysql-migration/migrations/2022-09-14-21-30-reset-msb-metadata.lua
@@ -6,7 +6,7 @@ table.insert(migrations, {
         [[ UPDATE player SET metadata = JSON_SET(metadata, '$.sugar', 70) ]],
         [[ UPDATE player SET metadata = JSON_SET(metadata, '$.protein', 70) ]],
         [[ UPDATE player SET metadata = JSON_SET(metadata, '$.max_stamina', 100) ]],
-        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.strength', 100) ]],
+        [[ UPDATE player SET metadata = JSON_SET(metadata, '$.strength', 120) ]],
         [[ UPDATE player SET metadata = JSON_SET(metadata, '$.stress_level', 0) ]],
         [[ UPDATE player SET metadata = JSON_SET(metadata, '$.health_level', 100) ]],
     },
