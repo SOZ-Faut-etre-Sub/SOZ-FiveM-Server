2da36dd6a2ce28ec0bf5618ed2e801909ba885ef
Rigonkmalk <rigonkmalk@gmail.com>
2022-09-15 17:47:49 +0200

    fix(bb): working on audio for rogers

--- resources/[mapping]/[mapping-prod]/soz-rogers/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-rogers/fxmanifest.lua
@@ -1,3 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'
+
+files {
+    "audio/v_recycle_game.dat151.rel",
+    "audio/v_recycle_mix.dat15.rel",
+    "stream/ymap/1932213070.ymt",
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/v_recycle_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_recycle_mix.dat'
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "stream/ymap/3728417233.ymt"
