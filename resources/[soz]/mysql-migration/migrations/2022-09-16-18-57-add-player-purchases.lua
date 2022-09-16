c6107d822985fed04ea2c342a3cf828001d4329f
Lucas Perreau <lucasperreau@gmail.com>
2022-09-16 21:32:55 +0200

    chores: format lua code

--- resources/[soz]/mysql-migration/migrations/2022-09-16-18-57-add-player-purchases.lua
+++ resources/[soz]/mysql-migration/migrations/2022-09-16-18-57-add-player-purchases.lua
@@ -1,19 +1,19 @@
-table.insert({
+table.insert(migrations, {
     name = "add-player-purchases",
     queries = {
         [[
-            CREATE TABLE `player_purchases` (
+            CREATE TABLE IF NOT EXISTS `player_purchases` (
                 `id` INTEGER NOT NULL AUTO_INCREMENT,
                 `citizenid` VARCHAR(50) NOT NULL,
                 `shop_type` VARCHAR(25) NOT NULL,
                 `shop_id` VARCHAR(50) NULL,
                 `item_id` VARCHAR(50) NOT NULL,
                 `amount` INTEGER NOT NULL,
-                `date` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+                `date` int(11) NOT NULL,
 
-                INDEX `shop_id` (`shop_id`)
+                INDEX `shop_id` (`shop_id`),
                 PRIMARY KEY (`id`)
-            ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-        ]]
-    }
+            );
+        ]],
+    },
 })
