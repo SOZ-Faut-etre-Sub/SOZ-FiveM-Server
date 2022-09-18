efda884fc8b7dacac8f817f77b1569be5788edfd
Lucas Perreau <Mcfloy@users.noreply.github.com>
2022-09-18 10:53:36 +0200

    Update gta-server/resources/[soz]/soz-jobs/config/jobs/baun.lua
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- resources/[soz]/soz-jobs/config/jobs/baun.lua
+++ resources/[soz]/soz-jobs/config/jobs/baun.lua
@@ -40,9 +40,7 @@ BaunConfig.Cloakroom = {
                     [10] = {Drawable = 0, Texture = 0, Palette = 0},
                     [11] = {Drawable = 230, Texture = 7, Palette = 0},
                 },
-                Props = {
-                    [0] = {Drawable = 58, Texture = 2, Palette = 0},
-                },
+                Props = {[0] = {Drawable = 58, Texture = 2, Palette = 0}},
             },
             ["Tenue de service"] = {
                 Components = {
