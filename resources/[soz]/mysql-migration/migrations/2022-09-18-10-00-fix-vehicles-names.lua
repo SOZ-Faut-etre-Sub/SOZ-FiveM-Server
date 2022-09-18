ea8c95bd890f5989ccb387532cc9371b24413b77
Lucas Perreau <lucasperreau@gmail.com>
2022-09-18 10:05:21 +0200

    chores: use lua formatter

--- resources/[soz]/mysql-migration/migrations/2022-09-18-10-00-fix-vehicles-names.lua
+++ resources/[soz]/mysql-migration/migrations/2022-09-18-10-00-fix-vehicles-names.lua
@@ -4,5 +4,5 @@ table.insert(migrations, {
         UPDATE vehicles SET name = 'Oracle' where model = 'oracle2';
         UPDATE vehicles SET name = 'Oracle XS' where model = 'oracle';
         UPDATE vehicles SET name = 'Dubsta Luxury' where model = 'dubsta2';
-    ]]
+    ]],
 })
