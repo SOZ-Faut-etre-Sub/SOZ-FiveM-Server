5df151b214ecc5885213accdc578935ed1d02ba9
Frozennide <sebastien@sdeu.fr>
2022-09-20 21:58:44 +0200

    feat(bluebird): implement new processing storage to recycle items

--- resources/[soz]/soz-jobs/server/jobs/garbage.lua
+++ resources/[soz]/soz-jobs/server/jobs/garbage.lua
@@ -1,62 +1,24 @@
---- Events
-RegisterNetEvent("jobs:server:garbage:processBags", function(item)
-    if QBCore.Shared.Items[item] == nil then
-        return
-    end
+Citizen.CreateThread(function()
+    while true do
+        local processingItem = exports["soz-inventory"]:GetFirstItem(GarbageConfig.Processing.ProcessingStorage)
 
-    local Player = QBCore.Functions.GetPlayer(source)
-    local playerGarbageBagAmount = exports["soz-inventory"]:GetItem(Player.PlayerData.source, item, nil, true)
-    local bagToProcess = GarbageConfig.RecycleItem[item]
+        if processingItem then
+            local itemToProcess = GarbageConfig.RecycleItem[processingItem.item.name] or GarbageConfig.RecycleItem["default"]
 
-    if playerGarbageBagAmount < 1 then
-        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source, "Il vous manque ~r~un " .. QBCore.Shared.Items[item].label, "error")
+            if processingItem.amount < itemToProcess then
+                itemToProcess = processingItem.amount
+            end
 
-        return
-    elseif playerGarbageBagAmount < bagToProcess then
-        bagToProcess = playerGarbageBagAmount
-    end
+            if exports["soz-inventory"]:RemoveItem(GarbageConfig.Processing.ProcessingStorage, processingItem.item.name, itemToProcess) then
+                local resellPrice = GarbageConfig.SellPrice[processingItem.item.name] or GarbageConfig.SellPrice["default"]
 
-    if exports["soz-inventory"]:RemoveItem(Player.PlayerData.source, item, bagToProcess) then
-        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source,
-                           ("Vous avez recyclé ~g~%d %s"):format(bagToProcess, QBCore.Shared.Items[item].label))
+                TriggerEvent("banking:server:TransferMoney", "farm_garbage", "safe_garbage", itemToProcess * resellPrice)
+                TriggerEvent("monitor:server:event", "job_bluebird_recycling_garbage_bag", {
+                    item = processingItem.item.name,
+                }, {quantity = tonumber(itemToProcess)})
+            end
+        end
 
-        local resellPrice = GarbageConfig.SellPrice[item] or GarbageConfig.SellPrice["default"]
-        TriggerEvent("banking:server:TransferMoney", "farm_garbage", "safe_garbage", bagToProcess * resellPrice)
-        TriggerEvent("monitor:server:event", "job_bluebird_recycling_garbage_bag", {
-            player_source = Player.PlayerData.source,
-            item = item,
-        }, {quantity = tonumber(bagToProcess), position = GetEntityCoords(GetPlayerPed(Player.PlayerData.source))})
-    end
-
-    if exports["soz-inventory"]:GetItem(Player.PlayerData.source, item, nil, true) >= 1 then
-        TriggerClientEvent("jobs:client:garbage:processBags", Player.PlayerData.source, {item = item})
+        Citizen.Wait(GarbageConfig.Processing.Duration)
     end
 end)
-
-RegisterNetEvent("jobs:server:garbage:processExpired", function(slot)
-    local Player = QBCore.Functions.GetPlayer(source)
-    local playerGarbageExpired = exports["soz-inventory"]:GetSlot(Player.PlayerData.source, slot)
-
-    if not playerGarbageExpired then
-        return
-    end
-
-    if exports["soz-inventory"]:RemoveItem(Player.PlayerData.source, playerGarbageExpired.name, playerGarbageExpired.amount, playerGarbageExpired.metadata,
-                                           playerGarbageExpired.slot) then
-        TriggerClientEvent("hud:client:DrawNotification", Player.PlayerData.source,
-                           ("Vous avez recyclé ~g~%d %s"):format(playerGarbageExpired.amount, playerGarbageExpired.label))
-
-        TriggerEvent("banking:server:TransferMoney", "farm_garbage", "safe_garbage",
-                     math.floor(playerGarbageExpired.amount * GarbageConfig.SellPrice["default"]))
-        TriggerEvent("monitor:server:event", "job_bluebird_recycling_garbage_bag", {
-            player_source = Player.PlayerData.source,
-            item = playerGarbageExpired.name,
-        }, {
-            quantity = tonumber(playerGarbageExpired.amount),
-            position = GetEntityCoords(GetPlayerPed(Player.PlayerData.source)),
-        })
-    end
-
-    TriggerClientEvent("jobs:client:garbage:processExpired", Player.PlayerData.source, {})
-end)
-
