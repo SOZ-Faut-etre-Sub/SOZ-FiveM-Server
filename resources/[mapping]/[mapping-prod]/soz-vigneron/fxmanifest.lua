80e5ce984ec5370e499d0a527f564bc9bb277315
Rigonkmalk <rigonkmalk@gmail.com>
2022-09-21 16:26:37 +0200

    feat(cm): working on AudioOcclusion

--- resources/[mapping]/[mapping-prod]/soz-vigneron/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-vigneron/fxmanifest.lua
@@ -1,3 +1,13 @@
 fx_version 'bodacious'
 games { 'gta5' }
 this_is_a_map 'yes'
+
+files {
+    "audio/v_ranch_game.dat151.rel",
+    "audio/v_ranch_mix.dat15.rel",
+    "stream/ymap/3499865574.ymt",
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/v_ranch_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_ranch_mix.dat'
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "stream/3499865574.ymt"
