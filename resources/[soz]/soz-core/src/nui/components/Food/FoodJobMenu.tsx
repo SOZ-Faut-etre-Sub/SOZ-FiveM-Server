72fe1f3cd264a2f0922f833bce104ad2b922fe6f
Lucas Perreau <lucasperreau@gmail.com>
2022-09-21 17:51:05 +0200

    add blip

--- resources/[soz]/soz-core/src/nui/components/Food/FoodJobMenu.tsx
+++ resources/[soz]/soz-core/src/nui/components/Food/FoodJobMenu.tsx
@@ -1,15 +1,20 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 
+import { NuiEvent } from '../../../shared/event';
 import { MenuType } from '../../../shared/nui/menu';
+import { fetchNui } from '../../fetch';
 import { CraftList } from '../Shared/CraftList';
 import {
     MainMenu,
     Menu,
     MenuContent,
+    MenuItemCheckbox,
     MenuItemSelect,
     MenuItemSelectOption,
+    MenuItemSubMenuLink,
     MenuItemText,
     MenuTitle,
+    SubMenu,
 } from '../Styleguide/Menu';
 
 export type FoodRecipe = {
@@ -28,13 +33,28 @@ export type FoodRecipe = {
 type FoodStateProps = {
     data: {
         recipes: FoodRecipe[];
+        state: {
+            displayMilkBlip: boolean;
+        };
         onDuty: boolean;
     };
 };
 
 export const FoodJobMenu: FunctionComponent<FoodStateProps> = ({ data }) => {
     const banner = 'https://nui-img/soz/menu_job_food';
+    const [blips, setBlips] = useState(null);
     const [currentRecipe, setCurrentRecipe] = useState<FoodRecipe>(null);
+
+    useEffect(() => {
+        if (data && data.state) {
+            setBlips(data.state);
+        }
+    }, [data]);
+
+    const displayBlip = async (blip: string, value: boolean) => {
+        setBlips({ ...blips, [blip]: value });
+        await fetchNui(NuiEvent.FoodDisplayBlip, { blip, value });
+    };
 
     if (!data.recipes) {
         return null;
@@ -56,6 +76,18 @@ export const FoodJobMenu: FunctionComponent<FoodStateProps> = ({ data }) => {
     return (
         <Menu type={MenuType.FoodJobMenu}>
             <MainMenu>
+                <MenuTitle banner={banner}></MenuTitle>
+                <MenuContent>
+                    <MenuItemSubMenuLink id="recipe">Livre de recettes</MenuItemSubMenuLink>
+                    <MenuItemCheckbox
+                        checked={data.state.displayMilkBlip}
+                        onChange={value => displayBlip('displayMilkBlip', value)}
+                    >
+                        Afficher le point de collecte de lait
+                    </MenuItemCheckbox>
+                </MenuContent>
+            </MainMenu>
+            <SubMenu id="recipe">
                 <MenuTitle banner={banner}>Livre de recettes</MenuTitle>
                 <MenuContent>
                     <MenuItemSelect title={'Recette'}>
@@ -72,7 +104,7 @@ export const FoodJobMenu: FunctionComponent<FoodStateProps> = ({ data }) => {
                     </MenuItemSelect>
                     {currentRecipe && <CraftList inputs={currentRecipe.inputs} />}
                 </MenuContent>
-            </MainMenu>
+            </SubMenu>
         </Menu>
     );
 };
