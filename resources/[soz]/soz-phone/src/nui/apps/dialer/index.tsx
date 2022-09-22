3ab46a78d4c6c5eb81641ecd0d059de2c8b4fa5c
Frozennide <sebastien@sdeu.fr>
2022-09-22 21:28:09 +0200

    feat(phone): reduce animation duration

--- resources/[soz]/soz-phone/src/nui/apps/dialer/index.tsx
+++ resources/[soz]/soz-phone/src/nui/apps/dialer/index.tsx
@@ -26,10 +26,10 @@ export const DialerApp: React.FC = () => {
             <Transition
                 appear={true}
                 show={true}
-                enter="transition-all origin-[20%_90%] duration-500"
+                enter="transition-all origin-[20%_90%] duration-300"
                 enterFrom="scale-[0.0] opacity-0"
                 enterTo="scale-100 opacity-100"
-                leave="transition-all origin-[20%_90%] duration-500"
+                leave="transition-all origin-[20%_90%] duration-300"
                 leaveFrom="scale-100 opacity-100"
                 leaveTo="scale-[0.0] opacity-0"
             >
