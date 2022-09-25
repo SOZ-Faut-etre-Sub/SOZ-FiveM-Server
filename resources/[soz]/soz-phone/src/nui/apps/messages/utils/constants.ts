a8158b0e4bfd430cd1a988d9fac0d8ceda1efd54
Frozennide <sebastien@sdeu.fr>
2022-09-25 20:52:31 +0200

    feat(phone): add full screen / copy feature on image for messages

--- resources/[soz]/soz-phone/src/nui/apps/messages/utils/constants.ts
+++ resources/[soz]/soz-phone/src/nui/apps/messages/utils/constants.ts
@@ -3,25 +3,25 @@ import { Message, MessageConversation } from '@typings/messages';
 
 export const MockMessageConversations: MessageConversation[] = [
     {
-        conversation_id: '4444+5555',
+        conversation_id: '555-1111+555-2222',
         avatar: 'https://i.imgur.com/GCBVgXD.jpeg',
-        phoneNumber: '215-8139',
+        phoneNumber: '555-2222',
         unread: 0,
         display: 'Taso',
         updatedAt: 1598420000000,
     },
     {
-        conversation_id: '4444+7777',
+        conversation_id: '555-1111+555-3333',
         avatar: '',
-        phoneNumber: '603-275-8373',
+        phoneNumber: '555-3333',
         unread: 3,
         display: 'Chip',
         updatedAt: 1598400000000,
     },
     {
-        conversation_id: '555-1111+555-2222',
+        conversation_id: '555-1111+555-4444',
         avatar: '',
-        phoneNumber: '555-2222',
+        phoneNumber: '555-4444',
         unread: 0,
         display: '',
         updatedAt: 1598400000000,
@@ -31,37 +31,40 @@ export const MockMessageConversations: MessageConversation[] = [
 export const MockConversationMessages: Message[] = [
     {
         id: 1,
-        author: '215-8139',
+        author: '555-1111',
         message: 'Hello',
-        conversation_id: '4444+5555',
+        conversation_id: '555-1111+555-3333',
     },
     {
         id: 2,
-        author: '215-8139',
+        author: '555-1111',
         message:
             'Hi asljdf klasdfjkasjdf sdfjf asdf djkjas k kksdfjjsl ks kldfs d fd asd asdfjasdjfjasdkljjfklasjldfjlj asdf sadfdsdkafjkljsdklfjklfjdf',
+        conversation_id: '555-1111+555-3333',
     },
     {
         id: 3,
-        author: '111-1134',
+        author: '555-3333',
         message: 'Hello',
-        conversation_id: '4444+5555',
+        conversation_id: '555-1111+555-3333',
     },
     {
         id: 4,
-        author: '111-1134',
+        author: '555-2222',
         message: 'Hello Rocky',
-        conversation_id: '4444+7777',
+        conversation_id: '555-1111+555-2222',
     },
     {
         id: 5,
-        author: '215-8139',
+        author: '555-1111',
         message: 'https://i.tasoagc.dev/LtuA.png',
+        conversation_id: '555-1111+555-3333',
     },
     {
         id: 6,
-        author: '215-8139',
+        author: '555-1111',
         message: 'vec2(355.555,-8754.54)',
+        conversation_id: '555-1111+555-3333',
     },
 ];
 
