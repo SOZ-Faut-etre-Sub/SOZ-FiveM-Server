a0a676bf1820387f9bae76398483628d0ca7f147
Frozennide <sebastien@sdeu.fr>
2022-09-29 20:18:54 +0200

    feat(phone): drop custom mysql connector and use oxmysql

--- resources/[soz]/soz-phone/src/server/settings/settings.db.ts
+++ resources/[soz]/soz-phone/src/server/settings/settings.db.ts
@@ -1,20 +1,16 @@
-import { ResultSetHeader } from 'mysql2';
-
 import { PreDBSettings } from '../../../typings/settings';
-import DbInterface from '../db/db_wrapper';
 
 export class _SettingsDB {
     async addPicture(identifier: string, { number, url }: PreDBSettings): Promise<number> {
-        const query = `INSERT INTO phone_profile (number, avatar) VALUES (?, ?) ON DUPLICATE KEY UPDATE avatar=?`;
-        const [setResult] = await DbInterface._rawExec(query, [number, url, url]);
-
-        return (<ResultSetHeader>setResult).insertId;
+        return exports.oxmysql.insert_async(
+            `INSERT INTO phone_profile (number, avatar) VALUES (?, ?) ON DUPLICATE KEY UPDATE avatar=?`,
+            [number, url, url]
+        );
     }
 
     async getProfilePicture(number: string): Promise<string | null> {
-        const query = `SELECT avatar FROM phone_profile WHERE number = ?`;
-        const [results] = await DbInterface._rawExec(query, [number]);
-        return (results as any[])[0] ? (results as any[])[0]['avatar'] : null;
+        const result = exports.oxmysql.single_async(`SELECT avatar FROM phone_profile WHERE number = ?`, [number]);
+        return result['avatar'] ?? null;
     }
 }
 
