1c17a842921a645a9c58aa720d66dd284b124fe8
Joel Wurtz <joel.wurtz@gmail.com>
2022-10-03 17:37:55 +0200

    feat(zumble): generate cert in docker

--- resources/[soz]/soz-voip/server/modules/player.lua
+++ resources/[soz]/soz-voip/server/modules/player.lua
@@ -1,8 +1,18 @@
+local http_endpoint = GetConvar("soz_voip_mumble_http_endpoint", "")
+
 --- Functions
 RegisterNetEvent("voip:server:player:mute", function(mute, target)
-    ZumbleSetPlayerMuted(target or source, mute)
+    if http_endpoint ~= "" then
+        ZumbleSetPlayerMuted(target or source, mute)
+    else
+        MumbleSetPlayerMuted(target or source, mute)
+    end
 end)
 
 QBCore.Functions.CreateCallback("voip:server:player:isMuted", function(source, cb, target)
-    cb(ZumbleIsPlayerMuted(target or source))
+    if http_endpoint ~= "" then
+        cb(ZumbleIsPlayerMuted(target or source))
+    else
+        cb(MumbleIsPlayerMuted(target or source))
+    end
 end)
