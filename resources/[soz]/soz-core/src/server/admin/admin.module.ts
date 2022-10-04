7c9ccd6dc1e6f1a51aa55c2956e05e0e23e3c4c4
Lucas Perreau <lucasperreau@gmail.com>
2022-10-04 21:48:21 +0200

    fix the player module

--- resources/[soz]/soz-core/src/server/admin/admin.module.ts
+++ resources/[soz]/soz-core/src/server/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '../../core/decorators/module';
 import { AdminMenuInteractiveProvider } from './admin.menu.interactive.provider';
+import { AdminMenuPlayerProvider } from './admin.menu.player.provider';
 import { AdminMenuVehicleProvider } from './admin.menu.vehicle.provider';
 import { AdminProvider } from './admin.provider';
 
 @Module({
-    providers: [AdminMenuVehicleProvider, AdminMenuInteractiveProvider, AdminProvider],
+    providers: [AdminMenuInteractiveProvider, AdminMenuPlayerProvider, AdminMenuVehicleProvider, AdminProvider],
 })
 export class AdminModule {}
