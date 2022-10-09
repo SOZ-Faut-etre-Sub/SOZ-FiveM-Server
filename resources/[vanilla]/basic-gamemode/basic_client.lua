77a794e750bc7fe006352d0b5bf0cf2c72608c58
Joel Wurtz <joel.wurtz@gmail.com>
2022-10-09 16:46:14 +0200

    feat(server): add a vanilla directory to test behavior in a vanilla way, also add vmenu trainer to this vanilla system

---
+++ resources/[vanilla]/basic-gamemode/basic_client.lua
@@ -0,0 +1,6 @@
+
+CreateThread(function()
+    exports.spawnmanager:addSpawnPoint({ x = -802.311, y = 175.056, z = 72.8446, heading = 0, model = 'a_m_y_skater_01' })
+    exports.spawnmanager:setAutoSpawn(true)
+    exports.spawnmanager:forceRespawn()
+end)
