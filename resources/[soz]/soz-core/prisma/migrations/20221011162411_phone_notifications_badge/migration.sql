57a7c9d1da611aa479637e2fb321aac4146670cb
Joel Wurtz <joel.wurtz@gmail.com>
2022-10-12 01:06:21 +0200

    hotfix migration

--- resources/[soz]/soz-core/prisma/migrations/20221011162411_phone_notifications_badge/migration.sql
+++ resources/[soz]/soz-core/prisma/migrations/20221011162411_phone_notifications_badge/migration.sql
@@ -5,8 +5,9 @@
   - Made the column `unread` on table `phone_messages_conversations` required. This step will fail if there are existing NULL values in that column.
 
 */
+UPDATE `phone_messages_conversations` SET `unread` = 0;
 -- AlterTable
+
 ALTER TABLE `phone_messages_conversations` DROP COLUMN `unreadCount`,
     MODIFY `unread` INTEGER NOT NULL DEFAULT 0;
 
-UPDATE `phone_messages_conversations` SET `unread` = 0;
