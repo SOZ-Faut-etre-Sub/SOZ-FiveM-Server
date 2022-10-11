29a4ed31c454a996563b64e0b5417534389d0d87
Silverlord <jeremy@bernard.sh>
2022-10-11 10:05:47 +0200

    1137: Add several column to investigation table

---
+++ resources/[soz]/soz-core/prisma/migrations/20221008132348_update_investigation_users_management/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `investigation` ADD COLUMN `granted_jobgrade` LONGTEXT NULL,
+    ADD COLUMN `granted_services` LONGTEXT NULL,
+    ADD COLUMN `in_charge` LONGTEXT NULL,
+    ADD COLUMN `supervisors` LONGTEXT NULL;
