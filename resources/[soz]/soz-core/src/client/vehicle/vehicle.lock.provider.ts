58bd27654fb0130d23cae1ebcd18b546a4aa40e2
Joel Wurtz <joel.wurtz@gmail.com>
2022-10-12 01:02:58 +0200

    feat(vehicle): wip lock system

--- resources/[soz]/soz-core/src/client/vehicle/vehicle.lock.provider.ts
+++ resources/[soz]/soz-core/src/client/vehicle/vehicle.lock.provider.ts
@@ -1,4 +1,45 @@
+import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
+import { Tick, TickInterval } from '../../core/decorators/tick';
+import { VehicleEntityState } from '../../shared/vehicle';
+import { PlayerService } from '../player/player.service';
 
 @Provider()
-export class VehicleLockProvider {}
+export class VehicleLockProvider {
+    @Inject(PlayerService)
+    private playerService: PlayerService;
+
+    @Tick(TickInterval.EVERY_FRAME)
+    private async checkPlayerCanEnterVehicle() {
+        const player = this.playerService.getPlayer();
+
+        if (!player) {
+            return;
+        }
+
+        const ped = PlayerPedId();
+        const vehicle = GetVehiclePedIsTryingToEnter(ped);
+
+        if (!vehicle) {
+            return;
+        }
+
+        const vehicleState = Entity(vehicle).state as VehicleEntityState;
+        const vehicleId = vehicleState.id || null;
+
+        // Not a player vehicle
+        if (!vehicleId) {
+            if (!Entity(vehicle).state.forced && !player.metadata.godmode) {
+                SetVehicleDoorsLocked(vehicle, 2);
+            } else {
+                SetVehicleDoorsLocked(vehicle, 0);
+            }
+        } else {
+            if (Entity(vehicle).state.open) {
+                SetVehicleDoorsLocked(vehicle, 0);
+            } else {
+                // Check if player is owner
+            }
+        }
+    }
+}
--- resources/[soz]/soz-core/src/shared/vehicle.ts
+++ resources/[soz]/soz-core/src/shared/vehicle.ts
@@ -3,3 +3,9 @@ export type VehicleProperty = 'modEngine' | 'modBrakes' | 'modTransmission' | 'm
 export type VehicleProperties = {
     [key in VehicleProperty]: number;
 };
+
+export type VehicleEntityState = StateBagInterface & {
+    id?: number;
+    forced?: boolean;
+    open?: boolean;
+};
