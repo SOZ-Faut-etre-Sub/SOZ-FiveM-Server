5da3757d6eb2b59ada7b56a9461458a915ac90d5
Frozennide <sebastien@sdeu.fr>
2022-10-11 18:25:50 +0200

    feat(phone): switch from mysql-migration to soz-core

---
+++ resources/[soz]/soz-core/prisma/migrations/20221011162411_phone_notifications_badge/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `unreadCount` on the `phone_messages_conversations` table. All the data in the column will be lost.
+  - Made the column `unread` on table `phone_messages_conversations` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE `phone_messages_conversations` DROP COLUMN `unreadCount`,
+    MODIFY `unread` INTEGER NOT NULL DEFAULT 0;
+
+UPDATE `phone_messages_conversations` SET `unread` = 0;
