b09cd06c457af93582ba72d51d6972864fdcaa64
Lucas Perreau <lucasperreau@gmail.com>
2022-10-12 22:10:05 +0200

    fix the transfer of money to the player

--- resources/[soz]/soz-core/src/client/bank/bank.service.ts
+++ resources/[soz]/soz-core/src/client/bank/bank.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '../../core/decorators/injectable';
+import { Err, Ok, Result } from '../../shared/result';
 
 @Injectable()
 export class BankService {
@@ -12,10 +13,14 @@ export class BankService {
     }
 
     // TODO: Enforce better type for source and target
-    public transferCashMoney(source: string, target: string, amount: number): Promise<[boolean, string]> {
+    public transferCashMoney(source: string, target: number, amount: number): Promise<Result<boolean, string>> {
         return new Promise(resolve => {
-            exports['soz-bank'].TransferMoney(source, target, amount, (success, reason) => {
-                resolve([success, reason]);
+            exports['soz-bank'].TransferCashMoney(source, target, amount, (success, reason) => {
+                if (success) {
+                    resolve(Ok(true));
+                } else {
+                    resolve(Err(reason));
+                }
             });
         });
     }
