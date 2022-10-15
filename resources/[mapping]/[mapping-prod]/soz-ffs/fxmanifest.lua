28e1d84d683c264fbc28072c270501468e0afd45
Rigonkmalk <rigonkmalk@gmail.com>
2022-10-15 21:32:22 +0200

    feat(ffs): delete mexicam radio

--- resources/[mapping]/[mapping-prod]/soz-ffs/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-ffs/fxmanifest.lua
@@ -2,4 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'
 
+files {
+    "audio/v_sweat_game.dat151.rel",
+}
+
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_ffs_logo.ytyp'
+data_file 'AUDIO_GAMEDATA' 'audio/v_sweat_game.dat'
