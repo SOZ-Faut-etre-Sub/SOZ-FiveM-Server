9932ffecac0e68069eba657d46aa186b19dda7da
Frozennide <sebastien@sdeu.fr>
2022-10-15 16:08:16 +0200

    fix(core/job/stonk): remove unused code

--- resources/[soz]/soz-core/src/client/job/stonk/stonk.provider.ts
+++ resources/[soz]/soz-core/src/client/job/stonk/stonk.provider.ts
@@ -1,20 +1,10 @@
-import { Once, OnceStep, OnEvent } from '../../../core/decorators/event';
+import { Once, OnceStep } from '../../../core/decorators/event';
 import { Inject } from '../../../core/decorators/injectable';
 import { Provider } from '../../../core/decorators/provider';
-import { ClientEvent } from '../../../shared/event';
-import { MenuType } from '../../../shared/nui/menu';
 import { BlipFactory } from '../../blip';
-import { NuiMenu } from '../../nui/nui.menu';
-import { PlayerService } from '../../player/player.service';
 
 @Provider()
 export class StonkProvider {
-    @Inject(NuiMenu)
-    private nuiMenu: NuiMenu;
-
-    @Inject(PlayerService)
-    private playerService: PlayerService;
-
     @Inject(BlipFactory)
     private blipFactory: BlipFactory;
 
@@ -26,18 +16,5 @@ export class StonkProvider {
             sprite: 605,
             scale: 0.9,
         });
-    }
-
-    @OnEvent(ClientEvent.JOBS_STONK_OPEN_SOCIETY_MENU)
-    public onOpenSocietyMenu() {
-        if (this.nuiMenu.isOpen()) {
-            this.nuiMenu.closeMenu();
-            return;
-        }
-
-        // this.nuiMenu.openMenu(MenuType.FoodJobMenu, {
-        //     state: this.state,
-        //     onDuty: this.playerService.isOnDuty(),
-        // });
     }
 }
