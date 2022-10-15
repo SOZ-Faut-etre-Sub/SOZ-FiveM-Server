9dd752e677dc49268a7c0285a4b533fd75fa5ffa
Frozennide <sebastien@sdeu.fr>
2022-10-15 22:35:20 +0200

    feat(core/job/stonk): split shops between north and south

--- resources/[soz]/soz-locations/client/location/robsliquor.lua
+++ resources/[soz]/soz-locations/client/location/robsliquor.lua
@@ -1,5 +1,17 @@
 -- Robs Liquor PolyZone Locations
-Locations["robsliquor"] = {
+Locations["robsliquor-north"] = {
+    [4] = BoxZone:Create(vector3(1166.47, 2708.26, 38.16), 8.4, 7.2,
+                         {name = "robsliquor4", heading = 0, minZ = 37.16, maxZ = 40.16, debugPoly = LocationDebugPoly}),
+    [6] = BoxZone:Create(vector3(-161.23, 6325.47, 31.59), 8.4, 7.2,
+                         {
+        name = "robsliquor6",
+        heading = 135,
+        minZ = 30.59,
+        maxZ = 33.59,
+        debugPoly = LocationDebugPoly,
+    }),
+}
+Locations["robsliquor-south"] = {
     [1] = BoxZone:Create(vector3(-1223.87, -906.37, 12.33), 8.4, 7.2,
                          {name = "robsliquor", heading = 34, minZ = 11.33, maxZ = 14.33, debugPoly = LocationDebugPoly}),
     [2] = BoxZone:Create(vector3(-1487.55, -380.17, 40.16), 8.4, 7.2,
@@ -12,16 +24,6 @@ Locations["robsliquor"] = {
     }),
     [3] = BoxZone:Create(vector3(-2968.92, 390.49, 15.04), 8.4, 7.2,
                          {name = "robsliquor3", heading = 86, minZ = 14.04, maxZ = 17.04, debugPoly = LocationDebugPoly}),
-    [4] = BoxZone:Create(vector3(1166.47, 2708.26, 38.16), 8.4, 7.2,
-                         {name = "robsliquor4", heading = 0, minZ = 37.16, maxZ = 40.16, debugPoly = LocationDebugPoly}),
     [5] = BoxZone:Create(vector3(1136.57, -981.54, 46.42), 8.4, 7.2,
                          {name = "robsliquor5", heading = 98, minZ = 45.42, maxZ = 48.42, debugPoly = LocationDebugPoly}),
-    [6] = BoxZone:Create(vector3(-161.23, 6325.47, 31.59), 8.4, 7.2,
-                         {
-        name = "robsliquor6",
-        heading = 135,
-        minZ = 30.59,
-        maxZ = 33.59,
-        debugPoly = LocationDebugPoly,
-    }),
 }
--- resources/[soz]/soz-shops/client/shops/supermarket.lua
+++ resources/[soz]/soz-shops/client/shops/supermarket.lua
@@ -41,7 +41,11 @@ function SupermarketShop:GenerateMenu()
 end
 
 --- Exports shop
-ShopContext["247supermarket"] = SupermarketShop:new("Superette", "247supermarket", {sprite = 52, color = 2}, "ig_ashley")
-ShopContext["ltdgasoline"] = SupermarketShop:new("Superette", "ltdgasoline", {sprite = 52, color = 2}, "s_m_m_autoshop_02")
-ShopContext["robsliquor"] = SupermarketShop:new("Superette", "robsliquor", {sprite = 52, color = 2}, "a_m_m_genfat_02")
+ShopContext["247supermarket-north"] = SupermarketShop:new("Superette", "247supermarket-north", {sprite = 52, color = 2}, "ig_ashley")
+ShopContext["247supermarket-south"] = SupermarketShop:new("Superette", "247supermarket-south", {sprite = 52, color = 2}, "ig_ashley")
+ShopContext["ltdgasoline-north"] = SupermarketShop:new("Superette", "ltdgasoline-north", {sprite = 52, color = 2}, "s_m_m_autoshop_02")
+ShopContext["ltdgasoline-south"] = SupermarketShop:new("Superette", "ltdgasoline-south", {sprite = 52, color = 2}, "s_m_m_autoshop_02")
+ShopContext["robsliquor-north"] = SupermarketShop:new("Superette", "robsliquor-north", {sprite = 52, color = 2}, "a_m_m_genfat_02")
+ShopContext["robsliquor-south"] = SupermarketShop:new("Superette", "robsliquor-south", {sprite = 52, color = 2}, "a_m_m_genfat_02")
+
 ShopContext["zkea"] = SupermarketShop:new("Zkea", "zkea", {sprite = 123, color = 69}, "ig_brad")
