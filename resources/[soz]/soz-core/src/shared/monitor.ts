ca2279f6c32f6d1344a3ddb1c9d08b56836fb2df
Frozennide <sebastien@sdeu.fr>
2022-10-15 16:03:11 +0200

    feat(core/monitor): add log service

--- resources/[soz]/soz-core/src/shared/monitor.ts
+++ resources/[soz]/soz-core/src/shared/monitor.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '../core/decorators/injectable';
 import { SOZ_CORE_IS_SERVER } from '../globals';
 
+export type LogLevels = 'TRACE' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL';
+
 @Injectable()
 export class Monitor {
     public publish(
@@ -13,6 +15,14 @@ export class Monitor {
             TriggerEvent('monitor:server:event', name, indexed, data);
         } else {
             TriggerServerEvent('monitor:server:event', name, indexed, data, add_player_data);
+        }
+    }
+
+    public log(level: LogLevels, message: string, data: Record<string, any> = {}): void {
+        if (SOZ_CORE_IS_SERVER) {
+            TriggerEvent('monitor:server:Log', level, message, data);
+        } else {
+            TriggerServerEvent('monitor:server:Log', level, message, data);
         }
     }
 }
