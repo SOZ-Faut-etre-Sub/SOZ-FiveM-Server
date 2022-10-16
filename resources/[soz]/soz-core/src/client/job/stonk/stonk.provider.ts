661ad893795aa75a4321f384977c834172b83811
Frozennide <sebastien@sdeu.fr>
2022-10-16 22:09:12 +0200

    feat(core/job/stonk): implement blip in f3

--- resources/[soz]/soz-core/src/client/job/stonk/stonk.provider.ts
+++ resources/[soz]/soz-core/src/client/job/stonk/stonk.provider.ts
@@ -1,12 +1,36 @@
-import { Once, OnceStep } from '../../../core/decorators/event';
+import { Once, OnceStep, OnEvent, OnNuiEvent } from '../../../core/decorators/event';
 import { Inject } from '../../../core/decorators/injectable';
 import { Provider } from '../../../core/decorators/provider';
+import { ClientEvent, NuiEvent } from '../../../shared/event';
+import { baunCraftProcesses } from '../../../shared/job/baun';
+import { MenuType } from '../../../shared/nui/menu';
 import { BlipFactory } from '../../blip';
+import { NuiMenu } from '../../nui/nui.menu';
+import { PlayerService } from '../../player/player.service';
 
 @Provider()
 export class StonkProvider {
     @Inject(BlipFactory)
     private blipFactory: BlipFactory;
+
+    @Inject(PlayerService)
+    private playerService: PlayerService;
+
+    @Inject(NuiMenu)
+    private nuiMenu: NuiMenu;
+
+    private state = {
+        displayResellJewelbagBlip: false,
+        displayResellBigBagBlip: false,
+        displayResellMediumBagBlip: false,
+        displayResellSmallBagBlip: false,
+    };
+
+    @OnNuiEvent(NuiEvent.StonkDisplayBlip)
+    public async onDisplayBlip({ blip, value }: { blip: string; value: boolean }) {
+        this.state[blip] = value;
+        this.blipFactory.hide(blip, !value);
+    }
 
     @Once(OnceStep.PlayerLoaded)
     public onPlayerLoaded() {
@@ -15,6 +39,51 @@ export class StonkProvider {
             coords: { x: 6.25, y: -709.11, z: 46.22 },
             sprite: 605,
             scale: 0.9,
+        });
+
+        this.blipFactory.create('displayResellJewelbagBlip', {
+            name: 'Point de vente des sacs de bijoux',
+            coords: { x: 301.56, y: -884.64, z: 29.24 },
+            sprite: 605,
+            scale: 0.6,
+        });
+        this.blipFactory.hide('displayResellJewelbagBlip', true);
+
+        this.blipFactory.create('displayResellBigBagBlip', {
+            name: "Point de vente des gros sac d'argent",
+            coords: { x: -693.44, y: -581.7, z: 31.55 },
+            sprite: 605,
+            scale: 0.6,
+        });
+        this.blipFactory.hide('displayResellBigBagBlip', true);
+
+        this.blipFactory.create('displayResellMediumBagBlip', {
+            name: "Point de vente des sac d'argent moyen",
+            coords: { x: -1381.23, y: -498.95, z: 33.16 },
+            sprite: 605,
+            scale: 0.6,
+        });
+        this.blipFactory.hide('displayResellMediumBagBlip', true);
+
+        this.blipFactory.create('displayResellSmallBagBlip', {
+            name: "Point de vente des petit sac d'argent",
+            coords: { x: -864.59, y: -192.82, z: 37.7 },
+            sprite: 605,
+            scale: 0.6,
+        });
+        this.blipFactory.hide('displayResellSmallBagBlip', true);
+    }
+
+    @OnEvent(ClientEvent.JOBS_STONK_OPEN_SOCIETY_MENU)
+    public onOpenSocietyMenu() {
+        if (this.nuiMenu.isOpen()) {
+            this.nuiMenu.closeMenu();
+            return;
+        }
+
+        this.nuiMenu.openMenu(MenuType.StonkJobMenu, {
+            state: this.state,
+            onDuty: this.playerService.isOnDuty(),
         });
     }
 }
