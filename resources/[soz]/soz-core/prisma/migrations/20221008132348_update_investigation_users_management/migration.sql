ca1ea7ded9e8638b81484636549e0225621c21cf
Silverlord <jeremy@bernard.sh>
2022-10-19 23:01:56 +0200

    Add concerned people feature to investigation

--- resources/[soz]/soz-core/prisma/migrations/20221008132348_update_investigation_users_management/migration.sql
+++ resources/[soz]/soz-core/prisma/migrations/20221008132348_update_investigation_users_management/migration.sql
@@ -2,4 +2,5 @@
 ALTER TABLE `investigation` ADD COLUMN `granted_jobgrade` LONGTEXT NULL,
     ADD COLUMN `granted_services` LONGTEXT NULL,
     ADD COLUMN `in_charge` LONGTEXT NULL,
-    ADD COLUMN `supervisors` LONGTEXT NULL;
+    ADD COLUMN `supervisors` LONGTEXT NULL,
+    ADD COLUMN `concerned_people` LONGTEXT NULL;
