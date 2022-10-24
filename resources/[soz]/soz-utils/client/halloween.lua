32c2d9754d9f0156cf913a22c0f780aff35ce78c
Lucas Perreau <lucasperreau@gmail.com>
2022-10-24 09:50:31 +0200

    set position

--- resources/[soz]/soz-utils/client/halloween.lua
+++ resources/[soz]/soz-utils/client/halloween.lua
@@ -3,7 +3,8 @@ CreateThread(function()
 end)
 
 if GetConvarInt("feature_halloween", 0) == 1 then
-    AddEventHandler("populationPedCreating", function(_, _, _, _, setters)
-            setters.setModel("u_m_y_zombie_01")
+    AddEventHandler("populationPedCreating", function(x, y, z, _, setters)
+        setters.setModel("u_m_y_zombie_01")
+        setters.setPosition(x, y, z + 0.01)
     end)
 end
