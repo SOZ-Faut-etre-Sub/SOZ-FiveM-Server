9d4354bf04088da7713c1511fe19222440c97826
ZeWorm <116103586+ZeWorm@users.noreply.github.com>
2022-10-26 20:28:45 +0200

    WIP [Halloween] (maps) : nouvelles zones nord

---
+++ resources/[mapping]/[mapping-test]/soz-halloween-upw/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+files {
+    "audio/soz_upw_game.dat151.rel",
+    "audio/soz_upw_mix.dat15.rel",
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/soz_upw_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/soz_upw_mix.dat'
