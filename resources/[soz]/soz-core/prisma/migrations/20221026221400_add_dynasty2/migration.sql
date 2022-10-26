a94e9c1d1c54d5c0ca776688f997fc68517972e1
Lucas Perreau <lucasperreau@gmail.com>
2022-10-26 22:24:28 +0200

    fix(dealership): add dynasty2 reference

---
+++ resources/[soz]/soz-core/prisma/migrations/20221026221400_add_dynasty2/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO vehicles (model, hash, name, price, category, dealership_id, required_licence, size, job_name)
+VALUES  ('dynasty2', 1047668917, 'Dynasty', 0, 'Sportsclassics', NULL, 'car', 1, NULL);
