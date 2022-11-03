76543c7b1634fa1a05b6f6fb7d94dd5d3570a272
Frozennide <sebastien@sdeu.fr>
2022-11-03 17:08:17 +0100

    fix(afk): word can be null

--- resources/[soz]/soz-core/src/client/afk/afk.provider.ts
+++ resources/[soz]/soz-core/src/client/afk/afk.provider.ts
@@ -84,7 +84,7 @@ export class AfkProvider {
                 }
             );
 
-            if (word.toLowerCase() === afkWord) {
+            if (word?.toLowerCase() === afkWord) {
                 this.afkAttempts = 1;
                 this.afkTimer = AFK_SECONDS_UNTIL_KICK;
 
