c8d84adced12f326770815158503affe7d0ad29a
Frozennide <sebastien@sdeu.fr>
2022-11-06 16:05:12 +0100

    feat(boss-shop): migrate boss shop for stonk

--- resources/[soz]/soz-core/src/shared/shop/boss.ts
+++ resources/[soz]/soz-core/src/shared/shop/boss.ts
@@ -11,9 +11,27 @@ export const BossShop: (ShopConfig & { job: string })[] = [
             heading: 340,
             minZ: 75.37,
             maxZ: 77.37,
-            debugPoly: true,
         },
         targets: [],
         products: [{ id: 'weapon_hatchet', type: 'weapon', price: 100 }],
+    },
+    {
+        name: 'stonk',
+        job: 'cash-transfer',
+        zone: {
+            center: [-15.5, -708.83, 45.02],
+            length: 0.6,
+            width: 2.6,
+            heading: 25,
+            minZ: 45.02,
+            maxZ: 48.02,
+        },
+        targets: [],
+        products: [
+            { id: 'outfit', type: 'item', metadata: { label: 'STONK', type: 'stonk' }, price: 100 },
+            { id: 'armor', type: 'item', metadata: { label: 'STONK', type: 'stonk' }, price: 150 },
+            { id: 'weapon_pistol', type: 'weapon', price: 300 },
+            { id: 'pistol_ammo', type: 'weapon_ammo', price: 10 },
+        ],
     },
 ];
