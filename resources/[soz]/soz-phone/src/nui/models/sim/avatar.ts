ae1521797d41ca4e944646c4f5a78b9934b25bda
Frozennide <sebastien@sdeu.fr>
2022-11-21 23:10:42 +0100

    fix(phone): optimize avatar

--- resources/[soz]/soz-phone/src/nui/models/sim/avatar.ts
+++ resources/[soz]/soz-phone/src/nui/models/sim/avatar.ts
@@ -1,5 +1,8 @@
 import { createModel } from '@rematch/core';
 
+import { ServerPromiseResp } from '../../../../typings/common';
+import { SettingsEvents } from '../../../../typings/settings';
+import { fetchNui } from '../../utils/fetchNui';
 import { RootModel } from '../index';
 
 export const avatar = createModel<RootModel>()({
@@ -12,6 +15,12 @@ export const avatar = createModel<RootModel>()({
     effects: dispatch => ({
         setAvatar(payload) {
             dispatch.avatar.SET_AVATAR(payload);
+        },
+        // loader
+        async loadAvatar() {
+            fetchNui<ServerPromiseResp<string>>(SettingsEvents.GET_AVATAR).then(avatar => {
+                dispatch.avatar.setAvatar(avatar.data || '');
+            });
         },
     }),
 });
