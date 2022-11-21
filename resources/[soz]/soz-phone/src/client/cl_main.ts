e40780786fc4d974dfa1608ba53a17198bbabfd1
Frozennide <sebastien@sdeu.fr>
2022-11-21 23:10:42 +0100

    fix(phone): optimize avatar

--- resources/[soz]/soz-phone/src/client/cl_main.ts
+++ resources/[soz]/soz-phone/src/client/cl_main.ts
@@ -131,10 +131,6 @@ async function togglePhone(): Promise<void> {
 onNet(PhoneEvents.SEND_CREDENTIALS, (number: string, societyNumber: string | null) => {
     sendMessage('SIMCARD', PhoneEvents.SET_NUMBER, number);
     sendMessage('SOCIETY_SIMCARD', PhoneEvents.SET_SOCIETY_NUMBER, societyNumber);
-
-    ClUtils.emitNetPromise(SettingsEvents.SET_AVATAR).then(avatar => {
-        sendMessage('AVATAR', SettingsEvents.SET_AVATAR, avatar['data']);
-    });
 });
 
 on('onResourceStop', (resource: string) => {
