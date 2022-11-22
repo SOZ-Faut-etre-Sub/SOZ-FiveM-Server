5bb87b501e62a325a14c311f23dc8bfb57c86f1d
Frozennide <sebastien@sdeu.fr>
2022-11-22 21:19:01 +0100

    fix(phone): send only sound when conversation is open

--- resources/[soz]/soz-phone/src/nui/apps/messages/hooks/useMessageNotifications.ts
+++ resources/[soz]/soz-phone/src/nui/apps/messages/hooks/useMessageNotifications.ts
@@ -1,18 +1,18 @@
 import { useApp } from '@os/apps/hooks/useApps';
 import { useNotifications } from '@os/notifications/hooks/useNotifications';
 import { MessageConversation } from '@typings/messages';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const NOTIFICATION_ID = 'messages:broadcast';
 
 export const useMessageNotifications = () => {
     const navigate = useNavigate();
+    const { pathname } = useLocation();
     const { removeId, addNotification, addNotificationAlert } = useNotifications();
     const { icon: Icon } = useApp('messages');
 
     const setNotification = ({ conversationName, conversationId, message }) => {
         const group: MessageConversation = null;
-
         const id = `${NOTIFICATION_ID}:${conversationId}`;
 
         const notification = {
@@ -27,8 +27,10 @@ export const useMessageNotifications = () => {
         };
 
         addNotificationAlert(notification, n => {
-            removeId(id);
-            addNotification(n);
+            if (!pathname.includes(`/messages/${conversationId}`)) {
+                removeId(id);
+                addNotification(n);
+            }
         });
     };
 
