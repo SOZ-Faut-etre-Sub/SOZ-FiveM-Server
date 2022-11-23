eabb65d3b29cd229e42f38f781433b6f17a53ea5
blaqqendwhyt <57453491+blaqqendwhyt@users.noreply.github.com>
2022-11-23 19:05:41 +0100

    sheriffdodge

---
+++ resources/[vehicles]/[vehicles-prod]/soz-newdodge/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+
+files {
+    "meta/*.meta",
+    "audio/*.rel"
+}
+
+data_file 'CARCOLS_FILE'            "meta/carcols.meta"
+data_file "VEHICLE_METADATA_FILE"   "meta/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE"  "meta/carvariations.meta"
+data_file 'HANDLING_FILE'   "meta/handling.meta"
+data_file "AUDIO_GAMEDATA" "audio/dominator_sheriff_game.dat"
