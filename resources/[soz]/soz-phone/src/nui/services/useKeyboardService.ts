b50dee5e82686a575eb685ddba4d7dcd0c03b3c4
Sébastien DEU <sebastien@deu.sh>
2022-11-24 12:05:39 +0100

    fix(phone): add debouncer for backspace

--- resources/[soz]/soz-phone/src/nui/services/useKeyboardService.ts
+++ resources/[soz]/soz-phone/src/nui/services/useKeyboardService.ts
@@ -1,27 +1,32 @@
 import { useCallback, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { debounce } from '../utils/debounce';
+
 export const useKeyboardService = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
 
-    const onKeyUp = useCallback(
-        (event: KeyboardEvent) => {
-            if (event.key !== 'Backspace') {
-                return;
-            }
+    const onKeyUp = debounce(
+        useCallback(
+            (event: KeyboardEvent) => {
+                if (event.key !== 'Backspace') {
+                    return;
+                }
 
-            if (['input', 'textarea'].includes((event.target as HTMLElement).nodeName.toLowerCase())) {
-                return;
-            }
+                if (['input', 'textarea'].includes((event.target as HTMLElement).nodeName.toLowerCase())) {
+                    return;
+                }
 
-            if (pathname === '/') {
-                return;
-            }
+                if (pathname === '/') {
+                    return;
+                }
 
-            navigate(-1);
-        },
-        [navigate, pathname]
+                navigate(-1);
+            },
+            [navigate, pathname]
+        ),
+        100
     );
 
     useEffect(() => {
