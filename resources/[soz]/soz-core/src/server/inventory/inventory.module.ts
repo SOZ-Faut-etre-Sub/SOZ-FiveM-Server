b1072cf9e11aca318aab3b23609da78291e7147d
Joel Wurtz <joel.wurtz@gmail.com>
2022-12-07 14:01:36 +0100

    fix(cloakroom): readd missing provider in inventory

--- resources/[soz]/soz-core/src/server/inventory/inventory.module.ts
+++ resources/[soz]/soz-core/src/server/inventory/inventory.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '../../core/decorators/module';
+import { InventoryProvider } from './inventory.provider';
 import { InventoryUsageProvider } from './inventory.usage.provider';
 
 @Module({
-    providers: [InventoryUsageProvider],
+    providers: [InventoryUsageProvider, InventoryProvider],
 })
 export class InventoryModule {}
