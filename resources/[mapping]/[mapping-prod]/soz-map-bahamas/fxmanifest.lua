206bf5bc5c3fa49b4d5dc48dce9a81de5b04612e
Rigonkmalk <rigonkmalk@gmail.com>
2022-12-07 21:52:39 +0100

    Revert "WIP: feat(baun/sound): testing purpose for sound in baun"
    This reverts commit a782ea3b9f1b7e820baca00ac7731ddefc5af2b7.

--- resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
@@ -3,11 +3,7 @@ game "gta5"
 this_is_a_map "yes"
 
 files {
-    "audio/v_bahama_game.dat151.rel",
-    "audio/v_bahama_mix.dat151.rel",
-    "stream/3728417233.ymt"
+    "audio/v_bahama_game.dat151.rel"
 }
 
 data_file 'AUDIO_GAMEDATA' 'audio/v_bahama_game.dat'
-data_file 'AUDIO_DYNAMIXDATA' 'audio/v_bahama_mix.dat'
-data_file 'AUDIO_OCCLUSION' 'stream/3728417233.ymt'
