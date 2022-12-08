d43bbd8d7c3fc33267eb797d0170ddbc4a65e135
Joel Wurtz <joel.wurtz@gmail.com>
2022-12-08 22:20:21 +0100

    fix lint

--- resources/[soz]/soz-shops/client/shops/_shell.lua
+++ resources/[soz]/soz-shops/client/shops/_shell.lua
@@ -31,7 +31,7 @@ end
 
 function ShopShell:AddTargetModel()
     exports["qb-target"]:AddTargetModel({self.ped}, {
-        --options = {self:GetPedAction(), self:GunSmith(), self.target},
+        -- options = {self:GetPedAction(), self:GunSmith(), self.target},
         options = {self:GetPedAction(), self.target},
         distance = 2.5,
     })
