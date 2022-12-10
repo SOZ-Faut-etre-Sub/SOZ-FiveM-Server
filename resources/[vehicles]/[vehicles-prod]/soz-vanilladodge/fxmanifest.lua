6510167a1a7e028b5257f397d5b0684157e938a4
Rigonkmalk <rigonkmalk@gmail.com>
2022-12-10 21:03:25 +0100

    feat(dodge_vanilla): add dodge vanilla

---
+++ resources/[vehicles]/[vehicles-prod]/soz-vanilladodge/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+
+files {
+    "meta/*.meta"
+}
+
+data_file "VEHICLE_METADATA_FILE"   "meta/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE"  "meta/carvariations.meta"
+data_file 'HANDLING_FILE'   "meta/handling.meta"
