cf61436f933f1fd17a07d0a1075ab98903d0c605
Frozennide <sebastien@sdeu.fr>
2023-01-02 22:00:11 +0100

    feat(clothes): add lsmc clothes

--- resources/[clothes]/soz_clothes/fxmanifest.lua
+++ resources/[clothes]/soz_clothes/fxmanifest.lua
@@ -10,3 +10,6 @@ data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_soz_bcso.meta'
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_soz_lspd.meta'
 data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_soz_lspd.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_soz_lsmc.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_soz_lsmc.meta'
