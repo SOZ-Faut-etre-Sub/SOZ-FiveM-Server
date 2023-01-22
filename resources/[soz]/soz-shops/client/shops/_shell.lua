c55cd1ce876be793b3872ec2232b786497e7b09b
Laikker <jeremy.kennel@wanadoo.fr>
2023-01-22 02:14:11 +0100

    DLC2 Heist

--- resources/[soz]/soz-shops/client/shops/_shell.lua
+++ resources/[soz]/soz-shops/client/shops/_shell.lua
@@ -70,7 +70,7 @@ function ShopShell:AddTargetModel()
             self:GetPedAction(),
             self:GunSmith(),
             self:ZkeaStock(),
-            self:ZkeaUpgrade(),
+            self:ZkeaStock(),
             {
                 icon = "c:stonk/collecter.png",
                 label = "Collecter",
