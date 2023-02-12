f18e9af944f20e87a82c75b839cccad71b3bf855
mb322 <mb322@ic.ac.uk>
2023-02-12 19:33:39 +0000

    Car charger plug & cable

--- resources/[props]/soz-props-upw/fxmanifest.lua
+++ resources/[props]/soz-props-upw/fxmanifest.lua
@@ -3,3 +3,4 @@ game "gta5"
 
 data_file 'DLC_ITYP_REQUEST' 'stream/sozupwpile.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/upwcarcharger.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/car_charger_plug.ytyp'
