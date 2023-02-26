b5fcf91f64a733f53a36e8ce3d1951b68b58ef2e
mb322 <mb322@ic.ac.uk>
2023-02-26 19:03:16 +0000

    Server side provider

--- resources/[soz]/soz-core/src/server/shop/shop.module.ts
+++ resources/[soz]/soz-core/src/server/shop/shop.module.ts
@@ -2,8 +2,9 @@ import { Module } from '../../core/decorators/module';
 import { BossShopProvider } from './boss.shop.provider';
 import { MaskShopProvider } from './mask.shop.provider';
 import { MigrationProvider } from './migration.provider';
+import { ShopProvider } from './shop.provider';
 
 @Module({
-    providers: [MaskShopProvider, BossShopProvider, MigrationProvider],
+    providers: [MaskShopProvider, BossShopProvider, MigrationProvider, ShopProvider],
 })
 export class ShopModule {}
