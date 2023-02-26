b60236bf377d4d7b69f0ee289e837ec2c7af16ca
mb322 <mb322@ic.ac.uk>
2023-02-26 18:58:07 +0000

    Typing

--- resources/[soz]/soz-core/src/shared/shop.ts
+++ resources/[soz]/soz-core/src/shared/shop.ts
@@ -1,7 +1,10 @@
 import { TargetOptions } from '../client/target/target.factory';
+import { Component, OutfitItem, Prop } from './cloth';
 import { InventoryItemMetadata, Item, ItemType } from './item';
 import { JobType } from './job';
+import { PlayerLicenceType } from './player';
 import { Zone } from './polyzone/box.zone';
+import { Vector3 } from './polyzone/vector';
 
 export enum ClothingBrand {
     PONSONBYS = 'ponsonbys',
@@ -9,14 +12,17 @@ export enum ClothingBrand {
     BINCO = 'binco',
 }
 
+// Superette
 export type ShopProduct = {
     id: string;
     type: ItemType;
     item?: Item;
     metadata?: Partial<InventoryItemMetadata>;
+    requiredLicense?: PlayerLicenceType;
     price: number;
 };
 
+// Boss
 export type ShopConfig = {
     name: string;
     zone: Zone;
@@ -27,4 +33,129 @@ export type ShopConfig = {
 export type BossShopMenu = {
     job: JobType;
     products: ShopProduct[];
+};
+
+// Clothing
+export type ClothingShop = {
+    id: number;
+    name: string;
+    products?: Record<number, ClothingShopItem>;
+    categories?: Record<number, ClothingShopCategory>;
+    stocks?: Record<number, number>;
+};
+
+export type ClothingShopCategory = {
+    id: number;
+    name: string;
+    parentId?: number;
+};
+
+export type ClothingShopItem = {
+    id: number;
+    shopId: number;
+    categoryId?: number;
+    label: string;
+    price: number;
+    modelHash?: number;
+    components?: Record<Component, OutfitItem>;
+    props?: Record<Prop, OutfitItem>;
+};
+
+export type ClothingShopItemData = {
+    components?: Record<Component, OutfitItem>;
+    props?: Record<Prop, OutfitItem>;
+    label?: string;
+    modelHash?: number;
+    correspondingDrawables?: Record<number, number>;
+};
+
+// Tattoo
+export type TattooShopItem = {
+    Name?: string;
+    LocalizedName?: string;
+    Collection?: string;
+    HashNameMale?: string;
+    HashNameFemale?: string;
+    Zone?: string;
+    Price?: number;
+};
+
+export type TattooShopCategory = {
+    label: string;
+    cam: Vector3[];
+    player: Vector3;
+};
+
+// Jewelry
+export type JewelryShopItem = {
+    label: string;
+    overlay: string;
+    components?: Record<Component, OutfitItem> | null;
+    props?: Record<Prop, OutfitItem> | null;
+    price: number;
+};
+
+export type ShopJewelryCategory = {
+    categoryId: number;
+    propId?: Prop;
+    componentId?: Component;
+    overlay: string;
+    price: number;
+    items: Record<string, Record<string, Record<string, { GXT?: string; Localized: string }>>>;
+};
+
+export type ShopJewelryContent = Record<string, ShopJewelryCategory>;
+
+// Barber
+export type BarberShopLabelEntry = {
+    value: number;
+    label: string;
+};
+export type BarberShopColorEntry = {
+    value: number;
+    label: string;
+    r: number;
+    g: number;
+    b: number;
+};
+export type BarberShopLabels = Record<string, BarberShopLabelEntry[]>;
+export type BarberShopColors = Record<string, BarberShopColorEntry[]>;
+
+export type BarberShopCategory = {
+    price: number;
+    category: string;
+    label: string;
+    overlay: string;
+    components: Record<string, boolean>;
+    items: BarberShopLabelEntry[];
+};
+
+export type BarberShopContent = Record<number, BarberShopCategory[]>;
+
+export type BarberConfiguration = {
+    Hair: {
+        HairType?: number;
+        HairColor?: number;
+        HairSecondaryColor?: number;
+        BeardType?: number;
+        BeardColor?: number;
+    };
+    Makeup: {
+        FullMakeupType?: number;
+        FullMakeupOpacity?: number;
+        FullMakeupPrimaryColor?: number;
+        FullMakeupSecondaryColor?: number;
+        BlushType?: number;
+        BlushOpacity?: number;
+        BlushColor?: number;
+        LipstickType?: number;
+        LipstickOpacity?: number;
+        LipstickColor?: number;
+    };
+};
+
+export type BarberShopItem = {
+    config: BarberConfiguration;
+    price: number;
+    overlay: string;
 };
