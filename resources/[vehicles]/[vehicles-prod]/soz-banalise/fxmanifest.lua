cff0dd22ffe73c434b9cc196c8b3ebe3604cbc3a
Rigonkmalk <rigonkmalk@gmail.com>
2023-02-27 20:52:00 +0100

    feat(vehicle/bana): add vehicle_name

--- resources/[vehicles]/[vehicles-prod]/soz-banalise/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-banalise/fxmanifest.lua
@@ -6,6 +6,10 @@ files {
 	"audio/*.rel"
 }
 
+client_script {
+    'vehicle_names.lua'
+}
+
 data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
 data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
 data_file "AUDIO_GAMEDATA" "audio/emergency_bana_game.dat"
