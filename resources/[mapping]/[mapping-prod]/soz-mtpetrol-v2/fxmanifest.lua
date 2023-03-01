3f40cba2cb97ff0f2dd6f302464954e436122a0c
Rigonkmalk <rigonkmalk@gmail.com>
2023-03-01 18:37:37 +0100

    feat(garage/doortuning): add AutoOpen Detection

--- resources/[mapping]/[mapping-prod]/soz-mtpetrol-v2/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-mtpetrol-v2/fxmanifest.lua
@@ -3,8 +3,7 @@ game 'gta5'
 this_is_a_map 'yes'
 
 files {
-
-"interiorproxies.meta"
+    "meta/*.meta"
 }
 
-data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+data_file "DOOR_TUNING_FILE" "meta/doortuning.meta"
