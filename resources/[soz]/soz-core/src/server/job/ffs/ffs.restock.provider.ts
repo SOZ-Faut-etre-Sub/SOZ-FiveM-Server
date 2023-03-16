dc6b7ef0904d82f6ab695a8a37401854b5571fab
mb322 <mb322@ic.ac.uk>
2023-03-16 16:34:08 +0000

    FFS restock adapted and fixed

--- resources/[soz]/soz-core/src/server/job/ffs/ffs.restock.provider.ts
+++ resources/[soz]/soz-core/src/server/job/ffs/ffs.restock.provider.ts
@@ -7,7 +7,7 @@ import { Provider } from '../../../core/decorators/provider';
 import { ServerEvent } from '../../../shared/event';
 import { FfsConfig, Garment, LuxuryGarment } from '../../../shared/job/ffs';
 import { toVector3Object, Vector3 } from '../../../shared/polyzone/vector';
-import { ClothingBrand } from '../../../shared/shop';
+import { ClothingBrand, ClothingShopItem } from '../../../shared/shop';
 import { PrismaService } from '../../database/prisma.service';
 import { InventoryManager } from '../../inventory/inventory.manager';
 import { Monitor } from '../../monitor/monitor';
@@ -43,14 +43,20 @@ export class FightForStyleRestockProvider {
 
     public garmentToCategory(garment: Garment | LuxuryGarment): number {
         switch (garment) {
-            case 'garment_top' || 'luxury_garment_top':
+            case 'garment_top':
+            case 'luxury_garment_top':
                 return 1;
-            case 'garment_pant' || 'luxury_garment_pant':
+            case 'garment_pant':
+            case 'luxury_garment_pant':
                 return 6;
-            case 'garment_shoes' || 'luxury_garment_shoes':
+            case 'garment_shoes':
+            case 'luxury_garment_shoes':
                 return 10;
-            case 'garment_underwear' || 'luxury_garment_underwear':
+            case 'garment_underwear':
+            case 'luxury_garment_underwear':
                 return 19;
+            default:
+                return -1;
         }
     }
 
@@ -100,42 +106,71 @@ export class FightForStyleRestockProvider {
     public async restockLoop(brand: ClothingBrand, garment: Garment | LuxuryGarment, amount: number) {
         const sexes = [1885233650, -1667301416];
         const category = this.garmentToCategory(garment);
+        if (category == -1) {
+            console.error('Invalid category for item ', garment);
+            return;
+        }
         const repo = await this.clothingShopRepository.get();
-        const categoryItems = Object.values(repo.shops[brand].products).filter(
-            product => product.categoryId === category || repo.categories[product.categoryId].parentId === category
-        );
-        let amountLeft = amount;
+        const shopId = repo.shops[brand].id;
 
+        // Fetch all items from this category
+        const allItemsByGender: Record<number, ClothingShopItem[]> = {
+            [1885233650]: [],
+            [-1667301416]: [],
+        };
+        for (const [genderHash, shop_content] of Object.entries(repo.categories)) {
+            for (const shop_category of Object.values(shop_content[shopId])) {
+                if (
+                    shop_category.content != null &&
+                    (shop_category.id == category || shop_category.parentId == category)
+                ) {
+                    shop_category.content.forEach(item => {
+                        allItemsByGender[genderHash].push(item);
+                    });
+                }
+            }
+        }
+
+        let amountLeft = amount;
         while (amountLeft > 0) {
-            const loopAmount = Math.min(10, amountLeft);
+            const loopAmount = Math.min(5, amountLeft); // <--- LoopAmount decreased to 5 to increase the diversity of restocked items
             amountLeft -= loopAmount;
             const loopSex = sexes[Math.floor(Math.random() * 2)];
-            const loopItems = categoryItems.filter(item => item.modelHash === loopSex);
+            console.log('loopSex', loopSex);
+            const loopItems = allItemsByGender[loopSex];
             const randomItem = loopItems[Math.floor(Math.random() * loopItems.length)];
+            console.log('randomItem', randomItem);
             if (!randomItem || !loopItems) {
                 return;
             }
-
-            // Clothes with same drawable
-            const sameDrawableItems = loopItems.filter(item => {
-                for (const [componentId, component] of Object.entries(randomItem.components)) {
-                    if (
-                        componentId != '8' && // <---- This will prevent undershirt from being correctly updated
-                        componentId != '3' && // <---- This will prevent gloves from being correctly updated
-                        item.components[componentId] != null &&
-                        item.components[componentId].Drawable === component.Drawable
-                    ) {
-                        return true;
+            // If item has a modelLabel lag, use it to find other items with same modelLabel (for tops and undershirts)
+            let sameModelsIds: number[] = [];
+            if (randomItem.modelLabel) {
+                const sameModelLabelItems = loopItems.filter(
+                    item => item.modelLabel != null && item.modelLabel === randomItem.modelLabel
+                );
+                sameModelsIds = sameModelLabelItems.map(item => item.id);
+            } else {
+                // Else use legacy method to find items with same drawable
+                const sameDrawableItems = loopItems.filter(item => {
+                    for (const [componentId, component] of Object.entries(randomItem.components)) {
+                        if (
+                            item.components[componentId] != null &&
+                            item.components[componentId].Drawable === component.Drawable
+                        ) {
+                            return true;
+                        }
                     }
-                }
-            });
-            const sameDrawableIds = sameDrawableItems.map(item => item.id);
+                });
+                sameModelsIds = sameDrawableItems.map(item => item.id);
+            }
+            console.log(sameModelsIds);
 
             // Update SQL database
             await this.prismaService.shop_content.updateMany({
                 where: {
                     id: {
-                        in: sameDrawableIds,
+                        in: sameModelsIds,
                     },
                 },
                 data: {
@@ -146,7 +181,7 @@ export class FightForStyleRestockProvider {
             });
 
             // Update repository
-            Object.values(sameDrawableIds).forEach(id => {
+            Object.values(sameModelsIds).forEach(id => {
                 repo.shops[brand].stocks[id] += loopAmount;
             });
         }
