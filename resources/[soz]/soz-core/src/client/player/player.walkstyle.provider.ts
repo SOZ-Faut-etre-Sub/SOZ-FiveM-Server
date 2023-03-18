b7369807f0ac447ee5c94c13c95577405f8e2640
Laikker <jeremy.kennel@wanadoo.fr>
2023-03-18 22:44:19 +0100

    Fix unwanted walkstyle at login + add crouched walkstyle

--- resources/[soz]/soz-core/src/client/player/player.walkstyle.provider.ts
+++ resources/[soz]/soz-core/src/client/player/player.walkstyle.provider.ts
@@ -6,6 +6,12 @@ import { PlayerData } from '../../shared/player';
 import { ResourceLoader } from '../resources/resource.loader';
 import { PlayerService } from './player.service';
 
+/**
+ * Added here as missing in @citizenfx/client/natives_universal.d.ts
+ * Returns The current movement clipset hash.
+ */
+declare function GetPedMovementClipset(ped: number): number;
+
 @Provider()
 export class PlayerWalkstyleProvider {
     @Inject(PlayerService)
@@ -14,29 +20,37 @@ export class PlayerWalkstyleProvider {
     @Inject(ResourceLoader)
     private resourceLoader: ResourceLoader;
 
-    private currentWalkStyle: string | null = null;
     private overrideWalkStyle = false;
 
     @OnEvent(ClientEvent.PLAYER_UPDATE_WALK_STYLE)
     async applyWalkStyle(walkStyle: string | null, overrideWalkStyle = false, transitionSpeed = 1.0): Promise<void> {
-        if (this.currentWalkStyle === walkStyle) {
+        const ped = PlayerPedId();
+        if (GetPedMovementClipset(ped) == GetHashKey(walkStyle)) {
             return;
         }
+
         if (this.overrideWalkStyle && !overrideWalkStyle) {
             return;
         }
 
         this.overrideWalkStyle = overrideWalkStyle;
 
-        ResetPedMovementClipset(PlayerPedId(), transitionSpeed);
-        this.currentWalkStyle = walkStyle;
+        ResetPedMovementClipset(ped, transitionSpeed);
 
         if (walkStyle === null || walkStyle === '') {
             return;
         }
 
         await this.resourceLoader.loadAnimationSet(walkStyle);
-        SetPedMovementClipset(PlayerPedId(), walkStyle, transitionSpeed);
+        SetPedMovementClipset(ped, walkStyle, transitionSpeed);
+
+        if (walkStyle == 'move_ped_crouched') {
+            SetPedWeaponMovementClipset(ped, 'move_ped_crouched');
+            SetPedStrafeClipset(ped, 'move_ped_crouched_strafing');
+        } else {
+            ResetPedWeaponMovementClipset(ped);
+            ResetPedStrafeClipset(ped);
+        }
     }
 
     @OnEvent(ClientEvent.PLAYER_REFRESH_WALK_STYLE)
