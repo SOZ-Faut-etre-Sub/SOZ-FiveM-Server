fd217b1933fb731132e68c990f4ad5326991525f
Rigonkmalk <rigonkmalk@gmail.com>
2023-03-23 18:01:54 +0100

    bahama: add assets; fixed somes issues

---
+++ resources/[mapping]/[mapping-prod]/soz-map-bahamas/client.lua
@@ -0,0 +1,3 @@
+Citizen.CreateThread(function()
+    RequestIpl("hei_sm_16_interior_v_bahama_milo_")
+)
--- resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-map-bahamas/fxmanifest.lua
@@ -1,3 +1,7 @@
 fx_version "cerulean"
 game "gta5"
 this_is_a_map "yes"
+
+client_scripts {
+    "client.lua",
+}
