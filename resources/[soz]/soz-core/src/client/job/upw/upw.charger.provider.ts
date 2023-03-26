e0dadb05370ba43d4a918b797bd84be246ed266b
mb322 <mb322@ic.ac.uk>
2023-03-26 12:43:04 +0100

    Dynamic textures for chargers

--- resources/[soz]/soz-core/src/client/job/upw/upw.charger.provider.ts
+++ resources/[soz]/soz-core/src/client/job/upw/upw.charger.provider.ts
@@ -1,15 +1,23 @@
 import { ProgressService } from '@public/client/progress.service';
+import { UpwChargerRepository } from '@public/client/resources/upw.station.repository';
 import { OnEvent } from '@public/core/decorators/event';
 import { Inject } from '@public/core/decorators/injectable';
 import { Provider } from '@public/core/decorators/provider';
+import { Tick } from '@public/core/decorators/tick';
+import { emitRpc } from '@public/core/rpc';
 import { wait } from '@public/core/utils';
 import { ClientEvent, ServerEvent } from '@public/shared/event';
-import { UpwCharger } from '@public/shared/fuel';
+import { UpwCharger, UpwStation } from '@public/shared/fuel';
+import { getDistance, Vector3 } from '@public/shared/polyzone/vector';
+import { RpcEvent } from '@public/shared/rpc';
 
 @Provider()
 export class UpwChargerProvider {
     @Inject(ProgressService)
     private progressService: ProgressService;
+
+    @Inject(UpwChargerRepository)
+    private upwChargerRepository: UpwChargerRepository;
 
     @OnEvent(ClientEvent.UPW_CREATE_CHARGER)
     public async onCreateCharger(charger: UpwCharger) {
@@ -32,5 +40,27 @@ export class UpwChargerProvider {
             return;
         }
         TriggerServerEvent(ServerEvent.UPW_CREATE_CHARGER, charger);
+    }
+
+    @Tick(5000)
+    public async updateChargerTexture() {
+        const position = GetEntityCoords(PlayerPedId(), false) as Vector3;
+        const chargers = this.upwChargerRepository.getClosestCharger(position);
+        if (getDistance(position, chargers.position) > 50) {
+            return;
+        }
+        const station = await emitRpc<UpwStation>(RpcEvent.UPW_GET_STATION, chargers.station);
+        if (!station) {
+            return;
+        }
+        if (station.stock >= station.max_stock * 0.8) {
+            AddReplaceTexture('upwcarchargertex', 'cc_emit_full', 'upwcarchargertex', 'cc_emit_full');
+        } else if (station.stock >= station.max_stock * 0.5) {
+            AddReplaceTexture('upwcarchargertex', 'cc_emit_full', 'upwcarchargertex', 'cc_emit_66');
+        } else if (station.stock >= station.max_stock * 0.2) {
+            AddReplaceTexture('upwcarchargertex', 'cc_emit_full', 'upwcarchargertex', 'cc_emit_33');
+        } else {
+            AddReplaceTexture('upwcarchargertex', 'cc_emit_full', 'upwcarchargertex', 'cc_emit_empty');
+        }
     }
 }
