65aeb7b88b5af0f3c76a02a2f52f4f5bcc31d4d6
Laikker <jeremy.kennel@wanadoo.fr>
2023-03-28 02:23:42 +0200

    Crimi updates
    * random alert message
    * bin content replaced at vandalism
    * alert chances on vandalism

--- resources/[soz]/soz-core/src/server/inventory/inventory.provider.ts
+++ resources/[soz]/soz-core/src/server/inventory/inventory.provider.ts
@@ -1,3 +1,6 @@
+import { OnEvent } from '@public/core/decorators/event';
+import { ServerEvent } from '@public/shared/event';
+
 import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
 import { Rpc } from '../../core/decorators/rpc';
@@ -11,6 +14,27 @@ import { InventoryManager } from './inventory.manager';
 export class InventoryProvider {
     @Inject(InventoryManager)
     private inventoryManager: InventoryManager;
+
+    private lockedBin: string[] = [];
+
+    @Rpc(RpcEvent.BIN_IS_LOCKED)
+    public isBinLock(source: number, id: string) {
+        return !this.lockedBin.includes(id);
+    }
+
+    @OnEvent(ServerEvent.BIN_CHANGE_LOCK_STATUS)
+    public onBinChangeLockStatus(source: number, id: string, status: boolean) {
+        if (status) {
+            if (!this.lockedBin.includes(id)) {
+                this.lockedBin.push(id);
+            }
+        } else {
+            const index = this.lockedBin.indexOf(id, 0);
+            if (index > -1) {
+                this.lockedBin.splice(index, 1);
+            }
+        }
+    }
 
     @Rpc(RpcEvent.INVENTORY_SEARCH)
     public onSearch(source: number, storageId: string, itemId: string) {
