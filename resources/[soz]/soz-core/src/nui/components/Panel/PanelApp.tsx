b84c1f4b41a7051a55ac93e4b96d2d367b4f00ce
Joel Wurtz <joel.wurtz@gmail.com>
2023-03-30 23:42:58 +0200

    fix(zpad): fix clicking on panel bordel close it

--- resources/[soz]/soz-core/src/nui/components/Panel/PanelApp.tsx
+++ resources/[soz]/soz-core/src/nui/components/Panel/PanelApp.tsx
@@ -34,77 +34,79 @@ export const PanelApp: FunctionComponent = () => {
     }
 
     return (
-        <div className="absolute flex flex-col h-full w-full p-40">
-            <div
-                style={{
-                    height: '97px',
-                }}
-                className="flex flex-row items-center"
-            >
+        <div className="absolute h-full w-full p-40">
+            <div ref={refOutside} className="flex flex-col h-full">
                 <div
-                    style={{ width: '99px', backgroundImage: `url(/public/images/panel/top-left.png)` }}
-                    className="h-full z-20"
-                ></div>
-                <div
-                    style={{ backgroundImage: `url(/public/images/panel/top.png)` }}
-                    className="grow h-full bg-center z-20"
-                ></div>
-                <div
-                    style={{ width: '103px', backgroundImage: `url(/public/images/panel/top-right.png)` }}
-                    className="h-full z-20"
-                ></div>
-            </div>
-            <div className="flex flex-row items-center grow">
-                <div
-                    style={{ width: '99px', backgroundImage: `url(/public/images/panel/left.png)` }}
-                    className="h-full bg-center z-20"
-                ></div>
-                <div ref={refOutside} className="grow h-full relative z-0">
+                    style={{
+                        height: '97px',
+                    }}
+                    className="flex flex-row items-center"
+                >
                     <div
-                        className="absolute z-10"
-                        style={{
-                            zIndex: -1,
-                            width: 'calc(100% + 60px)',
-                            height: 'calc(100% + 60px)',
-                            top: '-30px',
-                            left: '-30px',
-                        }}
-                    >
-                        <iframe
-                            src={showPanel}
+                        style={{ width: '99px', backgroundImage: `url(/public/images/panel/top-left.png)` }}
+                        className="h-full z-20"
+                    ></div>
+                    <div
+                        style={{ backgroundImage: `url(/public/images/panel/top.png)` }}
+                        className="grow h-full bg-center z-20"
+                    ></div>
+                    <div
+                        style={{ width: '103px', backgroundImage: `url(/public/images/panel/top-right.png)` }}
+                        className="h-full z-20"
+                    ></div>
+                </div>
+                <div className="flex flex-row items-center grow">
+                    <div
+                        style={{ width: '99px', backgroundImage: `url(/public/images/panel/left.png)` }}
+                        className="h-full bg-center z-20"
+                    ></div>
+                    <div className="grow h-full relative z-0">
+                        <div
+                            className="absolute z-10"
                             style={{
-                                width: '100%',
-                                height: '100%',
-                                overflow: 'hidden',
+                                zIndex: -1,
+                                width: 'calc(100% + 60px)',
+                                height: 'calc(100% + 60px)',
+                                top: '-30px',
+                                left: '-30px',
                             }}
-                            height="100%"
-                            width="100%"
-                        ></iframe>
+                        >
+                            <iframe
+                                src={showPanel}
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    overflow: 'hidden',
+                                }}
+                                height="100%"
+                                width="100%"
+                            ></iframe>
+                        </div>
                     </div>
+                    <div
+                        style={{ width: '103px', backgroundImage: `url(/public/images/panel/right.png)` }}
+                        className="h-full bg-center  z-20"
+                    ></div>
                 </div>
                 <div
-                    style={{ width: '103px', backgroundImage: `url(/public/images/panel/right.png)` }}
-                    className="h-full bg-center  z-20"
-                ></div>
-            </div>
-            <div
-                style={{
-                    height: '101px',
-                }}
-                className="flex flex-row items-center  z-20"
-            >
-                <div
-                    style={{ width: '99px', backgroundImage: `url(/public/images/panel/bottom-left.png)` }}
-                    className="h-full  z-20"
-                ></div>
-                <div
-                    style={{ backgroundImage: `url(/public/images/panel/bottom.png)` }}
-                    className="h-full grow bg-center z-20"
-                ></div>
-                <div
-                    style={{ width: '103px', backgroundImage: `url(/public/images/panel/bottom-right.png)` }}
-                    className="h-full z-20"
-                ></div>
+                    style={{
+                        height: '101px',
+                    }}
+                    className="flex flex-row items-center  z-20"
+                >
+                    <div
+                        style={{ width: '99px', backgroundImage: `url(/public/images/panel/bottom-left.png)` }}
+                        className="h-full  z-20"
+                    ></div>
+                    <div
+                        style={{ backgroundImage: `url(/public/images/panel/bottom.png)` }}
+                        className="h-full grow bg-center z-20"
+                    ></div>
+                    <div
+                        style={{ width: '103px', backgroundImage: `url(/public/images/panel/bottom-right.png)` }}
+                        className="h-full z-20"
+                    ></div>
+                </div>
             </div>
         </div>
     );
