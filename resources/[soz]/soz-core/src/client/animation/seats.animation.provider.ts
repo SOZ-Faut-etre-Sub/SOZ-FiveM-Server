e4830dd7e733da9783193a81e91caf1f9237a5b2
poulpito <poulpito@ouranos.fr>
2023-04-06 17:05:46 +0200

    Refactor offset config + multiple seat slot on bench

--- resources/[soz]/soz-core/src/client/animation/seats.animation.provider.ts
+++ resources/[soz]/soz-core/src/client/animation/seats.animation.provider.ts
@@ -4,9 +4,10 @@ import { Provider } from '../../core/decorators/provider';
 import { Inject } from '../../core/decorators/injectable';
 import { TargetFactory } from '../target/target.factory';
 import { LoungerTargetList, SeatsTargetList, SeatsTargetConfig } from '../../config/seats';
+import { getDistance } from '@public/shared/polyzone/vector';
 
-let Px:number = 0.0, Py:number = 0.0, Pz:number = 0.0;
-let IsSit:boolean = false;
+let Px = 0.0, Py = 0.0, Pz = 0.0;
+let IsSit = false;
 
 @Provider()
 export class SeatAnimationProvider {
@@ -19,10 +20,17 @@ export class SeatAnimationProvider {
         keys: [{ mapper: 'keyboard', key: 'End' }],
     })
     public async Unsit() {
-        if (Px == null || !IsSit){return;}
+        if (isNaN(Px) || !IsSit){return;}
         const ped = PlayerPedId();
         SetPedCoordsKeepVehicle(ped, Px , Py, Pz); 
         IsSit = false;
+    }
+
+    public Relative2Absolute(Ox, Oy, heading){
+        heading = heading * (Math.PI/180);
+        let x = Ox * Math.cos(heading) - Oy * Math.sin(heading);
+        let y = Ox * Math.sin(heading) + Oy * Math.cos(heading);
+        return [x, y, heading];
     }
 
     public SeatCalculation(entity,ped){
@@ -30,32 +38,46 @@ export class SeatAnimationProvider {
         const [x, y, z] = GetEntityCoords(entity);
         let heading = GetEntityHeading(entity);
         if (heading >= 180) {heading = heading - 179} else {heading = heading + 179}
-        const Offset = SeatsTargetConfig[GetEntityModel(entity)]
-        let heading2 = (heading || 0.0) * (Math.PI/180);
-        let x2 = (Offset.x || 0.0)*Math.cos(heading2) - (Offset.y || 0.0)*Math.sin(heading2);
-        let y2 = (Offset.x || 0.0)*Math.sin(heading2) + (Offset.y || 0.0)*Math.cos(heading2);
-        return [x + x2, y + y2, z + (Offset.z || 0.0), Offset.heading || heading];
+
+        let Offset;
+        let x2 = 0.0, y2 = 0.0, heading2 = 0.0, BestDistance = 2.0;
+        let BestSeat = "0";
+
+        if (Object.keys(SeatsTargetConfig[GetEntityModel(entity)]).length === 1) {
+            Offset = SeatsTargetConfig[GetEntityModel(entity)][0].offset;
+            [x2, y2, heading2] = this.Relative2Absolute(Offset.x, Offset.y, heading);
+        } else {
+            for (const [key, value] of Object.entries(SeatsTargetConfig[GetEntityModel(entity)])) {
+                Offset = SeatsTargetConfig[GetEntityModel(entity)][key].offset;
+                [x2, y2, heading2] = this.Relative2Absolute(Offset.x, Offset.y, heading);
+                let distance = getDistance([Px, Py, Pz], [x+x2, y+y2, z]);
+                if (distance < BestDistance){BestDistance = distance; BestSeat = key}
+            }
+            Offset = SeatsTargetConfig[GetEntityModel(entity)][BestSeat].offset;
+            [x2, y2, heading2] = this.Relative2Absolute(Offset.x, Offset.y, heading); 
+        }
+        return [x + x2, y + y2, z + Offset.z, heading + Offset.heading];
     }
 
-    public SimpleSeat(entity){
+    public async SimpleSeat(entity){
         const ped = PlayerPedId();
         const [Cx,Cy,Cz,Ch] = this.SeatCalculation(entity, ped);
         IsSit = true;
         TaskStartScenarioAtPosition(ped, "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER", Cx, Cy, Cz, Ch, -1, true, true);
     }
-    public SitnDrink(entity){
+    public async SitnDrink(entity){
         const ped = PlayerPedId();
         const [Cx,Cy,Cz,Ch] = this.SeatCalculation(entity, ped);
         IsSit = true;
         TaskStartScenarioAtPosition(ped, "PROP_HUMAN_SEAT_CHAIR_DRINK_BEER", Cx, Cy, Cz, Ch, -1, false, true);
     }
-    public SitnEat(entity){
+    public async SitnEat(entity){
         const ped = PlayerPedId();
         const [Cx,Cy,Cz,Ch] = this.SeatCalculation(entity, ped);
         IsSit = true;
         TaskStartScenarioAtPosition(ped, "PROP_HUMAN_SEAT_CHAIR_FOOD", Cx, Cy, Cz, Ch, -1, false, true);
     }
-    public LoungerSeat(entity){
+    public async LoungerSeat(entity){
         const ped = PlayerPedId();
         const [Cx,Cy,Cz,Ch] = this.SeatCalculation(entity, ped);
         IsSit = true;
@@ -70,7 +92,7 @@ export class SeatAnimationProvider {
             [
                 {
                     label: 'S\'asseoir',
-                    icon: 'fas person-seat',
+                    icon: 'fas fa-person-seat',
                     action: async entity => this.SimpleSeat(entity),
                 }
             ],
@@ -81,7 +103,7 @@ export class SeatAnimationProvider {
             [
                 {
                     label: 'S\'asseoir et Boire',
-                    icon: 'fas beer-mug',
+                    icon: 'fas fa-beer-mug',
                     action: async entity => this.SitnDrink(entity),
                 }
             ],
@@ -92,7 +114,7 @@ export class SeatAnimationProvider {
             [
                 {
                     label: 'S\'asseoir et Manger',
-                    icon: 'fas burger',
+                    icon: 'fas fa-burger',
                     action: async entity => this.SitnEat(entity),
                 }
             ],
@@ -103,7 +125,7 @@ export class SeatAnimationProvider {
             [
                 {
                     label: 'S\'allonger',
-                    icon: 'fas burger',
+                    icon: 'fas fa-umbrella-beach',
                     action: async entity => this.LoungerSeat(entity),
                 }
             ],
