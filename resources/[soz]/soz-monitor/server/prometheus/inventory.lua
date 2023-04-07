1c4dc7efac11f79888ba7cdf8418827b8dc48c87
Joel Wurtz <joel.wurtz@gmail.com>
2023-04-07 16:12:38 +0200

    lint

--- resources/[soz]/soz-monitor/server/prometheus/inventory.lua
+++ resources/[soz]/soz-monitor/server/prometheus/inventory.lua
@@ -39,21 +39,21 @@ local function OverrideConnected()
     playerInventoryListConnected = newPlayerInventoryListConnected
 end
 
---Citizen.CreateThread(function()
+-- Citizen.CreateThread(function()
 --    while true do
 --        BuildListFromDatabase()
 --
 --        Citizen.Wait(60 * 1000)
 --    end
---end)
+-- end)
 
---Citizen.CreateThread(function()
+-- Citizen.CreateThread(function()
 --    while true do
 --        OverrideConnected()
 --
 --        Citizen.Wait(4 * 1000)
 --    end
---end)
+-- end)
 
 local function BuildInventoryMetrics(inventoryMetrics, name, description, type, getValue)
     local lines = {}
