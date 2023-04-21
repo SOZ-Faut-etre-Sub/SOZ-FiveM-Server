28260f4d6c80ece3d8eb4cb238a172281bc9c5f8
Rigonkmalk <rigonkmalk@gmail.com>
2023-04-21 16:10:35 +0200

    feat(car): working on bcso gt500

--- resources/[vehicles]/[vehicles-prod]/soz-highspeed-car/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-highspeed-car/fxmanifest.lua
@@ -8,3 +8,4 @@ files {
 data_file "HANDLING_FILE" "meta/handling.meta"
 data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
 data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
+data_file "CARCOLS_FILE" "meta/carcols.meta"
