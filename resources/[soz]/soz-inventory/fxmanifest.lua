5066ff91a400513e3e9d7291dd3382f0cb6e2494
Corentin Chateau <38319075+Moustash@users.noreply.github.com>
2023-04-28 15:53:22 +0200

    fix(inventory/cars) Disabling control when inventory and phone simult… (#3449)
    * fix(inventory/cars) Disabling control when inventory and phone simultaneous
    
    * fix(correcting style
    
    * Fixing typos and technical optimization
    
    * Fixing github suggestion
    
    * Lua formatting fixes (again)
    
    * fix(inventory/cars) Disabling control when inventory and phone simultaneous
    
    ---------
    
    Co-authored-by: Moustash <toshiirox@gmail.com>

--- resources/[soz]/soz-inventory/fxmanifest.lua
+++ resources/[soz]/soz-inventory/fxmanifest.lua
@@ -4,7 +4,14 @@ lua54 "yes"
 
 shared_scripts {"shared/*.lua", "config.lua", "storages/*.lua"}
 
-client_scripts {"client/main.lua", "client/player.lua", "client/keys.lua", "client/bin.lua", "client/forceconsume.lua"}
+client_scripts {
+    "client/main.lua",
+    "client/player.lua",
+    "client/keys.lua",
+    "client/bin.lua",
+    "client/forceconsume.lua",
+    "client/cl_controls.lua",
+}
 
 server_scripts {
     "@oxmysql/lib/MySQL.lua",
