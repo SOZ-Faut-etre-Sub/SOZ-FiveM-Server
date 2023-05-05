c3a481ec0de018aa0665ba2c2ae5bd0abe95f5d5
Joel Wurtz <joel.wurtz@gmail.com>
2023-05-05 15:12:50 +0200

    fix(hud): fix health position, fix hud in bike

--- resources/[soz]/soz-core/src/nui/components/Hud/Minimap.tsx
+++ resources/[soz]/soz-core/src/nui/components/Hud/Minimap.tsx
@@ -40,35 +40,43 @@ export const Minimap: FunctionComponent = () => {
     const healthPercent = ((playerHealth - 100) * 100) / (player.metadata.max_health - 100);
     const armorPercent = player.metadata.armor.current;
 
+    // console.log(JSON.stringify(minimap, null, 4));
+    const top = (minimap.bottom - 0.003) * 100;
+    const height = 100 - top;
+    const left = (minimap.left + 0.004) * 100;
+    const width = minimap.width * 100;
+
     return (
         <div
-            className="absolute flex py-1 justify-between"
+            className="absolute flex flex-col justify-around"
             style={{
-                height: `calc(100vh - (100vh * ${minimap.bottom}))`,
-                width: `calc(100vw * ${minimap.width})`,
-                top: `calc((100vh * ${minimap.bottom}) - .5rem )`,
-                left: `calc(100vw * ${minimap.left})`,
+                height: `${height}vh`,
+                width: `${width}vw`,
+                top: `${top}vh`,
+                left: `${left}vw`,
             }}
         >
-            <div className="h-5 w-full">
-                <StatusBar
-                    hideCondition={hideHealthCondition}
-                    percent={healthPercent}
-                    backgroundPrimary="rgba(60,152,30,0.5)"
-                    backgroundSecondary="linear-gradient(to top, rgba(71,190,32,0.6) 31%, rgba(79,228,30,0.6) 100%)"
-                >
-                    <IconHealth className="text-white w-3 h-3" />
-                </StatusBar>
-            </div>
-            <div className="h-5 w-full ml-2">
-                <StatusBar
-                    percent={armorPercent}
-                    hideCondition={hideArmorCondition}
-                    backgroundPrimary="rgba(19,90,128,0.5)"
-                    backgroundSecondary="linear-gradient(to top, rgba(19,120,187,0.6) 31%, rgba(23,147,218,0.6) 100%)"
-                >
-                    <IconArmor className="text-white w-3 h-3" />
-                </StatusBar>
+            <div className="flex w-full justify-around mt-[-0.6rem]">
+                <div className="h-5 max-h-[100%] w-[50%] pr-1">
+                    <StatusBar
+                        hideCondition={hideHealthCondition}
+                        percent={healthPercent}
+                        backgroundPrimary="rgba(60,152,30,0.5)"
+                        backgroundSecondary="linear-gradient(to top, rgba(71,190,32,0.6) 31%, rgba(79,228,30,0.6) 100%)"
+                    >
+                        <IconHealth className="text-white h-full" />
+                    </StatusBar>
+                </div>
+                <div className="h-5 max-h-[100%] w-[50%] pl-1">
+                    <StatusBar
+                        percent={armorPercent}
+                        hideCondition={hideArmorCondition}
+                        backgroundPrimary="rgba(19,90,128,0.5)"
+                        backgroundSecondary="linear-gradient(to top, rgba(19,120,187,0.6) 31%, rgba(23,147,218,0.6) 100%)"
+                    >
+                        <IconArmor className="text-white h-full" />
+                    </StatusBar>
+                </div>
             </div>
         </div>
     );
