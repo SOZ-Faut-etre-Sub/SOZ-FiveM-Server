ba90c18a8e020483d6294cec1ec68e4031981742
Laikker <jeremy.kennel@wanadoo.fr>
2023-07-04 20:33:09 +0200

    Alwaus display cayo map + colored cayo map

--- resources/[soz]/soz-core/fxmanifest.lua
+++ resources/[soz]/soz-core/fxmanifest.lua
@@ -8,4 +8,6 @@ ui_page "public/index.html"
 client_script {"client.js", "build/client.js"}
 server_script {"server.js", "build/server.js"}
 
-files {"public/**/*", "build/nui.js"}
+files {"public/**/*", "build/nui.js", "stream/int3232302352.gfx"}
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
