e09b3a262dc21b99ae6fb8cfef836bbc786fdb3e
Sébastien DEU <sebastien@deu.email>
2023-07-09 15:07:09 +0200

    chore: cleanup JSX

--- resources/[soz]/soz-phone/src/nui/os/call/components/CallContactContainer.tsx
+++ resources/[soz]/soz-phone/src/nui/os/call/components/CallContactContainer.tsx
@@ -8,22 +8,18 @@ const CallContactContainer = () => {
     const { call } = useCall();
 
     const { getDisplayByNumber, getPictureByNumber } = useContact();
-    let displayNumber = false;
 
-    const getDisplayOrNumber = () => {
-        const numberToCheck = call.isTransmitter ? call?.receiver : call?.transmitter;
-        displayNumber = numberToCheck == getDisplayByNumber(numberToCheck) ? false : true;
-        return getDisplayByNumber(numberToCheck);
-    }
-       
+    const remoteNumber = call.isTransmitter ? call?.receiver : call?.transmitter;
+    const shouldDisplayNumber = remoteNumber != getDisplayByNumber(remoteNumber);
+
     return (
         <div className="flex flex-col justify-center items-center mt-24 text-white">
             <ContactPicture
                 size={'large'}
                 picture={call.isTransmitter ? getPictureByNumber(call.receiver) : getPictureByNumber(call?.transmitter)}
             />
-            <div className="text-3xl font-light max-w-[90%] truncate">{getDisplayOrNumber()}</div>
-            {displayNumber ? <p>{call.isTransmitter ? call.receiver : call?.transmitter}</p> : null}
+            <div className="text-3xl font-light max-w-[90%] truncate">{getDisplayByNumber(remoteNumber)}</div>
+            {shouldDisplayNumber && <p>{remoteNumber}</p>}
         </div>
     );
 };
