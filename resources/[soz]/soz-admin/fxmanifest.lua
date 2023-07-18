b61847638d9cbccb50c2f3bae6c73306e66cf62e
Joel Wurtz <joel.wurtz@gmail.com>
2023-07-19 00:25:51 +0200

    lint lua

--- resources/[soz]/soz-admin/fxmanifest.lua
+++ resources/[soz]/soz-admin/fxmanifest.lua
@@ -2,14 +2,8 @@ fx_version "cerulean"
 games {"gta5"}
 lua54 "yes"
 
-client_scripts {
-    "client/spectate.lua",
-}
+client_scripts {"client/spectate.lua"}
 
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "server/main.lua",
-    "server/module/*.lua",
-}
+server_scripts {"@oxmysql/lib/MySQL.lua", "server/main.lua", "server/module/*.lua"}
 
 dependencies {"qb-core", "soz-jobs"}
