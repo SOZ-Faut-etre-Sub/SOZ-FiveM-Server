b371fce3622e9c3d80d05d7bb039a3627ae8059f
Laikker <jeremy.kennel@wanadoo.fr>
2023-08-02 21:47:27 +0200

    FDO update

--- resources/[soz]/soz-core/src/server/weapon/weapon.provider.ts
+++ resources/[soz]/soz-core/src/server/weapon/weapon.provider.ts
@@ -14,9 +14,11 @@ import { PlayerStateService } from '../player/player.state.service';
 
 const DIR_WATER_HYDRANT = 13;
 const EXP_TAG_RAYGUN = 70;
+const SMOKEGRENADELAUNCHER = 19;
+const SMOKEGRENADE = 20;
 const FLARE = 22;
 
-const excludeExplosionAlert = [DIR_WATER_HYDRANT, EXP_TAG_RAYGUN, FLARE];
+const excludeExplosionAlert = [DIR_WATER_HYDRANT, EXP_TAG_RAYGUN, SMOKEGRENADELAUNCHER, SMOKEGRENADE, FLARE];
 
 @Provider()
 export class WeaponProvider {
