992c58aead938c9645f17cfb5ab72463c31649a7
Joel Wurtz <joel.wurtz@gmail.com>
2023-08-03 23:03:07 +0200

    fix(vehicle): better management for medium in garage list

--- resources/[soz]/soz-core/src/client/world/object.factory.ts
+++ resources/[soz]/soz-core/src/client/world/object.factory.ts
@@ -1,10 +1,15 @@
 import { Once, OnceStep } from '@core/decorators/event';
+import { Inject } from '@core/decorators/injectable';
 import { Provider } from '@core/decorators/provider';
+import { Logger } from '@core/logger';
 import { SOZ_CORE_IS_SERVER } from '@public/globals';
 import { Vector4 } from '@public/shared/polyzone/vector';
 
 @Provider()
 export class ObjectFactory {
+    @Inject(Logger)
+    private readonly logger: Logger;
+
     private objects: number[] = [];
 
     public create(model: number, position: Vector4, freeze: boolean): number {
@@ -13,6 +18,13 @@ export class ObjectFactory {
             net = true;
         }
         const entity = CreateObjectNoOffset(model, position[0], position[1], position[2], net, false, false);
+
+        if (!entity) {
+            this.logger.error(`Failed to create object ${model} at ${position[0]}, ${position[1]}, ${position[2]}`);
+
+            return;
+        }
+
         SetEntityHeading(entity, position[3]);
 
         if (freeze) {
