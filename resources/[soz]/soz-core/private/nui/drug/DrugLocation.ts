a15384a5ef9041945321f6bf56110234c59e1992
Joel Wurtz <joel.wurtz@gmail.com>
2023-08-15 22:59:02 +0200

    fix typescript for oss

--- resources/[soz]/soz-core/private/nui/drug/DrugLocation.ts
+++ resources/[soz]/soz-core/private/nui/drug/DrugLocation.ts
@@ -1,9 +1,20 @@
 import { createModel } from '@rematch/core';
 
 import { RootModel } from '../../../src/nui/models';
+import { DrugNuiZone } from '../../shared/drugs';
 
 export const drugLocation = createModel<RootModel>()({
-    state: {},
-    reducers: {},
+    state: [] as DrugNuiZone[],
+    reducers: {
+        setZones(state, any) {
+            return state;
+        },
+        addUpdateZone(state, any) {
+            return state;
+        },
+        removeZone(state, any) {
+            return state;
+        },
+    },
     effects: () => ({}),
 });
