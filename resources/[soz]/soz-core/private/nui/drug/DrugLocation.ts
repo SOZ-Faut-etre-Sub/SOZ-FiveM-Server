12a048f794c28b5f28e16db898ea9ad524cd3dd7
Joel Wurtz <joel.wurtz@gmail.com>
2023-08-15 18:00:24 +0200

    fix oss, state should not be null

--- resources/[soz]/soz-core/private/nui/drug/DrugLocation.ts
+++ resources/[soz]/soz-core/private/nui/drug/DrugLocation.ts
@@ -1,1 +1,9 @@
-export const drugLocation = null;
+import { createModel } from '@rematch/core';
+
+import { RootModel } from '../../../src/nui/models';
+
+export const drugLocation = createModel<RootModel>()({
+    state: {},
+    reducers: {},
+    effects: () => ({}),
+});
