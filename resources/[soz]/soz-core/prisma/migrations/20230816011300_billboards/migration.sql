7cf28ce2c86acc2cdc5298df376ccb6ce682e76d
Moustash <corentin.chateau.49@gmail.com>
2023-09-05 22:18:01 +0200

    Migrations

--- resources/[soz]/soz-core/prisma/migrations/20230816011300_billboards/migration.sql
+++ resources/[soz]/soz-core/prisma/migrations/20230816011300_billboards/migration.sql
@@ -16,3 +16,8 @@ CREATE TABLE IF NOT EXISTS `billboard` (
 
     PRIMARY KEY (`id`)
 );
+
+INSERT INTO `billboard` (`id`, `name`, `position`, `originDictName`, `originTextureName`, `imageUrl`, `previewImageUrl`, `templateImageUrl`, `width`, `height`, `lastEdit`, `lastEditor`, `enabled`) VALUES
+	(1, 'Triptyque Gauche', '[-2.19, -1012.34, 77.18]', 'soz_billboards_txd', 'triptyque_gauche', '', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711826889129984/triptyque_gauche_template.webp', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711826889129984/triptyque_gauche_template.webp', 412, 1024, '2023-08-16 01:22:34', '', 0),
+	(2, 'Triptyque Droite', '[-2.19, -1012.34, 77.18]', 'soz_billboards_txd', 'triptyque_droite', '', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711826410971176/triptyque_droite_template.webp', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711826410971176/triptyque_droite_template.webp', 412, 1024, '2023-09-01 07:52:31', '', 0),
+	(3, 'Triptyque Centre', '[-2.19, -1012.34, 77.18]', 'soz_billboards_txd', 'triptyque_centre', '', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711827543445574/triptyque_centre_template.webp', 'https://cdn.discordapp.com/attachments/877529170039672852/1148711827543445574/triptyque_centre_template.webp', 342, 1024, '2023-08-16 01:22:34', '', 1);
