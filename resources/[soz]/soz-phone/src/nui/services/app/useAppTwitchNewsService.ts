3c426f8a9f46cc1389fc226929cec4de7eb81d6f
Moustash <corentin.chateau.49@gmail.com>
2023-09-10 23:38:10 +0200

    Removing Twitch news notification

--- resources/[soz]/soz-phone/src/nui/services/app/useAppTwitchNewsService.ts
+++ resources/[soz]/soz-phone/src/nui/services/app/useAppTwitchNewsService.ts
@@ -1,22 +1,14 @@
 import { useNuiEvent } from '@libs/nui/hooks/useNuiEvent';
-import { TwitchNewsEvents, TwitchNewsMessage } from '@typings/twitch-news';
+import { TwitchNewsEvents } from '@typings/twitch-news';
 import { useEffect } from 'react';
-import { store } from '../../store';
-import { useTwitchNewsNotifications } from '../../apps/twitch-news/hooks/useTwitchNewsNotifications';
 
+import { store } from '../../store';
 
 export const useAppTwitchNewsService = () => {
-    const { setNotification } = useTwitchNewsNotifications();
-
     useEffect(() => {
         store.dispatch.appTwitchNews.loadNews();
     }, []);
 
-    const handleMessageBroadcast = (message: TwitchNewsMessage) => {
-        store.dispatch.appTwitchNews.appendNews(message);
-        setNotification({ message: message.message });
-    };
-
-    useNuiEvent('TWITCH_NEWS', TwitchNewsEvents.CREATE_NEWS_BROADCAST, handleMessageBroadcast);
+    useNuiEvent('TWITCH_NEWS', TwitchNewsEvents.CREATE_NEWS_BROADCAST, store.dispatch.appTwitchNews.appendNews);
     useNuiEvent('TWITCH_NEWS', TwitchNewsEvents.RELOAD_NEWS, store.dispatch.appTwitchNews.loadNews);
 };
