3a209ce17a883c2cf0aed051cc717eeef419721e
Rigonkmalk <rigonkmalk@gmail.com>
2023-09-18 23:28:48 +0200

    refactor(fxmanifest): update to cerulean fxmanifest

--- resources/[mapping]/[mapping-prod]/soz-map-baun/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-map-baun/fxmanifest.lua
@@ -1,6 +1,6 @@
-fx_version "cerulean"
-game "gta5"
-this_is_a_map "yes"
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
 
 client_scripts {
     "client.lua",
--- resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
@@ -1,8 +1,8 @@
 -- This mod was made for free by JRod and Larcius and the FiveM resource bundle was made by leonard1125.
 -- For more information check out https://www.gta5-mods.com/maps/forests-of-san-andreas-revised
 
-fx_version "adamant"
-game "gta5"
+fx_version 'cerulean'
+game 'gta5'
 
 author 'Larcius'
 description 'Forests of San Andreas: Revised'
