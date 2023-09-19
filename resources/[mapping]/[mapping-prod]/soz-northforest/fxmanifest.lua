86855f1a3a546cc15132bd6c5bc08c84bf3630c5
Rigonkmalk <rigonkmalk@gmail.com>
2023-09-19 23:43:27 +0200

    feat(forest): update forest north and south
    feat(forest): update forest south
    
    feat(forest): update forest
    
    feat(forest): update forest
    
    feat(forest): update forest
    
    feat(forest): update forest
    
    feat(forest): update forest
    
    feat(forest): update forest version; working on LOD of trees
    
    feat(trees): add marked trees

--- resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-northforest/fxmanifest.lua
@@ -1,12 +1,12 @@
--- This mod was made for free by JRod and Larcius and the FiveM resource bundle was made by leonard1125.
+-- This mod was made for free by JRod and Larcius.
 -- For more information check out https://www.gta5-mods.com/maps/forests-of-san-andreas-revised
 
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
 author 'Larcius'
-description 'Forests of San Andreas: Revised'
-version '3.3'
+description 'Forests of San Andreas (South): Revised'
+version '4.3'
 
-data_file('DLC_ITYP_REQUEST')('stream/ForestsOfSA_N/metadata/forest_n_slod.ytyp')
-data_file('DLC_ITYP_REQUEST')('stream/ForestsOfSA_S/metadata/forest_s_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/forests_s_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/forests_n_slod.ytyp')
