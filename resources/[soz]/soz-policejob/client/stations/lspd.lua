a81b39bd2524939760b9bf1a7984e982facff9c8
Joel Wurtz <joel.wurtz@gmail.com>
2023-10-11 21:45:02 +0200

    fix removing prisoner cloakroom for lspd and bcso

--- resources/[soz]/soz-policejob/client/stations/lspd.lua
+++ resources/[soz]/soz-policejob/client/stations/lspd.lua
@@ -4,3 +4,10 @@ CreateThread(function()
         menu = MenuV:CreateMenu(nil, "L'ordre et la justice !", "menu_job_lspd", "soz", "lspd:menu"),
     }
 end)
+
+--- Register Targets
+CreateThread(function()
+    exports["qb-target"]:AddBoxZone("lspd:prisonerCloakroom", vector3(580.91, -29.72, 76.63), 0.6, 9.0,
+                                    {name = "lspd:prisonerCloakroom", heading = 350, minZ = 75.63, maxZ = 78.63},
+                                    {options = PoliceJob.Functions.GetCloakroomPrisonerAction(), distance = 2.5})
+end)
