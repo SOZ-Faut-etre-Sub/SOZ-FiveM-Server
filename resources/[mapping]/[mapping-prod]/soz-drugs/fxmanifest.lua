93fd1227c186cdb08b7eedd15df8a6ed62f9c579
Aurukh <133538913+Aurukh@users.noreply.github.com>
2023-10-24 14:39:29 +0200

    update fxmanifest

--- resources/[mapping]/[mapping-prod]/soz-drugs/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-drugs/fxmanifest.lua
@@ -6,4 +6,5 @@ data_file 'DLC_ITYP_REQUEST' 'stream/soz_mushroom_pot.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_props_ciguatoxine.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_props_pandoxine.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_props_krakenine.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/soz_hw_drugs.ytyp'
 
