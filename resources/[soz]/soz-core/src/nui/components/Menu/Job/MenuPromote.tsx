757c59c119ea5544715ba5a4b8d771bbae640474
AurelienFT <aurefook@gmail.com>
2023-11-19 17:30:18 +0100

    Fix banner jobs.

--- resources/[soz]/soz-core/src/nui/components/Menu/Job/MenuPromote.tsx
+++ resources/[soz]/soz-core/src/nui/components/Menu/Job/MenuPromote.tsx
@@ -9,12 +9,19 @@ import { MainMenu, Menu, MenuContent, MenuItemButton, MenuTitle } from '../../St
 type MenuPromoteProps = {
     data: PromoteMenuData;
 };
+const banners_in_core = ['dmc', 'you-news'];
 
 export const MenuPromote: FunctionComponent<MenuPromoteProps> = ({ data }) => {
     if (!data) {
         return null;
     }
-    const banner = `https://nui-img/soz/menu_job_${data.job}`;
+    let banner;
+    if (banners_in_core.includes(data.job)) {
+        banner = `https://cfx-nui-soz-core/public/images/banner/menu_job_${data.job}.webp`;
+    } else {
+        banner = `https://nui-img/soz/menu_job_${data.job}`;
+    }
+
 
     return (
         <Menu type={MenuType.Promote}>
