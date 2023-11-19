1202fdac9911d78d8f39d6926be05ed11e7a56cb
Laikker <jeremy.kennel@wanadoo.fr>
2023-11-19 21:08:52 +0100

    Remove veh from currentFilling list in case of error

--- resources/[soz]/soz-core/src/server/vehicle/vehicle.fuel.provider.ts
+++ resources/[soz]/soz-core/src/server/vehicle/vehicle.fuel.provider.ts
@@ -50,111 +50,114 @@ export class VehicleFuelProvider {
         }
 
         this.currentFilling.add(vehicleNetworkId);
+        try {
+            const vehicleState = this.vehicleStateService.getVehicleState(vehicleNetworkId);
+            const fuelToFill = Math.floor(100 - vehicleState.condition.fuelLevel);
 
-        const vehicleState = this.vehicleStateService.getVehicleState(vehicleNetworkId);
-        const fuelToFill = Math.floor(100 - vehicleState.condition.fuelLevel);
+            const [reservedFuel, station, maxFuelMoney] = await this.lockService.lock(
+                `fuel_station_${stationId}`,
+                async () => {
+                    const station = await this.prismaService.fuel_storage.findUnique({
+                        where: {
+                            id: stationId,
+                        },
+                    });
 
-        const [reservedFuel, station, maxFuelMoney] = await this.lockService.lock(
-            `fuel_station_${stationId}`,
-            async () => {
-                const station = await this.prismaService.fuel_storage.findUnique({
+                    const maxFuelForMoney =
+                        station.price > 0 ? Math.floor(player.money.money / station.price) : fuelToFill;
+                    const reservedFuel = Math.min(fuelToFill, station.stock, maxFuelForMoney);
+                    let reservedFuelTx = reservedFuel;
+
+                    if (station.station === 'Cayo_heli' || station.station === 'Cayo') {
+                        reservedFuelTx = 0;
+                    }
+
+                    await this.prismaService.fuel_storage.update({
+                        where: {
+                            id: station.id,
+                        },
+                        data: {
+                            stock: {
+                                decrement: reservedFuelTx,
+                            },
+                        },
+                    });
+
+                    return [reservedFuel, station, maxFuelForMoney];
+                },
+                1000
+            );
+
+            if (maxFuelMoney <= 0) {
+                this.notifier.notify(source, "Vous n'avez pas assez d'argent.", 'error');
+                this.currentFilling.delete(vehicleNetworkId);
+                TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
+
+                return;
+            }
+
+            if (reservedFuel <= 0) {
+                this.notifier.notify(source, 'La station vient de se vider.', 'error');
+                this.currentFilling.delete(vehicleNetworkId);
+                TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
+
+                return;
+            }
+
+            const duration = Math.max(reservedFuel * 600, 5000);
+
+            TriggerClientEvent(ClientEvent.VEHICLE_FUEL_START, source, duration, reservedFuel, station.price);
+
+            const { progress } = await this.progressService.progress(
+                source,
+                'filling_vehicle',
+                'Remplissage en cours...',
+                duration,
+                {
+                    name: 'gar_ig_5_filling_can',
+                    dictionary: 'timetable@gardener@filling_can',
+                    options: {
+                        enablePlayerControl: false,
+                        repeat: true,
+                        onlyUpperBody: true,
+                    },
+                },
+                {
+                    useAnimationService: true,
+                }
+            );
+
+            const totalFilled = Math.min(reservedFuel, Math.ceil(progress * reservedFuel));
+            const cost = Math.floor(totalFilled * station.price);
+            let leftOver = reservedFuel - totalFilled;
+
+            if (station.price > 0 && !this.playerMoneyService.remove(source, cost)) {
+                this.notifier.notify(source, "Vous n'avez pas assez d'argent.", 'error');
+                leftOver = reservedFuel;
+            } else {
+                this.vehicleStateService.updateVehicleCondition(vehicleNetworkId, {
+                    fuelLevel: vehicleState.condition.fuelLevel + totalFilled,
+                });
+
+                this.notifier.notify(source, `Vous avez payé $${cost} pour ${totalFilled}L de carburant.`, 'success');
+            }
+
+            TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
+
+            if (leftOver > 0) {
+                await this.prismaService.fuel_storage.update({
                     where: {
                         id: stationId,
                     },
-                });
-
-                const maxFuelForMoney = station.price > 0 ? Math.floor(player.money.money / station.price) : fuelToFill;
-                const reservedFuel = Math.min(fuelToFill, station.stock, maxFuelForMoney);
-                let reservedFuelTx = reservedFuel;
-
-                if (station.station === 'Cayo_heli' || station.station === 'Cayo') {
-                    reservedFuelTx = 0;
-                }
-
-                await this.prismaService.fuel_storage.update({
-                    where: {
-                        id: station.id,
-                    },
                     data: {
                         stock: {
-                            decrement: reservedFuelTx,
+                            increment: leftOver,
                         },
                     },
                 });
-
-                return [reservedFuel, station, maxFuelForMoney];
-            },
-            1000
-        );
-
-        if (maxFuelMoney <= 0) {
-            this.notifier.notify(source, "Vous n'avez pas assez d'argent.", 'error');
-            this.currentFilling.delete(vehicleNetworkId);
-            TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
-
-            return;
-        }
-
-        if (reservedFuel <= 0) {
-            this.notifier.notify(source, 'La station vient de se vider.', 'error');
-            this.currentFilling.delete(vehicleNetworkId);
-            TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
-
-            return;
-        }
-
-        const duration = Math.max(reservedFuel * 600, 5000);
-
-        TriggerClientEvent(ClientEvent.VEHICLE_FUEL_START, source, duration, reservedFuel, station.price);
-
-        const { progress } = await this.progressService.progress(
-            source,
-            'filling_vehicle',
-            'Remplissage en cours...',
-            duration,
-            {
-                name: 'gar_ig_5_filling_can',
-                dictionary: 'timetable@gardener@filling_can',
-                options: {
-                    enablePlayerControl: false,
-                    repeat: true,
-                    onlyUpperBody: true,
-                },
-            },
-            {
-                useAnimationService: true,
             }
-        );
-
-        const totalFilled = Math.min(reservedFuel, Math.ceil(progress * reservedFuel));
-        const cost = Math.floor(totalFilled * station.price);
-        let leftOver = reservedFuel - totalFilled;
-
-        if (station.price > 0 && !this.playerMoneyService.remove(source, cost)) {
-            this.notifier.notify(source, "Vous n'avez pas assez d'argent.", 'error');
-            leftOver = reservedFuel;
-        } else {
-            this.vehicleStateService.updateVehicleCondition(vehicleNetworkId, {
-                fuelLevel: vehicleState.condition.fuelLevel + totalFilled,
-            });
-
-            this.notifier.notify(source, `Vous avez payé $${cost} pour ${totalFilled}L de carburant.`, 'success');
-        }
-
-        TriggerClientEvent(ClientEvent.VEHICLE_FUEL_STOP, source);
-        this.currentFilling.delete(vehicleNetworkId);
-
-        if (leftOver > 0) {
-            await this.prismaService.fuel_storage.update({
-                where: {
-                    id: stationId,
-                },
-                data: {
-                    stock: {
-                        increment: leftOver,
-                    },
-                },
-            });
+        } finally {
+            this.currentFilling.delete(vehicleNetworkId);
         }
     }
 }
