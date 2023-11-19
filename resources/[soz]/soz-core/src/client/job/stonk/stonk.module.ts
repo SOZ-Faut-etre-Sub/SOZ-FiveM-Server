a35626a303e751d9342c856e6dc4690278726b6e
Laikker <jeremy.kennel@wanadoo.fr>
2023-11-19 17:31:01 +0100

    Store stonk bag collect history with citizenId and not source

--- resources/[soz]/soz-core/src/client/job/stonk/stonk.module.ts
+++ resources/[soz]/soz-core/src/client/job/stonk/stonk.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '../../../core/decorators/module';
 import { StonkCloakRoomProvider } from './stonk.cloakroom.provider';
-import { StonkCollectProvider } from './stonk.collect.provider';
 import { StonkDeliveryProvider } from './stonk.delivery.provider';
 import { StonkProvider } from './stonk.provider';
 import { StonkResellProvider } from './stonk.resell.provider';
 
 @Module({
-    providers: [
-        StonkProvider,
-        StonkResellProvider,
-        StonkCollectProvider,
-        StonkDeliveryProvider,
-        StonkCloakRoomProvider,
-    ],
+    providers: [StonkProvider, StonkResellProvider, StonkDeliveryProvider, StonkCloakRoomProvider],
 })
 export class StonkModule {}
