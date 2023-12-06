09d3eeca5af0964d87adc3c63be3578d14ba56da
Laikker <jeremy.kennel@wanadoo.fr>
2023-12-06 22:06:27 +0100

    Fix sync of animal no spawn after hunting

--- resources/[soz]/soz-core/src/server/job/food/food.hunt.provider.ts
+++ resources/[soz]/soz-core/src/server/job/food/food.hunt.provider.ts
@@ -1,31 +1,16 @@
 import { OnEvent } from '@core/decorators/event';
 import { Inject } from '@core/decorators/injectable';
 import { Provider } from '@core/decorators/provider';
+import { Monitor } from '@public/client/monitor/monitor';
 import { Rpc } from '@public/core/decorators/rpc';
-import { FieldProvider } from '@public/server/farm/field.provider';
-import { InventoryManager } from '@public/server/inventory/inventory.manager';
-import { ItemService } from '@public/server/item/item.service';
-import { Notifier } from '@public/server/notifier';
-import { ProgressService } from '@public/server/player/progress.service';
 import { ClientEvent, ServerEvent } from '@public/shared/event';
+import { FoodHuntConfig } from '@public/shared/job/food';
 import { RpcServerEvent } from '@public/shared/rpc';
 
 @Provider()
 export class FoodHuntProvider {
-    @Inject(ProgressService)
-    private progressService: ProgressService;
-
-    @Inject(InventoryManager)
-    private inventoryManager: InventoryManager;
-
-    @Inject(FieldProvider)
-    private fieldService: FieldProvider;
-
-    @Inject(ItemService)
-    private itemService: ItemService;
-
-    @Inject(Notifier)
-    private notifier: Notifier;
+    @Inject(Monitor)
+    public monitor: Monitor;
 
     private zonesValue: Record<string, number> = {};
     private zonesDespawnTime: Record<string, number> = {};
@@ -41,12 +26,21 @@ export class FoodHuntProvider {
         }
 
         this.zonesValue[zoneId]++;
-        console.log(zoneId, this.zonesValue[zoneId]);
 
-        if (this.zonesValue[zoneId] >= 10) {
+        if (this.zonesValue[zoneId] >= FoodHuntConfig.noSpawnHarvestCount) {
             this.zonesValue[zoneId] = 0;
-            this.zonesDespawnTime[zoneId] = Date.now() + 3600_000;
-            TriggerClientEvent(ClientEvent.FOOD_HUNT_SYNC, zoneId, this.zonesValue[zoneId]);
+            this.zonesDespawnTime[zoneId] = Date.now() + FoodHuntConfig.noSpawnDelay;
+            TriggerClientEvent(ClientEvent.FOOD_HUNT_SYNC, -1, zoneId, this.zonesDespawnTime[zoneId]);
+
+            this.monitor.publish(
+                'food_hunt_no_spawn',
+                {
+                    zoneId: zoneId,
+                },
+                {
+                    endDate: new Date(this.zonesDespawnTime[zoneId]).toString(),
+                }
+            );
         }
     }
 
