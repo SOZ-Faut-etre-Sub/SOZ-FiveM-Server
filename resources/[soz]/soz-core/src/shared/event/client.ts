687e50f0e0f6498c76ef9525d0d70986c437088f
Laikker <jeremy.kennel@wanadoo.fr>
2024-06-13 03:31:11 +0200

    Fix Smuggling convoy

--- resources/[soz]/soz-core/src/shared/event/client.ts
+++ resources/[soz]/soz-core/src/shared/event/client.ts
@@ -372,6 +372,7 @@ export enum ClientEvent {
     GANG_SMUGGLING_EXPORT_POSITION = 'soz-core:client:gang:smuggling:export-position',
     GANG_SMUGGLING_MINE_DETONATION = 'soz-core:client:gang:smuggling:mine-c4-detonation',
     GANG_SMUGGLING_ELECTRONIC_DETONATION = 'soz-core:client:gang:smuggling:electronic-c4-detonation',
+    GANG_SMUGGLING_CONVOY_TASK = 'soz-core:client:gang:smuggling:convoy-task',
 
     GANG_SMUGGLING_UPDATE_OPENED_RESALERS = 'soz-core:client:gang:smuggling:update-resalers',
     GANG_SMUGGLING_SPAWN_GUARD_RESALER = 'soz-core:client:gang:smuggling:spawn-guard-resalers',
