088d725dcf399e4234b03b7d194eadbc49c15f63
Moustash <corentin.chateau.49@gmail.com>
2024-06-30 16:47:26 +0200

    Tenues CM

--- resources/[soz]/soz-core/src/shared/job/food.ts
+++ resources/[soz]/soz-core/src/shared/job/food.ts
@@ -311,7 +311,7 @@ export const FoodCloakroom: WardrobeConfig = {
                 [5]: { Drawable: 86, Texture: 9, Palette: 0 },
                 [6]: { Drawable: 10, Texture: 0, Palette: 0 },
                 [7]: { Drawable: 31, Texture: 2, Palette: 0 },
-                [8]: { Drawable: 148, Texture: 10, Palette: 0 },
+                [8]: { Drawable: 147, Texture: 10, Palette: 0 },
                 [9]: { Drawable: 0, Texture: 0, Palette: 0 },
                 [10]: { Drawable: 0, Texture: 0, Palette: 0 },
                 [11]: { Drawable: 32, Texture: 4, Palette: 0 },
@@ -387,14 +387,14 @@ export const FoodCloakroom: WardrobeConfig = {
         ['Tenue de prestation']: {
             Components: {
                 [3]: { Drawable: 7, Texture: 0, Palette: 0 },
-                [4]: { Drawable: 133, Texture: 14, Palette: 0 },
+                [4]: { Drawable: 37, Texture: 4, Palette: 0 },
                 [5]: { Drawable: 86, Texture: 9, Palette: 0 },
-                [6]: { Drawable: 13, Texture: 0, Palette: 0 },
+                [6]: { Drawable: 29, Texture: 0, Palette: 0 },
                 [7]: { Drawable: 31, Texture: 2, Palette: 0 },
                 [8]: { Drawable: 38, Texture: 2, Palette: 0 },
                 [9]: { Drawable: 0, Texture: 0, Palette: 0 },
                 [10]: { Drawable: 0, Texture: 0, Palette: 0 },
-                [11]: { Drawable: 25, Texture: 7, Palette: 0 },
+                [11]: { Drawable: 57, Texture: 4, Palette: 0 },
             },
             Props: {
                 [Prop.Hat]: {
