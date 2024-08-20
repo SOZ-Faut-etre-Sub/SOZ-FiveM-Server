5ca465d823bc3392c6be82ea445c935849e73c19
Rigonkmalk <rigonkmalk@gmail.com>
2024-08-20 16:33:19 +0200

    chore(props): merged all props and housing in two differents folder

--- resources/[props]/soz-props-animations/fxmanifest.lua
+++ resources/[props]/soz-props-animations/fxmanifest.lua
@@ -1,7 +1,2 @@
 fx_version "cerulean"
 game "gta5"
-
-data_file 'DLC_ITYP_REQUEST' 'stream/ydr/samnick_prop_lighter01.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/ydr/pata_freevalentinesday2.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/ydr/rpemotes_prop_saxophone01.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/ydr/rpemotes_prop_saxophone02.ytyp'
--- resources/[props]/soz-props-light/fxmanifest.lua
+++
@@ -1,4 +0,0 @@
-fx_version "cerulean"
-game "gta5"
-
-data_file 'DLC_ITYP_REQUEST' 'stream/prop_kino_light_01_assets.ytyp'
