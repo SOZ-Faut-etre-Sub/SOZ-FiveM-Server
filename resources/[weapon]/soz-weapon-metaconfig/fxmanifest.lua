6ea608b79df3ec5a8115eea3c43e8aba3b853397
Laikker <jeremy.kennel@wanadoo.fr>
2024-08-25 16:02:04 +0200

    Cyber EMP no damage + fix drone retrieval

--- resources/[weapon]/soz-weapon-metaconfig/fxmanifest.lua
+++ resources/[weapon]/soz-weapon-metaconfig/fxmanifest.lua
@@ -2,7 +2,11 @@ fx_version "cerulean"
 game "gta5"
 
 files {
+    'stream/explosion.meta',
     'stream/weapons.meta',
+    'stream/weapons_emplauncher.meta',
 }
 
+data_file "EXPLOSION_INFO_FILE" "stream/explosion.meta"
 data_file "WEAPONINFO_FILE_PATCH" "stream/weapons.meta"
+data_file "WEAPONINFO_FILE_PATCH" "stream/weapons_emplauncher.meta"
