d95698e4efa8e7b80dcd2f4ac057f07634535b58
Laikker <jeremy.kennel@wanadoo.fr>
2024-08-25 22:20:44 +0200

    HC fix rebase main

--- resources/[soz]/soz-core/src/server/vehicle/vehicle.order.provider.ts
+++ resources/[soz]/soz-core/src/server/vehicle/vehicle.order.provider.ts
@@ -1,11 +1,17 @@
+import { VehicleBusinessImportConf } from '@private/shared/business.vehicle';
+import { CartelWaitingPlanerOrderTime } from '@private/shared/gang';
+import { Once, OnceStep } from '@public/core/decorators/event';
 import { JobType } from '@public/shared/job';
 import { UpwConfig } from '@public/shared/job/upw';
+import { PlayerData } from '@public/shared/player';
+import { formatDuration } from '@public/shared/utils/timeformat';
+import { getDefaultVehicleConfiguration, VehicleConfiguration } from '@public/shared/vehicle/modification';
+import { PlayerVehicleState } from '@public/shared/vehicle/player.vehicle';
 
-import { BankService } from '../../client/bank/bank.service';
 import { Inject } from '../../core/decorators/injectable';
 import { Provider } from '../../core/decorators/provider';
 import { Rpc } from '../../core/decorators/rpc';
-import { Tick, TickInterval } from '../../core/decorators/tick';
+import { Tick } from '../../core/decorators/tick';
 import { uuidv4 } from '../../core/utils';
 import { BennysConfig } from '../../shared/job/bennys';
 import { RpcServerEvent } from '../../shared/rpc';
@@ -15,10 +21,17 @@ import {
     VehicleCondition,
     VehicleOrder,
     VehicleOrderConfig,
+    VehicleOrderCostMuliplier,
+    VehicleOrderMode,
 } from '../../shared/vehicle/vehicle';
+import { BankService } from '../bank/bank.service';
 import { PrismaService } from '../database/prisma.service';
+import { Monitor } from '../monitor/monitor';
 import { Notifier } from '../notifier';
+import { PlayerMoneyService } from '../player/player.money.service';
 import { PlayerService } from '../player/player.service';
+import { VehicleRepository } from '../repository/vehicle.repository';
+import { VehicleService } from './vehicle.service';
 
 const Configs: Partial<Record<JobType, VehicleOrderConfig>> = {
     [JobType.Upw]: UpwConfig.Order,
@@ -39,91 +52,189 @@ export class VehicleOrderProvider {
     @Inject(PlayerService)
     private playerService: PlayerService;
 
+    @Inject(PlayerMoneyService)
+    private playerMoneyService: PlayerMoneyService;
+
+    @Inject(VehicleService)
+    private vehicleService: VehicleService;
+
+    @Inject(VehicleRepository)
+    private vehicleRepository: VehicleRepository;
+
+    @Inject(Monitor)
+    private monitor: Monitor;
+
     private ordersInProgress: Map<string, VehicleOrder> = new Map();
 
     private orderedVehicle = 0;
 
-    // Tick every minute to check the orders to complete.
-    @Tick(TickInterval.EVERY_MINUTE)
+    @Once(OnceStep.DatabaseConnected)
+    public async init() {
+        const orders = await this.prismaService.vehicle_order.findMany();
+        for (const order of orders) {
+            const data = JSON.parse(order.data);
+            this.ordersInProgress.set(order.id, {
+                uuid: order.id,
+                deliverDate: order.deliverDate.getTime(),
+                model: order.model,
+                gang: data.gang,
+                job: data.job,
+                citizenId: data.citizenId,
+                license: data.license,
+            });
+        }
+    }
+
+    @Tick(10_000)
     public async onTick() {
         for (const [uuid, vehicleOrder] of this.ordersInProgress.entries()) {
             if (vehicleOrder.deliverDate < Date.now()) {
                 await this.addVehicle(vehicleOrder);
                 this.ordersInProgress.delete(uuid);
+                await this.prismaService.vehicle_order.delete({
+                    where: {
+                        id: uuid,
+                    },
+                });
             }
         }
     }
 
     @Rpc(RpcServerEvent.VEHICLE_ORDER_GET)
-    public getOrders(source: number): VehicleOrder[] {
+    public getOrders(source: number, mode: VehicleOrderMode): VehicleOrder[] {
         const player = this.playerService.getPlayer(source);
         if (!player) {
             return [];
         }
 
-        return Array.from(this.ordersInProgress.values()).filter(order => (order.job = player.job.id));
+        if (mode == VehicleOrderMode.Job) {
+            return Array.from(this.ordersInProgress.values()).filter(order => order.job == player.job.id);
+        } else {
+            return Array.from(this.ordersInProgress.values()).filter(order => order.gang == player.gang.id);
+        }
     }
 
     @Rpc(RpcServerEvent.VEHICLE_ORDER_CANCEL)
-    public async onCancelOrder(source: number, uuid: string) {
+    public async onCancelOrder(source: number, uuid: string, mode: VehicleOrderMode) {
         const order = this.ordersInProgress.get(uuid);
         if (!order) {
             this.notifier.notify(source, `Cette commande n'existe pas.`);
             return;
         }
         this.ordersInProgress.delete(uuid);
-        this.notifier.notify(source, `Commande annulée.`);
+        const vehDef = await this.vehicleRepository.findByModel(order.model);
+        this.notifier.notify(
+            source,
+            `${order.gang ? "L'importation" : 'La commande'} du véhicule ~b~${vehDef.name}~s~ a bien été ~r~annulé~s~.`
+        );
 
-        return this.getOrders(source);
+        return this.getOrders(source, mode);
     }
 
     @Rpc(RpcServerEvent.VEHICLE_ORDER_DO)
-    public async onOrderVehicle(source: number, model: string) {
+    public async onOrderVehicle(source: number, model: string, mode: VehicleOrderMode, phone: string) {
         const player = this.playerService.getPlayer(source);
         if (!player) {
             return;
         }
 
-        const config = Configs[player.job.id];
-        const vehicle = await this.prismaService.vehicle.findFirst({
-            where: {
-                model,
-                NOT: {
-                    dealershipId: null,
-                    price: 0,
-                },
-            },
-        });
+        const vehicle = await this.vehicleRepository.findByModel(model);
         if (!vehicle || vehicle.price === 0) {
             this.notifier.notify(source, `Ce modèle de véhicule n'est pas disponible.`);
-            return this.getOrders(source);
+            return this.getOrders(source, mode);
         }
-        const vehiclePrice = Math.ceil(vehicle.price * 0.01);
-        
-        const transferred = await this.bankService.transferFarmMoney(source, 'farm_bennys', 'bennys', vehiclePrice, 'money', true);
 
-        if (!transferred) {
-            this.notifier.notify(
+        let playerDst: PlayerData = null;
+        let waitTime = 0;
+        const vehiclePrice = Math.ceil(vehicle.price * VehicleOrderCostMuliplier[mode]);
+        if (mode == VehicleOrderMode.Job) {
+            const config = Configs[player.job.id];
+            const transferred = await this.bankService.transferFarmMoney(
                 source,
-                `Il faut ~r~${vehiclePrice.toLocaleString()}$~s~ sur le compte de l'entreprise.`
+                config.account,
+                config.farm,
+                vehiclePrice,
+                'money',
+                true
             );
-            return this.getOrders(source);
-        } else {
-            this.notifier.notify(source, `Virement de ~g~${vehiclePrice.toLocaleString()}$~s~ effectué.`);
+
+            if (!transferred) {
+                this.notifier.notify(
+                    source,
+                    `Il faut ~r~${vehiclePrice.toLocaleString()}$~s~ sur le compte de l'entreprise.`
+                );
+                return this.getOrders(source, mode);
+            } else {
+                this.notifier.notify(source, `Virement de ~g~${vehiclePrice.toLocaleString()}$~s~ effectué.`);
+            }
+            waitTime = config.waitingTime;
+        } else if (mode == VehicleOrderMode.Crimi) {
+            playerDst = this.playerService.getPlayerByPhone(phone);
+            if (!playerDst) {
+                this.notifier.notify(source, `Le numéro n'est pas actif.`, 'error');
+                return this.getOrders(source, mode);
+            }
+
+            if (!this.playerMoneyService.remove(source, vehiclePrice, 'marked_money')) {
+                this.notifier.notify(
+                    source,
+                    `Vous n'avez ~r~pas assez d'argent sale~s~ sur vous pour importer ce véhicule. Reviens lorsque tu auras la somme requise !`,
+                    'error'
+                );
+                return this.getOrders(source, mode);
+            }
+
+            waitTime = VehicleBusinessImportConf.VehicleBusinessImportDuration;
+        } else if (mode == VehicleOrderMode.Cartel) {
+            if (!this.playerMoneyService.remove(source, vehiclePrice, 'money')) {
+                this.notifier.notify(source, `Vous n'avez pas assez d'argent.`, 'error');
+                return this.getOrders(source, mode);
+            }
+            playerDst = this.playerService.getPlayer(source);
+            waitTime = CartelWaitingPlanerOrderTime;
         }
 
         const uuid = uuidv4();
-        const coef = GetConvar('soz_core_environment', 'development') ? 30 : 1;
-        this.ordersInProgress.set(uuid, {
+        const order: VehicleOrder = {
             uuid,
             model,
-            deliverDate: Date.now() + (config.waitingTime * 60_000) / coef,
-            job: player.job.id,
+            deliverDate: Date.now() + waitTime * 60_000,
+            job: mode == VehicleOrderMode.Job ? player.job.id : null,
+            gang: [VehicleOrderMode.Crimi, VehicleOrderMode.Cartel].includes(mode) ? player.gang.id : null,
+            citizenId: playerDst ? playerDst.citizenid : null,
+            license: playerDst ? playerDst.license : null,
+        };
+        this.ordersInProgress.set(uuid, order);
+
+        const duration = formatDuration(Date.now() - order.deliverDate);
+        this.notifier.notify(
+            source,
+            `${order.gang ? "L'importation" : 'La commande'} du véhicule ~b~${vehicle.name}~s~ a bien été ~g~réalisé~s~. Elle sera livrée dans ~b~${duration}~s~.`
+        );
+
+        await this.prismaService.vehicle_order.create({
+            data: {
+                id: order.uuid,
+                model: order.model,
+                deliverDate: new Date(order.deliverDate),
+                data: JSON.stringify({
+                    gang: order.gang,
+                    job: order.job,
+                    citizenId: order.citizenId,
+                    license: order.license,
+                }),
+            },
         });
 
-        this.notifier.notify(source, `Votre ${vehicle.model} arrive dans une heure.`);
+        this.monitor.traceEvent('vehicle_order_add', {
+            player_source: source,
+            target_job: order.job,
+            vehicle_model: order.model,
+            id: order.uuid,
+            citizen_ids: [order.citizenId],
+        });
 
-        return this.getOrders(source);
+        return this.getOrders(source, mode);
     }
 
     private async addVehicle(order: VehicleOrder) {
@@ -147,23 +258,48 @@ export class VehicleOrderProvider {
             fuelLevel: fuel,
         };
 
+        const garage = order.job != null ? Configs[order.job].garage : 'garage_gang_' + order.gang;
+        const plate =
+            order.job != null ? 'ESSAI N' + (this.orderedVehicle + 1) : await this.vehicleService.generatePlate();
+        const mods: VehicleConfiguration =
+            order.job != null ? BennysConfig.UpgradeConfiguration : getDefaultVehicleConfiguration();
+        const state = order.job != null ? PlayerVehicleState.InJobGarage : PlayerVehicleState.InGarage;
+
+        const nowInSeconds = Math.round(Date.now() / 1000);
+        const crimiImport = order.gang != null;
+
         await this.prismaService.playerVehicle.create({
             data: {
+                citizenid: order.citizenId,
+                license: order.license,
                 vehicle: order.model,
                 hash: GetHashKey(order.model).toString(),
-                mods: JSON.stringify(BennysConfig.UpgradeConfiguration),
+                mods: JSON.stringify(mods),
                 condition: JSON.stringify(condition),
-                plate: 'ESSAI N' + (this.orderedVehicle + 1),
-                garage: Configs[order.job].garage,
+                plate: plate,
+                garage: garage,
                 job: order.job,
                 category: category,
                 fuel: 100,
                 engine: 1000,
                 body: 1000,
-                state: 3,
+                state: state,
                 life_counter: 3,
+                boughttime: nowInSeconds,
+                parkingtime: nowInSeconds,
+                crimiImport: crimiImport,
             },
         });
+
         this.orderedVehicle++;
+
+        this.monitor.traceEvent('vehicle_order_deliver', {
+            garage_id: garage,
+            target_job: order.job,
+            vehicle_model: order.model,
+            id: order.uuid,
+            citizen_ids: [order.citizenId],
+            vehicle_plate: plate,
+        });
     }
 }
