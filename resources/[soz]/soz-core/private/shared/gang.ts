bae2248b299501b197dd81c20d6b768b391b7bb7
Moustash <corentin.chateau.49@gmail.com>
2024-08-29 17:17:37 +0200

    Oss compliant

--- resources/[soz]/soz-core/private/shared/gang.ts
+++ resources/[soz]/soz-core/private/shared/gang.ts
@@ -1,4 +1,5 @@
 export type Gang = any;
 export type GangType = any;
 export type BusinessVehicleMappingUiInput = any;
+export type BusinessSmugglingPrintUiInput = any;
 export const CartelWaitingPlanerOrderTime = 4;
