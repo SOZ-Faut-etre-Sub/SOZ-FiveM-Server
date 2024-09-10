aaa9517a56f139386641cb3375a21c0e480b8168
Rigonkmalk <rigonkmalk@gmail.com>
2024-09-10 23:56:15 +0200

    feat: change path for ytyp

--- resources/[mapping]/[mapping-prod]/soz-forest-north/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-forest-north/fxmanifest.lua
@@ -8,5 +8,5 @@ author 'Larcius'
 description 'Forests of San Andreas (South): Revised'
 version '4.3'
 
-data_file('DLC_ITYP_REQUEST')('stream/forests_s_slod.ytyp')
-data_file('DLC_ITYP_REQUEST')('stream/forests_n_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/south/forests_s_slod.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream//north/forests_n_slod.ytyp')
