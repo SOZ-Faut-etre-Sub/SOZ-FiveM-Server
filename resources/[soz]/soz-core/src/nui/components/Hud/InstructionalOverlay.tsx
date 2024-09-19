7177f9b912c2d81a46b2f4001227d08dc5fcb2f0
Sébastien DEU <sebastien@deu.email>
2024-09-19 21:17:59 +0200

    refactor: center instructional overlay in HUD

--- resources/[soz]/soz-core/src/nui/components/Hud/InstructionalOverlay.tsx
+++ resources/[soz]/soz-core/src/nui/components/Hud/InstructionalOverlay.tsx
@@ -13,34 +13,34 @@ export function InstructionalOverlay() {
     const styles = useSpring({
         from: {
             opacity: 0,
-            top: '-50vh',
         },
         to: {
             opacity: 1,
             top: text.length > 0 ? `${100 - minimap.bottom * 100}vh` : '-50vh',
-            right: `${minimap.left * 100}vw`,
         },
     });
 
     useNuiEvent('hud', 'SetInstructional', setText);
 
     return (
-        <animated.div className="absolute -mt-12 w-fit h-10" style={styles}>
-            <GlassMorphismContainer
-                borderClassName="rounded-full"
-                className="flex items-center gap-2 text-white px-5 py-1.5 w-full h-10"
-            >
-                {text.map(t => {
-                    if (BindName[t]) {
-                        return (
-                            <span key={t} className="bg-white/10 border border-slate-300/10 px-2 rounded-md">
-                                {bindKeyToName(BindName[t])}
-                            </span>
-                        );
-                    }
-                    return t;
-                })}
-            </GlassMorphismContainer>
+        <animated.div className="absolute inset-x-0 -mt-12 flex justify-center" style={styles}>
+            <div className="h-10 w-fit">
+                <GlassMorphismContainer
+                    borderClassName="rounded-full"
+                    className="flex items-center gap-2 text-white px-5 py-1.5 w-full h-10"
+                >
+                    {text.map(t => {
+                        if (BindName[t]) {
+                            return (
+                                <span key={t} className="bg-white/10 border border-slate-300/10 px-2 rounded-md">
+                                    {bindKeyToName(BindName[t])}
+                                </span>
+                            );
+                        }
+                        return t;
+                    })}
+                </GlassMorphismContainer>
+            </div>
         </animated.div>
     );
 }
