a1db3e2eb8474cbe0324dccbe905bffe456eb0b0
Sébastien DEU <1666044+sebastiendeu@users.noreply.github.com>
2024-09-19 12:58:18 +0200

    chore: disable target check

--- resources/[soz]/soz-core/src/client/target/target.provider.ts
+++ resources/[soz]/soz-core/src/client/target/target.provider.ts
@@ -73,6 +73,8 @@ export class TargetProvider {
 
     @Tick(TickInterval.EVERY_SECOND)
     public async checkTargetMode(): Promise<void> {
+        return; // need a better check
+        
         if (!this._targetFound) return;
 
         const [entityId, entityCoords] = await this.screenService.getEntityOnPosition(
