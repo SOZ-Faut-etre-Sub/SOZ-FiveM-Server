f6c1b98a04255b7922650e72330b68f61cc574ef
Rigonkmalk <rigonkmalk@gmail.com>
2024-09-20 20:59:20 +0200

    feat: add male hair

--- resources/[clothes]/soz_hairs/fxmanifest.lua
+++ resources/[clothes]/soz_hairs/fxmanifest.lua
@@ -2,7 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 files {
-    'mp_f_freemode_01_soz_hairs.meta'
+    'data/*.meta'
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_soz_hairs.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_soz_hairs.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_soz_hairs.meta'
