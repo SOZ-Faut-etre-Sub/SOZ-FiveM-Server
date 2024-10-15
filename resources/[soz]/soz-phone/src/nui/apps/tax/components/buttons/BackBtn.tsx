ad7f8cd55a863b59f5a85804d2f9afbc76e5dfbe
Moustash <corentin.chateau.49@gmail.com>
2024-10-15 16:44:00 +0200

    Fix lint 2

--- resources/[soz]/soz-phone/src/nui/apps/tax/components/buttons/BackBtn.tsx
+++ resources/[soz]/soz-phone/src/nui/apps/tax/components/buttons/BackBtn.tsx
@@ -1,11 +1,6 @@
-import cn from 'classnames';
 import { Link } from 'react-router-dom';
 
-import { useConfig } from '../../../../hooks/usePhone';
-
 const BackBtn = () => {
-    const config = useConfig();
-
     return (
         <Link to={'/tax'} className={'mb-8 text-green-500'}>
             <span className={'font-semibold'}>Retour</span>
