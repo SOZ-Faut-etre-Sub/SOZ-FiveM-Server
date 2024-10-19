5f3bde6813f6a81f22a747238c5c212602c807a1
Laikker <jeremy.kennel@wanadoo.fr>
2024-10-19 22:59:34 +0200

    fix oss

---
+++ resources/[soz]/soz-core/private/client/repository/gang.repository.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@public/core/decorators/injectable';
+
+@Injectable()
+export class GangRepository {
+    public find(id: number) {
+        return null;
+    }
+}
