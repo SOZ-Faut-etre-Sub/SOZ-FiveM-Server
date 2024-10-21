2cb9922aaeb2857bf40555b9178daf3fb0537978
Laikker <jeremy.kennel@wanadoo.fr>
2024-10-22 00:32:42 +0200

    price x10 on weapon attachment at gunsmith

--- resources/[soz]/soz-core/src/shared/weapons/attachment.ts
+++ resources/[soz]/soz-core/src/shared/weapons/attachment.ts
@@ -18,7 +18,7 @@ export const WEAPON_CUSTOM_PRICE = {
     label: 1350,
     repair: 90, // per percentage price
     tint: 1350,
-    attachment: 3600,
+    attachment: 36000,
 };
 
 export const REPAIR_HEALTH_REDUCER = 0.95; // 5% health loss
