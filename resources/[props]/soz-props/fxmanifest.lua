f5c7868479cc156c68c1d65297840d76d6977312
rigonkmalk <rigonkmalk@gmail.com>
2024-11-01 12:11:46 +0100

    chore: merged ytyps

--- resources/[props]/soz-props/fxmanifest.lua
+++ resources/[props]/soz-props/fxmanifest.lua
@@ -2,6 +2,3 @@ fx_version "cerulean"
 game "gta5"
 
 data_file 'DLC_ITYP_REQUEST' 'stream/soz_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/soz_cctv.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/soz_hw24_props.ytyp'
-
