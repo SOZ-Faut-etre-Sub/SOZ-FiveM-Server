3d698ca40dab8f7057e7770c7931e1a7a592c7d0
Joel Wurtz <joel.wurtz@gmail.com>
2024-11-02 19:46:39 +0100

    feat(halloween): add new craft items, add blessed water

---
+++ resources/[soz]/soz-core/src/client/job/upw/upw.halloween.provider.ts
@@ -0,0 +1,32 @@
+import { JobType } from '@public/shared/job';
+
+import { Once } from '../../../core/decorators/event';
+import { Inject } from '../../../core/decorators/injectable';
+import { Provider } from '../../../core/decorators/provider';
+import { ServerEvent } from '../../../shared/event/server';
+import { Feature } from '../../../shared/features';
+import { FeatureProvider } from '../../feature/feature.provider';
+import { TargetFactory } from '../../target/target.factory';
+
+@Provider()
+export class UpwHalloweenProvider {
+    @Inject(TargetFactory)
+    private targetFactory: TargetFactory;
+
+    @Inject(FeatureProvider)
+    private featureProvider: FeatureProvider;
+
+    @Once()
+    public onStart() {
+        this.targetFactory.createForModel(GetHashKey('prop_storagetank_06'), [
+            {
+                label: "Un filet d'eau lumineux s'échappe de la cuve...",
+                job: JobType.Upw,
+                category: 'society',
+                action: () => {
+                    TriggerServerEvent(ServerEvent.UPW_GET_BLESSED_WATER);
+                },
+            },
+        ]);
+    }
+}
