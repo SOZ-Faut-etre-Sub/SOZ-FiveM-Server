09267568282bf3dec0a1e97eb0465345c7b4544f
Exaustia <Ferrer.benoit@orange.fr>
2024-11-11 18:29:34 +0100

    Create fxmanifest.lua
    add custom umbrella

--- resources/[soz]/soz-core/src/client/item/item.provider.ts
+++ resources/[soz]/soz-core/src/client/item/item.provider.ts
@@ -85,7 +85,13 @@ export class ItemProvider {
     }
 
     @OnEvent(ClientEvent.ITEM_UMBRELLA_TOGGLE)
-    async onUmbrellaToggle(): Promise<void> {
+    async onUmbrellaToggle(itemName: string): Promise<void> {
+        const models = {
+            ['umbrella']: 'p_amb_brolly_01',
+            ['umbrella_white']: 'p_amb_brolly_02',
+            ['umbrella_black']: 'p_amb_brolly_03',
+        };
+
         this.animationService.toggleAnimation({
             base: {
                 dictionary: 'amb@world_human_drinking@coffee@male@base',
@@ -100,7 +106,7 @@ export class ItemProvider {
             props: [
                 {
                     bone: 57005,
-                    model: 'p_amb_brolly_01',
+                    model: models[itemName],
                     position: [0.12, 0.005, 0.0],
                     rotation: [280.0, 10.0, 350.0],
                 },
