30c31ee0c28f9154b0dee85f0b1adb8d7f2e761e
rigonkmalk <rigonkmalk@gmail.com>
2024-11-20 21:21:49 +0100

    feat: add volatus1 and polmav sasp logo

--- resources/[vehicles]/[vehicles-prod]/soz-metaconfig/vehicle_names.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-metaconfig/vehicle_names.lua
@@ -56,6 +56,8 @@ Citizen.CreateThread(function()
 
     AddTextEntryByHash(GetHashKey('SASP1'), "Dominator GTX")
     AddTextEntryByHash(GetHashKey('SASP1_LIV_1'), "SASP Livery")
+    AddTextEntryByHash(GetHashKey('volatus_sign_1'), "Default")
+    AddTextEntryByHash(GetHashKey('volatus_sign_2'), "GOUV")
 
     AddTextEntryByHash(GetHashKey('POLICE_ANTENNA'), "Antenna")
     AddTextEntryByHash(GetHashKey('POLICE_DIVIDER'), "Divider")
