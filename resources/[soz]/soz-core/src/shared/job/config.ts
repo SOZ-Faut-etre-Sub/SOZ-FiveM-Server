89628772ef370dd078e40e5c2795341b0d2df82c
Jeremy Bernard <jeremy@bernard.sh>
2024-11-25 10:59:20 +0100

    feat(panel): allow veh check & transfert for gouv in police panel

--- resources/[soz]/soz-core/src/shared/job/config.ts
+++ resources/[soz]/soz-core/src/shared/job/config.ts
@@ -523,6 +523,8 @@ export const JobRegistry: Record<JobType, Omit<Job, 'id'>> = {
             [JobPermission.GouvUpdateTax]: { label: 'Mettre à jour les taxes' },
             [JobPermission.GouvManageRadar]: { label: 'Permet de gérer les radars' },
             [JobPermission.GouvManageFine]: { label: 'Ajouter, mettre à jour et supprimer les amendes' },
+            [JobPermission.VehicleRegistrar]: { label: 'Accès au registre des véhicules sur le panel' },
+            [JobPermission.VehicleTransfert]: { label: 'Accès aux transferts de véhicules sur le panel' },
             [JobPermission.MdrViewCitizenData]: { label: 'Accès aux casiers judiciaires' },
         },
         bossZones: [
