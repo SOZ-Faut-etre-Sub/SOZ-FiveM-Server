29f9dd71ab17596c1339ac7b6594cfd198fe33cb
Laikker <jeremy.kennel@wanadoo.fr>
2024-11-25 20:09:52 +0100

    Update weather temperature

--- resources/[soz]/soz-core/src/server/weather/weather.provider.ts
+++ resources/[soz]/soz-core/src/server/weather/weather.provider.ts
@@ -30,8 +30,8 @@ import { FeatureProvider } from '../feature/feature.provider';
 import { Monitor } from '../monitor/monitor';
 import { Pollution } from '../pollution';
 import { Store } from '../store/store';
-import { Halloween, Polluted, Summer, Winter, WMOWeatherMapping } from './forecast';
-import { DaySummerTemperature, ForecastAdderTemperatures, NightSummerTemperature } from './temperature';
+import { Halloween, Polluted, SpringAutumn, Winter, WMOWeatherMapping } from './forecast';
+import { DayAutumnTemperature, ForecastAdderTemperatures, NightAutumnTemperature } from './temperature';
 
 const MAX_FORECASTS = 6;
 const UPDATE_TIME_INTERVAL = 5;
@@ -61,11 +61,11 @@ export class WeatherProvider {
     private timeReady = false;
     private currentTime: Time = { hour: 2, minute: 0, second: 0 };
     // See forecast.ts for the list of available forecasts
-    private forecast: Forecast = Summer;
+    private forecast: Forecast = SpringAutumn;
     // See temperature.ts for the list of available temperature ranges,
     // please ensure that the day and night temperature ranges are using the same season
-    private dayTemperatureRange: TemperatureRange = DaySummerTemperature;
-    private nightTemperatureRange: TemperatureRange = NightSummerTemperature;
+    private dayTemperatureRange: TemperatureRange = DayAutumnTemperature;
+    private nightTemperatureRange: TemperatureRange = NightAutumnTemperature;
 
     private defaultWeather: Weather = 'OVERCAST';
 
