4f1ef9f3abbc4f17d0b8b8872df7b9f410a739f9
rigonkmalk <rigonkmalk@gmail.com>
2024-11-28 15:20:09 +0100

    feat: add lake and ice sea + skidoo + dam ice

---
+++ resources/[vehicles]/[vehicles-prod]/skidoo800R/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+	"data/*.meta"
+}
+
+data_file 'HANDLING_FILE'			'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
