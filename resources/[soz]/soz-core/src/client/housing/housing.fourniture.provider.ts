b72b6f80a4bc5652b9847657a50ed1d8e6091c0a
DreamXZE <dreamxze@hotmail.com>
2025-01-18 16:00:27 +0100

    Remove alpha on debug prop

--- resources/[soz]/soz-core/src/client/housing/housing.fourniture.provider.ts
+++ resources/[soz]/soz-core/src/client/housing/housing.fourniture.provider.ts
@@ -1089,7 +1089,7 @@ export class HousingFournitureProvider {
             this.objectService.applyEntityMatrix(entity, prop.matrix);
         }
 
-        SetEntityAlpha(entity, 200, false);
+        // SetEntityAlpha(entity, 200, false);
         SetEntityCollision(entity, false, false);
         SetEntityInvincible(entity, true);
         FreezeEntityPosition(entity, true);
