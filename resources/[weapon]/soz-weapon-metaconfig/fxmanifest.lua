0668a5bbe1cb13dbf9ff912407a5fb036c427644
Laikker <jeremy.kennel@wanadoo.fr>
2025-01-19 00:26:13 +0100

    Fix snowball damage

--- resources/[weapon]/soz-weapon-metaconfig/fxmanifest.lua
+++ resources/[weapon]/soz-weapon-metaconfig/fxmanifest.lua
@@ -5,8 +5,10 @@ files {
     'stream/explosion.meta',
     'stream/weapons.meta',
     'stream/weapons_emplauncher.meta',
+    'stream/weaponsnowball.meta',
 }
 
 data_file "EXPLOSION_INFO_FILE" "stream/explosion.meta"
 data_file "WEAPONINFO_FILE_PATCH" "stream/weapons.meta"
 data_file "WEAPONINFO_FILE_PATCH" "stream/weapons_emplauncher.meta"
+data_file "WEAPONINFO_FILE_PATCH" "stream/weaponsnowball.meta"
