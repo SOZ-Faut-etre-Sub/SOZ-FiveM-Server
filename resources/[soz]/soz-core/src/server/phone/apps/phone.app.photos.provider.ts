72cac1b1fb4a341a9051dd1dc7db39a227329d8b
Sébastien DEU <sebastien@deu.email>
2025-02-08 11:26:45 +0100

    feat(phone): add photo ordering by descending ID

--- resources/[soz]/soz-core/src/server/phone/apps/phone.app.photos.provider.ts
+++ resources/[soz]/soz-core/src/server/phone/apps/phone.app.photos.provider.ts
@@ -29,6 +29,9 @@ export class PhoneAppPhotosProvider {
             where: {
                 identifier: player.citizenid,
             },
+            orderBy: {
+                id: 'desc',
+            },
         });
     }
 
