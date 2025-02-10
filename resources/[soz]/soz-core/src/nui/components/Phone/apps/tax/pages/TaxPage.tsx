92f6ddc38696ef263c6211f9aae5f90c283fbcd3
Sébastien DEU <sebastien@deu.email>
2025-02-10 20:03:58 +0100

    feat(phone): improve TaxPage styling with theme support

--- resources/[soz]/soz-core/src/nui/components/Phone/apps/tax/pages/TaxPage.tsx
+++ resources/[soz]/soz-core/src/nui/components/Phone/apps/tax/pages/TaxPage.tsx
@@ -25,45 +25,50 @@ export const TaxPage = () => {
     useAppTitleGetBackUpdater(() => navigate(-1));
 
     return (
-        <AppWrapper scrollable>
-            <AppContent>
+        <AppWrapper className="flex" scrollable>
+            <AppContent
+                className={clsx({
+                    'text-gray-100': theme === 'dark',
+                    'text-gray-700': theme === 'light',
+                })}
+            >
                 {tax ? (
-                    <>
-                        <div className="flex justify-center">
-                            <img
-                                src={getPath(`images/phone/apps/tax/icon/${tax.id}.webp`)}
-                                alt="taxes"
-                                className="h-16"
-                            />
+                    <div className="grow flex flex-col justify-between mb-10">
+                        <div>
+                            <div className="flex justify-center">
+                                <img
+                                    src={getPath(`images/phone/apps/tax/icon/${tax.id}.webp`)}
+                                    alt="taxes"
+                                    className="h-16"
+                                />
+                            </div>
+                            <div className="pt-2 flex justify-center mb-12 py-1">
+                                <h1 className="text-xl font-bold">{taxDescription.title}</h1>
+                            </div>
+                            <div className="flex flex-col gap-4 ">
+                                <section className="flex flex-col gap-4">
+                                    <span className="font-semibold text-green-500 text-lg">
+                                        Présentation de la taxe
+                                    </span>
+                                    {taxDescription.description}
+                                </section>
+                                <section className="flex flex-col gap-4">
+                                    <span className="font-semibold text-green-500 text-lg">Qui la modifie?</span>
+                                    {taxDescription.whoModifies}
+                                </section>
+                            </div>
                         </div>
-                        <div className="pt-2 flex justify-center mb-12 py-1">
-                            <h1
-                                className={clsx(
-                                    {
-                                        'text-gray-100': theme === 'dark',
-                                        'text-gray-700': theme === 'light',
-                                    },
-                                    'text-xl font-bold'
-                                )}
-                            >
-                                {taxDescription.title}
-                            </h1>
+
+                        <div
+                            className={clsx('h-14 font-semibold shadow-lg rounded-lg p-4 flex justify-between', {
+                                'bg-ios-700 text-gray-100 ': theme === 'dark',
+                                'bg-white text-gray-700 ': theme === 'light',
+                            })}
+                        >
+                            <span>Montant de la taxe</span>
+                            <span>{tax.value || 'N/A'}%</span>
                         </div>
-                        <div className="flex flex-col gap-4 ">
-                            <section className="flex flex-col gap-4">
-                                <span className="font-semibold text-green-500 text-lg">Présentation de la taxe</span>
-                                {taxDescription.description}
-                            </section>
-                            <section className="flex flex-col gap-4">
-                                <span className="font-semibold text-green-500 text-lg">Qui la modifie?</span>
-                                {taxDescription.whoModifies}
-                            </section>
-                        </div>
-                        <div className="h-14 bg-white shadow-lg rounded-lg p-4 flex justify-between mt-14">
-                            <span className="text-black font-semibold">Montant de la taxe</span>
-                            <span className="text-black font-semibold">{tax.value || 'N/A'}%</span>
-                        </div>
-                    </>
+                    </div>
                 ) : (
                     <span className="text-white mt-14">Tax not found</span>
                 )}
