c511765cce6b32768b7cb87df75fbb80a5e4fc8e
Laikker <jeremy.kennel@wanadoo.fr>
2025-03-01 16:57:40 +0100

    Fix unique item metadata creation

--- resources/[soz]/soz-core/src/server/inventory/inventory.ts
+++ resources/[soz]/soz-core/src/server/inventory/inventory.ts
@@ -183,45 +183,6 @@ export class Inventory {
         amount = Math.round(amount);
         metadata = metadata || {};
 
-        if (itemObject.type === 'weapon') {
-            metadata = {
-                serial: uuidv4(),
-                tint: 0,
-                health: 2000,
-                maxHealth: 2000,
-                ...metadata,
-            };
-        }
-
-        if (itemObject.expiresIn && !metadata?.expiration) {
-            metadata = {
-                ...metadata,
-                expiration: startOfMinute(addMinutes(new Date(), itemObject.expiresIn)).toUTCString(),
-            };
-        }
-
-        if (itemObject.durability && !metadata?.expiration) {
-            metadata = {
-                ...metadata,
-                expiration: startOfDay(addDays(new Date(), itemObject.durability)).toUTCString(),
-            };
-        }
-
-        if (!metadata?.creation && itemObject.type === 'evidence') {
-            metadata = {
-                ...metadata,
-                creation: new Date().toUTCString(),
-            };
-        }
-
-        if (itemObject.storageItemType) {
-            metadata = {
-                id: uuidv4(),
-                storageElements: [],
-                ...metadata,
-            };
-        }
-
         if (!bypassCheck && !isItemAllowed(itemObject.type, id, metadata, this._configuration)) {
             return Err('not_allowed');
         }
@@ -307,6 +268,47 @@ export class Inventory {
             }
 
             slot = foundSlot ?? currentMaxSlot + 1;
+        }
+
+        metadata = metadata || {};
+
+        if (item.type === 'weapon') {
+            metadata = {
+                serial: uuidv4(),
+                tint: 0,
+                health: 2000,
+                maxHealth: 2000,
+                ...metadata,
+            };
+        }
+
+        if (item.expiresIn && !metadata?.expiration) {
+            metadata = {
+                ...metadata,
+                expiration: startOfMinute(addMinutes(new Date(), item.expiresIn)).toUTCString(),
+            };
+        }
+
+        if (item.durability && !metadata?.expiration) {
+            metadata = {
+                ...metadata,
+                expiration: startOfDay(addDays(new Date(), item.durability)).toUTCString(),
+            };
+        }
+
+        if (!metadata?.creation && item.type === 'evidence') {
+            metadata = {
+                ...metadata,
+                creation: new Date().toUTCString(),
+            };
+        }
+
+        if (item.storageItemType) {
+            metadata = {
+                id: uuidv4(),
+                storageElements: [],
+                ...metadata,
+            };
         }
 
         const inventoryItem: InventoryItem = {
