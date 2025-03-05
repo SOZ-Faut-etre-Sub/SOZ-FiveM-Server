566bcdfc8908e257103d0d87be25941957bde2c7
Laikker <jeremy.kennel@wanadoo.fr>
2025-03-05 01:10:04 +0100

    Gang can full NPC vehicle

--- resources/[soz]/soz-core/src/shared/event/server.ts
+++ resources/[soz]/soz-core/src/shared/event/server.ts
@@ -627,6 +627,7 @@ export enum ServerEvent {
     GANG_VEHBIZ_REPAIR_ENGINE = 'soz-core:server:gang:veh:repair-engine',
     GANG_VEHBIZ_REPAIR_BODY = 'soz-core:server:gang:veh:repair-body',
     GANG_VEHBIZ_REPAIRKIT = 'soz-core:server:gang:veh:repair-kit',
+    GANG_VEHBIZ_FULL = 'soz-core:server:gang:veh:full',
 
     GANG_SMUGGLING_BLACK_MARKET_BOX_USE = 'soz-core:server:gang:smuggling:black-market-box-use',
     GANG_SMUGGLING_BLACK_MARKET_PRICE_UPDATE = 'soz-core:server:gang:smuggling:black-market-price',
