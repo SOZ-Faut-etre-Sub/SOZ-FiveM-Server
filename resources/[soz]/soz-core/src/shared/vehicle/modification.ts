28c50217624f664b51e3b305e99777f4d6349daf
Moustash <corentin.chateau.49@gmail.com>
2025-03-05 16:40:39 +0100

    [ADMIN BLOG] - Ajustements

--- resources/[soz]/soz-core/src/shared/vehicle/modification.ts
+++ resources/[soz]/soz-core/src/shared/vehicle/modification.ts
@@ -1989,27 +1989,27 @@ export const VehicleModificationPricing: Partial<
     Record<keyof VehicleModification | 'manualGearBox', VehicleLsCustomBaseConfigItem>
 > = {
     engine: {
-        priceByLevels: [0.04, 0.06, 0.07, 0.09, 0.1],
+        priceByLevels: [0.04, 0.06, 0.08, 0.1, 0.12],
         type: 'list',
     },
     brakes: {
-        priceByLevels: [0.04, 0.05, 0.06, 0.1, 0.1],
+        priceByLevels: [0.03, 0.04, 0.06, 0.08, 0.1],
         type: 'list',
     },
     transmission: {
-        priceByLevels: [0.02, 0.03, 0.04, 0.05, 0.1],
+        priceByLevels: [0.03, 0.04, 0.06, 0.08, 0.1],
         type: 'list',
     },
     suspension: {
-        priceByLevels: [0.01, 0.02, 0.03, 0.05, 0.1],
+        priceByLevels: [0.02, 0.03, 0.04, 0.06, 0.1],
         type: 'list',
     },
     armor: {
-        priceByLevels: [0.02, 0.04, 0.05, 0.06, 0.08],
+        priceByLevels: [0.04, 0.06, 0.08, 0.1, 0.12],
         type: 'list',
     },
     turbo: {
-        priceByLevels: [0, 0.05],
+        priceByLevels: [0, 0.08],
         type: 'toggle',
     },
     manualGearBox: {
