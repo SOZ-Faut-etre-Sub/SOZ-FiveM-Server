018d3179ab385a430fc3fe1d9b991d8894e42b1d
Sébastien DEU <sebastien@deu.email>
2025-03-11 22:55:15 +0100

    feat(animation): add 'Porte Documents' animation with weapon model integration

--- resources/[soz]/soz-core/src/config/animation.ts
+++ resources/[soz]/soz-core/src/config/animation.ts
@@ -11457,6 +11457,32 @@ export const Animations: AnimationConfigList = [
                             ],
                         },
                     },
+                    {
+                        type: 'animation',
+                        name: 'Porte Documents',
+                        rightLabel: null,
+                        icon: null,
+                        animation: {
+                            base: {
+                                dictionary: 'move_weapon@jerrycan@generic',
+                                name: 'idle',
+                                options: {
+                                    repeat: true,
+                                    onlyUpperBody: true,
+                                    enablePlayerControl: true,
+                                    freezeLastFrame: true,
+                                },
+                            },
+                            props: [
+                                {
+                                    bone: 28422,
+                                    model: 'w_am_brfcase',
+                                    position: [0, 0, 0],
+                                    rotation: [-90, 0, 90],
+                                },
+                            ],
+                        },
+                    },
                 ],
             },
             {
