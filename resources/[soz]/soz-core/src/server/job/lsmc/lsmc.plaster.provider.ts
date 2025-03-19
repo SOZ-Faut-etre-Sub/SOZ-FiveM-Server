0ef133ab01ace4ebe3614dd7b8d2c39cf4b5212d
Laikker <jeremy.kennel@wanadoo.fr>
2025-03-20 00:23:25 +0100

    Fix plaster notif typo

--- resources/[soz]/soz-core/src/server/job/lsmc/lsmc.plaster.provider.ts
+++ resources/[soz]/soz-core/src/server/job/lsmc/lsmc.plaster.provider.ts
@@ -43,10 +43,10 @@ export class LSMCPlasterProvider {
         const index = player.metadata.plaster.indexOf(location);
         if (index != -1) {
             player.metadata.plaster.splice(index, 1);
-            this.notifier.notify(source, 'Vous avez ~g~retiré~s~ une plâtre sur ' + plasterConfig.label);
+            this.notifier.notify(source, 'Vous avez ~g~retiré~s~ un plâtre sur ' + plasterConfig.label);
         } else {
             player.metadata.plaster.push(location);
-            this.notifier.notify(source, 'Vous avez ~g~posé~s~ une plâtre sur ' + plasterConfig.label);
+            this.notifier.notify(source, 'Vous avez ~g~posé~s~ un plâtre sur ' + plasterConfig.label);
         }
 
         this.monitor.traceEvent('lsmc_plaster', {
