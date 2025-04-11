2d66c20ac36e25eb977c32e7f7f7b5170a713f98
Laikker <jeremy.kennel@wanadoo.fr>
2025-04-11 02:49:11 +0200

    What if zone damage + more migration cleanup

--- resources/[soz]/soz-core/src/client/world/whatif.provider.ts
+++ resources/[soz]/soz-core/src/client/world/whatif.provider.ts
@@ -1,4 +1,4 @@
-import { Tick } from '@public/core/decorators/tick';
+import { Tick, TickInterval } from '@public/core/decorators/tick';
 import { Feature } from '@public/shared/features';
 import { Vector3 } from '@public/shared/polyzone/vector';
 import { WhatIfExcludeZone } from '@public/shared/whatif';
@@ -8,6 +8,7 @@ import { Provider } from '../../core/decorators/provider';
 import { FeatureProvider } from '../feature/feature.provider';
 import { Notifier } from '../notifier';
 import { AudioService } from '../nui/audio.service';
+import { PlayerService } from '../player/player.service';
 
 @Provider()
 export class WhatIfProvider {
@@ -19,6 +20,9 @@ export class WhatIfProvider {
 
     @Inject(AudioService)
     private audioService: AudioService;
+
+    @Inject(PlayerService)
+    private playerService: PlayerService;
 
     private inZone = false;
     private audio: string = null;
@@ -32,7 +36,10 @@ export class WhatIfProvider {
         const coords = GetEntityCoords(PlayerPedId()) as Vector3;
         const inZone = WhatIfExcludeZone.isPointInside(coords);
         if (inZone) {
-            this.notifier.notify(' Cette zone est iradiée, tu devrais partir !', 'warning');
+            this.notifier.notify(
+                '☠️ Cette zone est ~r~irradiée~s~, tu vas mourir définitivement si tu y restes. ~b~Éloigne-toi aussi vite que tu le peux !~s~',
+                'warning'
+            );
         }
 
         if (inZone && !this.inZone) {
@@ -44,5 +51,24 @@ export class WhatIfProvider {
         }
 
         this.inZone = inZone;
+    }
+
+    @Tick(TickInterval.EVERY_SECOND)
+    public whatIfZoneDamage() {
+        if (!this.featureProvider.isFeatureEnabled(Feature.WhatIfFirstEpisode)) {
+            return;
+        }
+
+        if (!this.inZone) {
+            return;
+        }
+
+        const player = this.playerService.getPlayer();
+        if (!player || player.metadata.godmode || player.metadata.isdead) {
+            return;
+        }
+
+        const playerPed = PlayerPedId();
+        SetEntityHealth(playerPed, GetEntityHealth(playerPed) - 5);
     }
 }
