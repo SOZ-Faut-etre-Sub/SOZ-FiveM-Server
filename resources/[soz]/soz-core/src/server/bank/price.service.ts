c8bd04b5b410b3eb3f2046c219aa2add1a1ee195
Sébastien DEU <sebastien@deu.email>
2025-06-04 01:10:05 +0200

    refactor(casino): replace single casino chip with multi-value chip system

--- resources/[soz]/soz-core/src/server/bank/price.service.ts
+++ resources/[soz]/soz-core/src/server/bank/price.service.ts
@@ -18,20 +18,4 @@ export class PriceService {
 
         return Math.round(price + price * taxValue);
     }
-
-    public async getGain(bet: number, win: number, taxType?: TaxType) {
-        if (!taxType) {
-            return win;
-        }
-
-        const tax = await this.taxRepository.getTaxValue(taxType);
-        const taxValue = tax / 100;
-
-        const gain = win - bet;
-        if (gain <= 0) {
-            return win;
-        }
-
-        return Math.round(win - gain * taxValue);
-    }
 }
