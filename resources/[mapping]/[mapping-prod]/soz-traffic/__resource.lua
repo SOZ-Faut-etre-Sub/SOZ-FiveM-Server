a4c8fe7e274c051a9a817e04518849648d27269b
Vincent-Sniteur <vincent.sniteur@gmail.com>
2025-06-06 21:49:41 +0200

    Add files popgrounds & popcycle

---
+++ resources/[mapping]/[mapping-prod]/soz-traffic/__resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
+
+files {
+	'popcycle.dat', -- Gestion des zones des scénario de spawn
+	'popgroups.ymt', -- Gestion des listes de scénario
+}
