9764dd912c2abcaf9c540d54fba3b24dd4668867
Frozennide <sebastien@deu.email>
2025-06-23 22:24:00 +0200

    refactor(nui): migrate GameViewRenderer to new glassmorphism component

--- resources/[soz]/soz-core/src/nui/components/Styleguide/GlassMorphism.tsx
+++ resources/[soz]/soz-core/src/nui/components/Styleguide/GlassMorphism.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext, useEffect, useRef } from 'react';
+import { FunctionComponent, useContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { GlassMorphismContext } from '../../providers/GlassMorphismProvider';
@@ -9,37 +9,32 @@ interface GlassMorphismProps {
 }
 
 export const GlassMorphism: FunctionComponent<GlassMorphismProps> = ({ globalHide }) => {
+    const gameView = useContext(GlassMorphismContext);
+
     const glassmorphism = useSelector((state: RootState) => state.hud.useGlassmorphism);
 
-    const glassmorphismWorker = useContext(GlassMorphismContext);
-    const canvas = useRef<HTMLCanvasElement>(null);
-
     useEffect(() => {
-        if (canvas.current.hasAttribute('transfered')) return;
-        canvas.current.setAttribute('transfered', 'true');
-
-        const context = canvas.current.transferControlToOffscreen();
-
-        glassmorphismWorker.postMessage(
-            {
-                type: 'canvas',
-                canvas: context,
-            },
-            [context]
-        );
-    }, []);
-
-    useEffect(() => {
-        glassmorphismWorker.postMessage({
-            type: glassmorphism ? 'enable' : 'disable',
-        });
+        if (glassmorphism) {
+            gameView.enable();
+        } else {
+            gameView.disable();
+        }
     }, [glassmorphism]);
 
     useEffect(() => {
-        glassmorphismWorker.postMessage({
-            type: globalHide ? 'hide' : 'show',
-        });
+        if (globalHide) {
+            gameView.hide();
+        } else {
+            gameView.show();
+        }
     }, [globalHide]);
 
-    return <canvas className="absolute" ref={canvas} width={window.innerWidth} height={window.innerHeight} />;
+    return (
+        <canvas
+            ref={ref => gameView.setGameCanvas(ref)}
+            className="absolute"
+            width={window.innerWidth}
+            height={window.innerHeight}
+        />
+    );
 };
