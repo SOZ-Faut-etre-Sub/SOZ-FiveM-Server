9464a81e7611f92633ffcc77c70bee4717de0e3d
Laikker <jeremy.kennel@wanadoo.fr>
2025-06-27 00:28:07 +0200

    Add missing barber location

--- resources/[soz]/soz-core/src/client/location/zones/barber.zones.ts
+++ resources/[soz]/soz-core/src/client/location/zones/barber.zones.ts
@@ -17,6 +17,7 @@ export class BarberZones implements LocationZone {
             barber4: new BoxZone([-278.48, 6227.89, 31.7], 12.0, 5.6, { heading: 46, minZ: 30.7, maxZ: 33.7 }),
             barber5: new BoxZone([-1282.72, -1117.55, 6.99], 12.0, 5.6, { heading: 90, minZ: 5.99, maxZ: 8.99 }),
             barber6: new BoxZone([1932.01, 3730.55, 32.84], 12.0, 5.6, { heading: 30, minZ: 31.84, maxZ: 34.84 }),
+            barber7: new BoxZone([-33.49, -152.25, 57.08], 12.0, 5.6, { heading: 341, minZ: 56.08, maxZ: 59.08 }),
         };
     }
 }
