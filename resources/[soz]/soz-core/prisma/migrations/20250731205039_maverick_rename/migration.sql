bb845c9d3bbb42a6bbefd3a9edaccf39ac50a8c2
Laikker <jeremy.kennel@wanadoo.fr>
2025-07-31 21:17:29 +0200

    Rename maverick2 in DB

---
+++ resources/[soz]/soz-core/prisma/migrations/20250731205039_maverick_rename/migration.sql
@@ -0,0 +1,3 @@
+update vehicles set model='maverick3', hash=578476845 where model='maverick2';
+update player_vehicles set vehicle='maverick3', hash=578476845 where vehicle='maverick2';
+update concess_entreprise set vehicle='maverick3' where vehicle='maverick2';
