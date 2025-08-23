c30d4c8e0105338c01a0af71f78749cf5a0b832c
rigonkmalk <rigonkmalk@gmail.com>
2025-08-24 00:57:19 +0200

    fix: feedback from mtp + add qol

--- resources/[mapping]/[mapping-prod]/soz-mtpetrol-v2/fxmanifest.lua
+++ resources/[mapping]/[mapping-prod]/soz-mtpetrol-v2/fxmanifest.lua
@@ -1,3 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'
+
+author 'Rigonkmalk'
+description 'MTPetrol Office'
+
+files {
+    'soz_mtp_timecycles.xml'
+}
+
+data_file 'TIMECYCLEMOD_FILE' 'soz_mtp_timecycles.xml' -- custom timecycles
